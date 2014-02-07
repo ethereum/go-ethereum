827f341c97e9243b0c04a2fbde363b949bc3215d
obscuren <geffobscura@gmail.com>
2014-02-07 22:25:59 +0100

    Genesis logging

--- ethereum.go
+++ ethereum.go
@@ -20,10 +20,12 @@ var StartConsole bool
 var StartMining bool
 var UseUPnP bool
 var OutboundPort string
+var ShowGenesis bool
 
 func Init() {
 	flag.BoolVar(&StartConsole, "c", false, "debug and testing console")
 	flag.BoolVar(&StartMining, "m", false, "start dagger mining")
+	flag.BoolVar(&ShowGenesis, "g", false, "prints genesis header and exits")
 	flag.BoolVar(&UseUPnP, "upnp", false, "enable UPnP support")
 	flag.StringVar(&OutboundPort, "port", "30303", "listening port")
 
@@ -52,14 +54,19 @@ func main() {
 	ethchain.InitFees()
 	ethutil.ReadConfig()
 
-	log.Printf("Starting Ethereum v%s\n", ethutil.Config.Ver)
-
 	// Instantiated a eth stack
 	ethereum, err := eth.New(eth.CapDefault, UseUPnP)
 	if err != nil {
 		log.Println(err)
 		return
 	}
+
+	if ShowGenesis {
+		fmt.Println(ethereum.BlockManager.BlockChain().Genesis())
+		os.Exit(0)
+	}
+
+	log.Printf("Starting Ethereum v%s\n", ethutil.Config.Ver)
 
 	if StartConsole {
 		err := os.Mkdir(ethutil.Config.ExecPath, os.ModePerm)
