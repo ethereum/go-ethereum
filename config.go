954f8979386d75c3038d65cea94f481f5c15aba2
obscuren <geffobscura@gmail.com>
2014-02-11 18:46:10 +0100

    Use seed

--- config.go
+++ config.go
@@ -12,12 +12,14 @@ var ShowGenesis bool
 var AddPeer string
 var MaxPeer int
 var GenAddr bool
+var UseSeed bool
 
 func Init() {
 	flag.BoolVar(&StartConsole, "c", false, "debug and testing console")
 	flag.BoolVar(&StartMining, "m", false, "start dagger mining")
 	flag.BoolVar(&ShowGenesis, "g", false, "prints genesis header and exits")
 	flag.BoolVar(&UseUPnP, "upnp", false, "enable UPnP support")
+	flag.BoolVar(&UseSeed, "seed", true, "seed peers")
 	flag.BoolVar(&GenAddr, "genaddr", false, "create a new priv/pub key")
 	flag.StringVar(&OutboundPort, "p", "30303", "listening port")
 	flag.IntVar(&MaxPeer, "x", 5, "maximum desired peers")
