8f69c2ac45a1109985ad8cc3b98dcd2e315dc8e9
obscuren <geffobscura@gmail.com>
2014-02-20 23:10:36 +0100

    Added contract addr acessors

--- ethchain/contract.go
+++ ethchain/contract.go
@@ -30,6 +30,14 @@ func (c *Contract) RlpDecode(data []byte) {
 	c.state = ethutil.NewTrie(ethutil.Config.Db, decoder.Get(2).Interface())
 }
 
+func (c *Contract) Addr(addr []byte) *ethutil.Value {
+	return ethutil.NewValueFromBytes([]byte(c.state.Get(string(addr))))
+}
+
+func (c *Contract) SetAddr(addr []byte, value interface{}) {
+	c.state.Update(string(addr), string(ethutil.NewValue(value).Encode()))
+}
+
 func (c *Contract) State() *ethutil.Trie {
 	return c.state
 }
