e60ff6ca41832c8124acfab6b8408516d60ac140
obscuren <geffobscura@gmail.com>
2014-02-25 10:54:15 +0100

    Moved ui lib

---
+++ ui/ui_lib.go
@@ -0,0 +1,38 @@
+package ethui
+
+import (
+	"github.com/ethereum/eth-go"
+	"github.com/ethereum/eth-go/ethutil"
+	"github.com/niemeyer/qml"
+)
+
+// UI Library that has some basic functionality exposed
+type UiLib struct {
+	engine    *qml.Engine
+	eth       *eth.Ethereum
+	connected bool
+}
+
+// Opens a QML file (external application)
+func (ui *UiLib) Open(path string) {
+	component, err := ui.engine.LoadFile(path[7:])
+	if err != nil {
+		ethutil.Config.Log.Debugln(err)
+	}
+	win := component.CreateWindow(nil)
+
+	go func() {
+		win.Show()
+		win.Wait()
+	}()
+}
+
+func (ui *UiLib) Connect() {
+	if !ui.connected {
+		ui.eth.Start()
+	}
+}
+
+func (ui *UiLib) ConnectToPeer(addr string) {
+	ui.eth.ConnectToPeer(addr)
+}
