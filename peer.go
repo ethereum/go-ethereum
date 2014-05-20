b4e156e1d723fe53eff238a634f6e83cb9d80492
obscuren <geffobscura@gmail.com>
2014-05-20 13:29:46 +0200

    Up protocol version

--- peer.go
+++ peer.go
@@ -18,7 +18,7 @@ const (
 	// The size of the output buffer for writing messages
 	outputBufferSize = 50
 	// Current protocol version
-	ProtocolVersion = 9
+	ProtocolVersion = 10
 )
 
 type DiscReason byte
