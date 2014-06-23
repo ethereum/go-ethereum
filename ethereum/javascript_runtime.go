176b7802510a667b8973f2be232f7a8213b3474b
obscuren <geffobscura@gmail.com>
2014-06-23 11:28:05 +0200

    Added a execBlock method which replays the given block

--- ethereum/javascript_runtime.go
+++ ethereum/javascript_runtime.go
@@ -138,6 +138,7 @@ func (self *JSRE) initStdFuncs() {
 	eth.Set("require", self.require)
 	eth.Set("stopMining", self.stopMining)
 	eth.Set("startMining", self.startMining)
+	eth.Set("blockDo", self.execBlock)
 }
 
 /*
@@ -206,4 +207,19 @@ func (self *JSRE) require(call otto.FunctionCall) otto.Value {
 	t, _ := self.vm.Get("exports")
 
 	return t
+}
+
+func (self *JSRE) execBlock(call otto.FunctionCall) otto.Value {
+	hash, err := call.Argument(0).ToString()
+	if err != nil {
+		return otto.UndefinedValue()
+	}
+
+	err = self.ethereum.BlockDo(ethutil.FromHex(hash))
+	if err != nil {
+		fmt.Println(err)
+		return otto.FalseValue()
+	}
+
+	return otto.TrueValue()
 }
