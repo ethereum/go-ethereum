14e4f1283cdde91b38bd8cbf2b2fed69388aa75d
obscuren <geffobscura@gmail.com>
2014-08-14 10:52:13 +0200

    Removed old debug method

--- ethereal/assets/ext/pre.js
+++ ethereal/assets/ext/pre.js
@@ -1,18 +1,3 @@
-function debug(/**/) {
-	var args = arguments;
-	var msg = ""
-	for(var i = 0; i < args.length; i++){
-		if(typeof args[i] === "object") {
-			msg += " " + JSON.stringify(args[i])
-		} else {
-			msg += " " + args[i]
-		}
-	}
-
-	postData({call:"debug", args:[msg]})
-	document.getElementById("debug").innerHTML += "<br>" + msg
-}
-
 // Helper function for generating pseudo callbacks and sending data to the QML part of the application
 function postData(data, cb) {
 	data._seed = Math.floor(Math.random() * 1000000)
@@ -49,10 +34,4 @@ navigator.qt.onmessage = function(ev) {
 			}
 		}
 	}
-}
-
-window.onerror = function(message, file, lineNumber, column, errorObj) {
-	debug(file, message, lineNumber+":"+column, errorObj);
-
-	return false;
 }
