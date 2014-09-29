4266ce54fcc9291cd24652e81fc6ee82acccf21f
Paweł Bylica <pawel.bylica@imapp.pl>
2014-09-29 17:17:39 +0200

    PUSH instruction implementation

--- evmcc/Compiler.cpp
+++ evmcc/Compiler.cpp
@@ -3,6 +3,8 @@
 
 #include <llvm/IR/IRBuilder.h>
 
+#include <libevmface/Instruction.h>
+
 #include "Stack.h"
 
 namespace evmcc
@@ -37,64 +39,87 @@ std::unique_ptr<llvm::Module> Compiler::compile(const dev::bytes& bytecode)
 	using namespace llvm;
 
 	auto& context = getGlobalContext();
-
 	auto module = std::make_unique<Module>("main", context);
 	IRBuilder<> builder(context);
 
-	// Create globals for memory, memory size, stack and stack top
-	auto memory = new GlobalVariable(*module, Types.word8ptr, false,
-		GlobalValue::LinkageTypes::PrivateLinkage,
-		Constant::getNullValue(Types.word8ptr), "memory");
-	auto memSize = new GlobalVariable(*module, Types.size, false,
-		GlobalValue::LinkageTypes::PrivateLinkage,
-		ConstantInt::get(Types.size, 0), "memsize");
-	auto stack2 = new GlobalVariable(*module, Types.word256arr, false,
-		GlobalValue::LinkageTypes::PrivateLinkage,
-		ConstantAggregateZero::get(Types.word256arr), "stack");
-	auto stackTop2 = new GlobalVariable(*module, Types.size, false,
-		GlobalValue::LinkageTypes::PrivateLinkage,
-		ConstantInt::get(Types.size, 0), "stackTop");
-
-	// Create value for void* malloc(size_t)
-	auto mallocVal = Function::Create(FunctionType::get(Types.word8ptr, { Types.size }, false),
-		GlobalValue::LinkageTypes::ExternalLinkage, "malloc", module.get());
-
 	// Create main function
-	FunctionType* funcType = FunctionType::get(llvm::Type::getInt32Ty(context), false);
-	Function* mainFunc = Function::Create(funcType, Function::ExternalLinkage, "main", module.get());
+	auto mainFuncType = FunctionType::get(llvm::Type::getInt32Ty(context), false);
+	auto mainFunc = Function::Create(mainFuncType, Function::ExternalLinkage, "main", module.get());
 
-	BasicBlock* entryBlock = BasicBlock::Create(context, "entry", mainFunc);
+	auto entryBlock = BasicBlock::Create(context, "entry", mainFunc);
 	builder.SetInsertPoint(entryBlock);
 
-	// Initialize memory with call to malloc, update memsize
-	std::vector<Value*> mallocMemArgs = { ConstantInt::get(Types.size, 100) };
-	auto mallocMemCall = builder.CreateCall(mallocVal, mallocMemArgs, "malloc_mem");
-	builder.CreateStore(mallocMemCall, memory);
-	builder.CreateStore(ConstantInt::get(Types.size, 100), memSize);
-
+	// Init stack
 	auto stack = Stack(builder, module.get());
 
-	uint64_t words[] = { 1, 2, 3, 4 };
-	auto val = llvm::APInt(256, 4, words);
-	auto c = ConstantInt::get(Types.word256, val);
+	for (auto pc = bytecode.cbegin(); pc != bytecode.cend(); ++pc)
+	{
+		using dev::eth::Instruction;
 
-	stack.push(c);
-	stack.push(ConstantInt::get(Types.word256, 0x1122334455667788));
+		auto inst = static_cast<Instruction>(*pc);
+		switch (inst)
+		{
+		case Instruction::PUSH1:
+		case Instruction::PUSH2:
+		case Instruction::PUSH3:
+		case Instruction::PUSH4:
+		case Instruction::PUSH5:
+		case Instruction::PUSH6:
+		case Instruction::PUSH7:
+		case Instruction::PUSH8:
+		case Instruction::PUSH9:
+		case Instruction::PUSH10:
+		case Instruction::PUSH11:
+		case Instruction::PUSH12:
+		case Instruction::PUSH13:
+		case Instruction::PUSH14:
+		case Instruction::PUSH15:
+		case Instruction::PUSH16:
+		case Instruction::PUSH17:
+		case Instruction::PUSH18:
+		case Instruction::PUSH19:
+		case Instruction::PUSH20:
+		case Instruction::PUSH21:
+		case Instruction::PUSH22:
+		case Instruction::PUSH23:
+		case Instruction::PUSH24:
+		case Instruction::PUSH25:
+		case Instruction::PUSH26:
+		case Instruction::PUSH27:
+		case Instruction::PUSH28:
+		case Instruction::PUSH29:
+		case Instruction::PUSH30:
+		case Instruction::PUSH31:
+		case Instruction::PUSH32:
+		{
+			auto numBytes = static_cast<size_t>(inst) - static_cast<size_t>(Instruction::PUSH1) + 1;
+			auto value = llvm::APInt(256, 0);
+			for (decltype(numBytes) i = 0; i < numBytes; ++i)	// TODO: Use pc as iterator
+			{
+				++pc;
+				value <<= 8;
+				value |= *pc;
+			}
+			auto c = builder.getInt(value);
+			stack.push(c);
+			break;
+		}
+		}
+	}
 
-	auto top = stack.top();
-	stack.push(top);	// dup
+	//uint64_t words[] = { 1, 2, 3, 4 };
+	//auto val = llvm::APInt(256, 4, words);
+	//auto c = ConstantInt::get(Types.word256, val);
 
-	stack.pop();
+	//stack.push(c);
+	//stack.push(ConstantInt::get(Types.word256, 0x1122334455667788));
 
+	//auto top = stack.top();
+	//stack.push(top);	// dup
 
-	/*
-	std::vector<Value*> mallocStackArgs = { ConstantInt::get(sizeTy, 200) };
-	auto mallocStackCall = builder.CreateCall(mallocVal, mallocStackArgs, "malloc_stack");
-	auto mallocCast = builder.CreatePointerBitCastOrAddrSpaceCast(mallocStackCall, int256ptr);
-	builder.CreateStore(mallocCast, stackVal);
-	*/
+	//stack.pop();
 
-	builder.CreateRet(ConstantInt::get(Type::getInt32Ty(context), 13));
+	builder.CreateRet(ConstantInt::get(Type::getInt32Ty(context), 0));
 
 	return module;
 }
