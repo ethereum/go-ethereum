10b252dd0517175117d8d4a8ef30b3689a10eda5
obscuren <geffobscura@gmail.com>
2014-10-21 13:24:48 +0200

    WIP RPC interface

---
+++ rpc/message.go
@@ -0,0 +1,14 @@
+package rpc
+
+import "github.com/ethereum/eth-go/ethutil"
+
+type Message struct {
+	Call string        `json:"call"`
+	Args []interface{} `json:"args"`
+	Id   int           `json:"_id"`
+	Data interface{}   `json:"data"`
+}
+
+func (self *Message) Arguments() *ethutil.Value {
+	return ethutil.NewValue(self.Args)
+}
