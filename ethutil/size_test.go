a1d62abca4b07f35f7f80702c9f58bb40e703504
Taylor Gerring <taylor.gerring@gmail.com>
2014-11-05 22:16:44 +0100

    Restructure StorageSize string test

--- ethutil/size_test.go
+++ ethutil/size_test.go
@@ -1,12 +1,31 @@
 package ethutil
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestSize(t *testing.T) {
-	fmt.Println(StorageSize(2381273))
-	fmt.Println(StorageSize(2192))
-	fmt.Println(StorageSize(12))
+func TestStorageSizeString(t *testing.T) {
+	data1 := 2381273
+	data2 := 2192
+	data3 := 12
+
+	exp1 := "2.38 mB"
+	exp2 := "2.19 kB"
+	exp3 := "12.00 B"
+
+	res1 := StorageSize(data1).String()
+	res2 := StorageSize(data2).String()
+	res3 := StorageSize(data3).String()
+
+	if res1 != exp1 {
+		t.Errorf("Expected %s got %s", exp1, res1)
+	}
+
+	if res2 != exp2 {
+		t.Errorf("Expected %s got %s", exp2, res2)
+	}
+
+	if res3 != exp3 {
+		t.Errorf("Expected %s got %s", exp3, res3)
+	}
 }
