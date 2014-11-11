c24d143c9e08f9782d7ad063868ea7c647db2a03
Taylor Gerring <taylor.gerring@gmail.com>
2014-11-11 19:18:52 +0100

    Convert rand_test to checker

--- ethutil/rand_test.go
+++ ethutil/rand_test.go
@@ -1,9 +1,17 @@
 package ethutil
 
 import (
-	"testing"
+	checker "gopkg.in/check.v1"
 )
 
-func TestRandomUint64(t *testing.T) {
-	RandomUint64()
+type RandomSuite struct{}
+
+var _ = checker.Suite(&RandomSuite{})
+
+func (s *RandomSuite) TestRandomUint64(c *checker.C) {
+	res1, _ := RandomUint64()
+	res2, _ := RandomUint64()
+	c.Assert(res1, checker.NotNil)
+	c.Assert(res2, checker.NotNil)
+	c.Assert(res1, checker.Not(checker.Equals), res2)
 }
