790ded0f4743d02606d96a5febffb0907104de23
Taylor Gerring <taylor.gerring@gmail.com>
2014-11-15 19:11:43 -0600

    Update travis deps script

--- travis.sh
+++ travis.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-TEST_DEPS=$(go list -f '{{.TestImports}} {{.XTestImports}}' github.com/ethereum/go-ethereum/... | sed -e 's/\[//g' | sed -e 's/\]//g')
+TEST_DEPS=$(go list -f '{{.Imports}} {{.TestImports}} {{.XTestImports}}' github.com/ethereum/go-ethereum/... | sed -e 's/\[//g' | sed -e 's/\]//g' | sed -e 's/C //g')
 if [ "$TEST_DEPS" ]; then
   go get -race $TEST_DEPS
 fi
