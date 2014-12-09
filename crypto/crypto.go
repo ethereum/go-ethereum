87adff7e189ee0a1fd50a3ef30ba22482e60f314
obscuren <geffobscura@gmail.com>
2014-12-10 00:03:21 +0100

    Added Encrypt & Decrypt using the ECIES w/ tests

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -1,13 +1,34 @@
 package crypto
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
 	"crypto/sha256"
 
 	"code.google.com/p/go.crypto/ripemd160"
 	"github.com/ethereum/go-ethereum/ethutil"
+	"github.com/obscuren/ecies"
 	"github.com/obscuren/secp256k1-go"
 	"github.com/obscuren/sha3"
 )
+
+func init() {
+	// specify the params for the s256 curve
+	ecies.AddParamsForCurve(S256(), ecies.ECIES_AES128_SHA256)
+}
+
+func ToECDSA(prv []byte) *ecdsa.PrivateKey {
+	priv := new(ecdsa.PrivateKey)
+	priv.PublicKey.Curve = S256()
+	priv.D = ethutil.BigD(prv)
+	priv.PublicKey.X, priv.PublicKey.Y = S256().ScalarBaseMult(prv)
+	return priv
+}
+
+func FromECDSA(prv *ecdsa.PrivateKey) []byte {
+	return prv.D.Bytes()
+}
 
 // TODO refactor, remove (bin)
 func Sha3(data []byte) []byte {
@@ -44,4 +65,25 @@ func Ecrecover(data []byte) []byte {
 	r, _ := secp256k1.RecoverPubkey(in.hash, in.sig)
 
 	return r
+}
+
+func SigToPub(hash, sig []byte) []byte {
+	return Ecrecover(append(hash, sig...))
+}
+
+func Sign(hash, prv []byte) (sig []byte, err error) {
+	sig, err = secp256k1.Sign(hash, prv)
+	return
+}
+
+func Encrypt(pub, message []byte) ([]byte, error) {
+	x, y := elliptic.Unmarshal(S256(), pub)
+	epub := &ecdsa.PublicKey{S256(), x, y}
+
+	return ecies.Encrypt(rand.Reader, ecies.ImportECDSAPublic(epub), message, nil, nil)
+}
+
+func Decrypt(prv, ct []byte) ([]byte, error) {
+	key := ecies.ImportECDSA(ToECDSA(prv))
+	return key.Decrypt(rand.Reader, ct, nil, nil)
 }
