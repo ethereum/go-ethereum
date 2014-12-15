c7bc684909ab3d135d25a718d5ff2c725816e0fc
obscuren <geffobscura@gmail.com>
2014-12-15 12:35:21 +0100

    Moved peer helper metheds

--- eth/backend.go
+++ eth/backend.go
@@ -1,7 +1,6 @@
 package eth
 
 import (
-	"encoding/json"
 	"net"
 	"sync"
 
@@ -212,22 +211,6 @@ func (s *Ethereum) Stop() {
 // This function will wait for a shutdown and resumes main thread execution
 func (s *Ethereum) WaitForShutdown() {
 	<-s.shutdownChan
-}
-
-func WritePeers(path string, addresses []string) {
-	if len(addresses) > 0 {
-		data, _ := json.MarshalIndent(addresses, "", "    ")
-		ethutil.WriteFile(path, data)
-	}
-}
-
-func ReadPeers(path string) (ips []string, err error) {
-	var data string
-	data, err = ethutil.ReadAllFile(path)
-	if err != nil {
-		json.Unmarshal([]byte(data), &ips)
-	}
-	return
 }
 
 // now tx broadcasting is taken out of txPool
