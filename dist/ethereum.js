1428dcee21105c2682568e2124a0a5ce6a3e7ae3
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-01-13 13:03:40 +0100

    web3.contract -> web3.eth.contract

--- dist/ethereum.js
+++ dist/ethereum.js
@@ -1173,7 +1173,7 @@ web3.providers.WebSocketProvider = require('./lib/websocket');
 web3.providers.HttpRpcProvider = require('./lib/httprpc');
 web3.providers.QtProvider = require('./lib/qt');
 web3.providers.AutoProvider = require('./lib/autoprovider');
-web3.contract = require('./lib/contract');
+web3.eth.contract = require('./lib/contract');
 
 module.exports = web3;
 
