70d02b1d661b4c057f53bb1c5ca563e289c80dfd
Paweł Bylica <pawel.bylica@imapp.pl>
2015-01-15 16:27:57 +0100

    RETURN implementation: JIT returns data

--- libevmjit/interface.h
+++ libevmjit/interface.h
@@ -1,10 +1,19 @@
 #include <stddef.h>
+#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int evmjit_run(void* _data, void* _env);
+typedef struct evmjit_result
+{
+	int32_t  returnCode;
+	uint64_t returnDataSize;
+	void*    returnData;
+
+} evmjit_result;
+
+evmjit_result evmjit_run(void* _data, void* _env);
 
 // JIT object opaque type
 typedef struct evm_jit evm_jit;
