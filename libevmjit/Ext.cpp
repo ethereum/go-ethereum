a2492d498332939950708321781d9d64fe94319f
Paweł Bylica <pawel.bylica@imapp.pl>
2015-01-16 12:43:24 +0100

    Correct std::array intialization (again!)

--- libevmjit/Ext.cpp
+++ libevmjit/Ext.cpp
@@ -23,12 +23,9 @@ namespace jit
 Ext::Ext(RuntimeManager& _runtimeManager, Memory& _memoryMan):
 	RuntimeHelper(_runtimeManager),
 	m_memoryMan(_memoryMan)
-#ifdef __MSCVER
-	,
-	m_funcs({}),  // The only std::array initialization that works in both Visual Studio & GCC
-	m_argAllocas({})
-#endif
 {
+	m_funcs = decltype(m_funcs)();
+	m_argAllocas = decltype(m_argAllocas)();
 	m_size = m_builder.CreateAlloca(Type::Size, nullptr, "env.size");
 }
 
