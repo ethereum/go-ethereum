6e8085b839e15fa27f6b6d8086d6cb09e726b4e0
wanderer <mjbecze@gmail.com>
2015-01-18 22:14:53 -0500

    remove old requires

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
 module.exports = {
-  blockgenesis: require('./BasicTests/blockgenesistest'),
-  genesishashes: require('./BasicTests/genesishashestest'),
-  hexencode: require('./BasicTests/hexencodetest'),
-  keyaddrtests: require('./BasicTests/keyaddrtest'),
-  rlptest: require('./BasicTests/rlptest'),
-  trieTests: {
-    trietest: require('./TrieTests/trietest'),
-    trietestnextprev: require('./TrieTests/trietestnextprev'),
-    trieanyorder: require('./TrieTests/trieanyorder')
-  },
-  txtest: require('./BasicTests/txtest'),
+  basicTests: require('require-all')(__dirname + '/BasicTests/'),
+  trieTests: require('require-all')(__dirname + '/TrieTests/'),
   stateTests: require('require-all')(__dirname + '/StateTests/'),
-  vmTests: {
-    vmRandom: require('./VMTests/RandomTests/randomTest'),
-    vmArithmeticTest: require('./VMTests/vmArithmeticTest'),
-    vmBitwiseLogicOperationTest: require('./VMTests/vmBitwiseLogicOperationTest'),
-    vmBlockInfoTest: require('./VMTests/vmBlockInfoTest'),
-    vmEnvironmentalInfoTest: require('./VMTests/vmEnvironmentalInfoTest'),
-    vmIOandFlowOperationsTest: require('./VMTests/vmIOandFlowOperationsTest'),
-    vmLogTest: require('./VMTests/vmLogTest'),
-    vmPushDupSwapTest: require('./VMTests/vmPushDupSwapTest'),
-    vmSha3Test: require('./VMTests/vmSha3Test'),
-    vmtests: require('./VMTests/vmtests')
-  }
+  vmTests: require('require-all')(__dirname + '/VMTests')
 };
