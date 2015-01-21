f3e78e50ff5c02b9b365de0571f1782b205cb319
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-01-21 18:56:30 +0100

    changes to for natspec

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ web3.providers.HttpRpcProvider = require('./lib/httprpc');
 web3.providers.QtProvider = require('./lib/qt');
 web3.providers.AutoProvider = require('./lib/autoprovider');
 web3.eth.contract = require('./lib/contract');
+web3.abi = require('./lib/abi');
 
 module.exports = web3;
