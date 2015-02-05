398deb7766175fe53ce9e33d0a7f3f24e69ffa46
zelig <viktor.tron@gmail.com>
2015-02-05 13:20:17 +0100

    start on netstore

--- bzz/netstore.go
+++ bzz/netstore.go
@@ -2,9 +2,24 @@ package bzz
 
 /*
 DHT implements the chunk store that directly communicates with the bzz protocol on the one hand and the kademlia node table on the other.
-It accumulates requests from peers, keeping a request pool and does forwarding for incoming  requests and handles expiry/timeout.
+It does forwarding for incoming requests and handles expiry/timeout.
 */
+
+type peerPool interface {
+	GetPeers(target Key, peers []peer)
+}
 
 // it implements the ChunkStore interface
 type netStore struct {
+	peerPool peerPool
+	// cademlia
+}
+
+func (self *DPA) Put(chunk *Chunk) {
+
+	return
+}
+
+func (self *DPA) Get(key Key) (chunk *Chunk, err error) {
+	return
 }
