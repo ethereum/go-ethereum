74e8f1c3eab33ff36f945115c48a6db2ce75a4b9
Daniel A. Nagy <nagy.da@gmail.com>
2015-02-13 17:27:24 +0100

    Very simple directory upload tool

---
+++ bzz/bzzup/bzzup.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+INDEX='index.html'
+
+bzzroot="$1"
+[ "_$1" = _ ] && bzzroot=.
+
+delimiter='[{'
+
+pushd "$bzzroot" > /dev/null
+
+(for path in `find . -type f`
+do
+name=`echo "$path" | cut -c2-`
+[ _`basename "$name"` = "_$INDEX" ] && name=`dirname "$name"`
+echo -n "$delimiter"
+hash=`wget -q -O- --post-file="$path" http://localhost:8500/raw`
+mime=`mimetype -b "$path"`
+echo -n "\"hash\":\"$hash\",\"path\":\"$name\",\"content_type\":\"$mime\""
+delimiter='},{'
+
+done
+echo -n '}]') | wget -q -O- --post-data=`cat` http://localhost:8500/raw
+
+echo
+
+popd > /dev/null
+
