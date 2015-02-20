654f7f707c9bd327fcbf0dcb89715a5930f915eb
Felix Lange <fjl@twurst.com>
2015-02-20 12:15:08 +0100

    .travis.yml: speed up tests on Travis
    This should decrease test runtime to about 30 seconds.

--- gocoverage.sh
+++ gocoverage.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-# The script does automatic checking on a Go package and its sub-packages, including:
-# 6. test coverage (http://blog.golang.org/cover)
 
 set -e
 
-# Run test coverage on each subdirectories and merge the coverage profile.
+# Add godep workspace to GOPATH. We do it manually instead of using
+# 'godep go test' or 'godep restore' so godep doesn't need to be installed.
+GOPATH="$PWD/Godeps/_workspace:$GOPATH"
 
+# Install packages before testing. Not doing this would cause
+# 'go test' to recompile all package dependencies before testing each package.
+go install ./...
+
+# Run test coverage on each subdirectories and merge the coverage profile.
 echo "mode: count" > profile.cov
 
 # Standard go tooling behavior is to ignore dirs with leading underscors
