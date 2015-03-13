a1518a75f28389e31d1214851545304b3ba84089
Fefe <zsfelfoldi@gmail.com>
2015-03-13 22:15:00 +0100

    explicit binding of jeth.Send

--- cmd/ethereum/js.go
+++ cmd/ethereum/js.go
@@ -92,6 +92,13 @@ func (js *jsre) apiBindings() {
 	ethApi := rpc.NewEthereumApi(js.xeth, js.ethereum.DataDir)
 	js.re.Bind("jeth", rpc.NewJeth(ethApi, js.re.ToVal))
 
+	jeth := rpc.NewJeth(ethApi, js.re.ToVal)
+	//js.re.Bind("jeth", jeth)
+	js.re.Set("jeth", struct{}{})
+	t, _ := js.re.Get("jeth")
+	jethObj := t.Object()
+	jethObj.Set("send", jeth.Send)
+
 	_, err := js.re.Eval(re.BigNumber_JS)
 
 	if err != nil {
