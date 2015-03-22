360c66c65d546cfbe3d2d910b13377485ba66e1f
Taylor Gerring <taylor.gerring@gmail.com>
2015-03-22 13:46:38 +0100

    Move Big* vars to big.go

--- common/common.go
+++ common/common.go
@@ -2,21 +2,7 @@ package common
 
 import (
 	"fmt"
-	"math/big"
 	"time"
-)
-
-// Common big integers often used
-var (
-	Big1     = big.NewInt(1)
-	Big2     = big.NewInt(2)
-	Big3     = big.NewInt(3)
-	Big0     = big.NewInt(0)
-	BigTrue  = Big1
-	BigFalse = Big0
-	Big32    = big.NewInt(32)
-	Big256   = big.NewInt(0xff)
-	Big257   = big.NewInt(257)
 )
 
 func Bench(pre string, cb func()) {
