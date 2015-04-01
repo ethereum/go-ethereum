216ea425e4579f95c01572d2da0d83890a05c162
obscuren <geffobscura@gmail.com>
2015-04-01 17:36:56 +0200

    corrected

--- eth/protocol.go
+++ eth/protocol.go
@@ -14,7 +14,7 @@ import (
 
 const (
 	ProtocolVersion    = 60
-	NetworkId          = 3
+	NetworkId          = 0
 	ProtocolLength     = uint64(8)
 	ProtocolMaxMsgSize = 10 * 1024 * 1024
 	maxHashes          = 256
