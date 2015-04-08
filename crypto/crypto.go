b3a3fdf9a447bd2b3f862380d87c675138da78e7
Bas van Kervel <basvankervel@ziggo.nl>
2015-04-08 23:03:47 +0200

    Support for import/export hex encoded keys, closes #635

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -121,7 +121,7 @@ func HexToECDSA(hexkey string) (*ecdsa.PrivateKey, error) {
 
 // LoadECDSA loads a secp256k1 private key from the given file.
 func LoadECDSA(file string) (*ecdsa.PrivateKey, error) {
-	buf := make([]byte, 32)
+	buf := make([]byte, 64)
 	fd, err := os.Open(file)
 	if err != nil {
 		return nil, err
@@ -130,13 +130,13 @@ func LoadECDSA(file string) (*ecdsa.PrivateKey, error) {
 	if _, err := io.ReadFull(fd, buf); err != nil {
 		return nil, err
 	}
-	return ToECDSA(buf), nil
+	return ToECDSA(common.HexBytes2Bytes(buf)), nil
 }
 
 // SaveECDSA saves a secp256k1 private key to the given file with restrictive
 // permissions
 func SaveECDSA(file string, key *ecdsa.PrivateKey) error {
-	return ioutil.WriteFile(file, FromECDSA(key), 0600)
+	return ioutil.WriteFile(file, common.Bytes2HexBytes(FromECDSA(key)), 0600)
 }
 
 func GenerateKey() (*ecdsa.PrivateKey, error) {
