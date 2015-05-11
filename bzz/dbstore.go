05c64815b740a753cefe7d2693e1cd3c68c58025
Daniel A. Nagy <nagy.da@gmail.com>
2015-05-11 18:24:20 +0200

    Increased disk capacity to 20G

--- bzz/dbstore.go
+++ bzz/dbstore.go
@@ -385,7 +385,7 @@ func newDbStore(path string) (s *dbStore, err error) {
 		return
 	}
 
-	s.setCapacity(50000) // TODO define default capacity as constant
+	s.setCapacity(5000000) // TODO define default capacity as constant
 
 	s.gcStartPos = make([]byte, 1)
 	s.gcStartPos[0] = kpIndex
