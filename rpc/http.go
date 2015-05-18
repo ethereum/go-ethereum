a2598e649de8b41727084749527aa0bc27965856
Taylor Gerring <taylor.gerring@gmail.com>
2015-05-18 10:31:03 -0500

    Permit multiple CORS domains
    Separated by spaces

--- rpc/http.go
+++ rpc/http.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"strings"
 
 	"github.com/ethereum/go-ethereum/logger"
 	"github.com/ethereum/go-ethereum/logger/glog"
@@ -39,7 +40,7 @@ func Start(pipe *xeth.XEth, config RpcConfig) error {
 	if len(config.CorsDomain) > 0 {
 		var opts cors.Options
 		opts.AllowedMethods = []string{"POST"}
-		opts.AllowedOrigins = []string{config.CorsDomain}
+		opts.AllowedOrigins = strings.Split(config.CorsDomain, " ")
 
 		c := cors.New(opts)
 		handler = newStoppableHandler(c.Handler(JSONRPC(pipe)), l.stop)
