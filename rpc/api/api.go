bbfa0a3dcb82e704828d1438261ff26c50c4ccc3
Bas van Kervel <basvankervel@ziggo.nl>
2015-06-08 11:01:02 +0200

    added API/IPC commandline flags

--- rpc/api/api.go
+++ rpc/api/api.go
@@ -27,6 +27,11 @@ var (
 	}, ",")
 )
 
+const (
+	// List with all API's which are offered over the IPC interface by default
+	DefaultIpcApis = "eth"
+)
+
 // Ethereum RPC API interface
 type EthereumApi interface {
 	// API identifier
