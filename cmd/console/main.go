d2c863f22c3399d216ae9f295b0be5cb739d2007
Bas van Kervel <bas@ethdev.com>
2015-06-08 19:51:38 +0200

    only load supported api's

--- cmd/console/main.go
+++ cmd/console/main.go
@@ -25,11 +25,11 @@ import (
 	"io"
 	"os"
 
-	"github.com/mattn/go-colorable"
-	"github.com/mattn/go-isatty"
 	"github.com/codegangsta/cli"
 	"github.com/ethereum/go-ethereum/cmd/utils"
 	"github.com/ethereum/go-ethereum/logger"
+	"github.com/mattn/go-colorable"
+	"github.com/mattn/go-isatty"
 )
 
 const (
@@ -40,7 +40,7 @@ const (
 var (
 	gitCommit       string // set via linker flag
 	nodeNameVersion string
-	app           = utils.NewApp(Version, "the ether console")
+	app             = utils.NewApp(Version, "the ether console")
 )
 
 func init() {
@@ -96,6 +96,6 @@ func run(ctx *cli.Context) {
 	ipcpath := ctx.GlobalString(utils.IPCPathFlag.Name)
 
 	repl := newJSRE(jspath, ipcpath)
-	repl.welcome()
+	repl.welcome(ipcpath)
 	repl.interactive()
 }
