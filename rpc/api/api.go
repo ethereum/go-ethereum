73aa9cd26687af13902b542cb8b1aef412bfbfc2
Bas van Kervel <basvankervel@ziggo.nl>
2015-06-08 11:01:02 +0200

    added API/IPC commandline flags

--- rpc/api/api.go
+++ rpc/api/api.go
@@ -2,6 +2,11 @@ package api
 
 import "github.com/ethereum/go-ethereum/rpc/shared"
 
+const (
+	// List with all API's which are offered over the IPC interface by default
+	DefaultIpcApis = "eth"
+)
+
 // Ethereum RPC API interface
 type EthereumApi interface {
 	// Execute the given request and returns the response or an error
