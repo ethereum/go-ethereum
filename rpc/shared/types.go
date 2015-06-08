8ebf2d8fad729a8261f237bb05b6073e6c1b652f
Bas van Kervel <basvankervel@ziggo.nl>
2015-06-08 10:41:04 +0200

    added RPC/IPC support

--- rpc/shared/types.go
+++ rpc/shared/types.go
@@ -1,6 +1,10 @@
 package shared
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"github.com/ethereum/go-ethereum/logger"
+	"github.com/ethereum/go-ethereum/logger/glog"
+)
 
 // RPC request
 type Request struct {
@@ -35,4 +39,25 @@ type ErrorObject struct {
 	Code    int    `json:"code"`
 	Message string `json:"message"`
 	// Data    interface{} `json:"data"`
+}
+
+func NewRpcResponse(id interface{}, jsonrpcver string, reply interface{}, err error) *interface{} {
+	var response interface{}
+
+	switch err.(type) {
+		case nil:
+		response = &SuccessResponse{Jsonrpc: jsonrpcver, Id: id, Result: reply}
+		case *NotImplementedError:
+		jsonerr := &ErrorObject{-32601, err.Error()}
+		response = &ErrorResponse{Jsonrpc: jsonrpcver, Id: id, Error: jsonerr}
+		case *DecodeParamError, *InsufficientParamsError, *ValidationError, *InvalidTypeError:
+		jsonerr := &ErrorObject{-32602, err.Error()}
+		response = &ErrorResponse{Jsonrpc: jsonrpcver, Id: id, Error: jsonerr}
+		default:
+		jsonerr := &ErrorObject{-32603, err.Error()}
+		response = &ErrorResponse{Jsonrpc: jsonrpcver, Id: id, Error: jsonerr}
+	}
+
+	glog.V(logger.Detail).Infof("Generated response: %T %s", response, response)
+	return &response
 }
