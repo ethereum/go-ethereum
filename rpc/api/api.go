955798e0f6832a3ac9ac1d5dc8db211cf841ef15
Bas van Kervel <bas@ethdev.com>
2015-06-08 14:42:15 +0200

    added miner API

--- rpc/api/api.go
+++ rpc/api/api.go
@@ -4,10 +4,11 @@ import "github.com/ethereum/go-ethereum/rpc/shared"
 
 const (
 	// List with all API's which are offered over the IPC interface by default
-	DefaultIpcApis = "eth,web3"
+	DefaultIpcApis = "eth,web3,miner"
 
 	EthApiName = "eth"
 	MergedApiName = "merged"
+	MinerApiName = "miner"
 	Web3ApiName = "web3"
 )
 
