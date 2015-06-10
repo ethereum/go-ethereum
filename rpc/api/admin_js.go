e62c7a9d433fc25d7ce02dce642efe268139cde1
Bas van Kervel <bas@ethdev.com>
2015-06-10 09:42:14 +0200

    upgrade web3.js with _extend support

--- rpc/api/admin_js.go
+++ rpc/api/admin_js.go
@@ -1,66 +1,66 @@
 package api
 
 const Admin_JS = `
-web3.extend({
+web3._extend({
 	property: 'admin',
 	methods:
 	[
-		new web3.extend.Method({
+		new web3._extend.Method({
 			name: 'addPeer',
 			call: 'admin_addPeer',
 			params: 1,
-			inputFormatter: [web3.extend.utils.formatInputString],
-			outputFormatter: web3.extend.formatters.formatOutputBool
+			inputFormatter: [web3._extend.utils.formatInputString],
+			outputFormatter: web3._extend.formatters.formatOutputBool
 		}),
-		new web3.extend.Method({
+		new web3._extend.Method({
 			name: 'peers',
 			call: 'admin_peers',
 			params: 0,
 			inputFormatter: [],
 			outputFormatter: function(obj) { return obj; }
 		}),
-		new web3.extend.Method({
+		new web3._extend.Method({
 			name: 'exportChain',
 			call: 'admin_exportChain',
 			params: 1,
-			inputFormatter: [web3.extend.utils.formatInputString],
+			inputFormatter: [web3._extend.utils.formatInputString],
 			outputFormatter: function(obj) { return obj; }
 		}),
-		new web3.extend.Method({
+		new web3._extend.Method({
 			name: 'importChain',
 			call: 'admin_importChain',
 			params: 1,
-			inputFormatter: [web3.extend.utils.formatInputString],
+			inputFormatter: [web3._extend.utils.formatInputString],
 			outputFormatter: function(obj) { return obj; }
 		}),
-		new web3.extend.Method({
+		new web3._extend.Method({
 			name: 'verbosity',
 			call: 'admin_verbosity',
 			params: 1,
-			inputFormatter: [web3.extend.utils.formatInputInt],
-			outputFormatter: web3.extend.formatters.formatOutputBool
+			inputFormatter: [web3._extend.utils.formatInputInt],
+			outputFormatter: web3._extend.formatters.formatOutputBool
 		}),
-		new web3.extend.Method({
+		new web3._extend.Method({
 			name: 'syncStatus',
 			call: 'admin_syncStatus',
 			params: 1,
-			inputFormatter: [web3.extend.utils.formatInputInt],
+			inputFormatter: [web3._extend.utils.formatInputInt],
 			outputFormatter: function(obj) { return obj; }
 		}),
-		new web3.extend.Method({
+		new web3._extend.Method({
 			name: 'setSolc',
 			call: 'admin_setSolc',
 			params: 1,
-			inputFormatter: [web3.extend.utils.formatInputString],
-			outputFormatter: web3.extend.formatters.formatOutputString
+			inputFormatter: [web3._extend.utils.formatInputString],
+			outputFormatter: web3._extend.formatters.formatOutputString
 		})
 	],
 	properties:
 	[
-		new web3.extend.Property({
+		new web3._extend.Property({
 			name: 'nodeInfo',
 			getter: 'admin_nodeInfo',
-			outputFormatter: web3.extend.formatters.formatOutputString
+			outputFormatter: web3._extend.formatters.formatOutputString
 		})
 	]
 });
--- rpc/api/personal_js.go
+++ rpc/api/personal_js.go
@@ -1,30 +1,30 @@
 package api
 
 const Personal_JS = `
-web3.extend({
+web3._extend({
 	property: 'personal',
 	methods:
 	[
-		new web3.extend.Method({
+		new web3._extend.Method({
 			name: 'listAccounts',
 			call: 'personal_listAccounts',
 			params: 0,
 			inputFormatter: [],
 			outputFormatter: function(obj) { return obj; }
 		}),
-		new web3.extend.Method({
+		new web3._extend.Method({
 			name: 'newAccount',
 			call: 'personal_newAccount',
 			params: 1,
-			inputFormatter: [web3.extend.formatters.formatInputString],
-			outputFormatter: web3.extend.formatters.formatOutputString
+			inputFormatter: [web3._extend.formatters.formatInputString],
+			outputFormatter: web3._extend.formatters.formatOutputString
 		}),
-		new web3.extend.Method({
+		new web3._extend.Method({
 			name: 'unlockAccount',
 			call: 'personal_unlockAccount',
 			params: 3,
-			inputFormatter: [web3.extend.formatters.formatInputString,web3.extend.formatters.formatInputString,web3.extend.formatters.formatInputInt],
-			outputFormatter: web3.extend.formatters.formatOutputBool
+			inputFormatter: [web3._extend.formatters.formatInputString,web3._extend.formatters.formatInputString,web3._extend.formatters.formatInputInt],
+			outputFormatter: web3._extend.formatters.formatOutputBool
 		})
 	],
 	properties:
