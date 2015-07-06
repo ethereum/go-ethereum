bb0f997d576917815e8e104fa5100a5cbd4e6d3a
zelig <viktor.tron@gmail.com>
2015-07-06 18:00:39 +0100

    account fixes * etherbase defaults to first created account * etherbase cli option now allows account index * introduce ethereum.SetEtherbase and miner.setEtherbase * use slightly modified ISO8601 standard * remove obsolete crypto code adapt tests * remove unused abi from accounts

--- rpc/api/miner_js.go
+++ rpc/api/miner_js.go
@@ -20,6 +20,13 @@ web3._extend({
 			outputFormatter: web3._extend.formatters.formatOutputBool
 		}),
 		new web3._extend.Method({
+			name: 'setEtherbase',
+			call: 'miner_setEtherbase',
+			params: 1,
+			inputFormatter: [web3._extend.formatters.formatInputInt],
+			outputFormatter: web3._extend.formatters.formatOutputBool
+		}),
+		new web3._extend.Method({
 			name: 'setExtra',
 			call: 'miner_setExtra',
 			params: 1,
