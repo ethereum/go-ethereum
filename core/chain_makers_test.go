db5ec711e896c4abf4d1dc132851b818de7a9212
Jeffrey Wilcke <geffobscura@gmail.com>
2015-07-22 13:41:31 +0200

    cmd/geth, core, eth: Version 1.0.0
    Genesis release. Closes #1402

--- core/chain_makers_test.go
+++ core/chain_makers_test.go
@@ -28,6 +28,9 @@ import (
 )
 
 func ExampleGenerateChain() {
+	params.MinGasLimit = big.NewInt(125000)      // Minimum the gas limit may ever be.
+	params.GenesisGasLimit = big.NewInt(3141592) // Gas limit of the Genesis block.
+
 	var (
 		key1, _ = crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
 		key2, _ = crypto.HexToECDSA("8a1f9a8f95be41cd7ccb6168179afb4504aefe388d1e14474d32c45c72ce7b7a")
