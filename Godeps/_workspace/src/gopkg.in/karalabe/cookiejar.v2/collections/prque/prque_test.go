b738172dccc61449d0240b2e9072109d3b77e2dc
Felix Lange <fjl@twurst.com>
2015-07-24 14:23:02 +0200

    Godeps: use BSD-licensed version of gopkg.in/karalabe/cookiejar.v2

--- Godeps/_workspace/src/gopkg.in/karalabe/cookiejar.v2/collections/prque/prque_test.go
+++ Godeps/_workspace/src/gopkg.in/karalabe/cookiejar.v2/collections/prque/prque_test.go
@@ -1,19 +1,10 @@
 // CookieJar - A contestant's algorithm toolbox
 // Copyright (c) 2013 Peter Szilagyi. All rights reserved.
 //
-// CookieJar is dual licensed: you can redistribute it and/or modify it under
-// the terms of the GNU General Public License as published by the Free Software
-// Foundation, either version 3 of the License, or (at your option) any later
-// version.
-//
-// The toolbox is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-// more details.
-//
-// Alternatively, the CookieJar toolbox may be used in accordance with the terms
-// and conditions contained in a signed written agreement between you and the
-// author(s).
+// CookieJar is dual licensed: use of this source code is governed by a BSD
+// license that can be found in the LICENSE file. Alternatively, the CookieJar
+// toolbox may be used in accordance with the terms and conditions contained
+// in a signed written agreement between you and the author(s).
 
 package prque
 
