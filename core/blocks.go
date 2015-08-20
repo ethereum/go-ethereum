3793991c0e14a205cae3b2f8ccc8a45792ec2b80
Jeffrey Wilcke <geffobscura@gmail.com>
2015-08-20 18:50:47 +0200

    remove 0x

--- core/blocks.go
+++ core/blocks.go
@@ -20,5 +20,5 @@ import "github.com/ethereum/go-ethereum/common"
 
 // Set of manually tracked bad hashes (usually hard forks)
 var BadHashes = map[common.Hash]bool{
-	common.HexToHash("0x05bef30ef572270f654746da22639a7a0c97dd97a7050b9e252391996aaeb689"): true,
+	common.HexToHash("05bef30ef572270f654746da22639a7a0c97dd97a7050b9e252391996aaeb689"): true,
 }
