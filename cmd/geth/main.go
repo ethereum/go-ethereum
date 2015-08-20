54088b0b8b76b9538cf10fa5a606a4170f571065
Jeffrey Wilcke <geffobscura@gmail.com>
2015-08-20 13:08:08 +0200

    cmd/geth: bumped version 1.0.3

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -48,10 +48,10 @@ import (
 
 const (
 	ClientIdentifier = "Geth"
-	Version          = "1.0.2"
+	Version          = "1.0.3"
 	VersionMajor     = 1
 	VersionMinor     = 0
-	VersionPatch     = 2
+	VersionPatch     = 3
 )
 
 var (
