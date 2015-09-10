571d3236c49c5dcb0c0451f68db2f998fc97e8b7
Jeff R. Allen <jra@nella.org>
2015-09-10 21:48:20 +0600

    Change go-uuid to use the current supported repository.

--- crypto/key_store_passphrase.go
+++ crypto/key_store_passphrase.go
@@ -36,9 +36,9 @@ import (
 	"io"
 	"reflect"
 
-	"code.google.com/p/go-uuid/uuid"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto/randentropy"
+	"github.com/pborman/uuid"
 	"golang.org/x/crypto/pbkdf2"
 	"golang.org/x/crypto/scrypt"
 )
