1de796f10134bb4aa245591e0d8802e320892efb
Jeffrey Wilcke <geffobscura@gmail.com>
2015-10-05 13:01:34 +0200

    cmd, core, eth: added official testnet

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -314,6 +314,7 @@ JavaScript API. See https://github.com/ethereum/go-ethereum/wiki/Javascipt-Conso
 		utils.ExecFlag,
 		utils.WhisperEnabledFlag,
 		utils.DevModeFlag,
+		utils.TestNetFlag,
 		utils.VMDebugFlag,
 		utils.VMForceJitFlag,
 		utils.VMJitCacheFlag,
