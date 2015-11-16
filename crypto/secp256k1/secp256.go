e344e1d4903f47d525258305d986f4aa0dd0ab52
Felix Lange <fjl@twurst.com>
2015-11-16 18:23:51 +0100

    crypto/secp256k1: drop pkgsrc paths from CFLAGS
    They cause compiler warnings for people who don't have these
    directories. People with pkgsrc can add the directory through CGO_CFLAGS
    instead.

--- crypto/secp256k1/secp256.go
+++ crypto/secp256k1/secp256.go
@@ -20,11 +20,11 @@ package secp256k1
 
 /*
 #cgo CFLAGS: -I./libsecp256k1
-#cgo darwin CFLAGS: -I/usr/local/include -I/opt/pkg/include
+#cgo darwin CFLAGS: -I/usr/local/include
 #cgo freebsd CFLAGS: -I/usr/local/include
 #cgo linux,arm CFLAGS: -I/usr/local/arm/include
 #cgo LDFLAGS: -lgmp
-#cgo darwin LDFLAGS: -L/usr/local/lib -L/opt/pkg/lib
+#cgo darwin LDFLAGS: -L/usr/local/lib
 #cgo freebsd LDFLAGS: -L/usr/local/lib
 #cgo linux,arm LDFLAGS: -L/usr/local/arm/lib
 #define USE_NUM_GMP
