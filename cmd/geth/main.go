1c63d08ed1ec52a250fdef98fdb8694cea685bf8
Jeffrey Wilcke <geffobscura@gmail.com>
2015-11-19 11:55:38 +0100

    cmd/geth, cmd/utils: removed legalese
    Removed the legalese confirmation dialog. This closes #1992

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -405,8 +405,6 @@ func makeDefaultExtra() []byte {
 }
 
 func run(ctx *cli.Context) {
-	utils.CheckLegalese(utils.MustDataDir(ctx))
-
 	cfg := utils.MakeEthConfig(ClientIdentifier, nodeNameVersion, ctx)
 	cfg.ExtraData = makeExtra(ctx)
 
@@ -421,8 +419,6 @@ func run(ctx *cli.Context) {
 }
 
 func attach(ctx *cli.Context) {
-	utils.CheckLegalese(utils.MustDataDir(ctx))
-
 	var client comms.EthereumClient
 	var err error
 	if ctx.Args().Present() {
@@ -454,8 +450,6 @@ func attach(ctx *cli.Context) {
 }
 
 func console(ctx *cli.Context) {
-	utils.CheckLegalese(utils.MustDataDir(ctx))
-
 	cfg := utils.MakeEthConfig(ClientIdentifier, nodeNameVersion, ctx)
 	cfg.ExtraData = makeExtra(ctx)
 
@@ -488,8 +482,6 @@ func console(ctx *cli.Context) {
 }
 
 func execJSFiles(ctx *cli.Context) {
-	utils.CheckLegalese(utils.MustDataDir(ctx))
-
 	cfg := utils.MakeEthConfig(ClientIdentifier, nodeNameVersion, ctx)
 	ethereum, err := eth.New(cfg)
 	if err != nil {
@@ -515,8 +507,6 @@ func execJSFiles(ctx *cli.Context) {
 }
 
 func unlockAccount(ctx *cli.Context, am *accounts.Manager, addr string, i int, inputpassphrases []string) (addrHex, auth string, passphrases []string) {
-	utils.CheckLegalese(ctx.GlobalString(utils.DataDirFlag.Name))
-
 	var err error
 	passphrases = inputpassphrases
 	addrHex, err = utils.ParamToAddress(addr, am)
@@ -541,16 +531,12 @@ func unlockAccount(ctx *cli.Context, am *accounts.Manager, addr string, i int, i
 }
 
 func blockRecovery(ctx *cli.Context) {
-	utils.CheckLegalese(utils.MustDataDir(ctx))
-
 	if len(ctx.Args()) < 1 {
 		glog.Fatal("recover requires block number or hash")
 	}
 	arg := ctx.Args().First()
 
 	cfg := utils.MakeEthConfig(ClientIdentifier, nodeNameVersion, ctx)
-	utils.CheckLegalese(cfg.DataDir)
-
 	blockDb, err := ethdb.NewLDBDatabase(filepath.Join(cfg.DataDir, "blockchain"), cfg.DatabaseCache)
 	if err != nil {
 		glog.Fatalln("could not open db:", err)
@@ -611,8 +597,6 @@ func startEth(ctx *cli.Context, eth *eth.Ethereum) {
 }
 
 func accountList(ctx *cli.Context) {
-	utils.CheckLegalese(utils.MustDataDir(ctx))
-
 	am := utils.MakeAccountManager(ctx)
 	accts, err := am.Accounts()
 	if err != nil {
@@ -664,8 +648,6 @@ func getPassPhrase(ctx *cli.Context, desc string, confirmation bool, i int, inpu
 }
 
 func accountCreate(ctx *cli.Context) {
-	utils.CheckLegalese(utils.MustDataDir(ctx))
-
 	am := utils.MakeAccountManager(ctx)
 	passphrase, _ := getPassPhrase(ctx, "Your new account is locked with a password. Please give a password. Do not forget this password.", true, 0, nil)
 	acct, err := am.NewAccount(passphrase)
@@ -676,8 +658,6 @@ func accountCreate(ctx *cli.Context) {
 }
 
 func accountUpdate(ctx *cli.Context) {
-	utils.CheckLegalese(utils.MustDataDir(ctx))
-
 	am := utils.MakeAccountManager(ctx)
 	arg := ctx.Args().First()
 	if len(arg) == 0 {
@@ -693,8 +673,6 @@ func accountUpdate(ctx *cli.Context) {
 }
 
 func importWallet(ctx *cli.Context) {
-	utils.CheckLegalese(utils.MustDataDir(ctx))
-
 	keyfile := ctx.Args().First()
 	if len(keyfile) == 0 {
 		utils.Fatalf("keyfile must be given as argument")
@@ -715,8 +693,6 @@ func importWallet(ctx *cli.Context) {
 }
 
 func accountImport(ctx *cli.Context) {
-	utils.CheckLegalese(utils.MustDataDir(ctx))
-
 	keyfile := ctx.Args().First()
 	if len(keyfile) == 0 {
 		utils.Fatalf("keyfile must be given as argument")
@@ -731,8 +707,6 @@ func accountImport(ctx *cli.Context) {
 }
 
 func makedag(ctx *cli.Context) {
-	utils.CheckLegalese(utils.MustDataDir(ctx))
-
 	args := ctx.Args()
 	wrongArgs := func() {
 		utils.Fatalf(`Usage: geth makedag <block number> <outputdir>`)
