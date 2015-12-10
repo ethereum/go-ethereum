ebfc3388143d848007a66bf12c46c45b62a5f77c
Felix Lange <fjl@twurst.com>
2015-12-10 14:11:33 +0100

    p2p: add connectivity docs and protocol-level Prefer channel

--- p2p/server.go
+++ p2p/server.go
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-// Package p2p implements the Ethereum p2p network protocols.
 package p2p
 
 import (
