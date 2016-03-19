e70a2b449160d651a898e46288d4e30cbcd3c737
Jeffrey Wilcke <jeffrey@ethereum.org>
2016-03-19 01:24:23 +0100

    core/vm: fix possible jump table race

--- core/vm/jump_table_test.go
+++ core/vm/jump_table_test.go
@@ -1,24 +1,13 @@
 package vm
 
-import (
-	"math/big"
-	"testing"
-
-	"github.com/ethereum/go-ethereum/params"
-)
+import "testing"
 
 func TestInit(t *testing.T) {
-	params.HomesteadBlock = big.NewInt(1)
-
-	jumpTable.init(big.NewInt(0))
-	if jumpTable[DELEGATECALL].valid {
+	if frontierJumpTable[DELEGATECALL].valid {
 		t.Error("Expected DELEGATECALL not to be present")
 	}
 
-	for _, n := range []int64{1, 2, 100} {
-		jumpTable.init(big.NewInt(n))
-		if !jumpTable[DELEGATECALL].valid {
-			t.Error("Expected DELEGATECALL to be present for block", n)
-		}
+	if !homesteadJumpTable[DELEGATECALL].valid {
+		t.Error("Expected DELEGATECALL not to be present")
 	}
 }
