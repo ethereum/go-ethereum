fd36448d6afa16ecee7f56f9dc124ef4e231b9f5
Péter Szilágyi <peterke@gmail.com>
2016-04-13 11:49:22 +0300

    accounts: disable filesystem notifications on ios

--- accounts/watch.go
+++ accounts/watch.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-// +build darwin freebsd linux netbsd solaris windows
+// +build darwin,!ios freebsd linux netbsd solaris windows
 
 package accounts
 
