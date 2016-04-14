d04a2e7557596258faa896fea57dc793651af2e8
Felix Lange <fjl@twurst.com>
2016-04-14 18:18:24 +0200

    all: update license information

--- core/vm/jit_test.go
+++ core/vm/jit_test.go
@@ -1,4 +1,4 @@
-// Copyright 2014 The go-ethereum Authors
+// Copyright 2015 The go-ethereum Authors
 // This file is part of the go-ethereum library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
@@ -13,6 +13,7 @@
 //
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
 package vm
 
 import (
