9ce64fa6483ab87aa485415bae25638cdcffbe38
Jason Coombs <jc@jasoncoombs.com>
2016-06-30 16:46:51 -1000

    -a instead of -L - Bash conditional expressions
    see http://www.gnu.org/software/bash/manual/bashref.html#Bash-Conditional-Expressions

--- build/env.sh
+++ build/env.sh
@@ -10,14 +10,14 @@ if [ ! -f "$root/build/env.sh" ]; then
 fi
 
 # Create build/bin if it doesn't exist yet.
-if [ ! -L "$root/build/bin" ]; then
+if [ ! -a "$root/build/bin" ]; then
     mkdir -p "$root/build/bin"
 fi
 
 # Create fake Go workspace if it doesn't exist yet.
 workspace="$root/../_go_build/_workspace"
 ethdir="$workspace/src/github.com/ethereum"
-if [ ! -L "$ethdir/go-ethereum" ]; then
+if [ ! -a "$ethdir/go-ethereum" ]; then
     mkdir -p "$ethdir"
     cd "$ethdir"
     ln -s "$root" go-ethereum
