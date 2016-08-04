f5f042ffdc9fed3094b86f3dbbc85bb63a4f9537
Felix Lange <fjl@twurst.com>
2016-08-04 21:18:13 +0200

    rpc: ensure client doesn't block for slow subscribers
    I initially made the client block if the 100-element buffer was
    exceeded. It turns out that this is inconvenient for simple uses of the
    client which subscribe and perform calls on the same goroutine, e.g.
    
        client, _ := rpc.Dial(...)
        ch := make(chan int) // note: no buffer
        sub, _ := client.EthSubscribe(ch, "something")
        for event := range ch {
            client.Call(...)
        }
    
    This innocent looking code will lock up if the server suddenly decides
    to send 2000 notifications. In this case, the client's main loop won't
    accept the call because it is trying to deliver a notification to ch.
    
    The issue is kind of hard to explain in the docs and few people will
    actually read them. Buffering is the simple option and works with close
    to no overhead for subscribers that always listen.

--- rpc/client_test.go
+++ rpc/client_test.go
@@ -296,6 +296,57 @@ func TestClientSubscribeClose(t *testing.T) {
 	}
 }
 
+// This test checks that Client doesn't lock up when a single subscriber
+// doesn't read subscription events.
+func TestClientNotificationStorm(t *testing.T) {
+	server := newTestServer("eth", new(NotificationTestService))
+	defer server.Stop()
+
+	doTest := func(count int, wantError bool) {
+		client := DialInProc(server)
+		defer client.Close()
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		// Subscribe on the server. It will start sending many notifications
+		// very quickly.
+		nc := make(chan int)
+		sub, err := client.EthSubscribe(nc, "someSubscription", count, 0)
+		if err != nil {
+			t.Fatal("can't subscribe:", err)
+		}
+		defer sub.Unsubscribe()
+
+		// Process each notification, try to run a call in between each of them.
+		for i := 0; i < count; i++ {
+			select {
+			case val := <-nc:
+				if val != i {
+					t.Fatalf("(%d/%d) unexpected value %d", i, count, val)
+				}
+			case err := <-sub.Err():
+				if wantError && err != ErrSubscriptionQueueOverflow {
+					t.Fatalf("(%d/%d) got error %q, want %q", i, count, err, ErrSubscriptionQueueOverflow)
+				} else if !wantError {
+					t.Fatalf("(%d/%d) got unexpected error %q", i, count, err)
+				}
+				return
+			}
+			var r int
+			err := client.CallContext(ctx, &r, "eth_echo", i)
+			if err != nil {
+				if !wantError {
+					t.Fatalf("(%d/%d) call error: %v", i, count, err)
+				}
+				return
+			}
+		}
+	}
+
+	doTest(8000, false)
+	doTest(10000, true)
+}
+
 func TestClientHTTP(t *testing.T) {
 	server := newTestServer("service", new(Service))
 	defer server.Stop()
