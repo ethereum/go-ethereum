79de990d2e96c6984a26c03314783247a5f07f8e
Max Wolter <awishformore@gmail.com>
2016-08-08 23:14:57 +0200

    accounts/abi/bind: moved bound contract to stable Go Ethereum API

--- accounts/abi/bind/base.go
+++ accounts/abi/bind/base.go
@@ -22,9 +22,11 @@ import (
 	"math/big"
 	"sync/atomic"
 
+	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/ethereum/go-ethereum/crypto"
 	"golang.org/x/net/context"
 )
@@ -54,27 +56,38 @@ type TransactOpts struct {
 	Context context.Context // Network context to support cancellation and timeouts (nil = no timeout)
 }
 
+// SubscribeOpts is the collection of options to fine tune the contract subscription
+// and unsubscription requests.
+type SubscribeOpts struct {
+	History   bool     // Whether to also retrieve events from the past
+	FromBlock *big.Int // Block at which we want to start retrieving past events
+
+	Context context.Context // Network context to support cancellation and timeouts (nil = no timeout)
+}
+
 // BoundContract is the base wrapper object that reflects a contract on the
 // Ethereum network. It contains a collection of methods that are used by the
 // higher level contract bindings to operate.
 type BoundContract struct {
-	address    common.Address     // Deployment address of the contract on the Ethereum blockchain
-	abi        abi.ABI            // Reflect based ABI to access the correct Ethereum methods
-	caller     ContractCaller     // Read interface to interact with the blockchain
-	transactor ContractTransactor // Write interface to interact with the blockchain
+	address common.Address // Deployment address of the contract on the Ethereum blockchain
+	abi     abi.ABI        // Reflect based ABI to access the correct Ethereum methods
+
+	backend ContractBackend
 
 	latestHasCode  uint32 // Cached verification that the latest state contains code for this contract
 	pendingHasCode uint32 // Cached verification that the pending state contains code for this contract
+
+	subscriptions map[chan<- vm.Log]ethereum.Subscription
+	errors        map[chan<- vm.Log]error
 }
 
 // NewBoundContract creates a low level contract interface through which calls
 // and transactions may be made through.
-func NewBoundContract(address common.Address, abi abi.ABI, caller ContractCaller, transactor ContractTransactor) *BoundContract {
+func NewBoundContract(address common.Address, abi abi.ABI, backend ContractBackend) *BoundContract {
 	return &BoundContract{
-		address:    address,
-		abi:        abi,
-		caller:     caller,
-		transactor: transactor,
+		address: address,
+		abi:     abi,
+		backend: backend,
 	}
 }
 
@@ -82,7 +95,7 @@ func NewBoundContract(address common.Address, abi abi.ABI, caller ContractCaller
 // deployment address with a Go wrapper.
 func DeployContract(opts *TransactOpts, abi abi.ABI, bytecode []byte, backend ContractBackend, params ...interface{}) (common.Address, *types.Transaction, *BoundContract, error) {
 	// Otherwise try to deploy the contract
-	c := NewBoundContract(common.Address{}, abi, backend, backend)
+	c := NewBoundContract(common.Address{}, abi, backend)
 
 	input, err := c.abi.Pack("", params...)
 	if err != nil {
@@ -107,9 +120,9 @@ func (c *BoundContract) Call(opts *CallOpts, result interface{}, method string, 
 	}
 	// Make sure we have a contract to operate on, and bail out otherwise
 	if (opts.Pending && atomic.LoadUint32(&c.pendingHasCode) == 0) || (!opts.Pending && atomic.LoadUint32(&c.latestHasCode) == 0) {
-		if code, err := c.caller.HasCode(opts.Context, c.address, opts.Pending); err != nil {
+		if code, err := c.backend.CodeAt(opts.Context, c.address, nil); err != nil {
 			return err
-		} else if !code {
+		} else if len(code) == 0 {
 			return ErrNoCode
 		}
 		if opts.Pending {
@@ -123,7 +136,18 @@ func (c *BoundContract) Call(opts *CallOpts, result interface{}, method string, 
 	if err != nil {
 		return err
 	}
-	output, err := c.caller.ContractCall(opts.Context, c.address, input, opts.Pending)
+	// Create the call message we use to make the call
+	callMsg := ethereum.CallMsg{
+		To:   c.address,
+		Data: input,
+	}
+	var output []byte
+	// Call pending or not depending on options
+	if opts.Pending {
+		output, err = c.backend.PendingCallContract(opts.Context, callMsg)
+	} else {
+		output, err = c.backend.CallContract(opts.Context, callMsg, nil)
+	}
 	if err != nil {
 		return err
 	}
@@ -158,7 +182,7 @@ func (c *BoundContract) transact(opts *TransactOpts, contract *common.Address, i
 	}
 	nonce := uint64(0)
 	if opts.Nonce == nil {
-		nonce, err = c.transactor.PendingAccountNonce(opts.Context, opts.From)
+		nonce, err = c.backend.PendingNonceAt(opts.Context, opts.From)
 		if err != nil {
 			return nil, fmt.Errorf("failed to retrieve account nonce: %v", err)
 		}
@@ -168,7 +192,7 @@ func (c *BoundContract) transact(opts *TransactOpts, contract *common.Address, i
 	// Figure out the gas allowance and gas price values
 	gasPrice := opts.GasPrice
 	if gasPrice == nil {
-		gasPrice, err = c.transactor.SuggestGasPrice(opts.Context)
+		gasPrice, err = c.backend.SuggestGasPrice(opts.Context)
 		if err != nil {
 			return nil, fmt.Errorf("failed to suggest gas price: %v", err)
 		}
@@ -177,17 +201,25 @@ func (c *BoundContract) transact(opts *TransactOpts, contract *common.Address, i
 	if gasLimit == nil {
 		// Gas estimation cannot succeed without code for method invocations
 		if contract != nil && atomic.LoadUint32(&c.pendingHasCode) == 0 {
-			if code, err := c.transactor.HasCode(opts.Context, c.address, true); err != nil {
+			var code []byte
+			if code, err = c.backend.CodeAt(opts.Context, c.address, nil); err != nil {
 				return nil, err
-			} else if !code {
+			} else if len(code) == 0 {
 				return nil, ErrNoCode
 			}
 			atomic.StoreUint32(&c.pendingHasCode, 1)
 		}
 		// If the contract surely has code (or code is not needed), estimate the transaction
-		gasLimit, err = c.transactor.EstimateGasLimit(opts.Context, opts.From, contract, value, input)
+		callMsg := ethereum.CallMsg{
+			From:     opts.From,
+			To:       *contract,
+			GasPrice: gasPrice,
+			Value:    value,
+			Data:     input,
+		}
+		gasLimit, err = c.backend.EstimateGas(opts.Context, callMsg)
 		if err != nil {
-			return nil, fmt.Errorf("failed to exstimate gas needed: %v", err)
+			return nil, fmt.Errorf("failed to estimate gas needed: %v", err)
 		}
 	}
 	// Create the transaction, sign it and schedule it for execution
@@ -204,8 +236,112 @@ func (c *BoundContract) transact(opts *TransactOpts, contract *common.Address, i
 	if err != nil {
 		return nil, err
 	}
-	if err := c.transactor.SendTransaction(opts.Context, signedTx); err != nil {
+	if err := c.backend.SendTransaction(opts.Context, signedTx); err != nil {
 		return nil, err
 	}
 	return signedTx, nil
+}
+
+// Subscribe subscribes to the contract for the given topics. Subscription events
+// will be submitted to the provided channel. Upon cancelation of the subscription,
+// the channel will be closed. A channel should only be used for ones subscription.
+func (c *BoundContract) Subscribe(opts *SubscribeOpts, channel chan<- vm.Log, topics ...[]common.Hash) error {
+	// Check if we already have a subscription on this channel
+	_, ok := c.subscriptions[channel]
+	if ok {
+		return fmt.Errorf("cannot reuse channel for multiple subscriptions")
+	}
+
+	// Build the filter query with our desired options and topics
+	filterQuery := ethereum.FilterQuery{
+		FromBlock: opts.FromBlock,
+		ToBlock:   nil,
+		Addresses: []common.Address{c.address},
+		Topics:    topics,
+	}
+
+	// If we don't want to miss any real-time event logs, we need to subscribe right away
+	tempChannel := make(chan vm.Log)
+	tempSub, err := c.backend.SubscribeFilterLogs(opts.Context, filterQuery, tempChannel)
+	if err != nil {
+		return fmt.Errorf("could not create subscription (%v)", err)
+	}
+
+	// Get the desired historical data and feed it into the subscription channel
+	if opts.History {
+		var logs []vm.Log
+		logs, err = c.backend.FilterLogs(opts.Context, filterQuery)
+		if err != nil {
+			return fmt.Errorf("could not retrieve history of subscription events")
+		}
+		for _, log := range logs {
+			channel <- log
+		}
+	}
+
+	// Feed the real-time events that happenend in the meantime to the subscription channel
+	// and create the new subscription as soon as none are left
+	var subscription ethereum.Subscription
+Feed:
+	for {
+		select {
+		case log := <-tempChannel:
+			channel <- log
+		default:
+			subscription, err = c.backend.SubscribeFilterLogs(opts.Context, filterQuery, channel)
+			tempSub.Unsubscribe()
+			for _ = range tempSub.Err() {
+			}
+		Drain:
+			for {
+				select {
+				case <-tempChannel:
+				default:
+					break Drain
+				}
+			}
+			close(tempChannel)
+			break Feed
+		}
+	}
+
+	// check if the subscription succeeded
+	if err != nil {
+		return fmt.Errorf("could not create subscription (%v)", err)
+	}
+	c.subscriptions[channel] = subscription
+
+	// Read from the error channel in the background
+	// When we receive an error, we save it with the channel index
+	// When the error channel is closed, it indicates an ended subscription
+	// We then close the subscription channel to notify the consumer and remove
+	// the subscription from our map
+	go func(errChannel <-chan error) {
+		for err := range errChannel {
+			c.errors[channel] = err
+		}
+		close(channel)
+		delete(c.subscriptions, channel)
+	}(subscription.Err())
+	return nil
+}
+
+// Error returns the error that was encountered for the subscription on the given
+// channel. It will reset the error upon retrieval.
+func (c *BoundContract) Error(channel chan<- vm.Log) error {
+	err := c.errors[channel]
+	delete(c.errors, channel)
+	return err
+}
+
+// Unsubscribe cancels the subscription if one exists on the given channel. The
+// subscription channel will be closed once the subscription was successfully
+// canceled.
+func (c *BoundContract) Unsubscribe(channel chan<- vm.Log) error {
+	subscription, ok := c.subscriptions[channel]
+	if !ok {
+		return fmt.Errorf("subscription doesn't exist or already closed")
+	}
+	subscription.Unsubscribe()
+	return nil
 }
