5fff491bbdbe2d2a6ddf8e5862b4a928a3832add
Jeffrey Wilcke <geffobscura@gmail.com>
2016-08-21 23:51:42 +0200

    cmd/geth: added copyright and license information
    As per GNU GPL requirement I've added the copyright and the license
    information as a subcommand as well as a copyright notice when
    displaying the help command.

--- cmd/geth/usage.go
+++ cmd/geth/usage.go
@@ -30,6 +30,8 @@ import (
 var AppHelpTemplate = `NAME:
    {{.App.Name}} - {{.App.Usage}}
 
+   Copyright 2013-2016 The go-ethereum Authors
+
 USAGE:
    {{.App.HelpName}} [options]{{if .App.Commands}} command [command options]{{end}} {{if .App.ArgsUsage}}{{.App.ArgsUsage}}{{else}}[arguments...]{{end}}
    {{if .App.Version}}
