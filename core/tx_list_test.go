7161efd70b00ffbf5c18cb4d0c9536b5339cf40e
Christopher Franko <chrisjfranko@gmail.com>
2016-10-11 18:22:02 -0400

    Update dockerfile and tx list deps

--- core/tx_list_test.go
+++ core/tx_list_test.go
@@ -21,8 +21,8 @@ import (
 	"math/rand"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/expanse-project/go-expanse/core/types"
+	"github.com/expanse-project/go-expanse/crypto"
 )
 
 // Tests that transactions can be added to strict lists and list contents and
