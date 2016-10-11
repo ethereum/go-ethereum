840505dfd251823fd0385d236d70f6c689fef429
Daniel A. Nagy <nagy.da@gmail.com>
2016-10-11 17:22:46 +0200

    swarm/storage: see #3108

--- swarm/storage/common_test.go
+++ swarm/storage/common_test.go
@@ -1,8 +1,25 @@
+// Copyright 2016 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
 package storage
 
 import (
 	"bytes"
 	"crypto/rand"
+	"fmt"
 	"io"
 	"sync"
 	"testing"
@@ -11,32 +28,31 @@ import (
 	"github.com/ethereum/go-ethereum/logger/glog"
 )
 
-type limitedReader struct {
-	r    io.Reader
-	off  int64
-	size int64
+type brokenLimitedReader struct {
+	lr    io.Reader
+	errAt int
+	off   int
+	size  int
 }
 
-func limitReader(r io.Reader, size int) *limitedReader {
-	return &limitedReader{r, 0, int64(size)}
-}
-
-func (self *limitedReader) Read(buf []byte) (int, error) {
-	limit := int64(len(buf))
-	left := self.size - self.off
-	if limit >= left {
-		limit = left
+func brokenLimitReader(data io.Reader, size int, errAt int) *brokenLimitedReader {
+	return &brokenLimitedReader{
+		lr:    data,
+		errAt: errAt,
+		size:  size,
 	}
-	n, err := self.r.Read(buf[:limit])
-	if err == nil && limit == left {
-		err = io.EOF
-	}
-	self.off += int64(n)
-	return n, err
 }
 
 func testDataReader(l int) (r io.Reader) {
-	return limitReader(rand.Reader, l)
+	return io.LimitReader(rand.Reader, int64(l))
+}
+
+func (self *brokenLimitedReader) Read(buf []byte) (int, error) {
+	if self.off+len(buf) > self.errAt {
+		return 0, fmt.Errorf("Broken reader")
+	}
+	self.off += len(buf)
+	return self.lr.Read(buf)
 }
 
 func testDataReaderAndSlice(l int) (r io.Reader, slice []byte) {
@@ -44,7 +60,7 @@ func testDataReaderAndSlice(l int) (r io.Reader, slice []byte) {
 	if _, err := rand.Read(slice); err != nil {
 		panic("rand error")
 	}
-	r = limitReader(bytes.NewReader(slice), l)
+	r = io.LimitReader(bytes.NewReader(slice), int64(l))
 	return
 }
 
@@ -76,14 +92,14 @@ func testStore(m ChunkStore, l int64, branches int64, t *testing.T) {
 			go func(chunk *Chunk) {
 				storedChunk, err := m.Get(chunk.Key)
 				if err == notFound {
-					glog.V(logger.Detail).Infof("[BZZ] chunk '%v' not found", chunk.Key.Log())
+					glog.V(logger.Detail).Infof("chunk '%v' not found", chunk.Key.Log())
 				} else if err != nil {
-					glog.V(logger.Detail).Infof("[BZZ] error retrieving chunk %v: %v", chunk.Key.Log(), err)
+					glog.V(logger.Detail).Infof("error retrieving chunk %v: %v", chunk.Key.Log(), err)
 				} else {
 					chunk.SData = storedChunk.SData
 					chunk.Size = storedChunk.Size
 				}
-				glog.V(logger.Detail).Infof("[BZZ] chunk '%v' not found", chunk.Key.Log())
+				glog.V(logger.Detail).Infof("chunk '%v' not found", chunk.Key.Log())
 				close(chunk.C)
 			}(ch)
 		}
