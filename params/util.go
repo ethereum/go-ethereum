211198fae5415c5d4d4506d0b8c7c300fbe258e6
Julian Yap <julianokyap@gmail.com>
2016-11-19 00:54:52 -1000

    Modify hard fork blocks

--- params/util.go
+++ params/util.go
@@ -27,16 +27,16 @@ var (
 	MainNetGenesisHash = common.HexToHash("0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3") // Mainnet genesis hash to enforce below configs on
 
 	TestNetHomesteadBlock = big.NewInt(494000)  // Testnet homestead block
-	MainNetHomesteadBlock = big.NewInt(1150000) // Mainnet homestead block
+	MainNetHomesteadBlock = big.NewInt(100) // Mainnet homestead block
 
 	TestNetHomesteadGasRepriceBlock = big.NewInt(1783000) // Testnet gas reprice block
-	MainNetHomesteadGasRepriceBlock = big.NewInt(2463000) // Mainnet gas reprice block
+	MainNetHomesteadGasRepriceBlock = big.NewInt(110) // Mainnet gas reprice block
 
 	TestNetHomesteadGasRepriceHash = common.HexToHash("0xf376243aeff1f256d970714c3de9fd78fa4e63cf63e32a51fe1169e375d98145") // Testnet gas reprice block hash (used by fast sync)
 	MainNetHomesteadGasRepriceHash = common.HexToHash("0x2086799aeebeae135c246c65021c82b4e15a2c451340993aacfd2751886514f0") // Mainnet gas reprice block hash (used by fast sync)
 
 	TestNetSpuriousDragon = big.NewInt(1885000)
-	MainNetSpuriousDragon = big.NewInt(2675000)
+	MainNetSpuriousDragon = big.NewInt(120)
 
 	TestNetChainID = big.NewInt(2) // Test net default chain ID
 	MainNetChainID = big.NewInt(1) // main net default chain ID
