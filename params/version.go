74a4108db5318dfe7139fd2a603611cf41b5d1df
Jeffrey Wilcke <jeffrey@ethereum.org>
2016-11-28 11:40:26 +0100

    params: stable version 1.5.4

--- params/version.go
+++ params/version.go
@@ -19,10 +19,10 @@ package params
 import "fmt"
 
 const (
-	VersionMajor = 1          // Major version component of the current release
-	VersionMinor = 5          // Minor version component of the current release
-	VersionPatch = 4          // Patch version component of the current release
-	VersionMeta  = "unstable" // Version metadata to append to the version string
+	VersionMajor = 1        // Major version component of the current release
+	VersionMinor = 5        // Minor version component of the current release
+	VersionPatch = 4        // Patch version component of the current release
+	VersionMeta  = "stable" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
