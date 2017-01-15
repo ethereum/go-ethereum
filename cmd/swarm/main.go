f9232e7094851b9e99e027b6e90fc60acf6e3066
5chdn <5chdn@users.noreply.github.com>
2017-01-15 20:21:23 +0100

    cmd/swarm: prepare enabling testnet flag for swarm node

--- cmd/swarm/main.go
+++ cmd/swarm/main.go
@@ -1,4 +1,4 @@
-// Copyright 2016 The go-ethereum Authors
+// Copyright 2017 The go-ethereum Authors
 // This file is part of go-ethereum.
 //
 // go-ethereum is free software: you can redistribute it and/or modify
@@ -127,7 +127,7 @@ func init() {
 	// Set up the cli app.
 	app.Action = bzzd
 	app.HideVersion = true // we have a command to print the version
-	app.Copyright = "Copyright 2013-2016 The go-ethereum Authors"
+	app.Copyright = "Copyright 2013-2017 The go-ethereum Authors"
 	app.Commands = []cli.Command{
 		{
 			Action:    version,
@@ -163,6 +163,7 @@ Prints the swarm hash of file or directory.
 		utils.DataDirFlag,
 		utils.BootnodesFlag,
 		utils.KeyStoreDirFlag,
+		utils.TestNetFlag,
 		utils.ListenPortFlag,
 		utils.NoDiscoverFlag,
 		utils.DiscoveryV5Flag,
