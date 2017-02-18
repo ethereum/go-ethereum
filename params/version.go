037c8b9ae9abe7b8739943838c91255abc2dd3b5
Jeffrey Wilcke <jeffrey@ethereum.org>
2017-02-18 09:28:15 +0100

    VERSION, params: bumped unstable version to 1.6 (#3685)

--- params/version.go
+++ params/version.go
@@ -20,8 +20,8 @@ import "fmt"
 
 const (
 	VersionMajor = 1          // Major version component of the current release
-	VersionMinor = 5          // Minor version component of the current release
-	VersionPatch = 10         // Patch version component of the current release
+	VersionMinor = 6          // Minor version component of the current release
+	VersionPatch = 0          // Patch version component of the current release
 	VersionMeta  = "unstable" // Version metadata to append to the version string
 )
 
