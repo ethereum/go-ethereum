09d0f16427e5c436c6d2f5fdcecda74dd2612e51
Péter Szilágyi <peterke@gmail.com>
2017-03-03 18:13:21 +0200

    core, eth, les: support resuming fast sync on heavy rollback

--- les/api_backend.go
+++ les/api_backend.go
@@ -50,6 +50,7 @@ func (b *LesApiBackend) CurrentBlock() *types.Block {
 }
 
 func (b *LesApiBackend) SetHead(number uint64) {
+	b.eth.protocolManager.downloader.Cancel()
 	b.eth.blockchain.SetHead(number)
 }
 
