0cc492f81595c28caa24964a105446e362164539
Felix Lange <fjl@twurst.com>
2017-04-14 10:29:00 +0200

    all: update license information

--- cmd/swarm/list.go
+++ cmd/swarm/list.go
@@ -1,4 +1,4 @@
-// Copyright 2016 The go-ethereum Authors
+// Copyright 2017 The go-ethereum Authors
 // This file is part of go-ethereum.
 //
 // go-ethereum is free software: you can redistribute it and/or modify
