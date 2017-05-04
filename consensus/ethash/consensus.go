764c2520c36357d7e46c150a4f08e1b587222890
Martin Holst Swende <martin@swende.se>
2017-05-04 11:54:39 +0200

    math/big: rename TTM63m1 -> MaxBig63, + go fmt

--- consensus/ethash/consensus.go
+++ consensus/ethash/consensus.go
@@ -244,12 +244,12 @@ func (ethash *Ethash) verifyHeader(chain consensus.ChainReader, header, parent *
 		return fmt.Errorf("invalid difficulty: have %v, want %v", header.Difficulty, expected)
 	}
 	// Verify that the gas limit is < 2^63-1
-	if header.GasLimit.Cmp(math.TT63m1) > 0{
-		return fmt.Errorf("invalid gasLimit: have %v, max %v", header.GasLimit, math.TT63m1)	
+	if header.GasLimit.Cmp(math.MaxBig63) > 0 {
+		return fmt.Errorf("invalid gasLimit: have %v, max %v", header.GasLimit, math.MaxBig63)
 	}
 	// Verify that the gasUsed is <= gasLimit
-	if header.GasUsed.Cmp(header.GasLimit) > 0{
-		return fmt.Errorf("invalid gasUsed: have %v, gasLimit %v", header.GasUsed, header.GasLimit)			
+	if header.GasUsed.Cmp(header.GasLimit) > 0 {
+		return fmt.Errorf("invalid gasUsed: have %v, gasLimit %v", header.GasUsed, header.GasLimit)
 	}
 
 	// Verify that the gas limit remains within allowed bounds
