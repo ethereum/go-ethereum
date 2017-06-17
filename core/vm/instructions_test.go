e556bf152e3575ce9bbe6928b8ceca559d523b91
Martin Holst Swende <martin@swende.se>
2017-06-17 19:31:56 +0200

    core/vm: Implemented benchmarks of precompiles

--- core/vm/instructions_test.go
+++ core/vm/instructions_test.go
@@ -1,6 +1,7 @@
 package vm
 
 import (
+	"fmt"
 	"math/big"
 	"testing"
 
@@ -66,6 +67,72 @@ func getBenchmarker(op func(pc *uint64, evm *EVM, contract *Contract, memory *Me
 	return x
 }
 
+func precompiledBenchMarker(addr, input, expected string) func(*testing.B) {
+
+	x := func(bench *testing.B) {
+
+		contract := NewContract(
+			AccountRef(common.HexToAddress("1337")),
+			nil,
+			new(big.Int),
+			4000000)
+
+		p := PrecompiledContracts[common.HexToAddress(addr)]
+		in := common.Hex2Bytes(input)
+
+		//Check if it is correct
+		res, err := RunPrecompiledContract(p, in, contract)
+		if err != nil {
+			bench.Error(err)
+			return
+		}
+		if common.Bytes2Hex(res) != expected {
+			bench.Error(fmt.Sprintf("Expected %v, got %v", expected, common.Bytes2Hex(res)))
+			return
+		}
+		data := make([]byte, len(in))
+		bench.ResetTimer()
+		for i := 0; i < bench.N; i++ {
+			copy(data, in)
+			RunPrecompiledContract(p, data, contract)
+		}
+
+	}
+	return x
+}
+
+func BenchmarkPrecompiledEcdsa(bench *testing.B) {
+	var (
+		addr = "01"
+		inp  = "38d18acb67d25c8bb9942764b62f18e17054f66a817bd4295423adf9ed98873e000000000000000000000000000000000000000000000000000000000000001b38d18acb67d25c8bb9942764b62f18e17054f66a817bd4295423adf9ed98873e789d1dd423d25f0772d2748d60f7e4b81bb14d086eba8e8e8efb6dcff8a4ae02"
+		exp  = "000000000000000000000000ceaccac640adf55b2028469bd36ba501f28b699d"
+	)
+	precompiledBenchMarker(addr, inp, exp)(bench)
+}
+func BenchmarkPrecompiledSha256(bench *testing.B) {
+	var (
+		addr = "02"
+		inp  = "38d18acb67d25c8bb9942764b62f18e17054f66a817bd4295423adf9ed98873e000000000000000000000000000000000000000000000000000000000000001b38d18acb67d25c8bb9942764b62f18e17054f66a817bd4295423adf9ed98873e789d1dd423d25f0772d2748d60f7e4b81bb14d086eba8e8e8efb6dcff8a4ae02"
+		exp  = "811c7003375852fabd0d362e40e68607a12bdabae61a7d068fe5fdd1dbbf2a5d"
+	)
+	precompiledBenchMarker(addr, inp, exp)(bench)
+}
+func BenchmarkPrecompiledRipeMD(bench *testing.B) {
+	var (
+		addr = "03"
+		inp  = "38d18acb67d25c8bb9942764b62f18e17054f66a817bd4295423adf9ed98873e000000000000000000000000000000000000000000000000000000000000001b38d18acb67d25c8bb9942764b62f18e17054f66a817bd4295423adf9ed98873e789d1dd423d25f0772d2748d60f7e4b81bb14d086eba8e8e8efb6dcff8a4ae02"
+		exp  = "0000000000000000000000009215b8d9882ff46f0dfde6684d78e831467f65e6"
+	)
+	precompiledBenchMarker(addr, inp, exp)(bench)
+}
+func BenchmarkPrecompiledIdentity(bench *testing.B) {
+	var (
+		addr = "04"
+		inp  = "38d18acb67d25c8bb9942764b62f18e17054f66a817bd4295423adf9ed98873e000000000000000000000000000000000000000000000000000000000000001b38d18acb67d25c8bb9942764b62f18e17054f66a817bd4295423adf9ed98873e789d1dd423d25f0772d2748d60f7e4b81bb14d086eba8e8e8efb6dcff8a4ae02"
+		exp  = "38d18acb67d25c8bb9942764b62f18e17054f66a817bd4295423adf9ed98873e000000000000000000000000000000000000000000000000000000000000001b38d18acb67d25c8bb9942764b62f18e17054f66a817bd4295423adf9ed98873e789d1dd423d25f0772d2748d60f7e4b81bb14d086eba8e8e8efb6dcff8a4ae02"
+	)
+	precompiledBenchMarker(addr, inp, exp)(bench)
+}
 func BenchmarkOpAdd(b *testing.B) {
 	x := "ABCDEF090807060504030201ffffffffffffffffffffffffffffffffffffffff"
 	y := "ABCDEF090807060504030201ffffffffffffffffffffffffffffffffffffffff"
