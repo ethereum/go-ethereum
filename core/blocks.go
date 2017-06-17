6c68b57de3290a98634d0b0296e5e1b5f791c1e1
Julian Yap <julianokyap@gmail.com>
2017-06-16 23:56:50 -1000

    Remove unused bad hashes

--- core/blocks.go
+++ core/blocks.go
@@ -19,7 +19,4 @@ package core
 import "github.com/ubiq/go-ubiq/common"
 
 // Set of manually tracked bad hashes (usually hard forks)
-var BadHashes = map[common.Hash]bool{
-	common.HexToHash("05bef30ef572270f654746da22639a7a0c97dd97a7050b9e252391996aaeb689"): true,
-	common.HexToHash("7d05d08cbc596a2e5e4f13b80a743e53e09221b5323c3a61946b20873e58583f"): true,
-}
+var BadHashes = map[common.Hash]bool{}
