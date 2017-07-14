52e7b2a9c897d930e77d2a27722a9a290dd7c0bc
Felix Lange <fjl@twurst.com>
2017-07-14 23:58:52 +0200

    core/state: simplify StateSync

--- eth/downloader/statesync.go
+++ eth/downloader/statesync.go
@@ -187,7 +187,7 @@ func (d *Downloader) runStateSync(s *stateSync) *stateSync {
 type stateSync struct {
 	d *Downloader // Downloader instance to access and manage current peerset
 
-	sched  *state.StateSync           // State trie sync scheduler defining the tasks
+	sched  *trie.TrieSync             // State trie sync scheduler defining the tasks
 	keccak hash.Hash                  // Keccak256 hasher to verify deliveries with
 	tasks  map[common.Hash]*stateTask // Set of tasks currently queued for retrieval
 
