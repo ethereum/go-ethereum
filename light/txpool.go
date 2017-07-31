c259e6874ece4ff3b6ee7b1ce77f58e64db1fb34
bas-vk <bas-vk@users.noreply.github.com>
2017-07-31 12:06:01 +0200

    light: update txpool signer to EIP155 (#14720)

--- light/txpool.go
+++ light/txpool.go
@@ -81,7 +81,7 @@ type TxRelayBackend interface {
 func NewTxPool(config *params.ChainConfig, eventMux *event.TypeMux, chain *LightChain, relay TxRelayBackend) *TxPool {
 	pool := &TxPool{
 		config:   config,
-		signer:   types.HomesteadSigner{},
+		signer:   types.NewEIP155Signer(config.ChainId),
 		nonce:    make(map[common.Address]uint64),
 		pending:  make(map[common.Hash]*types.Transaction),
 		mined:    make(map[common.Hash][]*types.Transaction),
