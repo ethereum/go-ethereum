15f68443690c2028938162648b5225b8b573ce37
Egon Elbre <egonelbre@gmail.com>
2017-08-08 09:32:38 +0300

    ethdb: add basic sanity test

--- ethdb/database_test.go
+++ ethdb/database_test.go
@@ -14,21 +14,72 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-package ethdb
+package ethdb_test
 
 import (
+	"bytes"
+	"io/ioutil"
 	"os"
-	"path/filepath"
+	"testing"
 
-	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethdb"
 )
 
-func newDb() *LDBDatabase {
-	file := filepath.Join("/", "tmp", "ldbtesttmpfile")
-	if common.FileExist(file) {
-		os.RemoveAll(file)
+func newDb() (*ethdb.LDBDatabase, func()) {
+	dirname, err := ioutil.TempDir(os.TempDir(), "ethdb_test_")
+	if err != nil {
+		panic("failed to create test file: " + err.Error())
 	}
-	db, _ := NewLDBDatabase(file, 0, 0)
+	db, err := ethdb.NewLDBDatabase(dirname, 0, 0)
+	if err != nil {
+		panic("failed to create test database: " + err.Error())
+	}
 
-	return db
+	return db, func() {
+		db.Close()
+		os.RemoveAll(dirname)
+	}
+}
+
+var test_values = []string{"", "a", "1251", "\x00123\x00"}
+
+func TestPutGet(t *testing.T) {
+	t.Parallel()
+
+	db, remove := newDb()
+	defer remove()
+
+	for _, v := range test_values {
+		err := db.Put([]byte(v), []byte(v))
+		if err != nil {
+			t.Fatalf("put failed: %v", err)
+		}
+	}
+
+	for _, v := range test_values {
+		data, err := db.Get([]byte(v))
+		if err != nil {
+			t.Fatalf("get failed: %v", err)
+		}
+		if !bytes.Equal(data, []byte(v)) {
+			t.Fatalf("get return wrong result, got %q expected %q", string(data), v)
+		}
+	}
+
+	for _, v := range test_values {
+		err := db.Put([]byte(v), []byte("?"))
+		if err != nil {
+			t.Fatalf("put override failed: %v", err)
+		}
+	}
+
+	for _, v := range test_values {
+		data, err := db.Get([]byte(v))
+		if err != nil {
+			t.Fatalf("get failed: %v", err)
+		}
+		if !bytes.Equal(data, []byte("?")) {
+			t.Fatalf("get return wrong result, got %q expected ?", string(data))
+		}
+	}
 }
