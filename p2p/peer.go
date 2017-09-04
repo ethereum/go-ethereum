dc92779c0a1e01de9561087a3a534d9d7cfdef3f
Martin Holst Swende <martin@swende.se>
2017-09-04 09:24:52 +0200

    p2p: change ping ticker to timer (#15071)
    Using a Timer over Ticker seems to be a lot better, though I cannot fully
    account for why that it behaves so (since Ticker should be more bursty, but not
    necessarily more active over time, but that may depend on how long window it
    uses to decide on when to tick next)

--- p2p/peer.go
+++ p2p/peer.go
@@ -190,7 +190,7 @@ loop:
 }
 
 func (p *Peer) pingLoop() {
-	ping := time.NewTicker(pingInterval)
+	ping := time.NewTimer(pingInterval)
 	defer p.wg.Done()
 	defer ping.Stop()
 	for {
@@ -200,6 +200,7 @@ func (p *Peer) pingLoop() {
 				p.protoErr <- err
 				return
 			}
+			ping.Reset(pingInterval)
 		case <-p.closed:
 			return
 		}
