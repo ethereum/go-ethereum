93f581cbc13c5f0313678451df905364911f589d
Péter Szilágyi <peterke@gmail.com>
2017-09-05 11:45:33 +0300

    cmd/evm, tests: minor polishes, dump state on fail

--- cmd/evm/staterunner.go
+++ cmd/evm/staterunner.go
@@ -23,6 +23,7 @@ import (
 	"io/ioutil"
 	"os"
 
+	"github.com/ethereum/go-ethereum/core/state"
 	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/tests"
@@ -38,84 +39,81 @@ var stateTestCommand = cli.Command{
 }
 
 type StatetestResult struct {
-	Name  string `json:"name"`
-	Pass  bool   `json:"pass"`
-	Fork  string `json:"fork"`
-	Error string `json:"error,omitempty"`
+	Name  string      `json:"name"`
+	Pass  bool        `json:"pass"`
+	Fork  string      `json:"fork"`
+	Error string      `json:"error,omitempty"`
+	State *state.Dump `json:"state,omitempty"`
 }
 
 func stateTestCmd(ctx *cli.Context) error {
+	if len(ctx.Args().First()) == 0 {
+		return errors.New("path-to-test argument required")
+	}
+	// Configure the go-ethereum logger
 	glogger := log.NewGlogHandler(log.StreamHandler(os.Stderr, log.TerminalFormat(false)))
 	glogger.Verbosity(log.Lvl(ctx.GlobalInt(VerbosityFlag.Name)))
 	log.Root().SetHandler(glogger)
-	logconfig := &vm.LogConfig{
+
+	// Configure the EVM logger
+	config := &vm.LogConfig{
 		DisableMemory: ctx.GlobalBool(DisableMemoryFlag.Name),
 		DisableStack:  ctx.GlobalBool(DisableStackFlag.Name),
 	}
 	var (
-		tracer      vm.Tracer
-		debugLogger *vm.StructLogger
-		//		statedb     *state.StateDB
-		//		chainConfig *params.ChainConfig
+		tracer   vm.Tracer
+		debugger *vm.StructLogger
 	)
-	if ctx.GlobalBool(MachineFlag.Name) {
-		tracer = NewJSONLogger(logconfig, os.Stderr)
-	} else if ctx.GlobalBool(DebugFlag.Name) {
-		debugLogger = vm.NewStructLogger(logconfig)
-		tracer = debugLogger
-	} else {
-		debugLogger = vm.NewStructLogger(logconfig)
-	}
+	switch {
+	case ctx.GlobalBool(MachineFlag.Name):
+		tracer = NewJSONLogger(config, os.Stderr)
 
-	if len(ctx.Args().First()) == 0 {
-		return errors.New("filename required")
-	}
+	case ctx.GlobalBool(DebugFlag.Name):
+		debugger = vm.NewStructLogger(config)
+		tracer = debugger
 
-	fn := ctx.Args().First()
-	src, err := ioutil.ReadFile(fn)
+	default:
+		debugger = vm.NewStructLogger(config)
+	}
+	// Load the test content from the input file
+	src, err := ioutil.ReadFile(ctx.Args().First())
 	if err != nil {
 		return err
 	}
-
 	var tests map[string]tests.StateTest
-
 	if err = json.Unmarshal(src, &tests); err != nil {
 		return err
 	}
-
-	var results = make([]StatetestResult, 0, len(tests))
-
+	// Iterate over all the tests, run them and aggregate the results
 	cfg := vm.Config{
 		Tracer: tracer,
 		Debug:  ctx.GlobalBool(DebugFlag.Name) || ctx.GlobalBool(MachineFlag.Name),
 	}
-
+	results := make([]StatetestResult, 0, len(tests))
 	for key, test := range tests {
 		for _, st := range test.Subtests() {
-			result := &StatetestResult{
-				Name: key,
-				Fork: st.Fork,
-				Pass: true,
-			}
-
-			if err = test.Run(st, cfg); err != nil {
-				result.Error = err.Error()
-				result.Pass = false
+			// Run the test and aggregate the result
+			result := &StatetestResult{Name: key, Fork: st.Fork, Pass: true}
+			if state, err := test.Run(st, cfg); err != nil {
+				// Test failed, mark as so and dump any state to aid debugging
+				result.Pass, result.Error = false, err.Error()
+				if ctx.GlobalBool(DumpFlag.Name) && state != nil {
+					dump := state.RawDump()
+					result.State = &dump
+				}
 			}
 			results = append(results, *result)
 
+			// Print any structured logs collected
 			if ctx.GlobalBool(DebugFlag.Name) {
-				if debugLogger != nil {
+				if debugger != nil {
 					fmt.Fprintln(os.Stderr, "#### TRACE ####")
-					vm.WriteTrace(os.Stderr, debugLogger.StructLogs())
+					vm.WriteTrace(os.Stderr, debugger.StructLogs())
 				}
 			}
 		}
 	}
-	json.NewEncoder(os.Stdout).Encode(results)
-
-	if err != nil {
-		return err
-	}
+	out, _ := json.MarshalIndent(results, "", "  ")
+	fmt.Println(string(out))
 	return nil
 }
--- tests/state_test.go
+++ tests/state_test.go
@@ -50,7 +50,8 @@ func TestState(t *testing.T) {
 					t.Skip("constantinople not supported yet")
 				}
 				withTrace(t, test.gasLimit(subtest), func(vmconfig vm.Config) error {
-					return st.checkFailure(t, name, test.Run(subtest, vmconfig))
+					_, err := test.Run(subtest, vmconfig)
+					return st.checkFailure(t, name, err)
 				})
 			})
 		}
