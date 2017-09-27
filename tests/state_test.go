b3689d86cceb3d43fbef7bdcee113411f89974b3
Martin Holst Swende <martin@swende.se>
2017-09-27 15:30:41 +0200

    tests: add ethash difficulty tests (#15191)

--- tests/state_test.go
+++ tests/state_test.go
@@ -39,14 +39,16 @@ func TestState(t *testing.T) {
 	st.fails(`^stRevertTest/RevertPrefoundEmptyOOG\.json/EIP158`, "bug in test")
 	st.fails(`^stRevertTest/RevertPrecompiledTouch\.json/Byzantium`, "bug in test")
 	st.fails(`^stRevertTest/RevertPrefoundEmptyOOG\.json/Byzantium`, "bug in test")
-	st.fails( `^stRandom/randomStatetest645\.json/EIP150/.*`, "known bug #15119")
-	st.fails( `^stRandom/randomStatetest645\.json/Frontier/.*`, "known bug #15119")
-	st.fails( `^stRandom/randomStatetest645\.json/Homestead/.*`, "known bug #15119")
-	st.fails( `^stRandom/randomStatetest644\.json/EIP150/.*`, "known bug #15119")
-	st.fails( `^stRandom/randomStatetest644\.json/Frontier/.*`, "known bug #15119")
-	st.fails( `^stRandom/randomStatetest644\.json/Homestead/.*`, "known bug #15119")
-
-
+	st.fails(`^stRandom/randomStatetest645\.json/EIP150/.*`, "known bug #15119")
+	st.fails(`^stRandom/randomStatetest645\.json/Frontier/.*`, "known bug #15119")
+	st.fails(`^stRandom/randomStatetest645\.json/Homestead/.*`, "known bug #15119")
+	st.fails(`^stRandom/randomStatetest644\.json/EIP150/.*`, "known bug #15119")
+	st.fails(`^stRandom/randomStatetest644\.json/Frontier/.*`, "known bug #15119")
+	st.fails(`^stRandom/randomStatetest644\.json/Homestead/.*`, "known bug #15119")
+	st.fails(`^stCreateTest/TransactionCollisionToEmpty\.json/EIP158/2`, "known bug ")
+	st.fails(`^stCreateTest/TransactionCollisionToEmpty\.json/EIP158/3`, "known bug ")
+	st.fails(`^stCreateTest/TransactionCollisionToEmpty\.json/Byzantium/2`, "known bug ")
+	st.fails(`^stCreateTest/TransactionCollisionToEmpty\.json/Byzantium/3`, "known bug ")
 	st.walk(t, stateTestDir, func(t *testing.T, name string, test *StateTest) {
 		for _, subtest := range test.Subtests() {
 			subtest := subtest
