d37a347cc87dedfb43f2f71b651398680d16a1fa
Bas van Kervel <basvankervel@gmail.com>
2017-10-04 15:08:59 +0200

    accounts/abi: add support for unpacking returned bytesN arrays

--- accounts/abi/reflect.go
+++ accounts/abi/reflect.go
@@ -77,6 +77,8 @@ func set(dst, src reflect.Value, output Argument) error {
 	switch {
 	case dstType.AssignableTo(src.Type()):
 		dst.Set(src)
+	case dstType.Kind() == reflect.Slice && srcType.Kind() == reflect.Slice:
+		return setSlice(dst, src, output)
 	case dstType.Kind() == reflect.Array && srcType.Kind() == reflect.Slice:
 		if dst.Len() < output.Type.SliceSize {
 			return fmt.Errorf("abi: cannot unmarshal src (len=%d) in to dst (len=%d)", output.Type.SliceSize, dst.Len())
@@ -89,5 +91,19 @@ func set(dst, src reflect.Value, output Argument) error {
 	default:
 		return fmt.Errorf("abi: cannot unmarshal %v in to %v", src.Type(), dst.Type())
 	}
+	return nil
+}
+
+
+// setSlice attempts to assign src to dst when slices are not assignable by default
+// e.g. src: [][]byte -> dst: [][15]byte
+func setSlice(dst, src reflect.Value, output Argument) error {
+	slice := reflect.MakeSlice(dst.Type(), src.Len(), src.Len())
+	for i := 0; i < src.Len(); i++ {
+		v := src.Index(i)
+		reflect.Copy(slice.Index(i), v)
+	}
+
+	dst.Set(slice)
 	return nil
 }
