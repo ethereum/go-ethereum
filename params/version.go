005665867d50a4b38e269b6b84156db2f75469a3
Péter Szilágyi <peterke@gmail.com>
2017-11-21 11:59:12 +0200

    VERSION, params: begin 1.8.0 release cycle

--- params/version.go
+++ params/version.go
@@ -21,10 +21,10 @@ import (
 )
 
 const (
-	VersionMajor = 1        // Major version component of the current release
-	VersionMinor = 7        // Minor version component of the current release
-	VersionPatch = 3        // Patch version component of the current release
-	VersionMeta  = "stable" // Version metadata to append to the version string
+	VersionMajor = 1          // Major version component of the current release
+	VersionMinor = 8          // Minor version component of the current release
+	VersionPatch = 0          // Patch version component of the current release
+	VersionMeta  = "unstable" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
