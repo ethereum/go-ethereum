3791497eeb1102e303c36af801b73709f382ac5a
lash <dev@holbrook.no>
2018-01-15 05:14:25 +0100

    swarm/pss: Add missing imports in pss 1.8+ test

--- swarm/pss/pss_go18plus_test.go
+++ swarm/pss/pss_go18plus_test.go
@@ -15,7 +15,9 @@ import (
 	"testing"
 	"time"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/p2p/discover"
 	"github.com/ethereum/go-ethereum/p2p/simulations"
