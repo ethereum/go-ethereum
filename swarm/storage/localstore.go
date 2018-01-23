172eb9e14d16669d71386307d246cf8d6bfcd680
Balint Gabor <balint.g@gmail.com>
2018-01-23 17:50:32 +0100

    swarm/storage, swarm/network: Fix delivery tests

--- swarm/storage/localstore.go
+++ swarm/storage/localstore.go
@@ -94,6 +94,7 @@ func NewTestLocalStoreForAddr(path string, basekey []byte) (*LocalStore, error) 
 // LocalStore is itself a chunk store
 // unsafe, in that the data is not integrity checked
 func (self *LocalStore) Put(chunk *Chunk) {
+	chunk.Size = int64(binary.LittleEndian.Uint64(chunk.SData[0:8]))
 	self.memStore.Put(chunk)
 	go func() {
 		self.DbStore.Put(chunk)
