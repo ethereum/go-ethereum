39389c752501bfb0431387aea44b4f32438bc8f4
etherinc <36263855+etherinc@users.noreply.github.com>
2018-02-13 19:33:02 +0530

    Update config.go

--- params/config.go
+++ params/config.go
@@ -24,7 +24,7 @@ import (
 )
 
 var (
-	MainnetGenesisHash = common.HexToHash("0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3") // Mainnet genesis hash to enforce below configs on
+	MainnetGenesisHash = common.HexToHash("0x3abfd94064d39abb1117bdcce540b9ced49e738943936df51e72531a28443d50") // Mainnet genesis hash to enforce below configs on
 	TestnetGenesisHash = common.HexToHash("0x41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d") // Testnet genesis hash to enforce below configs on
 )
 
