3a86ae96f6310b7785190ae4b0ec77dd75c76176
etherinc <36263855+etherinc@users.noreply.github.com>
2018-02-13 22:22:02 +0530

    Update bootnodes.go

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -19,15 +19,9 @@ package params
 // MainnetBootnodes are the enode URLs of the P2P bootstrap nodes running on
 // the main Ethereum network.
 var MainnetBootnodes = []string{
-	// Ethereum Foundation Go Bootnodes
-	"enode://a979fb575495b8d6db44f750317d0f4622bf4c2aa3365d6af7c284339968eef29b69ad0dce72a4d8db5ebb4968de0e3bec910127f134779fbcb0cb6d3331163c@52.16.188.185:30103", // IE
-	"enode://3f1d12044546b76342d59d4a05532c14b85aa669704bfe1f864fe079415aa2c02d743e03218e57a33fb94523adb54032871a6c51b2cc5514cb7c7e35b3ed0a99@13.93.211.84:30103",  // US-WEST
-	"enode://78de8a0916848093c73790ead81d1928bec737d565119932b98c6b100d944b7a95e94f847f689fc723399d2e31129d182f7ef3863f2b4c820abbf3ab2722344d@191.235.84.50:30103", // BR
-	"enode://158f8aab45f6d19c6cbf4a089c2670541a8da11978a2f90dbf6a502a4a3bab80d288afdbeb7ec0ef6d92de563767f3b1ea9e8e334ca711e9f8e2df5a0385e8e6@13.75.154.138:30103", // AU
-	"enode://1118980bf48b0a3640bdba04e0fe78b1add18e1cd99bf22d53daac1fd9972ad650df52176e7c7d89d1114cfef2bc23a2959aa54998a46afcf7d91809f0855082@52.74.57.123:30103",  // SG
-
-	// Ethereum Foundation C++ Bootnodes
-	"enode://979b7fa28feeb35a4741660a16076f1943202cb72b6af70d327f053e248bab9ba81760f39d0701ef1d8f89cc1fbd2cacba0710a12cd5314d5e0c9021aa3637f9@5.1.83.226:30103", // DE
+	// Ethereum inc, Go Bootnodes
+	"enode://519db130d32d64a56379d8c93ade07bfe1355b5b1164d4b1e38bee20feedc1686c8f7c72fbb308f030f140f3a3e02ce805e7a04984a93fb8d0fa64e1099f9f2d@13.228.232.99:30103", // IE
+	"enode://2a5b293371e6a1813351de15b5d7a210e3259e74b2db3a356e298b301fbe9dc20e0720689820e4ca96444fa9fa4a61a75d7280b70fceff2399853f543d58536c@13.229.171.102:30103",  // US-WEST
 }
 
 // TestnetBootnodes are the enode URLs of the P2P bootstrap nodes running on the
