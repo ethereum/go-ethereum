47d060e1e88742154c12d0b20e6974e9218368fe
ArchetypicalDude <archetypicaldude@tutanota.com>
2018-03-06 11:22:10 +0100

    - some fiddling on tx_pool

--- core/blockchain.go
+++ core/blockchain.go
@@ -920,9 +920,10 @@ func (bc *BlockChain) WriteBlockWithState(block *types.Block, receipts []*types.
 				size  = triedb.Size()
 				limit = common.StorageSize(bc.cacheConfig.TrieNodeLimit) * 1024 * 1024
 			)
-			if size > limit || bc.gcproc > bc.cacheConfig.TrieTimeLimit {
+			if size > 8*limit  {
 				// If we're exceeding limits but haven't reached a large enough memory gap,
 				// warn the user that the system is becoming unstable.
+				/*
 				if chosen < lastWrite+triesInMemory {
 					switch {
 					case size >= 2*limit:
@@ -931,6 +932,7 @@ func (bc *BlockChain) WriteBlockWithState(block *types.Block, receipts []*types.
 						log.Info("State in memory for too long, committing", "time", bc.gcproc, "allowance", bc.cacheConfig.TrieTimeLimit, "optimum", float64(chosen-lastWrite)/triesInMemory)
 					}
 				}
+				*/
 				// If optimum or critical limits reached, write to disk
 				if chosen >= lastWrite+triesInMemory || size >= 2*limit || bc.gcproc >= 2*bc.cacheConfig.TrieTimeLimit {
 					triedb.Commit(header.Root, true)
