9ca737c72c176237b8ad1a2b7c0e3d30a324e3ce
Derek May <32908855+riddlez666@users.noreply.github.com>
2018-03-13 15:13:34 -0700

    egem

--- build/env.sh
+++ build/env.sh
@@ -10,11 +10,11 @@ fi
 # Create fake Go workspace if it doesn't exist yet.
 workspace="$PWD/build/_workspace"
 root="$PWD"
-ethdir="$workspace/src/github.com/ethereum"
-if [ ! -L "$ethdir/go-ethereum" ]; then
+ethdir="$workspace/src/github.com/TeamEGEM"
+if [ ! -L "$ethdir/go-egem" ]; then
     mkdir -p "$ethdir"
     cd "$ethdir"
-    ln -s ../../../../../. go-ethereum
+    ln -s ../../../../../. go-egem
     cd "$root"
 fi
 
@@ -23,8 +23,8 @@ GOPATH="$workspace"
 export GOPATH
 
 # Run the command inside the workspace.
-cd "$ethdir/go-ethereum"
-PWD="$ethdir/go-ethereum"
+cd "$ethdir/go-egem"
+PWD="$ethdir/go-egem"
 
 # Launch the arguments with the configured environment.
 exec "$@"
