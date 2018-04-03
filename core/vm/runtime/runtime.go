d1aee31279f90b025b2e059ed6b8a39a0c7abc19
Felix Lange <fjl@twurst.com>
2018-04-03 17:00:04 +0200

    common: delete StringToAddress, StringToHash
    These functions are confusing because they don't parse hex, but use the
    bytes of the string. This change removes them, replacing all uses of
    StringToAddress(s) by BytesToAddress([]byte(s)).

--- core/vm/runtime/runtime.go
+++ core/vm/runtime/runtime.go
@@ -103,7 +103,7 @@ func Execute(code, input []byte, cfg *Config) ([]byte, *state.StateDB, error) {
 		cfg.State, _ = state.New(common.Hash{}, state.NewDatabase(db))
 	}
 	var (
-		address = common.StringToAddress("contract")
+		address = common.BytesToAddress([]byte("contract"))
 		vmenv   = NewEnv(cfg)
 		sender  = vm.AccountRef(cfg.Origin)
 	)
@@ -113,7 +113,7 @@ func Execute(code, input []byte, cfg *Config) ([]byte, *state.StateDB, error) {
 	// Call the code with the given configuration.
 	ret, _, err := vmenv.Call(
 		sender,
-		common.StringToAddress("contract"),
+		common.BytesToAddress([]byte("contract")),
 		input,
 		cfg.GasLimit,
 		cfg.Value,
