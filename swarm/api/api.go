221188af08a52eceb1d1eddc7c239bc867ce0fa7
Balint Gabor <balint.g@gmail.com>
2018-04-06 16:21:54 +0200

    swarm/api: Fix linter

--- swarm/api/api.go
+++ swarm/api/api.go
@@ -39,7 +39,6 @@ import (
 	"github.com/ethereum/go-ethereum/swarm/storage"
 )
 
-
 // TODO: this is bad, it should not be hardcoded how long is a hash
 var hashMatcher = regexp.MustCompile("^([0-9A-Fa-f]{64})([0-9A-Fa-f]{64})?")
 
