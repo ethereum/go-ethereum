475e8719baa05b135bd281b6a402d0cbf7f6c729
Péter Szilágyi <peterke@gmail.com>
2018-04-18 12:51:30 +0300

    vendor: pull in missing go-echd library

---
+++ vendor/github.com/wsddn/go-ecdh/ecdh.go
@@ -0,0 +1,14 @@
+package ecdh
+
+import (
+	"crypto"
+	"io"
+)
+
+// The main interface for ECDH key exchange.
+type ECDH interface {
+	GenerateKey(io.Reader) (crypto.PrivateKey, crypto.PublicKey, error)
+	Marshal(crypto.PublicKey) []byte
+	Unmarshal([]byte) (crypto.PublicKey, bool)
+	GenerateSharedSecret(crypto.PrivateKey, crypto.PublicKey) ([]byte, error)
+}
