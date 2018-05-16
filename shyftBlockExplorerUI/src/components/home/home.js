8e2967085285bf68ed3b4a70342cdda6a8742248
Dustin Brickwood <dustinbrickwood204@gmail.com>
2018-05-16 10:07:41 -0400

    modified UI

--- shyftBlockExplorerUI/src/components/home/home.js
+++ shyftBlockExplorerUI/src/components/home/home.js
@@ -4,14 +4,15 @@ import { Link } from 'react-router-dom'
 
 const home = props => {
 
+    const combinedClasses = ["btn", "btn-primary", classes.BlockButton]
     return (
        <div className={classes.Home}>
-            <span>THIS IS A WIP</span>
+            <span className={classes.Greeting}>THIS IS A WIP</span>
            <div className={classes.Transactions}>
-               <Link to="/transactions"><button>Transactions</button></Link>
+               <Link to="/transactions"><button className="btn btn-primary">Transactions</button></Link>
            </div>
-           <div className={classes.Transactions}>
-               <Link to="/blocks"><button>Blocks</button></Link>
+           <div className={classes.Blocks}>
+               <Link to="/blocks"><button className={combinedClasses.join(" ")}>Blocks</button></Link>
            </div>
        </div>
     );
