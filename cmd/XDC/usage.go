88fe0a6d9dadc5a97edc6d0b92b38d5c1bf5d71a
AnilChinchawale <anil24593@gmail.com>
2018-05-24 15:48:47 +0530

    updated USAGE.GO

--- cmd/XDC/usage.go
+++ cmd/XDC/usage.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with go-ethereum. If not, see <http://www.gnu.org/licenses/>.
 
-// Contains the geth command usage template and generator.
+// Contains the XDC command usage template and generator.
 
 package main
 
