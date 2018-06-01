6d60a23bb4dd0ee43f38c350720d930f3c74cf1f
Ryan Schneider <ryanleeschneider@gmail.com>
2018-06-01 13:44:48 -0700

    rpc: Use `http.Request`'s context instead of creating our own.

--- rpc/http.go
+++ rpc/http.go
@@ -181,7 +181,7 @@ func (srv *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// All checks passed, create a codec that reads direct from the request body
 	// untilEOF and writes the response to w and order the server to process a
 	// single request.
-	ctx := context.Background()
+	ctx := r.Context()
 	ctx = context.WithValue(ctx, "remote", r.RemoteAddr)
 	ctx = context.WithValue(ctx, "scheme", r.Proto)
 	ctx = context.WithValue(ctx, "local", r.Host)
