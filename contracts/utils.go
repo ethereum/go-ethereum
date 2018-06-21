d502c6b2e4430b76d0d17d14b1fa5780416e04a1
dinhln89 <lenhatdinh@gmail.com>
2018-06-21 11:24:06 +0700

    Fixed move method getBlockSingers by blockNumber to utils of contact package.

--- contracts/utils.go
+++ contracts/utils.go
@@ -2,10 +2,14 @@ package contracts
 
 import (
 	"github.com/ethereum/go-ethereum/accounts"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/contracts/blocksigner/contract"
 	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/log"
+	"github.com/ethereum/go-ethereum/node"
 	"github.com/ethereum/go-ethereum/params"
 	"math/big"
 )
@@ -13,6 +17,18 @@ import (
 const (
 	HexSignMethod = "2fb1b25f"
 )
+
+// Get ethClient over IPC of current node.
+func GetEthClient(ctx *node.ServiceContext) (*ethclient.Client, error) {
+	conf := ctx.GetConfig()
+	client, err := ethclient.Dial(conf.IPCEndpoint())
+	if err != nil {
+		log.Error("Fail to connect RPC", "error", err)
+		return nil, err
+	}
+
+	return client, nil
+}
 
 // Send tx sign for block number to smart contract blockSigner.
 func CreateTransactionSign(chainConfig *params.ChainConfig, pool *core.TxPool, manager *accounts.Manager, block *types.Block) {
@@ -40,4 +56,23 @@ func CreateTransactionSign(chainConfig *params.ChainConfig, pool *core.TxPool, m
 
 	// Add tx signed to local tx pool.
 	pool.AddLocal(txSigned)
+}
+
+// Get signers signed for blockNumber from blockSigner contract.
+func GetSignersFromContract(ctx *node.ServiceContext, blockNumber uint64) ([]common.Address, error) {
+	client, err := GetEthClient(ctx)
+	if err != nil {
+		log.Error("Fail to connect IPC from blockSigner", "error", err)
+		return nil, err
+	}
+	addr := common.HexToAddress(common.BlockSigners)
+	blockSigner, err := contract.NewBlockSigner(addr, client)
+	if err != nil {
+		log.Error("Fail get block signers", "error", err)
+		return nil, err
+	}
+	opts := new(bind.CallOpts)
+	addrs, err := blockSigner.GetSigners(opts, new(big.Int).SetUint64(blockNumber))
+
+	return addrs, nil
 }
