f3c626e2e4eb023649d27371b9560fade4d8fa31
etienne-napoleone <etienne@tomochain.com>
2018-06-25 11:29:03 +0700

    Update .travis.yml and docker_push.sh Tag images before pushing

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+docker tag etiennenapoleone/tomochain etiennenapoleone/tomochain:latest
+docker tag etiennenapoleone/tomochain etiennenapoleone/tomochain:$(git log --pretty=format:'%h' -n 1 | cat)
 docker push etiennenapoleone/tomochain:latest
 docker push etiennenapoleone/tomochain:$(git log --pretty=format:'%h' -n 1 | cat)
