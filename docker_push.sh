f839899618e93f03ffddc22d9ba62209bdba989b
etienne-napoleone <etienne@tomochain.com>
2018-06-25 15:54:29 +0700

    Update .travis.yml Change from tagging commit hash to travis build ID

--- docker_push.sh
+++ docker_push.sh
@@ -2,6 +2,6 @@
 
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 docker tag tomochain/tomochain tomochain/tomochain:latest
-docker tag tomochain/tomochain tomochain/tomochain:$(git log --pretty=format:'%h' -n 1 | cat)
+docker tag tomochain/tomochain tomochain/tomochain:$TRAVIS_BUILD_ID
 docker push tomochain/tomochain:latest
-docker push tomochain/tomochain:$(git log --pretty=format:'%h' -n 1 | cat)
+docker push tomochain/tomochain:$TRAVIS_BUILD_ID
