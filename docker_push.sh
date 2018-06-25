5259e9cc9191ca851ff63583930ed29ec88a65a2
etienne-napoleone <etienne@tomochain.com>
2018-06-25 10:51:47 +0700

    Update .travis.yml and cleanup

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 docker push etiennenapoleone/tomochain:latest
 docker push etiennenapoleone/tomochain:$(git log --pretty=format:'%h' -n 1 | cat)
---
+++ docker_rebuild.sh
@@ -0,0 +1,1 @@
+curl -X POST $INFRA_IMAGE
