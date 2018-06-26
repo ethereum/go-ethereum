96674c70c13421ecfc84edc74cbeb61e2874de2c
Adrià Cidre <adria.cidre@gmail.com>
2018-06-26 10:31:05 +0200

    whisper: Reduce message loop log from Warn to Info (#17055)

--- whisper/whisperv6/whisper.go
+++ whisper/whisperv6/whisper.go
@@ -650,7 +650,7 @@ func (whisper *Whisper) runMessageLoop(p *Peer, rw p2p.MsgReadWriter) error {
 		// fetch the next packet
 		packet, err := rw.ReadMsg()
 		if err != nil {
-			log.Warn("message loop", "peer", p.peer.ID(), "err", err)
+			log.Info("message loop", "peer", p.peer.ID(), "err", err)
 			return err
 		}
 		if packet.Size > whisper.MaxMessageSize() {
