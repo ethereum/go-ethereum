3248badf0c4a8f12de659645e3eca2ae1b1cb64e
dinhln89 <lenhatdinh@gmail.com>
2018-06-29 16:55:24 +0700

    Change IValidator to Tomo Validator name.

--- contracts/utils.go
+++ contracts/utils.go
@@ -161,7 +161,7 @@ func GetCandidatesOwnerByAddress(client bind.ContractBackend, addr common.Addres
 	owner := common.Address{}
 	validator, err := contract2.NewTomoValidator(common.HexToAddress(common.TomoValidator), client)
 	if err != nil {
-		log.Error("Fail get instance of IValidator", "error", err)
+		log.Error("Fail get instance of Tomo Validator", "error", err)
 		return owner, err
 	}
 	opts := new(bind.CallOpts)
