d7f57b92e336c4d9995b9d9291d7326756a2a483
AnilChinchawale <anil24593@gmail.com>
2018-07-18 17:16:14 +0530

    updated backend.go

--- eth/backend.go
+++ eth/backend.go
@@ -334,6 +334,7 @@ func (self *Ethereum) SetEtherbase(etherbase common.Address) {
 	self.miner.SetEtherbase(etherbase)
 }
 
+// ValidateMiner checks if node's address is in set of validators
 func (s *Ethereum) ValidateMiner() (bool, error) {
 	eb, err := s.Etherbase()
 	if err != nil {
@@ -353,18 +354,7 @@ func (s *Ethereum) ValidateMiner() (bool, error) {
 	} else {
 		return false, fmt.Errorf("Only verify miners in Clique protocol")
 	}
-	
 	return true, nil
-}
-
-func (s *Ethereum) Checkpoint() bool {
-	number := s.blockchain.CurrentHeader().Number.Uint64()
-	return number%s.chainConfig.Clique.Epoch == 1 || number == 0
-}
-
-func (s *Ethereum) Checkpoint() bool {
-	number := s.blockchain.CurrentHeader().Number.Uint64()
-	return number%s.chainConfig.Clique.Epoch == 1
 }
 
 func (s *Ethereum) StartMining(local bool) error {
