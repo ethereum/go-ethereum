ce56b4c5f99524f54e13d5985711c810f0a92d52
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-07-20 04:07:42 +0000

    new blocksigner contract
    add foreachstorage into simulate
    
    sign tx debug

--- contracts/blocksigner/blocksigner_test.go
+++ contracts/blocksigner/blocksigner_test.go
@@ -1,11 +1,14 @@
 package blocksigner
 
 import (
+	"context"
 	"math/big"
 	"testing"
+	"time"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/crypto"
 )
@@ -19,18 +22,36 @@ func TestBlockSigner(t *testing.T) {
 	contractBackend := backends.NewSimulatedBackend(core.GenesisAlloc{addr: {Balance: big.NewInt(1000000000)}})
 	transactOpts := bind.NewKeyedTransactor(key)
 
-	_, blockSigner, err := DeployBlockSigner(transactOpts, contractBackend)
+	blockSignerAddress, blockSigner, err := DeployBlockSigner(transactOpts, contractBackend)
 	if err != nil {
 		t.Fatalf("can't deploy root registry: %v", err)
 	}
 	contractBackend.Commit()
 
-	signers, err := blockSigner.GetSigners(big.NewInt(0))
+	d := time.Now().Add(1000 * time.Millisecond)
+	ctx, cancel := context.WithDeadline(context.Background(), d)
+	defer cancel()
+	code, _ := contractBackend.CodeAt(ctx, blockSignerAddress, nil)
+	t.Log("contract code", common.ToHex(code))
+	f := func(key, val common.Hash) bool {
+		t.Log(key.Hex(), val.Hex())
+		return true
+	}
+	contractBackend.ForEachStorageAt(ctx, blockSignerAddress, nil, f)
+
+	byte0 := [32]byte{}
+	signers, err := blockSigner.GetSigners(byte0)
 	if err != nil {
 		t.Fatalf("can't get candidates: %v", err)
 	}
 	for _, it := range signers {
 		t.Log("signer", it.String())
 	}
+
+	s, err := blockSigner.Sign(big.NewInt(1), byte0)
+	if err != nil {
+		t.Fatalf("can't sign: %v", err)
+	}
+	t.Log("tx data", s)
 	contractBackend.Commit()
 }
