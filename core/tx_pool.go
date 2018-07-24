10780e8a0005832f3135bbae1a7b5687b7e487c8
Osuke <arget-fee.free.dgm@hotmail.co.jp>
2018-07-25 00:44:41 +0900

    core: fix txpool guarantee comment (#17214)
    * fixed-typo
    
    * core: fix txpool guarantee comment

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -130,7 +130,7 @@ type TxPoolConfig struct {
 	PriceLimit uint64 // Minimum gas price to enforce for acceptance into the pool
 	PriceBump  uint64 // Minimum price bump percentage to replace an already existing transaction (nonce)
 
-	AccountSlots uint64 // Minimum number of executable transaction slots guaranteed per account
+	AccountSlots uint64 // Number of executable transaction slots guaranteed per account
 	GlobalSlots  uint64 // Maximum number of executable transaction slots for all accounts
 	AccountQueue uint64 // Maximum number of non-executable transaction slots permitted per account
 	GlobalQueue  uint64 // Maximum number of non-executable transaction slots for all accounts
