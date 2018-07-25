07a4c33033dd2ede0d9e6d22a9b01a344357af6f
etienne-napoleone <etienne@tomochain.com>
2018-07-25 16:10:24 +0700

    Docker tagging options + Docker release tagging

--- docker_push.sh
+++ docker_push.sh
@@ -2,6 +2,6 @@
 
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 docker tag tomochain/tomochain tomochain/tomochain:latest
-docker tag tomochain/tomochain tomochain/tomochain:$TRAVIS_BUILD_ID
+docker tag tomochain/tomochain tomochain/tomochain:$1
 docker push tomochain/tomochain:latest
-docker push tomochain/tomochain:$TRAVIS_BUILD_ID
+docker push tomochain/tomochain:$1
