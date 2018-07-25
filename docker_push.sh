95c68ffa997a4e1b291ecf352614a34536864ca2
etienne-napoleone <etienne@tomochain.com>
2018-07-25 16:48:58 +0700

    Refactor docker push script

--- docker_push.sh
+++ docker_push.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
-docker tag tomochain/tomochain tomochain/tomochain:latest
 docker tag tomochain/tomochain tomochain/tomochain:$1
-docker push tomochain/tomochain:latest
 docker push tomochain/tomochain:$1
