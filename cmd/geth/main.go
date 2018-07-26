021d6fbbbbdfe295dbb3619f8ec3fc8662c1e26a
a e r t h <aerth@users.noreply.github.com>
2018-07-26 03:57:20 -0700

    cmd: prevent accidental invalid commands (#17248)
    * cmd: stop parsing bootnodes if one is invalid
    
    * cmd/geth: require valid command as argument (or no arg)

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -251,6 +251,9 @@ func main() {
 // It creates a default node based on the command line arguments and runs it in
 // blocking mode, waiting for it to be shut down.
 func geth(ctx *cli.Context) error {
+	if args := ctx.Args(); len(args) > 0 {
+		return fmt.Errorf("invalid command: %q", args[0])
+	}
 	node := makeFullNode(ctx)
 	startNode(ctx, node)
 	node.Wait()
