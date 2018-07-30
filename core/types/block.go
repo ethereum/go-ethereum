3475a4b22586fb443aa35e380b2d3eb143d998b4
kane <kane@localhost.localdomain>
2018-07-30 02:16:23 -0400

    catch error if rlp.Encode failed

--- core/types/block.go
+++ core/types/block.go
@@ -129,7 +129,10 @@ func (h *Header) Size() common.StorageSize {
 
 func rlpHash(x interface{}) (h common.Hash) {
 	hw := sha3.NewKeccak256()
-	rlp.Encode(hw, x)
+	err := rlp.Encode(hw, x)
+	if err != nil {
+		panic("encode error: " + err.Error())
+	}
 	hw.Sum(h[:0])
 	return h
 }
