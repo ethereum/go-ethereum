83e2761c3a13524bd5d6597ac08994488cf872ef
Péter Szilágyi <peterke@gmail.com>
2018-08-01 19:09:08 +0300

    Revert "cmd/evm: change error msg output to stderr (#17118)"
    This reverts commit fb9f7261ec51e38eedb454594fc19f00de1a6834.

--- cmd/evm/disasm.go
+++ cmd/evm/disasm.go
@@ -45,6 +45,6 @@ func disasmCmd(ctx *cli.Context) error {
 	}
 
 	code := strings.TrimSpace(string(in[:]))
-	fmt.Fprintf(ctx.App.Writer, "%v\n", code)
+	fmt.Printf("%v\n", code)
 	return asm.PrintDisassembled(code)
 }
