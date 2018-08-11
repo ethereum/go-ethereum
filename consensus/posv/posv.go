a554c5b968d0053c49da9e09ea7535ff0176e1bf
Nguyen Ba Tam <nguyenbatam90@gmail.com>
2018-08-11 15:23:38 +0700

    remove 2 commands posv.propose & posv.discard

--- consensus/posv/posv.go
+++ consensus/posv/posv.go
@@ -819,7 +819,12 @@ func CalcDifficulty(snap *Snapshot, signer common.Address) *big.Int {
 // APIs implements consensus.Engine, returning the user facing RPC API to allow
 // controlling the signer voting.
 func (c *Posv) APIs(chain consensus.ChainReader) []rpc.API {
-	return []rpc.API{}
+	return []rpc.API{{
+		Namespace: "posv",
+		Version:   "1.0",
+		Service:   &API{chain: chain, posv: c},
+		Public:    false,
+	}}
 }
 
 func (c *Posv) RecoverSigner(header *types.Header) (common.Address, error) {
