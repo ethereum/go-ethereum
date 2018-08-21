86acdf1a5bb31196adbc92e4923e0398868ce876
Jeremy Schlatter <jeremy.schlatter@gmail.com>
2018-08-21 06:13:03 -0700

    vendor: update rjeczalik/notify so that it compiles on go1.11 (#17467)

--- vendor/github.com/rjeczalik/notify/watcher_fsevents_go1.11.go
+++ vendor/github.com/rjeczalik/notify/watcher_fsevents_go1.11.go
@@ -1,9 +1,9 @@
-// Copyright (c) 2014-2018 The Notify Authors. All rights reserved.
+// Copyright (c) 2018 The Notify Authors. All rights reserved.
 // Use of this source code is governed by the MIT license that can be
 // found in the LICENSE file.
 
-// +build debug
+// +build darwin,!kqueue,go1.11
 
 package notify
 
-var debugTag = true
+const refZero = 0
