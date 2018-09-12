725930f310a41f4fe67d9833bad1d0ce86ca1c2a
MestryOmkar <mestryomkar@gmail.com>
2018-09-12 18:25:20 +0530

    update smart contract

--- contracts/blocksigner/contract/BlockSigner.sol
+++ contracts/blocksigner/contract/BlockSigner.sol
@@ -1,24 +1,24 @@
 pragma solidity ^0.4.21;
 
- import "./libs/SafeMath.sol";
+import "./libs/SafeMath.sol";
 
- contract BlockSigner {
+contract BlockSigner {
     using SafeMath for uint256;
 
-     event Sign(address _signer, uint256 blockNumber);
+    event Sign(address _signer, uint256 _blockNumber);
 
-     mapping(uint256 => address[]) blockSigners;
+    mapping(uint256 => address[]) blockSigners;
 
-     function sign(uint256 _blockNumber) external {
+    function sign(uint256 _blockNumber) external {
         // consensus should validate all senders are validators, gas = 0
         require(block.number >= _blockNumber);
         require(block.number <= _blockNumber.add(990 * 2));
         blockSigners[_blockNumber].push(msg.sender);
 
-         emit Sign(msg.sender, _blockNumber);
+        emit Sign(msg.sender, _blockNumber);
     }
 
-     function getSigners(uint256 _blockNumber) public view returns(address[]) {
+    function getSigners(uint256 _blockNumber) public view returns(address[]) {
         return blockSigners[_blockNumber];
     }
 }
--- contracts/validator/validator.go
+++ contracts/validator/validator.go
@@ -1,24 +1,24 @@
 package validator
 
- import (
+import (
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/contracts/validator/contract"
 	"math/big"
 )
 
- type Validator struct {
+type Validator struct {
 	*contract.XDCValidatorSession
 	contractBackend bind.ContractBackend
 }
 
- func NewValidator(transactOpts *bind.TransactOpts, contractAddr common.Address, contractBackend bind.ContractBackend) (*Validator, error) {
+func NewValidator(transactOpts *bind.TransactOpts, contractAddr common.Address, contractBackend bind.ContractBackend) (*Validator, error) {
 	validator, err := contract.NewXDCValidator(contractAddr, contractBackend)
 	if err != nil {
 		return nil, err
 	}
 
- 	return &Validator{
+	return &Validator{
 		&contract.XDCValidatorSession{
 			Contract:     validator,
 			TransactOpts: *transactOpts,
@@ -27,16 +27,16 @@ package validator
 	}, nil
 }
 
- func DeployValidator(transactOpts *bind.TransactOpts, contractBackend bind.ContractBackend, candidates []common.Address, caps []*big.Int) (common.Address, *Validator, error) {
-	validatorAddr, _, _, err := contract.DeployXDCValidator(transactOpts, contractBackend, candidates, caps)
+func DeployValidator(transactOpts *bind.TransactOpts, contractBackend bind.ContractBackend, candidates []common.Address, caps []*big.Int) (common.Address, *Validator, error) {
+	validatorAddr, _, _, err := contract.DeployXDCValidator(transactOpts, contractBackend, candidates, caps, big.NewInt(50000), big.NewInt(99), big.NewInt(100))
 	if err != nil {
 		return validatorAddr, nil, err
 	}
 
- 	validator, err := NewValidator(transactOpts, validatorAddr, contractBackend)
+	validator, err := NewValidator(transactOpts, validatorAddr, contractBackend)
 	if err != nil {
 		return validatorAddr, nil, err
 	}
 
- 	return validatorAddr, validator, nil
+	return validatorAddr, validator, nil
 }
