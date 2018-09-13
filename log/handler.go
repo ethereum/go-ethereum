20cb3e3549e66eb5039e3d3eafc991242a41f741
Zhao Li <simplyzhao@gmail.com>
2018-09-13 18:10:27 +0800

    log: fix data race in RotatingFileHandler

--- log/handler.go
+++ log/handler.go
@@ -77,8 +77,9 @@ func FileHandler(path string, fmtr Format) (Handler, error) {
 
 // countingWriter wraps a WriteCloser object in order to count the written bytes.
 type countingWriter struct {
-	w     io.WriteCloser // the wrapped object
-	count uint           // number of bytes written
+	sync.Mutex                // lock
+	w          io.WriteCloser // the wrapped object
+	count      uint           // number of bytes written
 }
 
 // Write increments the byte counter by the number of bytes written.
@@ -157,9 +158,14 @@ func RotatingFileHandler(path string, limit uint, formatter Format) (Handler, er
 	if counter == nil {
 		counter = new(countingWriter)
 	}
-	h := StreamHandler(counter, formatter)
+	h := LazyHandler(FuncHandler(func(r *Record) error {
+		_, err := counter.Write(formatter.Format(r))
+		return err
+	}))
 
 	return FuncHandler(func(r *Record) error {
+		counter.Lock()
+		defer counter.Unlock()
 		if counter.count > limit {
 			counter.Close()
 			counter.w = nil
