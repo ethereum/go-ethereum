b66f793443f572082d24f115e706532a620ba3ee
HackyMiner <hackyminer@gmail.com>
2018-09-25 19:27:18 +0900

    rpc: increase maxRequestContentLength size to 512kB (#17595)

--- rpc/http.go
+++ rpc/http.go
@@ -37,7 +37,7 @@ import (
 
 const (
 	contentType             = "application/json"
-	maxRequestContentLength = 1024 * 128
+	maxRequestContentLength = 1024 * 512
 )
 
 var nullAddr, _ = net.ResolveTCPAddr("tcp", "127.0.0.1:0")
