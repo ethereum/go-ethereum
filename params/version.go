e20d9797fff6af3d655bd648f53c2c2bd7a74959
Tuna <ng.tuna@gmail.com>
2018-09-26 16:31:07 +0700

    bump up v0.2.1 - sorry Etienne I will make a PR next time

--- params/version.go
+++ params/version.go
@@ -23,7 +23,7 @@ import (
 const (
 	VersionMajor = 0       // Major version component of the current release
 	VersionMinor = 2       // Minor version component of the current release
-	VersionPatch = 0       // Patch version component of the current release
+	VersionPatch = 1       // Patch version component of the current release
 	VersionMeta  = "alpha" // Version metadata to append to the version string
 )
 
