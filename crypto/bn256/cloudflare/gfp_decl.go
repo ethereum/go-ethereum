d460c55eac30263481979f1b7f79d7a274107527
Tuna <ng.tuna@gmail.com>
2018-10-04 12:09:01 +0700

    add go1.11, remove go1.9

--- crypto/bn256/cloudflare/gfp_decl.go
+++ crypto/bn256/cloudflare/gfp_decl.go
@@ -5,6 +5,13 @@ package bn256
 // This file contains forward declarations for the architecture-specific
 // assembly implementations of these functions, provided that they exist.
 
+import (
+	"golang.org/x/sys/cpu"
+)
+
+//nolint:varcheck
+var hasBMI2 = cpu.X86.HasBMI2
+
 // go:noescape
 func gfpNeg(c, a *gfP)
 
