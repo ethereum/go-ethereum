6c313fff7bd7c1935d3193986df4c084b2e34c1c
Wenbiao Zheng <delweng@gmail.com>
2018-10-15 19:02:53 -0500

    cmd/geth: don't set GOMAXPROCS by default (#17148)
    This is no longer needed because Go uses all CPUs
    by default. The change allows setting GOMAXPROCS in environment if needed.

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -21,7 +21,6 @@ import (
 	"fmt"
 	"math"
 	"os"
-	"runtime"
 	godebug "runtime/debug"
 	"sort"
 	"strconv"
@@ -209,8 +208,6 @@ func init() {
 	app.Flags = append(app.Flags, metricsFlags...)
 
 	app.Before = func(ctx *cli.Context) error {
-		runtime.GOMAXPROCS(runtime.NumCPU())
-
 		logdir := ""
 		if ctx.GlobalBool(utils.DashboardEnabledFlag.Name) {
 			logdir = (&node.Config{DataDir: utils.MakeDataDir(ctx)}).ResolvePath("logs")
