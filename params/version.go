f983a239a128f2a42da2dbb17ce05b8f1f177dd1
Julian Yap <julianokyap@gmail.com>
2018-10-19 01:26:58 -0700

    Version 2.0.1

--- params/version.go
+++ params/version.go
@@ -19,10 +19,10 @@ package params
 import "fmt"
 
 const (
-	VersionMajor = 1      // Major version component of the current release
-	VersionMinor = 5      // Minor version component of the current release
-	VersionPatch = 14     // Patch version component of the current release
-	VersionMeta  = "flux" // Version metadata to append to the version string
+	VersionMajor = 2      // Major version component of the current release
+	VersionMinor = 0      // Minor version component of the current release
+	VersionPatch = 1      // Patch version component of the current release
+	VersionMeta  = "uip1" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
