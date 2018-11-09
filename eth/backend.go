2e505d6e990446b5563964b99d319121f4143a38
AnilChinchwale <anil24593@gmail.com>
2018-11-09 17:10:51 +0530

    change info at extra data in block from geth to XDC

--- eth/backend.go
+++ eth/backend.go
@@ -352,7 +352,7 @@ func makeExtraData(extra []byte) []byte {
 		// create default extradata
 		extra, _ = rlp.EncodeToBytes([]interface{}{
 			uint(params.VersionMajor<<16 | params.VersionMinor<<8 | params.VersionPatch),
-			"geth",
+			"XDC",
 			runtime.Version(),
 			runtime.GOOS,
 		})
