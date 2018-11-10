2006e3240af2da5c74a4019cfcbc3d876268ffa2
Nguyen Ba Tam <nguyenbatam90@gmail.com>
2018-11-10 10:42:46 +0700

    limit max threshold nonce in queue

--- common/constants.go
+++ common/constants.go
@@ -1,16 +1,17 @@
 package common
 
 const (
-	RewardMasterPercent     = 40
-	RewardVoterPercent      = 50
-	RewardFoundationPercent = 10
-	HexSignMethod           = "e341eaa4"
-	HexSetSecret            = "34d38600"
-	HexSetOpening           = "e11f5ba2"
-	EpocBlockSecret         = 800
-	EpocBlockOpening        = 850
-	EpocBlockRandomize      = 900
-	MaxMasternodes          = 150
-	LimitPenaltyEpoch       = 4
-	BlocksPerYear           = uint64(15768000)
+	RewardMasterPercent        = 40
+	RewardVoterPercent         = 50
+	RewardFoundationPercent    = 10
+	HexSignMethod              = "e341eaa4"
+	HexSetSecret               = "34d38600"
+	HexSetOpening              = "e11f5ba2"
+	EpocBlockSecret            = 800
+	EpocBlockOpening           = 850
+	EpocBlockRandomize         = 900
+	MaxMasternodes             = 150
+	LimitPenaltyEpoch          = 4
+	BlocksPerYear              = uint64(15768000)
+	LimitThresholdNonceInQueue = 10
 )
