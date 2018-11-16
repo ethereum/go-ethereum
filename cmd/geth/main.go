2072c26a96badbe45d6df56a4cd68ffd1b6fb12e
Péter Szilágyi <peterke@gmail.com>
2018-11-16 17:58:24 +0200

    cmd, core, params: add support for Goerli
    (cherry picked from commit b0ed083ead2d58cc25754eacdb48046eb2bc81cb)

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -121,6 +121,7 @@ var (
 		utils.DeveloperPeriodFlag,
 		utils.TestnetFlag,
 		utils.RinkebyFlag,
+		utils.GoerliFlag,
 		utils.VMEnableDebugFlag,
 		utils.NetworkIdFlag,
 		utils.ConstantinopleOverrideFlag,
