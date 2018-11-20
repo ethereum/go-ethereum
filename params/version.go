b749aeeeedb46a9858afda2aff476325ace6aea7
DinhLN <lenhatdinh@gmail.com>
2018-11-20 15:13:28 +0700

    Fixed gasprice minimum 2500 for get gasprice api.
    Remove check minimum gas price in tx pool.

--- params/version.go
+++ params/version.go
@@ -21,9 +21,9 @@ import (
 )
 
 const (
-	VersionMajor = 0       // Major version component of the current release
-	VersionMinor = 3       // Minor version component of the current release
-	VersionPatch = 0       // Patch version component of the current release
+	VersionMajor = 0      // Major version component of the current release
+	VersionMinor = 3      // Minor version component of the current release
+	VersionPatch = 0      // Patch version component of the current release
 	VersionMeta  = "beta" // Version metadata to append to the version string
 )
 
