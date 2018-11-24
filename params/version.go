dfbac0dcf22a25f0fa5a949e55204c7f8184bab4
MestryOmkar <mestryomkar@gmail.com>
2018-11-24 16:52:55 +0530

    updated version.go

--- params/version.go
+++ params/version.go
@@ -23,7 +23,7 @@ import (
 const (
 	VersionMajor = 0         // Major version component of the current release
 	VersionMinor = 0          // Minor version component of the current release
-	VersionPatch = 1.1          // Patch version component of the current release
+	VersionPatch = 1          // Patch version component of the current release
 	VersionMeta  = "unstable" // Version metadata to append to the version string
 )
 
