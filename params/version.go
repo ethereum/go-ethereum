24733a43a5c2fc3df418f46e68ea54dce7b87d41
Tuna <ng.tuna@gmail.com>
2018-12-10 17:31:42 +0700

    bump up to v1.1.0

--- params/version.go
+++ params/version.go
@@ -22,7 +22,7 @@ import (
 
 const (
 	VersionMajor = 1        // Major version component of the current release
-	VersionMinor = 0        // Minor version component of the current release
+	VersionMinor = 1        // Minor version component of the current release
 	VersionPatch = 0        // Patch version component of the current release
 	VersionMeta  = "stable" // Version metadata to append to the version string
 )
