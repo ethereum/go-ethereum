d6938a548a5d2d056ad4c7ad1069e3e14e9baf10
贺鹏飞 <hpf@hackerful.cn>
2018-12-20 21:03:48 +0800

    feat(core/tx_pool.go): ignore local tx on discard process
    When geth starts, if local txs are too many (>10k), load and try to discard underpriced txs can be very very slowly!
    But the local txs are never discard on this process, so we can ignore local txs.

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -630,7 +630,7 @@ func (pool *TxPool) add(tx *types.Transaction, local bool) (bool, error) {
 		return false, err
 	}
 	// If the transaction pool is full, discard underpriced transactions
-	if uint64(pool.all.Count()) >= pool.config.GlobalSlots+pool.config.GlobalQueue {
+	if !local && uint64(pool.all.Count()) >= pool.config.GlobalSlots+pool.config.GlobalQueue {
 		// If the new transaction is underpriced, don't accept it
 		if !local && pool.priced.Underpriced(tx, pool.locals) {
 			log.Trace("Discarding underpriced transaction", "hash", hash, "price", tx.GasPrice())
