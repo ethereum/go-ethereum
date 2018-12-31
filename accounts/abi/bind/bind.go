ce0552a5749689db59824fa92ab21047ba685eb2
Guillaume Ballet <gballet@gmail.com>
2018-12-31 11:47:19 +0100

    accounts/abi: empty string case

--- accounts/abi/bind/bind.go
+++ accounts/abi/bind/bind.go
@@ -392,6 +392,10 @@ func capitalise(input string) string {
 
 // decapitalise makes a camel-case string which starts with a lower case character.
 func decapitalise(input string) string {
+	if len(input) == 0 {
+		return input
+	}
+
 	goForm := abi.ToCamelCase(input)
 	return strings.ToLower(goForm[:1]) + goForm[1:]
 }
