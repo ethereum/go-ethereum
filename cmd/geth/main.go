f50d66f2d8e31b5e9920f8f9fb51ff7a6b487631
dragonvslinux <35779158+dragononcrypto@users.noreply.github.com>
2019-01-16 00:21:50 +0100

    cmd/geth: update cli copyright years (#18455)
    * Update copyright
    
    2018 -> 2019
    
    * Update copyright
    
    2018 -> 2019

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -172,7 +172,7 @@ func init() {
 	// Initialize the CLI app and start Geth
 	app.Action = geth
 	app.HideVersion = true // we have a command to print the version
-	app.Copyright = "Copyright 2013-2018 The go-ethereum Authors"
+	app.Copyright = "Copyright 2013-2019 The go-ethereum Authors"
 	app.Commands = []cli.Command{
 		// See chaincmd.go:
 		initCommand,
