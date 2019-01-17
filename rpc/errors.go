6085cb224036f187a8dd5eaea219ea3e3b8acf4d
Felix Lange <fjl@twurst.com>
2019-01-17 14:47:31 +0100

    rpc: implement full bi-directional communication
    New APIs added:
    
        client.RegisterName(namespace, service) // makes service available to server
        client.Notify(ctx, method, args...)     // sends a notification
        ClientFromContext(ctx)                  // to get a client in handler method
    
    This is essentially a rewrite of the server-side code. JSON-RPC
    processing code is now the same on both server and client side. Many
    minor issues were fixed in the process and there is a new test suite for
    JSON-RPC spec compliance (and non-compliance in some cases).
    
    List of behavior changes:
    
    - Method handlers are now called with a per-request context instead of a
      per-connection context. The context is canceled right after the method
      returns.
    - Subscription error channels are always closed when the connection
      ends. There is no need to also wait on the Notifier's Closed channel
      to detect whether the subscription has ended.
    - Parse errors just close the connection instead of reporting an error
      back to the client, except on HTTP connections. I'm not happy about
      this change, but it was non-trivial to implement this within the
      concurrency model used in Client.
    - Client now omits "params" instead of sending "params": null when there
      are no arguments to a call. The previous behavior was not compliant
      with the spec. The server still accepts "params": null.
    - Floating point numbers are allowed as "id". The spec doesn't allow
      them, but we handle request "id" as json.RawMessage and guarantee that
      the same number will be sent back.
    - Logging is improved significantly. There is now a message at DEBUG
      level for each RPC call served.

--- rpc/errors.go
+++ rpc/errors.go
@@ -18,16 +18,22 @@ package rpc
 
 import "fmt"
 
-// request is for an unknown service
-type methodNotFoundError struct {
-	service string
-	method  string
-}
+const defaultErrorCode = -32000
+
+type methodNotFoundError struct{ method string }
 
 func (e *methodNotFoundError) ErrorCode() int { return -32601 }
 
 func (e *methodNotFoundError) Error() string {
-	return fmt.Sprintf("The method %s%s%s does not exist/is not available", e.service, serviceMethodSeparator, e.method)
+	return fmt.Sprintf("the method %s does not exist/is not available", e.method)
+}
+
+type subscriptionNotFoundError struct{ namespace, subscription string }
+
+func (e *subscriptionNotFoundError) ErrorCode() int { return -32601 }
+
+func (e *subscriptionNotFoundError) Error() string {
+	return fmt.Sprintf("no %q subscription in %s namespace", e.subscription, e.namespace)
 }
 
 // received message isn't a valid request
@@ -50,17 +56,3 @@ type invalidParamsError struct{ message string }
 func (e *invalidParamsError) ErrorCode() int { return -32602 }
 
 func (e *invalidParamsError) Error() string { return e.message }
-
-// logic error, callback returned an error
-type callbackError struct{ message string }
-
-func (e *callbackError) ErrorCode() int { return -32000 }
-
-func (e *callbackError) Error() string { return e.message }
-
-// issued when a request is received after the server is issued to stop.
-type shutdownError struct{}
-
-func (e *shutdownError) ErrorCode() int { return -32000 }
-
-func (e *shutdownError) Error() string { return "server is shutting down" }
