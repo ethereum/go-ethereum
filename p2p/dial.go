7dd37e6f72897035d3495c63c4b7fb3af3710ee2
Anton Evangelatov <anton.evangelatov@gmail.com>
2019-01-18 11:32:13 +0100

    p2p: disable random node dialing

--- p2p/dial.go
+++ p2p/dial.go
@@ -217,15 +217,15 @@ func (s *dialstate) newTasks(nRunning int, peers map[enode.ID]*Peer, now time.Ti
 	}
 	// Use random nodes from the table for half of the necessary
 	// dynamic dials.
-	randomCandidates := needDynDials / 2
-	if randomCandidates > 0 {
-		n := s.ntab.ReadRandomNodes(s.randomNodes)
-		for i := 0; i < randomCandidates && i < n; i++ {
-			if addDial(dynDialedConn, s.randomNodes[i]) {
-				needDynDials--
-			}
-		}
-	}
+	//randomCandidates := needDynDials / 2
+	//if randomCandidates > 0 {
+	//n := s.ntab.ReadRandomNodes(s.randomNodes)
+	//for i := 0; i < randomCandidates && i < n; i++ {
+	//if addDial(dynDialedConn, s.randomNodes[i]) {
+	//needDynDials--
+	//}
+	//}
+	//}
 	// Create dynamic dials from random lookup results, removing tried
 	// items from the result buffer.
 	i := 0
