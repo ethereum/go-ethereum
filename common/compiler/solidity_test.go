2772e096b48d18fcdd47b0bd16f9d692b06c291b
Javier Sagredo <jasataco@gmail.com>
2019-02-03 12:41:38 +0100

    common/compiler: fixed testSource (#18978)

--- common/compiler/solidity_test.go
+++ common/compiler/solidity_test.go
@@ -23,9 +23,10 @@ import (
 
 const (
 	testSource = `
+pragma solidity >0.0.0;
 contract test {
    /// @notice Will multiply ` + "`a`" + ` by 7.
-   function multiply(uint a) returns(uint d) {
+   function multiply(uint a) public returns(uint d) {
        return a * 7;
    }
 }
