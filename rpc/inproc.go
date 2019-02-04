a6903c886a8f5d796f888c7c1644374ccd2e9bd7
Felix Lange <fjl@users.noreply.github.com>
2019-02-04 13:47:34 +0100

    rpc: implement full bi-directional communication (backport)
    New APIs added:
    
        client.RegisterName(namespace, service) // makes service available to server
        client.Notify(ctx, method, args...)     // sends a notification
        ClientFromContext(ctx)                  // to get a client in handler method
    
    This is essentially a rewrite of the server-side code. JSON-RPC
    processing code is now the same on both server and client side. Many
    minor issues were fixed in the process and there is a new test suite for
    JSON-RPC spec compliance (and non-compliance in some cases).
    
    List of behavior changes:
    
    - Method handlers are now called with a per-request context instead of a
      per-connection context. The context is canceled right after the method
      returns.
    - Subscription error channels are always closed when the connection
      ends. There is no need to also wait on the Notifier's Closed channel
      to detect whether the subscription has ended.
    - Client now omits "params" instead of sending "params": null when there
      are no arguments to a call. The previous behavior was not compliant
      with the spec. The server still accepts "params": null.
    - Floating point numbers are allowed as "id". The spec doesn't allow
      them, but we handle request "id" as json.RawMessage and guarantee that
      the same number will be sent back.
    - Logging is improved significantly. There is now a message at DEBUG
      level for each RPC call served.

--- rpc/inproc.go
+++ rpc/inproc.go
@@ -24,10 +24,10 @@ import (
 // DialInProc attaches an in-process connection to the given RPC server.
 func DialInProc(handler *Server) *Client {
 	initctx := context.Background()
-	c, _ := newClient(initctx, func(context.Context) (net.Conn, error) {
+	c, _ := newClient(initctx, func(context.Context) (ServerCodec, error) {
 		p1, p2 := net.Pipe()
 		go handler.ServeCodec(NewJSONCodec(p1), OptionMethodInvocation|OptionSubscriptions)
-		return p2, nil
+		return NewJSONCodec(p2), nil
 	})
 	return c
 }
