f48da43bae183a04a23d298cb1790d2f8d2cec51
Martin Holst Swende <martin@swende.se>
2019-02-12 11:29:05 +0100

    common/fdlimit: cap on MacOS file limits, fixes #18994 (#19035)
    * common/fdlimit: cap on MacOS file limits, fixes #18994
    
    * common/fdlimit: fix Maximum-check to respect OPEN_MAX
    
    * common/fdlimit: return error if OPEN_MAX is exceeded in Raise()
    
    * common/fdlimit: goimports
    
    * common/fdlimit: check value after setting fdlimit
    
    * common/fdlimit: make comment a bit more descriptive
    
    * cmd/utils: make fdlimit happy path a bit cleaner

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -950,10 +950,11 @@ func makeDatabaseHandles() int {
 	if err != nil {
 		Fatalf("Failed to retrieve file descriptor allowance: %v", err)
 	}
-	if err := fdlimit.Raise(uint64(limit)); err != nil {
+	raised, err := fdlimit.Raise(uint64(limit))
+	if err != nil {
 		Fatalf("Failed to raise file descriptor allowance: %v", err)
 	}
-	return limit / 2 // Leave half for networking and other stuff
+	return int(raised / 2) // Leave half for networking and other stuff
 }
 
 // MakeAddress converts an account specified directly as a hex encoded string or
