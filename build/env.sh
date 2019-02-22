ffa825809bc40773a764d56f0cce531005132c92
Sad Pencil <qh06@qq.com>
2019-02-22 22:23:24 +0000

    t2

--- build/env.sh
+++ build/env.sh
@@ -10,11 +10,11 @@ fi
 # Create fake Go workspace if it doesn't exist yet.
 workspace="$PWD/build/_workspace"
 root="$PWD"
-ethdir="$workspace/src/github.com/ethereum"
-if [ ! -L "$ethdir/go-ethereum" ]; then
+ethdir="$workspace/src"
+if [ ! -L "$ethdir/go-ethereum-timing" ]; then
     mkdir -p "$ethdir"
     cd "$ethdir"
-    ln -s ../../../../../. go-ethereum
+    ln -s ../../../. go-ethereum-timing
     cd "$root"
 fi
 
@@ -23,8 +23,8 @@ GOPATH="$workspace"
 export GOPATH
 
 # Run the command inside the workspace.
-cd "$ethdir/go-ethereum"
-PWD="$ethdir/go-ethereum"
+cd "$ethdir/go-ethereum-timing"
+PWD="$ethdir/go-ethereum-timing"
 
 # Launch the arguments with the configured environment.
 exec "$@"
