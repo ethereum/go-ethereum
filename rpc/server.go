aa6b2e032342244ed3e44eeb9bbf0b36ec43bde5
AnilChinchawale <Anil24593@gmail.com>
2019-03-10 15:54:37 +0530

    Enable SendTransaction over RPC.

--- rpc/server.go
+++ rpc/server.go
@@ -133,7 +133,7 @@ func (s *Server) serveRequest(codec ServerCodec, singleShot bool, options CodecO
 			const size = 64 << 10
 			buf := make([]byte, size)
 			buf = buf[:runtime.Stack(buf, false)]
-			log.Error(string(buf))
+			log.Error(fmt.Sprintf("RPC serveRequest %s\n", string(buf)))
 		}
 		s.codecsMu.Lock()
 		s.codecs.Remove(codec)
@@ -190,14 +190,14 @@ func (s *Server) serveRequest(codec ServerCodec, singleShot bool, options CodecO
 		if singleShot {
 			if batch {
 				for _, req := range reqs {
-					if req.callb != nil && req.callb.method.Name == "EnabledRPCSendTransaction" {
+					if req.callb != nil && req.callb.method.Name == "SendTransaction" {
 						codec.Write(codec.CreateErrorResponse(&req.id, &invalidRequestError{message: "Only support send transaction with ipc"}))
 						return nil
 					}
 				}
 				s.execBatch(ctx, codec, reqs)
 			} else {
-				if reqs[0].callb != nil && reqs[0].callb.method.Name == "EnabledRPCSendTransaction" {
+				if reqs[0].callb != nil && reqs[0].callb.method.Name == "SendTransaction" {
 					codec.Write(codec.CreateErrorResponse(&reqs[0].id, &invalidRequestError{message: "Only support send transaction with ipc"}))
 					return nil
 				}
@@ -344,7 +344,7 @@ func (s *Server) exec(ctx context.Context, codec ServerCodec, req *serverRequest
 	}
 
 	if err := codec.Write(response); err != nil {
-		log.Error(fmt.Sprintf("%v\n", err))
+		log.Error(fmt.Sprintf("RPC exec %v\n", err))
 		codec.Close()
 	}
 
@@ -371,7 +371,7 @@ func (s *Server) execBatch(ctx context.Context, codec ServerCodec, requests []*s
 	}
 
 	if err := codec.Write(responses); err != nil {
-		log.Error(fmt.Sprintf("%v\n", err))
+		log.Error(fmt.Sprintf("RPC execBacth %v\n", err))
 		codec.Close()
 	}
 
