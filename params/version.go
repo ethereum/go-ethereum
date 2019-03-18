6f850d525581bab58995f922034b6ec5ce4653f1
AnilChinchawale <Anil24593@gmail.com>
2019-03-18 15:14:51 +0530

    Version pump to v0.4.0

--- params/version.go
+++ params/version.go
@@ -22,7 +22,7 @@ import (
 
 const (
 	VersionMajor = 0          // Major version component of the current release
-	VersionMinor = 3          // Minor version component of the current release
+	VersionMinor = 4          // Minor version component of the current release
 	VersionPatch = 0          // Patch version component of the current release
 	VersionMeta  = "unstable" // Version metadata to append to the version string
 )
