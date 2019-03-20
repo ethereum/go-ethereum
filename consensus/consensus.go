a598f068f0a385829af770ccd657c43bb229dbdb
Martin Holst Swende <martin@swende.se>
2019-03-20 11:39:23 +0100

    consensus: nitpick dot

--- consensus/consensus.go
+++ consensus/consensus.go
@@ -80,7 +80,7 @@ type Engine interface {
 	Prepare(chain ChainReader, header *types.Header) error
 
 	// Finalize runs any post-transaction state modifications (e.g. block rewards)
-	// but does not assemble the block
+	// but does not assemble the block.
 	// Note: The block header and state database might be updated to reflect any
 	// consensus rules that happen at finalization (e.g. block rewards).
 	Finalize(chain ChainReader, header *types.Header, state *state.StateDB, txs []*types.Transaction,
