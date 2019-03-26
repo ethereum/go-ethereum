2d1eddf74f09fbae0f2ca33e13b129284915ee87
parmarrushabh <rushabhparmar17@gmail.com>
2019-03-26 14:34:58 +0530

    TIP random

--- common/constants.go
+++ common/constants.go
@@ -24,7 +24,7 @@ const (
 
 var TIP2019Block = big.NewInt(1050000)
 var TIPSigning = big.NewInt(3000000)
-var TIPRandomize = big.NewInt(3374780)
+var TIPRandomize = big.NewInt(3410000)
 var IsTestnet bool = false
 var StoreRewardFolder string
 var RollbackHash Hash
