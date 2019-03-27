f9a3d5b7ea29afa4b0d87fad0abb824ff9a20e73
Julian Yap <julianokyap@gmail.com>
2019-03-27 01:23:23 -0700

    Version 2.2.0

--- params/version.go
+++ params/version.go
@@ -21,10 +21,10 @@ import (
 )
 
 const (
-	VersionMajor = 2     // Major version component of the current release
-	VersionMinor = 2     // Minor version component of the current release
-	VersionPatch = 0     // Patch version component of the current release
-	VersionMeta  = "lit" // Version metadata to append to the version string
+	VersionMajor = 2      // Major version component of the current release
+	VersionMinor = 2      // Minor version component of the current release
+	VersionPatch = 0      // Patch version component of the current release
+	VersionMeta  = "woke" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
