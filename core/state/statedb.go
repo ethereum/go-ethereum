529e52ce89191c91e252e0f24c8be602ef0cd527
rjl493456442 <garyrong0905@gmail.com>
2019-04-01 16:05:48 +0800

    core: fix state iterator more elegant

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -474,27 +474,33 @@ func (self *StateDB) CreateAccount(addr common.Address) {
 	}
 }
 
-func (db *StateDB) ForEachStorage(addr common.Address, cb func(key, value common.Hash) bool) {
+func (db *StateDB) ForEachStorage(addr common.Address, cb func(key, value common.Hash) bool) error {
 	so := db.getStateObject(addr)
 	if so == nil {
-		return
+		return nil
 	}
 	it := trie.NewIterator(so.getTrie(db.db).NodeIterator(nil))
+
 	for it.Next() {
 		key := common.BytesToHash(db.trie.GetKey(it.Key))
 		if value, dirty := so.dirtyStorage[key]; dirty {
-			cb(key, value)
+			if !cb(key, value) {
+				return nil
+			}
 			continue
 		}
 
 		if len(it.Value) > 0 {
 			_, content, _, err := rlp.Split(it.Value)
 			if err != nil {
-				continue
+				return err
 			}
-			cb(key, common.BytesToHash(content))
+			if !cb(key, common.BytesToHash(content)) {
+				return nil
+			}
 		}
 	}
+	return nil
 }
 
 // Copy creates a deep, independent copy of the state.
