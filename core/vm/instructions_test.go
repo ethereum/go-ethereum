4fdaae41e461abc9733adbce6227ee9bfe931938
Martin Holst Swende <martin@swende.se>
2019-04-04 10:35:40 +0200

    tests: go format

--- core/vm/instructions_test.go
+++ core/vm/instructions_test.go
@@ -248,7 +248,7 @@ func xTestWriteExpectedValues(t *testing.T) {
 
 // TestJsonTestcases runs through all the testcases defined as json-files
 func TestJsonTestcases(t *testing.T) {
-	for name, _ := range twoOpMethods {
+	for name := range twoOpMethods {
 		data, err := ioutil.ReadFile(fmt.Sprintf("testdata/testcases_%v.json", name))
 		if err != nil {
 			t.Fatal("Failed to read file", err)
