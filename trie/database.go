4bf0d11e7c6f4ddf4daea1d3a8d72bfff66571ab
Péter Szilágyi <peterke@gmail.com>
2019-04-05 13:09:28 +0300

    trie: there's no point in retrieving the metaroot

--- trie/database.go
+++ trie/database.go
@@ -18,6 +18,7 @@ package trie
 
 import (
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"io"
 	"sync"
@@ -400,6 +401,10 @@ func (db *Database) node(hash common.Hash) node {
 // Node retrieves an encoded cached trie node from memory. If it cannot be found
 // cached, the method queries the persistent database for the content.
 func (db *Database) Node(hash common.Hash) ([]byte, error) {
+	// It doens't make sense to retrieve the metaroot
+	if hash == (common.Hash{}) {
+		return nil, errors.New("not found")
+	}
 	// Retrieve the node from the clean cache if available
 	if db.cleans != nil {
 		if enc, err := db.cleans.Get(string(hash[:])); err == nil && enc != nil {
