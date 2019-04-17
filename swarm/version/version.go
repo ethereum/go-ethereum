78ec90717aaa34c6ac981e791ca7fb04077cca33
Péter Szilágyi <peterke@gmail.com>
2019-04-17 15:50:41 +0300

    swarm/version: bump version due to Geth hotfix release

--- swarm/version/version.go
+++ swarm/version/version.go
@@ -23,7 +23,7 @@ import (
 const (
 	VersionMajor = 0          // Major version component of the current release
 	VersionMinor = 3          // Minor version component of the current release
-	VersionPatch = 15         // Patch version component of the current release
+	VersionPatch = 16         // Patch version component of the current release
 	VersionMeta  = "unstable" // Version metadata to append to the version string
 )
 
