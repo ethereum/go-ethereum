749ccab9a4fb778c144f2e435249a8bb697e974e
gary rong <garyrong0905@gmail.com>
2019-04-25 16:31:23 +0800

    eth/downloader: enable unsync-protection for light client (#19496)
    * eth/downloader: enable unsync-protection for light client
    
    * eth/downloader: fix tests

--- eth/downloader/downloader.go
+++ eth/downloader/downloader.go
@@ -580,7 +580,7 @@ func (d *Downloader) fetchHeight(p *peerConnection) (*types.Header, error) {
 				return nil, errBadPeer
 			}
 			head := headers[0]
-			if d.mode == FastSync && head.Number.Uint64() < d.checkpoint {
+			if (d.mode == FastSync || d.mode == LightSync) && head.Number.Uint64() < d.checkpoint {
 				p.log.Warn("Remote head below checkpoint", "number", head.Number, "hash", head.Hash())
 				return nil, errUnsyncedPeer
 			}
