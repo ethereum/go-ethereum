27cc03178aaae8331fc77436ad1441420a434a21
ANOTHEL <anothel1@naver.com>
2019-05-14 12:19:21 +0900

    add comment

--- core/vm/errors.go
+++ core/vm/errors.go
@@ -20,45 +20,45 @@ import "errors"
 
 // List execution errors
 var (
-	// ErrOutOfGas is returned when
+	// ErrOutOfGas is returned when gas is insufficient.
 	ErrOutOfGas = errors.New("out of gas")
 
-	// ErrCodeStoreOutOfGas is returned when
+	// ErrCodeStoreOutOfGas is returned when gas is insufficient for making contract.
 	ErrCodeStoreOutOfGas = errors.New("contract creation code storage out of gas")
 
-	// ErrDepth is returned when
+	// ErrDepth is returned when max call depth exceeded.
 	ErrDepth = errors.New("max call depth exceeded")
 
-	// ErrTraceLimitReached is returned when
+	// ErrTraceLimitReached is returned when logs reached the specified limit.
 	ErrTraceLimitReached = errors.New("the number of logs reached the specified limit")
 
-	// ErrInsufficientBalance is returned when
+	// ErrInsufficientBalance is returned when balance is insufficient for transfer.
 	ErrInsufficientBalance = errors.New("insufficient balance for transfer")
 
-	// ErrContractAddressCollision is returned when
+	// ErrContractAddressCollision is returned when contract address collide.
 	ErrContractAddressCollision = errors.New("contract address collision")
 
-	// ErrNoCompatibleInterpreter is returned when
+	// ErrNoCompatibleInterpreter is returned when interpreter is not compatible.
 	ErrNoCompatibleInterpreter = errors.New("no compatible interpreter")
 
 	// ErrBadPairingInput is returned if the bn256 pairing input is invalid.
 	ErrBadPairingInput = errors.New("bad elliptic curve pairing size")
 
-	// ErrWriteProtection is returned when
+	// ErrWriteProtection is returned when mode is readonly, it blocks writing.
 	ErrWriteProtection = errors.New("evm: write protection")
 
-	// ErrReturnDataOutOfBounds is returned when
+	// ErrReturnDataOutOfBounds is returned when return data is out of bounds.
 	ErrReturnDataOutOfBounds = errors.New("evm: return data out of bounds")
 
-	// ErrExecutionReverted is returned when
+	// ErrExecutionReverted is returned if execution is reverted.
 	ErrExecutionReverted = errors.New("evm: execution reverted")
 
-	// ErrMaxCodeSizeExceeded is returned when
+	// ErrMaxCodeSizeExceeded is returned when max code size is exceeded.
 	ErrMaxCodeSizeExceeded = errors.New("evm: max code size exceeded")
 
-	// ErrInvalidJump is returned when
+	// ErrInvalidJump is returned when jump destination is invalid.
 	ErrInvalidJump = errors.New("evm: invalid jump destination")
 
-	// ErrGasUintOverflow is returned when
+	// ErrGasUintOverflow is returned when gas uint64 is overflow.
 	ErrGasUintOverflow = errors.New("gas uint64 overflow")
 )
