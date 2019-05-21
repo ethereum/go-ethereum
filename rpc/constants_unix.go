afe95fd7aebaa7ca891a4535dc59eb76ce9dd7ca
Martin Holst Swende <martin@swende.se>
2019-05-21 09:57:15 +0200

    rpc: move unix path constant into separate file

--- rpc/constants_unix.go
+++ rpc/constants_unix.go
@@ -18,37 +18,16 @@
 
 package rpc
 
-import (
-	"context"
-	"fmt"
-	"net"
-	"os"
-	"path/filepath"
+/*
+#include <sys/un.h>
 
-	"github.com/ethereum/go-ethereum/log"
+int max_socket_path_size() {
+struct sockaddr_un s;
+return sizeof(s.sun_path);
+}
+*/
+import "C"
+
+var (
+	max_path_size = C.max_socket_path_size()
 )
-
-// ipcListen will create a Unix socket on the given endpoint.
-func ipcListen(endpoint string) (net.Listener, error) {
-	if len(endpoint) > int(max_path_size) {
-		log.Warn(fmt.Sprintf("The ipc endpoint is longer than %d characters. ", max_path_size),
-			"endpoint", endpoint)
-	}
-
-	// Ensure the IPC path exists and remove any previous leftover
-	if err := os.MkdirAll(filepath.Dir(endpoint), 0751); err != nil {
-		return nil, err
-	}
-	os.Remove(endpoint)
-	l, err := net.Listen("unix", endpoint)
-	if err != nil {
-		return nil, err
-	}
-	os.Chmod(endpoint, 0600)
-	return l, nil
-}
-
-// newIPCConnection will connect to a Unix socket on the given endpoint.
-func newIPCConnection(ctx context.Context, endpoint string) (net.Conn, error) {
-	return dialContext(ctx, "unix", endpoint)
-}
