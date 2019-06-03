809165fc5efa28633b69a5f5b018857da9c10bbf
Rafael Matias <rafael@ethereum.org>
2019-06-03 15:16:56 +0200

    ci/cd: fix azure blobstorage name (#1413)

--- build/ci.go
+++ build/ci.go
@@ -317,7 +317,7 @@ func doArchive(cmdline []string) {
 		arch   = flag.String("arch", runtime.GOARCH, "Architecture cross packaging")
 		atype  = flag.String("type", "zip", "Type of archive to write (zip|tar)")
 		signer = flag.String("signer", "", `Environment variable holding the signing key (e.g. LINUX_SIGNING_KEY)`)
-		upload = flag.String("upload", "", `Destination to upload the archives (usually "ethswarmstorage/builds")`)
+		upload = flag.String("upload", "", `Destination to upload the archives (usually "ethswarm/builds")`)
 		ext    string
 	)
 	flag.CommandLine.Parse(cmdline)
@@ -691,7 +691,7 @@ func xgoTool(args []string) *exec.Cmd {
 
 func doPurge(cmdline []string) {
 	var (
-		store = flag.String("store", "", `Destination from where to purge archives (usually "ethswarmstorage/builds")`)
+		store = flag.String("store", "", `Destination from where to purge archives (usually "ethswarm/builds")`)
 		limit = flag.Int("days", 30, `Age threshold above which to delete unstable archives`)
 	)
 	flag.CommandLine.Parse(cmdline)
