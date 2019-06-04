d97f0372b19c0d58261c8f357ddc27cb3265b5a4
Guillaume Ballet <gballet@gmail.com>
2019-06-04 17:40:34 +0200

    accounts/scwallet: don't error when pcsc socket is missing (#19662)
    * scwallet: don't error when pcsc socket is missing
    
    * review feedback
    
    * more review feedback

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -1161,7 +1161,7 @@ func setSmartCard(ctx *cli.Context, cfg *node.Config) {
 	// Sanity check that the smartcard path is valid
 	fi, err := os.Stat(path)
 	if err != nil {
-		log.Error("Failed to verify smartcard daemon path", "path", path, "err", err)
+		log.Info("Smartcard socket not found, disabling", err", err)
 		return
 	}
 	if fi.Mode()&os.ModeType != os.ModeSocket {
