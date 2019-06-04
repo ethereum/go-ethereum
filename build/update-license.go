42b81f94adba9aae7f7727951f35e92184b1eedb
Rafael Matias <rafael@skyle.net>
2019-06-04 15:35:36 +0200

    swarm: code cleanup, move to ethersphere/swarm (#19661)

--- build/update-license.go
+++ build/update-license.go
@@ -72,8 +72,6 @@ var (
 		"internal/jsre/deps",
 		"log/",
 		"common/bitutil/bitutil",
-		// don't license generated files
-		"contracts/chequebook/contract/code.go",
 	}
 
 	// paths with this prefix are licensed as GPL. all other files are LGPL.
--- swarm/docker/run-smoke.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -o errexit
-set -o pipefail
-set -o nounset
-
-/swarm-smoke $@ 2>&1 || true
