962fc2350295c234abea4e2f9e73629a852749b6
Guillaume Ballet <gballet@gmail.com>
2019-06-07 08:56:55 +0200

    scwallet: Disable macos support

--- vendor/github.com/gballet/go-libpcsclite/doc_darwin.go
+++ vendor/github.com/gballet/go-libpcsclite/doc_darwin.go
@@ -28,8 +28,8 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-// +build dragonfly freebsd netbsd openbsd solaris
+// +build darwin
 
 package pcsc
 
-const PCSCDSockName string = "/var/run/pcscd/pcscd.comm"
+const PCSCDSockName string = ""
