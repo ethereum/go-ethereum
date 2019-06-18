38c914be644ae50c18bb9dc1a413e560b57e4e13
Péter Szilágyi <peterke@gmail.com>
2019-06-18 11:49:30 +0300

    accounts/abi/bind/backends: use EIP155 on the simulated chain

--- accounts/abi/bind/backends/simulated.go
+++ accounts/abi/bind/backends/simulated.go
@@ -316,7 +316,7 @@ func (b *SimulatedBackend) SendTransaction(ctx context.Context, tx *types.Transa
 	b.mu.Lock()
 	defer b.mu.Unlock()
 
-	sender, err := types.Sender(types.HomesteadSigner{}, tx)
+	sender, err := types.Sender(types.NewEIP155Signer(b.config.ChainID), tx)
 	if err != nil {
 		panic(fmt.Errorf("invalid transaction: %v", err))
 	}
