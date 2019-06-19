f3bfaaf96704ef3f1612bbff9d5691eba60fce73
Martin Holst Swende <martin@swende.se>
2019-06-19 12:44:20 +0200

    ethapi, vm: detect and return error on eth_call when call times out, fixes #19186

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -802,6 +802,10 @@ func DoCall(ctx context.Context, b Backend, args CallArgs, blockNr rpc.BlockNumb
 	if err := vmError(); err != nil {
 		return nil, 0, false, err
 	}
+	// If the timer caused an abort, return an appropriate error message
+	if evm.Cancelled() {
+		return nil, 0, false, fmt.Errorf("execution aborted (timeout = %v)", timeout)
+	}
 	return res, gas, failed, err
 }
 
