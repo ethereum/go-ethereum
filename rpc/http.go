260de46157d81a513af0bc5e42ca75cd2046b914
Sjon Hortensius <sjon@hortensius.net>
2019-08-22 14:50:36 +0200

    Ref #19357, fix two nitpicks, add copyright header and rename constructor

--- rpc/http.go
+++ rpc/http.go
@@ -216,7 +216,7 @@ func NewHTTPServer(cors []string, vhosts []string, timeouts HTTPTimeouts, srv ht
 	// Wrap the CORS-handler within a host-handler
 	handler := newCorsHandler(srv, cors)
 	handler = newVHostHandler(vhosts, handler)
-	handler = Gzip(handler)
+	handler = newGzipHandler(handler)
 
 	// Make sure timeout values are meaningful
 	if timeouts.ReadTimeout < time.Second {
