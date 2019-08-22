46ec63b84932705219aad80174f8968357373b69
Andrey Petrov <andrey.petrov@shazow.net>
2019-08-22 05:47:24 -0400

    ethdb/dbtest: addd test suite for ethdb backends (#19960)
    - Move the existing tests from memorydb into a generalized testsuite
    that can be run by any ethdb backend implementation.
    - Add several more test cases to clarify some non-obvious nuances when
    implementing a custom ethdb backend, such as the behaviour of
    NewIteratorWithPrefix vs NewIteratorWithStart.
    - Add leveldb to the testsuite using in-memory storage for fast
    execution.

--- ethdb/leveldb/leveldb_test.go
+++ ethdb/leveldb/leveldb_test.go
@@ -1,4 +1,4 @@
-// Copyright 2018 The go-ethereum Authors
+// Copyright 2019 The go-ethereum Authors
 // This file is part of the go-ethereum library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
@@ -14,19 +14,27 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-package memorydb
+package leveldb
 
 import (
 	"testing"
 
 	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/ethereum/go-ethereum/ethdb/dbtest"
+	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/storage"
 )
 
-func TestMemoryDB(t *testing.T) {
+func TestLevelDB(t *testing.T) {
 	t.Run("DatabaseSuite", func(t *testing.T) {
 		dbtest.TestDatabaseSuite(t, func() ethdb.KeyValueStore {
-			return New()
+			db, err := leveldb.Open(storage.NewMemStorage(), nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+			return &Database{
+				db: db,
+			}
 		})
 	})
 }
