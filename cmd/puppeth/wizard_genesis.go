25a8cb4766983eef1a24e1fd5172c56fddfa4339
Jaynti Kanani <jdkanani@gmail.com>
2019-09-25 20:34:04 +0530

    fix puppeth

--- cmd/puppeth/wizard_genesis.go
+++ cmd/puppeth/wizard_genesis.go
@@ -108,9 +108,10 @@ func (w *wizard) makeGenesis() {
 		genesis.Difficulty = big.NewInt(1)
 		genesis.GasLimit = 10000000
 		genesis.Config.Bor = &params.BorConfig{
-			Period:        1,
-			ProducerDelay: 5,
-			Epoch:         60,
+			Period:            1,
+			ProducerDelay:     5,
+			Sprint:            60,
+			ValidatorContract: "0x0000000000000000000000000000000000001000",
 		}
 
 		// We also need the initial list of signers
