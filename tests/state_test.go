0e9136558e24332a90d4f2467bbd1fdc10ceb900
Péter Szilágyi <peterke@gmail.com>
2019-10-02 11:03:30 +0300

    tests: I hate github's editor

--- tests/state_test.go
+++ tests/state_test.go
@@ -43,7 +43,7 @@ func TestState(t *testing.T) {
 
 	// Very time consuming
 	st.skipLoad(`^stTimeConsuming/`)
-	
+
 	// Broken tests:
 	// Expected failures:
 	//st.fails(`^stRevertTest/RevertPrecompiledTouch(_storage)?\.json/Byzantium/0`, "bug in test")
