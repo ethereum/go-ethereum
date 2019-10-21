6f59d8d9d85351f57d62a1475f5f6b0f57577024
Piotr Dyraga <piotr.dyraga@keep.network>
2019-10-21 09:00:02 +0200

    accounts/abi/bind: take into account gas price during gas estimation
    The gas price was not passed to the `EstimateGas` function. As a result,
    conditional execution paths depending on `tx.gasprice` could be not
    correctly processed and we could get invalid gas estimates for contract
    function calls.

--- accounts/abi/bind/base.go
+++ accounts/abi/bind/base.go
@@ -218,7 +218,7 @@ func (c *BoundContract) transact(opts *TransactOpts, contract *common.Address, i
 			}
 		}
 		// If the contract surely has code (or code is not needed), estimate the transaction
-		msg := ethereum.CallMsg{From: opts.From, To: contract, Value: value, Data: input}
+		msg := ethereum.CallMsg{From: opts.From, To: contract, GasPrice: gasPrice, Value: value, Data: input}
 		gasLimit, err = c.transactor.EstimateGas(ensureContext(opts.Context), msg)
 		if err != nil {
 			return nil, fmt.Errorf("failed to estimate gas needed: %v", err)
