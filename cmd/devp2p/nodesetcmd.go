86b9e47cb88243042d337a1a0a44baae8e22337c
Felix Lange <fjl@twurst.com>
2019-10-29 13:32:50 +0100

    cmd/devp2p: add ropsten support in -eth-network filter

--- cmd/devp2p/nodesetcmd.go
+++ cmd/devp2p/nodesetcmd.go
@@ -163,6 +163,8 @@ func ethFilter(args []string) (nodeFilter, error) {
 		filter = forkid.NewStaticFilter(params.RinkebyChainConfig, params.RinkebyGenesisHash)
 	case "goerli":
 		filter = forkid.NewStaticFilter(params.GoerliChainConfig, params.GoerliGenesisHash)
+	case "ropsten":
+		filter = forkid.NewStaticFilter(params.TestnetChainConfig, params.TestnetGenesisHash)
 	default:
 		return nil, fmt.Errorf("unknown network %q", args[0])
 	}
