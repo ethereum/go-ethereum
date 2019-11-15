8d3ef0924a71c254ab47e5516bbb8b91e175234e
Felix Lange <fjl@twurst.com>
2019-11-15 12:07:13 +0100

    core/state: remove unused function xTestFuzzCutter

--- core/state/state_object_test.go
+++ core/state/state_object_test.go
@@ -18,10 +18,7 @@ package state
 
 import (
 	"bytes"
-	"fmt"
-	"math/rand"
 	"testing"
-	"time"
 
 	"github.com/ethereum/go-ethereum/common"
 )
@@ -45,24 +42,5 @@ func BenchmarkCutCustomTrim(b *testing.B) {
 	value := common.HexToHash("0x01")
 	for i := 0; i < b.N; i++ {
 		common.TrimLeftZeroes(value[:])
-	}
-}
-
-func xTestFuzzCutter(t *testing.T) {
-	rand.Seed(time.Now().Unix())
-	for {
-		v := make([]byte, 20)
-		zeroes := rand.Intn(21)
-		rand.Read(v[zeroes:])
-		exp := bytes.TrimLeft(v[:], "\x00")
-		got := common.TrimLeftZeroes(v)
-		if !bytes.Equal(exp, got) {
-
-			fmt.Printf("Input %x\n", v)
-			fmt.Printf("Exp %x\n", exp)
-			fmt.Printf("Got %x\n", got)
-			t.Fatalf("Error")
-		}
-		//break
 	}
 }
