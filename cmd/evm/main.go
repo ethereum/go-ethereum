9e71f55bfab91a26f5cfc06f0a4e48839b25f249
Michael Forney <mforney@mforney.org>
2019-11-17 06:45:54 -0800

    cmd/evm: Allow loading input from file (#20273)
    Make it possible to load input from a file. Simlar to `--code` / `--codefile`, have `--input`/`--inputfile`.

--- cmd/evm/main.go
+++ cmd/evm/main.go
@@ -79,6 +79,10 @@ var (
 		Name:  "input",
 		Usage: "input for the EVM",
 	}
+	InputFileFlag = cli.StringFlag{
+		Name:  "inputfile",
+		Usage: "file containing input for the EVM",
+	}
 	VerbosityFlag = cli.IntFlag{
 		Name:  "verbosity",
 		Usage: "sets the verbosity level",
@@ -130,6 +134,7 @@ func init() {
 		ValueFlag,
 		DumpFlag,
 		InputFlag,
+		InputFileFlag,
 		MemProfileFlag,
 		CPUProfileFlag,
 		StatDumpFlag,
