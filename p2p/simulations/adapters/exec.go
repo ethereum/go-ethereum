563410573da829cb1f65f22c1daa1244dffcca5b
Felix Lange <fjl@twurst.com>
2019-11-18 18:40:59 +0100

    p2p/simulations/adapters: fix staticcheck warnings

--- p2p/simulations/adapters/exec.go
+++ p2p/simulations/adapters/exec.go
@@ -19,7 +19,6 @@ package adapters
 import (
 	"bytes"
 	"context"
-	"crypto/ecdsa"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -146,7 +145,6 @@ type ExecNode struct {
 	client  *rpc.Client
 	wsAddr  string
 	newCmd  func() *exec.Cmd
-	key     *ecdsa.PrivateKey
 }
 
 // Addr returns the node's enode URL
