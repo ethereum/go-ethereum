444da8204fe7c8d34383ca776d754688a6b38ab0
Felix Lange <fjl@twurst.com>
2019-11-19 19:03:14 +0100

    rpc: remove 'exported or builtin' restriction for parameters
    There is no technial reason for this restriction because package reflect
    can create values of any type. Requiring parameters and return values to
    be exported causes a lot of noise in package exports.

--- rpc/testservice_test.go
+++ rpc/testservice_test.go
@@ -53,24 +53,24 @@ func sequentialIDGenerator() func() ID {
 
 type testService struct{}
 
-type Args struct {
+type echoArgs struct {
 	S string
 }
 
-type Result struct {
+type echoResult struct {
 	String string
 	Int    int
-	Args   *Args
+	Args   *echoArgs
 }
 
 func (s *testService) NoArgsRets() {}
 
-func (s *testService) Echo(str string, i int, args *Args) Result {
-	return Result{str, i, args}
+func (s *testService) Echo(str string, i int, args *echoArgs) echoResult {
+	return echoResult{str, i, args}
 }
 
-func (s *testService) EchoWithCtx(ctx context.Context, str string, i int, args *Args) Result {
-	return Result{str, i, args}
+func (s *testService) EchoWithCtx(ctx context.Context, str string, i int, args *echoArgs) echoResult {
+	return echoResult{str, i, args}
 }
 
 func (s *testService) Sleep(ctx context.Context, duration time.Duration) {
