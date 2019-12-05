7d4bcc3cbfc6ddb1982bd041666d0f5f5b3f1cf7
Ian Norden <iannordenn@gmail.com>
2019-12-05 14:20:51 -0600

    adjust verifyHeaders to validate GasLimit as calculated by new CalcGasLimitAndBaseFee function

--- consensus/misc/forks.go
+++ consensus/misc/forks.go
@@ -27,11 +27,12 @@ import (
 )
 
 var (
-	errInvalidInitialBaseFee = fmt.Errorf("initial BaseFee must equal %d", params.EIP1559InitialBaseFee)
-	errInvalidBaseFee        = errors.New("invalid BaseFee")
-	errMissingParentBaseFee  = errors.New("parent header is missing BaseFee")
-	errMissingBaseFee        = errors.New("current header is missing BaseFee")
-	errHaveBaseFee           = fmt.Errorf("BaseFee should not be set before block %d", params.EIP1559ForkBlockNumber)
+	errInvalidInitialBaseFee           = fmt.Errorf("initial BaseFee must equal %d", params.EIP1559InitialBaseFee)
+	errInvalidBaseFee                  = errors.New("invalid BaseFee")
+	errMissingParentBaseFee            = errors.New("parent header is missing BaseFee")
+	errMissingBaseFee                  = errors.New("current header is missing BaseFee")
+	errHaveBaseFee                     = fmt.Errorf("BaseFee should not be set before block %d", params.EIP1559ForkBlockNumber)
+	errInvalidEIP1559FinalizedGasLimit = fmt.Errorf("after EIP1559 finalization, GasLimit must equal %d", params.MaxGasEIP1559)
 )
 
 // VerifyForkHashes verifies that blocks conforming to network hard-forks do have
@@ -86,6 +87,25 @@ func VerifyEIP1559BaseFee(config *params.ChainConfig, header, parent *types.Head
 	// If we are before the EIP1559 activation block the current and parent BaseFees should be nil
 	if header.BaseFee != nil || parent.BaseFee != nil {
 		return errHaveBaseFee
+	}
+	return nil
+}
+
+// VerifyEIP1559GasLimit verifies that the header.GasLimit field is valid for the current block height
+// Only call this after activation has been confirmed (config.IsEIP1559(header.Number) == true)
+func VerifyEIP1559GasLimit(config *params.ChainConfig, header *types.Header) error {
+	// If EIP1559 has been finalized then header.GasLimit should be equal to the MaxGasEIP1559 (entire limit is in EIP1559 pool)
+	if config.IsEIP1559Finalized(header.Number) {
+		if header.GasLimit != params.MaxGasEIP1559 {
+			return errInvalidEIP1559FinalizedGasLimit
+		}
+		return nil
+	}
+	// Else if we are between activation and finalization, header.GasLimit must be valid based on the decay function
+	numOfIncrements := new(big.Int).Sub(header.Number, config.EIP1559Block).Uint64()
+	expectedGasLimit := (params.MaxGasEIP1559 / 2) + (numOfIncrements * params.EIP1559GasIncrementAmount)
+	if header.GasLimit != expectedGasLimit {
+		return fmt.Errorf("invalid GasLimit: have %d, need %d", header.GasLimit, expectedGasLimit)
 	}
 	return nil
 }
