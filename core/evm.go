7eaf1055385342fd6e2bc335ac2330e4cfb530d3
Ian Norden <iannordenn@gmail.com>
2019-12-06 14:13:35 -0600

    evm needs to handle nil GasPrice; genesis needs to handle BaseFee

--- core/evm.go
+++ core/evm.go
@@ -54,7 +54,7 @@ func NewEVMContext(msg Message, header *types.Header, chain ChainContext, author
 		Time:        new(big.Int).SetUint64(header.Time),
 		Difficulty:  new(big.Int).Set(header.Difficulty),
 		GasLimit:    header.GasLimit,
-		GasPrice:    new(big.Int).Set(msg.GasPrice()),
+		GasPrice:    msg.GasPrice(),
 		BaseFee:     header.BaseFee,
 	}
 }
