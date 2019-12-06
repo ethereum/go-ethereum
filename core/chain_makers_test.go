7c11f2b41185526d7913322892f51d4d5bca6b5c
Ian Norden <iannordenn@gmail.com>
2019-12-06 16:10:00 -0600

    finish chain_maker_tests for EIP1559

--- core/chain_makers_test.go
+++ core/chain_makers_test.go
@@ -102,23 +102,15 @@ func TestGenerateChain(t *testing.T) {
 	}
 }
 
-/*
-	We need to test:
-
-	1. GenerateChain fails with EIP1559 transactions before activation
-	2. GenerateChain works with both legacy and EIP1559 transactions between activation and finalization
-	3. GenerateChain fails with legacy transactions after finalization
-
-*/
-
 // TestEIP1559 tests the changes introduced by the EIP1559 forks
 func TestEIP1559GenerateChain(t *testing.T) {
 	generateChainBeforeActivation(t)
 	generateChainDuringTransition(t)
-	//generateChainAfterFinalization(t)
+	generateChainAfterFinalization(t)
+	generateChainAfterFinalization2(t)
 }
 
-// generateChainBeforeActivation
+// generateChainBeforeActivation demonstrates that we panic if we try to make a chain with EIP1559 transactions before EIP1559 activation
 func generateChainBeforeActivation(t *testing.T) {
 	// We expect a panic due to an ErrTxIsEIP1559 error because of the panic at line 119 in chain_makers.go
 	defer func() {
@@ -187,7 +179,7 @@ func generateChainBeforeActivation(t *testing.T) {
 	}
 }
 
-// generateChainDuringTransition
+// generateChainDuringTransition demonstrates that we can make a chain with both legacy and EIP1559 transactions during the transition phase
 func generateChainDuringTransition(t *testing.T) {
 	var (
 		key1, _ = crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
@@ -264,7 +256,147 @@ func generateChainDuringTransition(t *testing.T) {
 	}
 }
 
-// generateChainAfterFinalization
+// generateChainAfterFinalization demonstrates that we panic if we try to make a chain with legacy transactions after EIP1559 finalization
 func generateChainAfterFinalization(t *testing.T) {
-	panic("implement me")
+	defer func() {
+		if err := recover().(error); err != nil {
+			if err != ErrTxNotEIP1559 {
+				t.Fatal(err)
+			}
+		}
+	}()
+	var (
+		key1, _ = crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
+		key2, _ = crypto.HexToECDSA("8a1f9a8f95be41cd7ccb6168179afb4504aefe388d1e14474d32c45c72ce7b7a")
+		key3, _ = crypto.HexToECDSA("49a7b37aa6f6645917e7b807e9d1c00d4fa71f18343b0d4122a4d2df64dd6fee")
+		addr1   = crypto.PubkeyToAddress(key1.PublicKey)
+		addr2   = crypto.PubkeyToAddress(key2.PublicKey)
+		addr3   = crypto.PubkeyToAddress(key3.PublicKey)
+		db      = rawdb.NewMemoryDatabase()
+	)
+
+	// Ensure that key1 has some funds in the genesis block.
+	gspec := &Genesis{
+		Config:  params.EIP1559FinalizedChainConfig,
+		Alloc:   GenesisAlloc{addr1: {Balance: big.NewInt(1000000)}},
+		BaseFee: new(big.Int),
+	}
+	genesis := gspec.MustCommit(db)
+
+	// This call generates a chain of 5 blocks. The function runs for
+	// each block and adds different features to gen based on the
+	// block index.
+	signer := types.HomesteadSigner{}
+	chain, _ := GenerateChain(gspec.Config, genesis, ethash.NewFaker(), db, 5, func(i int, gen *BlockGen) {
+		switch i {
+		case 0:
+			// In block 1, addr1 sends addr2 some ether.
+			tx, _ := types.SignTx(types.NewTransaction(gen.TxNonce(addr1), addr2, big.NewInt(10000), params.TxGas, new(big.Int), nil, nil, nil), signer, key1)
+			gen.AddTx(tx)
+		case 1:
+			// In block 2, addr1 sends some more ether to addr2.
+			// addr2 attempts to pass it on to addr3 using a EIP1559 transaction
+			tx1, _ := types.SignTx(types.NewTransaction(gen.TxNonce(addr1), addr2, big.NewInt(1000), params.TxGas, new(big.Int), nil, nil, nil), signer, key1)
+			tx2, _ := types.SignTx(types.NewTransaction(gen.TxNonce(addr2), addr3, big.NewInt(1000), params.TxGas, nil, nil, new(big.Int), new(big.Int)), signer, key2)
+			gen.AddTx(tx1)
+			gen.AddTx(tx2)
+		case 2:
+			// Block 3 is empty but was mined by addr3.
+			gen.SetCoinbase(addr3)
+			gen.SetExtra([]byte("yeehaw"))
+		case 3:
+			// Block 4 includes blocks 2 and 3 as uncle headers (with modified extra data).
+			b2 := gen.PrevBlock(1).Header()
+			b2.Extra = []byte("foo")
+			gen.AddUncle(b2)
+			b3 := gen.PrevBlock(2).Header()
+			b3.Extra = []byte("foo")
+			gen.AddUncle(b3)
+		}
+	})
+
+	// Import the chain. This runs all block validation rules.
+	blockchain, _ := NewBlockChain(db, nil, gspec.Config, ethash.NewFaker(), vm.Config{}, nil)
+	defer blockchain.Stop()
+
+	if i, err := blockchain.InsertChain(chain); err != nil {
+		t.Fatalf("insert error (block %d): %v\n", chain[i].NumberU64(), err)
+	}
+}
+
+// generateChainAfterFinalization2 demonstrates that we can build a chain post EIP1559 finalization with EIP1559 transactions
+func generateChainAfterFinalization2(t *testing.T) {
+	var (
+		key1, _ = crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
+		key2, _ = crypto.HexToECDSA("8a1f9a8f95be41cd7ccb6168179afb4504aefe388d1e14474d32c45c72ce7b7a")
+		key3, _ = crypto.HexToECDSA("49a7b37aa6f6645917e7b807e9d1c00d4fa71f18343b0d4122a4d2df64dd6fee")
+		addr1   = crypto.PubkeyToAddress(key1.PublicKey)
+		addr2   = crypto.PubkeyToAddress(key2.PublicKey)
+		addr3   = crypto.PubkeyToAddress(key3.PublicKey)
+		db      = rawdb.NewMemoryDatabase()
+	)
+
+	// Ensure that key1 has some funds in the genesis block.
+	gspec := &Genesis{
+		Config:  params.EIP1559FinalizedChainConfig,
+		Alloc:   GenesisAlloc{addr1: {Balance: big.NewInt(1000000)}},
+		BaseFee: new(big.Int),
+	}
+	genesis := gspec.MustCommit(db)
+
+	// This call generates a chain of 5 blocks. The function runs for
+	// each block and adds different features to gen based on the
+	// block index.
+	signer := types.HomesteadSigner{}
+	chain, _ := GenerateChain(gspec.Config, genesis, ethash.NewFaker(), db, 5, func(i int, gen *BlockGen) {
+		switch i {
+		case 0:
+			// In block 1, addr1 sends addr2 some ether.
+			tx, _ := types.SignTx(types.NewTransaction(gen.TxNonce(addr1), addr2, big.NewInt(10000), params.TxGas, nil, nil, new(big.Int), new(big.Int)), signer, key1)
+			gen.AddTx(tx)
+		case 1:
+			// In block 2, addr1 sends some more ether to addr2.
+			// addr2 attempts to pass it on to addr3 using a EIP1559 transaction
+			tx1, _ := types.SignTx(types.NewTransaction(gen.TxNonce(addr1), addr2, big.NewInt(1000), params.TxGas, nil, nil, new(big.Int), new(big.Int)), signer, key1)
+			tx2, _ := types.SignTx(types.NewTransaction(gen.TxNonce(addr2), addr3, big.NewInt(1000), params.TxGas, nil, nil, new(big.Int), new(big.Int)), signer, key2)
+			gen.AddTx(tx1)
+			gen.AddTx(tx2)
+		case 2:
+			// Block 3 is empty but was mined by addr3.
+			gen.SetCoinbase(addr3)
+			gen.SetExtra([]byte("yeehaw"))
+		case 3:
+			// Block 4 includes blocks 2 and 3 as uncle headers (with modified extra data).
+			b2 := gen.PrevBlock(1).Header()
+			b2.Extra = []byte("foo")
+			gen.AddUncle(b2)
+			b3 := gen.PrevBlock(2).Header()
+			b3.Extra = []byte("foo")
+			gen.AddUncle(b3)
+		}
+	})
+
+	// Import the chain. This runs all block validation rules.
+	blockchain, _ := NewBlockChain(db, nil, gspec.Config, ethash.NewFaker(), vm.Config{}, nil)
+	defer blockchain.Stop()
+
+	if i, err := blockchain.InsertChain(chain); err != nil {
+		t.Fatalf("insert error (block %d): %v\n", chain[i].NumberU64(), err)
+	}
+
+	state, _ := blockchain.State()
+	if blockchain.CurrentBlock().Number().Uint64() != 5 {
+		t.Fatalf("expected last block to equal %d got %d", 5, blockchain.CurrentBlock().Number().Uint64())
+	}
+	if state.GetBalance(addr1).Uint64() != 989000 {
+		t.Fatalf("expected balance of addr1 to equal %d got %d", 989000, state.GetBalance(addr1).Uint64())
+	}
+	if state.GetBalance(addr2).Uint64() != 10000 {
+		t.Fatalf("expected balance of addr2 to equal %d got %d", 10000, state.GetBalance(addr2).Uint64())
+	}
+	// This value is different than in TestGenerateChain because the test config we use has Constantinople active (uses ConstantinopleBlockReward)
+	bal, _ := new(big.Int).SetString("7875000000000001000", 10)
+	if state.GetBalance(addr3).Cmp(bal) != 0 {
+		t.Fatalf("expected balance of addr3 to equal %s got %d", "19687500000000001000", state.GetBalance(addr3).Uint64())
+	}
 }
