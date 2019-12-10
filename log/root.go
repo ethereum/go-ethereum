f2f47f95ceb99bd5889b16205aeaa0070d10c9ac
Mike Kinney <mike.kinney@gmail.com>
2019-12-09 23:22:35 -0800

    log: golint fixes

--- log/root.go
+++ log/root.go
@@ -5,7 +5,7 @@ import (
 )
 
 var (
-	root          = &logger{[]interface{}{}, new(swapHandler)}
+	root = &logger{[]interface{}{}, new(swapHandler)}
 	// StdoutHandler is the standard output handler
 	StdoutHandler = StreamHandler(os.Stdout, LogfmtFormat())
 	// StderrHandler is the standard error handler
