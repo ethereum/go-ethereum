c2fa674c44210b8196433308dddf06a3e5b8282d
Luke Williams <luke@iquidus.io>
2019-12-26 00:46:28 +0100

    Andromeda: bump version meta

--- params/version.go
+++ params/version.go
@@ -21,10 +21,10 @@ import (
 )
 
 const (
-	VersionMajor = 3         // Major version component of the current release
-	VersionMinor = 0         // Minor version component of the current release
-	VersionPatch = 0         // Patch version component of the current release
-	VersionMeta  = "testing" // Version metadata to append to the version string
+	VersionMajor = 3           // Major version component of the current release
+	VersionMinor = 0           // Minor version component of the current release
+	VersionPatch = 0           // Patch version component of the current release
+	VersionMeta  = "andromeda" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
