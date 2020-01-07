4f7b7f84ae6bbb46001424f787dba93892dfc317
me020523 <me020523@gmail.com>
2020-01-07 17:31:20 +0800

    add node.go unit test file node_test.go (#20028)
    * add node.go unit test file node_test.go
    
    * add node_test.go file license and rollback trie_test.go
    
    * fix unused variable v
    
    * trie: fix license year
    
    Co-authored-by: Péter Szilágyi <peterke@gmail.com>

---
+++ trie/node_test.go
@@ -0,0 +1,94 @@
+// Copyright 2019 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+package trie
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/rlp"
+)
+
+func newTestFullNode(v []byte) []interface{} {
+	fullNodeData := []interface{}{}
+	for i := 0; i < 16; i++ {
+		k := bytes.Repeat([]byte{byte(i + 1)}, 32)
+		fullNodeData = append(fullNodeData, k)
+	}
+	fullNodeData = append(fullNodeData, v)
+	return fullNodeData
+}
+
+func TestDecodeNestedNode(t *testing.T) {
+	fullNodeData := newTestFullNode([]byte("fullnode"))
+
+	data := [][]byte{}
+	for i := 0; i < 16; i++ {
+		data = append(data, nil)
+	}
+	data = append(data, []byte("subnode"))
+	fullNodeData[15] = data
+
+	buf := bytes.NewBuffer([]byte{})
+	rlp.Encode(buf, fullNodeData)
+
+	if _, err := decodeNode([]byte("testdecode"), buf.Bytes()); err != nil {
+		t.Fatalf("decode nested full node err: %v", err)
+	}
+}
+
+func TestDecodeFullNodeWrongSizeChild(t *testing.T) {
+	fullNodeData := newTestFullNode([]byte("wrongsizechild"))
+	fullNodeData[0] = []byte("00")
+	buf := bytes.NewBuffer([]byte{})
+	rlp.Encode(buf, fullNodeData)
+
+	_, err := decodeNode([]byte("testdecode"), buf.Bytes())
+	if _, ok := err.(*decodeError); !ok {
+		t.Fatalf("decodeNode returned wrong err: %v", err)
+	}
+}
+
+func TestDecodeFullNodeWrongNestedFullNode(t *testing.T) {
+	fullNodeData := newTestFullNode([]byte("fullnode"))
+
+	data := [][]byte{}
+	for i := 0; i < 16; i++ {
+		data = append(data, []byte("123456"))
+	}
+	data = append(data, []byte("subnode"))
+	fullNodeData[15] = data
+
+	buf := bytes.NewBuffer([]byte{})
+	rlp.Encode(buf, fullNodeData)
+
+	_, err := decodeNode([]byte("testdecode"), buf.Bytes())
+	if _, ok := err.(*decodeError); !ok {
+		t.Fatalf("decodeNode returned wrong err: %v", err)
+	}
+}
+
+func TestDecodeFullNode(t *testing.T) {
+	fullNodeData := newTestFullNode([]byte("decodefullnode"))
+	buf := bytes.NewBuffer([]byte{})
+	rlp.Encode(buf, fullNodeData)
+
+	_, err := decodeNode([]byte("testdecode"), buf.Bytes())
+	if err != nil {
+		t.Fatalf("decode full node err: %v", err)
+	}
+}
