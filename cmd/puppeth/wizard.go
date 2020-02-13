327f5702b32a23876575e9296ba692cfc71f185b
alexwang <39109351+dipingxian2@users.noreply.github.com>
2020-02-13 12:20:45 +0800

    Update wizard.go

--- cmd/puppeth/wizard.go
+++ cmd/puppeth/wizard.go
@@ -76,7 +76,7 @@ type wizard struct {
 	servers  map[string]*sshClient // SSH connections to servers to administer
 	services map[string][]string   // Ethereum services known to be running on servers
 
-	in   *bufio.Reader // Wrapper around stdin to allow reading user input
+	in	*bufio.Reader // Wrapper around stdin to allow reading user input
 	lock sync.Mutex    // Lock to protect configs during concurrent service discovery
 	consensusType	string
 	blocksTime	uint64
