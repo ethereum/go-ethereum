e69e12f1c684b0543d465e9e44f378d8aa5b7fe2
Felix Lange <fjl@twurst.com>
2020-02-17 11:52:08 +0100

    p2p: remove MeteredPeerEvent
    This event was added for the dashboard, but we don't need it anymore
    since the dashboard is gone.

--- p2p/server.go
+++ p2p/server.go
@@ -755,9 +755,6 @@ running:
 				peers[c.node.ID()] = p
 				srv.log.Debug("Adding p2p peer", "peercount", len(peers), "id", p.ID(), "conn", c.flags, "addr", p.RemoteAddr(), "name", truncateName(c.name))
 				srv.dialsched.peerAdded(c)
-				if conn, ok := c.fd.(*meteredConn); ok {
-					conn.handshakeDone(p)
-				}
 				if p.Inbound() {
 					inboundCount++
 				}
