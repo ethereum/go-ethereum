f1a7997af38bb992d1a75c49ceb952117d3548ec
Adam Schmideg <adamschmideg@users.noreply.github.com>
2020-02-27 12:59:00 +0100

    crypto/bn256: fix import line (#20723)

--- crypto/bn256/bn256_slow.go
+++ crypto/bn256/bn256_slow.go
@@ -7,7 +7,7 @@
 // Package bn256 implements the Optimal Ate pairing over a 256-bit Barreto-Naehrig curve.
 package bn256
 
-import "github.com/ethereum/go-ethereum/crypto/bn256/google"
+import bn256 "github.com/ethereum/go-ethereum/crypto/bn256/google"
 
 // G1 is an abstract cyclic group. The zero value is suitable for use as the
 // output of an operation, but cannot be used as an input.
