5da0694ede6f16e7ca22fd60986cce207ca0c400
BurtonQin <bobbqqin@gmail.com>
2020-03-24 09:10:21 +0800

    Add Unlock before Fatal

--- common/prque/lazyqueue_test.go
+++ common/prque/lazyqueue_test.go
@@ -106,6 +106,7 @@ func TestLazyQueue(t *testing.T) {
 			if p.p != maxPri {
 				stop := make(chan struct{})
 				stopCh <- stop // No need to wait for `<-stop`. test failed anyway
+				lock.Unlock()
 				t.Fatalf("incorrect item (best known priority %d, popped %d)", maxPri, p.p)
 			}
 			q.Push(p)
