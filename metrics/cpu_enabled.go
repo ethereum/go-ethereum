0b6eabf0dd339d5e1aaf7028870ad55597413dbc
Bogdan Habic <habicbogdan@gmail.com>
2020-03-27 21:48:54 +0100

    metrics: disable CPU stats (gosigar) on Darwing when nocgo is passed.

--- metrics/cpu_enabled.go
+++ metrics/cpu_enabled.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-// +build !ios
+// +build darwin,cgo dragonfly freebsd linux nacl netbsd openbsd solaris windows
 
 package metrics
 
