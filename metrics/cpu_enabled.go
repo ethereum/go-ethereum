5bd80cba092758ff3aeaab7dbdb1b48138a9a57b
Bogdan Habic <habicbogdan@gmail.com>
2020-03-28 10:30:35 +0100

    metrics: enable CPU stats (gosigar) on arm64.

--- metrics/cpu_enabled.go
+++ metrics/cpu_enabled.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-// +build darwin,cgo dragonfly freebsd linux nacl netbsd openbsd solaris windows
+// +build darwin,cgo dragonfly freebsd linux nacl netbsd openbsd solaris windows arm64
 
 package metrics
 
