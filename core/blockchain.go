76eed9e50d65e014acf0828588ed8f6ce1777f8d
Martin Holst Swende <martin@swende.se>
2020-03-31 10:25:41 +0200

    snapshotter/tests: verify snapdb post-state against trie (#20812)
    * core/state/snapshot: basic trie-to-hash implementation
    
    * tests: validate snapshot after test
    
    * core/state/snapshot: fix review concerns

--- core/blockchain.go
+++ core/blockchain.go
@@ -520,6 +520,15 @@ func (bc *BlockChain) CurrentBlock() *types.Block {
 	return bc.currentBlock.Load().(*types.Block)
 }
 
+// Snapshot returns the blockchain snapshot tree. This method is mainly used for
+// testing, to make it possible to verify the snapshot after execution.
+//
+// Warning: There are no guarantees about the safety of using the returned 'snap' if the
+// blockchain is simultaneously importing blocks, so take care.
+func (bc *BlockChain) Snapshot() *snapshot.Tree {
+	return bc.snaps
+}
+
 // CurrentFastBlock retrieves the current fast-sync head block of the canonical
 // chain. The block is retrieved from the blockchain's internal cache.
 func (bc *BlockChain) CurrentFastBlock() *types.Block {
