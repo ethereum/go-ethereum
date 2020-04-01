b482bebec3c8bacacd78caa6ad5125a1e9671496
q9f <58883403+q9f@users.noreply.github.com>
2020-04-01 17:03:52 +0200

    mobile: allow for returning the goerli spec

--- mobile/geth.go
+++ mobile/geth.go
@@ -146,9 +146,9 @@ func NewNode(datadir string, config *NodeConfig) (stack *Node, _ error) {
 		if err := json.Unmarshal([]byte(config.EthereumGenesis), genesis); err != nil {
 			return nil, fmt.Errorf("invalid genesis spec: %v", err)
 		}
-		// If we have the testnet, hard code the chain configs too
-		if config.EthereumGenesis == TestnetGenesis() {
-			genesis.Config = params.TestnetChainConfig
+		// If we have the Ropsten testnet, hard code the chain configs too
+		if config.EthereumGenesis == RopstenGenesis() {
+			genesis.Config = params.RopstenChainConfig
 			if config.EthereumNetworkID == 1 {
 				config.EthereumNetworkID = 3
 			}
