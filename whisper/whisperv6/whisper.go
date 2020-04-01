1e2e1b41f8cf0c51f53dd064a4caa75faee14065
ucwong <ucwong@126.com>
2020-04-01 22:12:01 +0800

    cmd/devp2p, cmd/wnode, whisper: add missing calls to Timer.Stop (#20843)

--- whisper/whisperv6/whisper.go
+++ whisper/whisperv6/whisper.go
@@ -905,6 +905,7 @@ func (whisper *Whisper) update() {
 	defer whisper.wg.Done()
 	// Start a ticker to check for expirations
 	expire := time.NewTicker(expirationCycle)
+	defer expire.Stop()
 
 	// Repeat updates until termination is requested
 	for {
