6b302362999aa95221cfd25e5bb096d5b099f8d5
ucwong <ucwong@126.com>
2020-04-04 08:28:03 +0000

    whisper/mailserver : common/errors for dbfile

--- whisper/mailserver/mailserver.go
+++ whisper/mailserver/mailserver.go
@@ -27,6 +27,7 @@ import (
 	"github.com/ethereum/go-ethereum/rlp"
 	whisper "github.com/ethereum/go-ethereum/whisper/whisperv6"
 	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/errors"
 	"github.com/syndtr/goleveldb/leveldb/opt"
 	"github.com/syndtr/goleveldb/leveldb/util"
 )
