7161010f9561b8bffbaaf63f396de8faa042aeef
Martin Holst Swende <martin@swende.se>
2020-04-22 15:08:40 +0200

    cmd/utils: group flags by deprecation date + reorder by date,

--- cmd/utils/flags_legacy.go
+++ cmd/utils/flags_legacy.go
@@ -43,99 +43,104 @@ var ShowDeprecated = cli.Command{
 }
 
 var (
-	LegacyTestnetFlag = cli.BoolFlag{ // (Deprecated April 2020) TODO(q9f): Remove after Ropsten is discontinued.
-		Name:  "testnet",
-		Usage: "Pre-configured test network (Deprecated: Please choose one of --goerli, --rinkeby, or --ropsten.)",
-	}
-	LegacyLightServFlag = cli.IntFlag{ // (Deprecated June 2019) Deprecated in favor of light.serve, remove in 2021
-		Name:  "lightserv",
-		Usage: "Maximum percentage of time allowed for serving LES requests (deprecated, use --light.serve)",
-		Value: eth.DefaultConfig.LightServ,
-	}
-	LegacyLightPeersFlag = cli.IntFlag{ // (Deprecated June 2019) Deprecated in favor of light.maxpeers, remove in 2021
-		Name:  "lightpeers",
-		Usage: "Maximum number of light clients to serve, or light servers to attach to  (deprecated, use --light.maxpeers)",
-		Value: eth.DefaultConfig.LightPeers,
-	}
-	LegacyMinerThreadsFlag = cli.IntFlag{ // (Deprecated April 2018)
+	// (Deprecated April 2018)
+	LegacyMinerThreadsFlag = cli.IntFlag{
 		Name:  "minerthreads",
 		Usage: "Number of CPU threads to use for mining (deprecated, use --miner.threads)",
 		Value: 0,
 	}
-	LegacyMinerGasTargetFlag = cli.Uint64Flag{ // (Deprecated April 2018)
+	LegacyMinerGasTargetFlag = cli.Uint64Flag{
 		Name:  "targetgaslimit",
 		Usage: "Target gas floor for mined blocks (deprecated, use --miner.gastarget)",
 		Value: eth.DefaultConfig.Miner.GasFloor,
 	}
-	LegacyMinerGasPriceFlag = BigFlag{ // (Deprecated April 2018)
+	LegacyMinerGasPriceFlag = BigFlag{
 		Name:  "gasprice",
 		Usage: "Minimum gas price for mining a transaction (deprecated, use --miner.gasprice)",
 		Value: eth.DefaultConfig.Miner.GasPrice,
 	}
-	LegacyMinerEtherbaseFlag = cli.StringFlag{ // (Deprecated April 2018)
+	LegacyMinerEtherbaseFlag = cli.StringFlag{
 		Name:  "etherbase",
 		Usage: "Public address for block mining rewards (default = first account, deprecated, use --miner.etherbase)",
 		Value: "0",
 	}
-	LegacyMinerExtraDataFlag = cli.StringFlag{ // (Deprecated April 2018)
+	LegacyMinerExtraDataFlag = cli.StringFlag{
 		Name:  "extradata",
 		Usage: "Block extra data set by the miner (default = client version, deprecated, use --miner.extradata)",
 	}
-	LegacyRPCEnabledFlag = cli.BoolFlag{ // (Deprecated April 2020)
+
+	// (Deprecated June 2019)
+	LegacyLightServFlag = cli.IntFlag{
+		Name:  "lightserv",
+		Usage: "Maximum percentage of time allowed for serving LES requests (deprecated, use --light.serve)",
+		Value: eth.DefaultConfig.LightServ,
+	}
+	LegacyLightPeersFlag = cli.IntFlag{
+		Name:  "lightpeers",
+		Usage: "Maximum number of light clients to serve, or light servers to attach to  (deprecated, use --light.maxpeers)",
+		Value: eth.DefaultConfig.LightPeers,
+	}
+
+	// (Deprecated April 2020)
+	LegacyTestnetFlag = cli.BoolFlag{ // TODO(q9f): Remove after Ropsten is discontinued.
+		Name:  "testnet",
+		Usage: "Pre-configured test network (Deprecated: Please choose one of --goerli, --rinkeby, or --ropsten.)",
+	}
+	LegacyRPCEnabledFlag = cli.BoolFlag{
 		Name:  "rpc",
 		Usage: "Enable the HTTP-RPC server (deprecated, use --http)",
 	}
-	LegacyRPCListenAddrFlag = cli.StringFlag{ // (Deprecated April 2020)
+	LegacyRPCListenAddrFlag = cli.StringFlag{
 		Name:  "rpcaddr",
 		Usage: "HTTP-RPC server listening interface (deprecated, use --http.addr)",
 		Value: node.DefaultHTTPHost,
 	}
-	LegacyRPCPortFlag = cli.IntFlag{ // (Deprecated April 2020)
+	LegacyRPCPortFlag = cli.IntFlag{
 		Name:  "rpcport",
 		Usage: "HTTP-RPC server listening port (deprecated, use --http.port)",
 		Value: node.DefaultHTTPPort,
 	}
-	LegacyRPCCORSDomainFlag = cli.StringFlag{ // (Deprecated April 2020)
+	LegacyRPCCORSDomainFlag = cli.StringFlag{
 		Name:  "rpccorsdomain",
 		Usage: "Comma separated list of domains from which to accept cross origin requests (browser enforced) (deprecated, use --http.corsdomain)",
 		Value: "",
 	}
-	LegacyRPCVirtualHostsFlag = cli.StringFlag{ // (Deprecated April 2020)
+	LegacyRPCVirtualHostsFlag = cli.StringFlag{
 		Name:  "rpcvhosts",
 		Usage: "Comma separated list of virtual hostnames from which to accept requests (server enforced). Accepts '*' wildcard. (deprecated, use --http.vhosts)",
 		Value: strings.Join(node.DefaultConfig.HTTPVirtualHosts, ","),
 	}
-	LegacyRPCApiFlag = cli.StringFlag{ // (Deprecated April 2020)
+	LegacyRPCApiFlag = cli.StringFlag{
 		Name:  "rpcapi",
 		Usage: "API's offered over the HTTP-RPC interface (deprecated, use --http.api)",
 		Value: "",
 	}
-	LegacyWSListenAddrFlag = cli.StringFlag{ // (Deprecated April 2020)
+	LegacyWSListenAddrFlag = cli.StringFlag{
 		Name:  "wsaddr",
 		Usage: "WS-RPC server listening interface (deprecated, use --ws.addr)",
 		Value: node.DefaultWSHost,
 	}
-	LegacyWSPortFlag = cli.IntFlag{ // (Deprecated April 2020)
+	LegacyWSPortFlag = cli.IntFlag{
 		Name:  "wsport",
 		Usage: "WS-RPC server listening port (deprecated, use --ws.port)",
 		Value: node.DefaultWSPort,
 	}
-	LegacyWSApiFlag = cli.StringFlag{ // (Deprecated April 2020)
+	LegacyWSApiFlag = cli.StringFlag{
 		Name:  "wsapi",
 		Usage: "API's offered over the WS-RPC interface (deprecated, use --ws.api)",
 		Value: "",
 	}
-	LegacyWSAllowedOriginsFlag = cli.StringFlag{ // (Deprecated April 2020)
+	LegacyWSAllowedOriginsFlag = cli.StringFlag{
 		Name:  "wsorigins",
 		Usage: "Origins from which to accept websockets requests (deprecated, use --ws.origins)",
 		Value: "",
 	}
-	LegacyGpoBlocksFlag = cli.IntFlag{ // (Deprecated April 2020)
+	LegacyGpoBlocksFlag = cli.IntFlag{
 		Name:  "gpoblocks",
 		Usage: "Number of recent blocks to check for gas prices (deprecated, use --gpo.blocks)",
 		Value: eth.DefaultConfig.GPO.Blocks,
 	}
-	LegacyGpoPercentileFlag = cli.IntFlag{ // (Deprecated April 2020)
+	LegacyGpoPercentileFlag = cli.IntFlag{
 		Name:  "gpopercentile",
 		Usage: "Suggested gas price is the given percentile of a set of recent transaction gas prices (deprecated, use --gpo.percentile)",
 		Value: eth.DefaultConfig.GPO.Percentile,
