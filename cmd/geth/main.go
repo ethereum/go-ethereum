c69c3756d774720574d359ca1c6a7230b478282a
renaynay <41963722+renaynay@users.noreply.github.com>
2020-04-22 19:45:17 +0200

    making sure deprecated flags are still functional

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -247,6 +247,7 @@ func init() {
 	app.Flags = append(app.Flags, rpcFlags...)
 	app.Flags = append(app.Flags, consoleFlags...)
 	app.Flags = append(app.Flags, debug.Flags...)
+	app.Flags = append(app.Flags, debug.DeprecatedFlags...)
 	app.Flags = append(app.Flags, whisperFlags...)
 	app.Flags = append(app.Flags, metricsFlags...)
 
