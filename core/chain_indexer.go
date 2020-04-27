c82c55450871c56ed629ec7c172c568818f22481
Felix Lange <fjl@twurst.com>
2020-04-27 11:19:30 +0200

    core: panic for impossible child indexer relationship

--- core/chain_indexer.go
+++ core/chain_indexer.go
@@ -439,8 +439,8 @@ func (c *ChainIndexer) Sections() (uint64, uint64, common.Hash) {
 
 // AddChildIndexer adds a child ChainIndexer that can use the output of this one
 func (c *ChainIndexer) AddChildIndexer(indexer *ChainIndexer) {
-	if c == indexer {
-		return
+	if indexer == c {
+		panic("can't add indexer as a child of itself")
 	}
 	c.lock.Lock()
 	defer c.lock.Unlock()
