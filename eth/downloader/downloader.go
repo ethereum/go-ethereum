b0bbd4718511a8b2575e5feb8cbd54beb9675973
Péter Szilágyi <peterke@gmail.com>
2020-04-27 11:22:15 +0300

    eth: fix shutdown regression to abort downloads, not just cancel

--- eth/downloader/downloader.go
+++ eth/downloader/downloader.go
@@ -557,6 +557,8 @@ func (d *Downloader) spawnSync(fetchers []func() error) error {
 func (d *Downloader) cancel() {
 	// Close the current cancel channel
 	d.cancelLock.Lock()
+	defer d.cancelLock.Unlock()
+
 	if d.cancelCh != nil {
 		select {
 		case <-d.cancelCh:
@@ -565,7 +567,6 @@ func (d *Downloader) cancel() {
 			close(d.cancelCh)
 		}
 	}
-	d.cancelLock.Unlock()
 }
 
 // Cancel aborts all of the operations and waits for all download goroutines to
