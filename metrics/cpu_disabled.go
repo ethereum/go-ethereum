486623d69b1c1b5b4678cc0d63e47bf65e853a1b
ucwong <ucwong@126.com>
2020-05-23 03:16:03 +0000

    metrics | ReadCPUStats redeclared in this block (compile)

--- metrics/cpu_disabled.go
+++ metrics/cpu_disabled.go
@@ -20,4 +20,3 @@ package metrics
 
 // ReadCPUStats retrieves the current CPU stats. Internally this uses `gosigar`,
 // which is not supported on the platforms in this file.
-func ReadCPUStats(stats *CPUStats) {}
