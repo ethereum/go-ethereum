4873a9d3c3f78a4346228fcba18257634408d906
Felix Lange <fjl@twurst.com>
2020-05-26 13:24:22 +0200

    build: upgrade to golangci lint v1.27.0 (#21127)
    * build: upgrade to golangci-lint v1.27.0
    
    * build: raise lint timeout to 3 minutes

--- build/ci.go
+++ build/ci.go
@@ -356,7 +356,7 @@ func doLint(cmdline []string) {
 
 // downloadLinter downloads and unpacks golangci-lint.
 func downloadLinter(cachedir string) string {
-	const version = "1.24.0"
+	const version = "1.27.0"
 
 	csdb := build.MustLoadChecksums("build/checksums.txt")
 	base := fmt.Sprintf("golangci-lint-%s-%s-%s", version, runtime.GOOS, runtime.GOARCH)
