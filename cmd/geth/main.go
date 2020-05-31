a0bda32e8dcfa5135e53ff8a1d469cd4a65b6bed
renaynay <41963722+renaynay@users.noreply.github.com>
2020-05-31 17:33:17 +0200

    switching back from using renaynay fork to using upstream as PRs were merged on upstream

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -27,7 +27,7 @@ import (
 	"strings"
 	"time"
 
-	gopsutil "github.com/renaynay/gopsutil/mem"
+	gopsutil "github.com/shirou/gopsutil/mem"
 
 	"github.com/ethereum/go-ethereum/accounts"
 	"github.com/ethereum/go-ethereum/accounts/keystore"
