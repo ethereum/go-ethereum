7533ef7840bd7e33e0e6573547cd0fb98f7ef5ae
Peter Pratscher <ppratscher@users.noreply.github.com>
2020-06-16 10:52:49 +0200

    cmd/geth: fix redundant comment

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -338,7 +338,7 @@ func prepare(ctx *cli.Context) {
 	go metrics.CollectProcessMetrics(3 * time.Second)
 }
 
-// geth is the main entry point into the system if no special subcommand is ran.ipcdisable
+// geth is the main entry point into the system if no special subcommand is ran
 // It creates a default node based on the command line arguments and runs it in
 // blocking mode, waiting for it to be shut down.
 func geth(ctx *cli.Context) error {
