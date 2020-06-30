e4eafeb271f99d32a917342612a24911d4fa0f13
ucwong <ucwong@126.com>
2020-06-30 10:04:09 +0800

    p2p/peer:Fix panic when add a p2p service without func Run

--- p2p/peer.go
+++ p2p/peer.go
@@ -367,6 +367,10 @@ func (p *Peer) startProtocols(writeStart <-chan struct{}, writeErr chan<- error)
 		p.log.Trace(fmt.Sprintf("Starting protocol %s/%d", proto.Name, proto.Version))
 		go func() {
 			defer p.wg.Done()
+			if proto.Run == nil {
+				return
+			}
+
 			err := proto.Run(p, rw)
 			if err == nil {
 				p.log.Trace(fmt.Sprintf("Protocol %s/%d returned", proto.Name, proto.Version))
