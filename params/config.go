85ebea9e8c3188343d504dd0827c40d17bae1709
fudongbai <296179868@qq.com>
2020-08-09 12:52:10 +0800

    fix hex code of new smart contract

--- params/config.go
+++ params/config.go
@@ -242,7 +242,7 @@ var (
 		PetersburgBlock:     big.NewInt(0),
 		IstanbulBlock:       big.NewInt(0),
 		MuirGlacierBlock:    big.NewInt(0),
-		RamanujanBlock:      big.NewInt(260942),
+		RamanujanBlock:      big.NewInt(317974),
 		Parlia: &ParliaConfig{
 			Period: 3,
 			Epoch:  200,
