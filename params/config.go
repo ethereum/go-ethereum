45f17d59a0aa4e33653f7f71f85cd03a7576b2af
fudongbai <296179868@qq.com>
2020-08-11 16:25:01 +0800

    update chapel ramanujan fork

--- params/config.go
+++ params/config.go
@@ -224,7 +224,7 @@ var (
 		PetersburgBlock:     big.NewInt(0),
 		IstanbulBlock:       big.NewInt(0),
 		MuirGlacierBlock:    big.NewInt(0),
-		RamanujanBlock:      big.NewInt(1066095),
+		RamanujanBlock:      big.NewInt(1010000),
 		Parlia: &ParliaConfig{
 			Period: 3,
 			Epoch:  200,
