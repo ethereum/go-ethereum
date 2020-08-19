5172c0ce6b9b4f6c70cdddba2dc4def5199c7afb
Péter Szilágyi <peterke@gmail.com>
2020-08-19 10:17:48 +0300

    core: remove commented out legacy code

--- core/blockchain.go
+++ core/blockchain.go
@@ -284,38 +284,7 @@ func NewBlockChain(db ethdb.Database, cacheConfig *CacheConfig, chainConfig *par
 	// the head block (ethash cache or clique voting snapshot). Might as well do
 	// it in advance.
 	bc.engine.VerifyHeader(bc, bc.CurrentHeader(), true)
-	/*
-		// Ensure that the head block is above the freezer limit
-		if frozen, err := bc.db.Ancients(); err == nil && frozen > 0 {
-			var (
-				needRewind bool
-				low        uint64
-			)
-			// The head full block may be rolled back to a very low height due to
-			// blockchain repair. If the head full block is even lower than the ancient
-			// chain, truncate the ancient store.
-			fullBlock := bc.CurrentBlock()
-			if fullBlock != nil && fullBlock != bc.genesisBlock && fullBlock.NumberU64() < frozen-1 {
-				needRewind = true
-				low = fullBlock.NumberU64()
-			}
-			// In fast sync, it may happen that ancient data has been written to the
-			// ancient store, but the LastFastBlock has not been updated, truncate the
-			// extra data here.
-			fastBlock := bc.CurrentFastBlock()
-			if fastBlock != nil && fastBlock.NumberU64() < frozen-1 {
-				needRewind = true
-				if fastBlock.NumberU64() < low || low == 0 {
-					low = fastBlock.NumberU64()
-				}
-			}
-			if needRewind {
-				log.Warn("Truncating ancient chain", "from", bc.CurrentHeader().Number.Uint64(), "to", low)
-				if err := bc.SetHead(low); err != nil {
-					return nil, err
-				}
-			}
-		}*/
+
 	// Check the current state of the block hashes and make sure that we do not have any of the bad blocks in our chain
 	for hash := range BadHashes {
 		if header := bc.GetHeaderByHash(hash); header != nil {
