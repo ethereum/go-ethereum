73d0f52af2ed0414ebfd04ff43800d134f0bb294
Felix Lange <fjl@twurst.com>
2020-08-24 12:13:42 +0200

    p2p/discover: improve comment in UDPv4.findnode

--- p2p/discover/v4_udp.go
+++ p2p/discover/v4_udp.go
@@ -324,10 +324,11 @@ func (t *UDPv4) findnode(toid enode.ID, toaddr *net.UDPAddr, target v4wire.Pubke
 		Target:     target,
 		Expiration: uint64(time.Now().Add(expiration).Unix()),
 	})
-	// Ensure that callers don't see a timeout if the node actually responded.
-	// Since findnode can receive more than one neighbors response, the reply
-	// matcher will be active until the remote node sends enough nodes. If the
-	// remote end doesn't have enough nodes to send, we'll see a timeout.
+	// Ensure that callers don't see a timeout if the node actually responded. Since
+	// findnode can receive more than one neighbors response, the reply matcher will be
+	// active until the remote node sends enough nodes. If the remote end doesn't have
+	// enough nodes the reply matcher will time out waiting for the second reply, but
+	// there's no need for an error in that case.
 	err := <-rm.errc
 	if err == errTimeout && rm.reply != nil {
 		err = nil
