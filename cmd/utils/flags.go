fc6bd7cdc4097171d90086f2c29afa872a5d7735
Marius van der Wijden <m.vanderwijden@live.de>
2020-09-14 15:29:28 +0200

    cmd/utils: shorter description
    Co-authored-by: Martin Holst Swende <martin@swende.se>

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -128,7 +128,7 @@ var (
 	}
 	NetworkIdFlag = cli.Uint64Flag{
 		Name:  "networkid",
-		Usage: "Network identifier (integer, 1=Frontier, 3=Ropsten, 4=Rinkeby, 5=Görli), (for testnets: use --goerli, --ropsten, --rinkeby instead)",
+		Usage: "Explicitly set network id (integer)(For testnets: use --goerli, --ropsten, --rinkeby instead)",
 		Value: eth.DefaultConfig.NetworkId,
 	}
 	GoerliFlag = cli.BoolFlag{
