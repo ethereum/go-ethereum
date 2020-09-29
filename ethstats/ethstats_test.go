05df0e43d855f1ef65757ca0b85512d8321a2394
meows <b5c6@protonmail.com>
2020-09-29 13:03:31 -0500

    ethstats: fix ethstats url parsing to handle values with special chars
    Signed-off-by: meows <b5c6@protonmail.com>

--- ethstats/ethstats_test.go
+++ ethstats/ethstats_test.go
@@ -6,27 +6,49 @@ import (
 )
 
 func TestParseEthstatsURL(t *testing.T) {
-	cases := []struct{
-		url string
+	cases := []struct {
+		url              string
 		node, pass, host string
 	}{
 		{
-			url: `"debug meowsbits":mypass@ws://mordor.dash.fault.dev:3000`,
+			url:  `"debug meowsbits":mypass@ws://mordor.dash.fault.dev:3000`,
 			node: "debug meowsbits", pass: "mypass", host: "ws://mordor.dash.fault.dev:3000",
 		},
 		{
-			url: `"debug @meowsbits":mypass@ws://mordor.dash.fault.dev:3000`,
+			url:  `"debug @meowsbits":mypass@ws://mordor.dash.fault.dev:3000`,
 			node: "debug @meowsbits", pass: "mypass", host: "ws://mordor.dash.fault.dev:3000",
+		},
+		{
+			url:  `"debug: @meowsbits":mypass@ws://mordor.dash.fault.dev:3000`,
+			node: "debug: @meowsbits", pass: "mypass", host: "ws://mordor.dash.fault.dev:3000",
+		},
+		{
+			url:  `name:@ws://mordor.dash.fault.dev:3000`,
+			node: "name", pass: "", host: "ws://mordor.dash.fault.dev:3000",
+		},
+		{
+			url:  `:mypass@ws://mordor.dash.fault.dev:3000`,
+			node: "", pass: "mypass", host: "ws://mordor.dash.fault.dev:3000",
+		},
+		{
+			url:  `:@ws://mordor.dash.fault.dev:3000`,
+			node: "", pass: "", host: "ws://mordor.dash.fault.dev:3000",
 		},
 	}
 
 	for i, c := range cases {
 		parts, err := parseEthstatsURL(c.url)
 		if err != nil {
-			t.Error(err)
+			t.Fatal(err)
 		}
-		node, pass, host := parts[1], parts[3], parts[4]
-		node, _ = strconv.Unquote(node)
+		node, pass, host := parts[0], parts[1], parts[2]
+
+		// unquote because the value provided will be used as a CLI flag value, so unescaped quotes will be removed
+		nodeUnquote, err := strconv.Unquote(node)
+		if err == nil {
+			node = nodeUnquote
+		}
+
 		if node != c.node {
 			t.Errorf("case=%d mismatch node value, got: %v ,want: %v", i, node, c.node)
 		}
