2d7fd86a1e4f1327b3e3e23008462a177e6e1127
meows <b5c6@protonmail.com>
2020-10-02 16:28:20 -0500

    miner: remove ineff assign (lint)
    Signed-off-by: meows <b5c6@protonmail.com>

--- miner/miner.go
+++ miner/miner.go
@@ -106,7 +106,6 @@ func (miner *Miner) update() {
 					log.Info("Mining aborted due to sync")
 				}
 			case downloader.DoneEvent, downloader.FailedEvent:
-				canStart = true
 				if shouldStart {
 					miner.SetEtherbase(miner.coinbase)
 					miner.worker.start()
