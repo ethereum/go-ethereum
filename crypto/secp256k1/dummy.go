c068e4c778416a019683018f5484cb7ef8d95fca
Steve Ruckdashel <steven.ruckdashel@optum.com>
2020-10-21 13:18:14 -0500

    fixing up comments

--- crypto/secp256k1/dummy.go
+++ crypto/secp256k1/dummy.go
@@ -1,9 +1,16 @@
 // +build dummy
 
-// Package c contains only a C file.
+// This file is part of a workaround for `go mod vendor` which won't vendor
+// C files if there's no Go file in the same directory.
+// This would prevent the crypto/secp256k1/libsecp256k1/include/secp256k1.h file to be vendored.
 //
-// This Go file is part of a workaround for `go mod vendor`.
-// Please see the file dummy.go at the root of the module for more information.
+// This Go file imports the c directory where there is another dummy.go file which
+// is the second part of this workaround.
+//
+// These two files combined make it so `go mod vendor` behaves correctly.
+//
+// See this issue for reference: https://github.com/golang/go/issues/26366
+
 package secp256k1
 
 import (
