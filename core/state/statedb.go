b573180517b9d95cd34def81a35027ef9fc2db2a
rjl493456442 <garyrong0905@gmail.com>
2020-10-27 11:10:15 +0800

    core/state: address comment

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -874,7 +874,7 @@ func (s *StateDB) Commit(deleteEmptyObjects bool) (common.Hash, error) {
 			// - head-1 layer is paired with HEAD-1 state
 			// - head-127 layer(bottom-most diff layer) is paired with HEAD-127 state
 			if err := s.snaps.Cap(root, 128); err != nil {
-				log.Warn("Failed to cap snapshot tree", "root", root, "layers", 127, "err", err)
+				log.Warn("Failed to cap snapshot tree", "root", root, "layers", 128, "err", err)
 			}
 		}
 		s.snap, s.snapDestructs, s.snapAccounts, s.snapStorage = nil, nil, nil, nil
