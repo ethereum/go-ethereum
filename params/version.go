68aa1dea5080153a60a70388494a45ffcdeccb83
Ian Norden <iannordenn@gmail.com>
2020-11-13 15:11:25 -0600

    bump version meta; update github actions workflows

--- params/version.go
+++ params/version.go
@@ -24,7 +24,7 @@ const (
 	VersionMajor = 1        // Major version component of the current release
 	VersionMinor = 9        // Minor version component of the current release
 	VersionPatch = 24       // Patch version component of the current release
-	VersionMeta  = "stable" // Version metadata to append to the version string
+	VersionMeta  = "statediff-0.0.11" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
