ba0ae984a76d2634d7a1971dd4101a7313f873ba
Marius van der Wijden <m.vanderwijden@live.de>
2020-12-11 12:52:10 +0100

    cmd/geth: fixed account import parallel test

--- cmd/geth/accountcmd_test.go
+++ cmd/geth/accountcmd_test.go
@@ -102,6 +102,7 @@ func TestAccountImport(t *testing.T) {
 		},
 	}
 	for _, test := range tests {
+		test := test
 		t.Run(test.name, func(t *testing.T) {
 			t.Parallel()
 			importAccountWithExpect(t, test.key, test.output)
