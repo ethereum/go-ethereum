54ca3dd84de9856192ffe05cdbb10a14b11bc791
Martin Holst Swende <martin@swende.se>
2020-12-16 09:21:31 +0100

    crypto/fixed: update to latest upstream

--- crypto/fixed/keccakf.go
+++ crypto/fixed/keccakf.go
@@ -1,10 +1,9 @@
 // This file is copied from golang/x/crypto/sha3/keccakf.go
-
 // Copyright 2014 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//  +build !amd64 appengine gccgo
+//  +build !amd64 purego !gc
 
 package fixed
 
--- crypto/fixed/keccakf_amd64.go
+++ crypto/fixed/keccakf_amd64.go
@@ -4,7 +4,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build amd64,!appengine,!gccgo
+// +build amd64,!purego,gc
 
 package fixed
 
