bc805e225db381beb0a1c6be862dd9e8a647542d
Drygin <DryginAlexander@gmail.com>
2021-01-10 23:35:18 +0300

    eth: increase default max gas price to 1000 GWei

--- eth/gasprice/gasprice.go
+++ eth/gasprice/gasprice.go
@@ -31,7 +31,7 @@ import (
 
 const sampleNumber = 3 // Number of transactions sampled in a block
 
-var DefaultMaxPrice = big.NewInt(500 * params.GWei)
+var DefaultMaxPrice = big.NewInt(1000 * params.GWei)
 
 type Config struct {
 	Blocks     int
