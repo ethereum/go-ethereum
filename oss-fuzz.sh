e3063d04e29f3f37b4667bc696a92c9f85c3b287
Marius van der Wijden <m.vanderwijden@live.de>
2021-01-20 18:06:29 +0100

    oss-fuzz: fix abi fuzzer

--- oss-fuzz.sh
+++ oss-fuzz.sh
@@ -100,7 +100,7 @@ compile_fuzzer tests/fuzzers/rlp        Fuzz fuzzRlp
 compile_fuzzer tests/fuzzers/trie       Fuzz fuzzTrie
 compile_fuzzer tests/fuzzers/stacktrie  Fuzz fuzzStackTrie
 compile_fuzzer tests/fuzzers/difficulty Fuzz fuzzDifficulty
-compile_fuzzertests/fuzzers/abi         Fuzz fuzzAbi
+compile_fuzzer tests/fuzzers/abi        Fuzz fuzzAbi
 
 compile_fuzzer tests/fuzzers/bls12381  FuzzG1Add fuzz_g1_add
 compile_fuzzer tests/fuzzers/bls12381  FuzzG1Mul fuzz_g1_mul
