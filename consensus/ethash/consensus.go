db3811d5c0ea0e7ec860c468be4ffb37fc2de3d5
lightclient@protonmail.com <lightclient@protonmail.com>
2021-01-23 00:16:51 +0100

    params,consensus: add test fork Aleut

--- consensus/ethash/consensus.go
+++ consensus/ethash/consensus.go
@@ -277,7 +277,7 @@ func (ethash *Ethash) verifyHeader(chain consensus.ChainHeaderReader, header, pa
 	}
 
 	// Verify the block's gas usage and (if applicable) verify the base fee.
-	if chain.Config().IsYoloV2(header.Number) {
+	if chain.Config().IsAleut(header.Number) {
 		if err := misc.VerifyEip1559Header(parent, header); err != nil {
 			return err
 		}
