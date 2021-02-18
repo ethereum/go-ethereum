d73a0eaf3f9c903ffd344fd999e50b59c7e3be2a
Marius van der Wijden <m.vanderwijden@live.de>
2021-02-18 12:19:34 +0100

    typo

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -595,7 +595,7 @@ var (
 	}
 	AllowUnprotectedTxs = cli.BoolFlag{
 		Name:  "rpc.allow-unprotected-txs",
-		Usage: "Allow for unprotected (non EIP155 signed transactions to be submitted via RPC",
+		Usage: "Allow for unprotected (non EIP155 signed) transactions to be submitted via RPC",
 	}
 
 	// Network Settings
