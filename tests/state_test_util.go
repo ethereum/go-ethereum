67f8d32254575154e359e3d2617772d6bba55d61
Martin Holst Swende <martin@swende.se>
2021-02-22 13:05:46 +0100

    tests: handle legacy tests without access lists

--- tests/state_test_util.go
+++ tests/state_test_util.go
@@ -294,7 +294,11 @@ func (tx *stTransaction) toMessage(ps stPostState) (core.Message, error) {
 	if err != nil {
 		return nil, fmt.Errorf("invalid tx data %q", dataHex)
 	}
-	msg := types.NewMessage(from, to, tx.Nonce, value, gasLimit, tx.GasPrice, data, tx.AccessLists[ps.Indexes.Data], true)
+	var accessList *types.AccessList
+	if len(tx.AccessLists) > 0 {
+		accessList = tx.AccessLists[ps.Indexes.Data]
+	}
+	msg := types.NewMessage(from, to, tx.Nonce, value, gasLimit, tx.GasPrice, data, accessList, true)
 	return msg, nil
 }
 
