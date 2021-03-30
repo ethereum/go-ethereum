4a37ae510eb02bb7daf1fa5982f326e92a401c60
Felix Lange <fjl@twurst.com>
2021-03-30 20:09:30 +0200

    rpc: tighter shutdown synchronization in client subscription (#22597)
    This fixes a rare issue where the client subscription forwarding loop
    would attempt send on the subscription's channel after Unsubscribe has
    returned, leading to a panic if the subscription channel was already
    closed by the user. Example:
    
        sub, _ := client.Subscribe(..., channel, ...)
        sub.Unsubscribe()
        close(channel)
    
    The race occurred because Unsubscribe called quitWithServer to tell the
    forwarding loop to stop sending on sub.channel, but did not wait for the
    loop to actually come down. This is fixed by adding an additional channel
    to track the shutdown, on which Unsubscribe now waits.
    
    Fixes #22322

--- rpc/handler.go
+++ rpc/handler.go
@@ -189,7 +189,7 @@ func (h *handler) cancelAllRequests(err error, inflightReq *requestOp) {
 	}
 	for id, sub := range h.clientSubs {
 		delete(h.clientSubs, id)
-		sub.quitWithError(false, err)
+		sub.close(err)
 	}
 }
 
@@ -281,7 +281,7 @@ func (h *handler) handleResponse(msg *jsonrpcMessage) {
 		return
 	}
 	if op.err = json.Unmarshal(msg.Result, &op.sub.subid); op.err == nil {
-		go op.sub.start()
+		go op.sub.run()
 		h.clientSubs[op.sub.subid] = op.sub
 	}
 }
