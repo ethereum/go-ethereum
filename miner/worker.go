ec7596cd311b3599181b51329a30e77e55513ebf
Jason Paryani <github@jparyani.com>
2021-03-31 14:36:18 -0700

    Change flashbots bundle pricing formula to ignore gas fees

--- miner/worker.go
+++ miner/worker.go
@@ -1290,6 +1290,7 @@ func (w *worker) computeBundleGas(bundle types.Transactions, parent *types.Block
 
 	var totalGasUsed uint64 = 0
 	var tempGasUsed uint64
+	gasFees := new(big.Int)
 
 	coinbaseBalanceBefore := env.state.GetBalance(w.coinbase)
 
@@ -1299,13 +1300,12 @@ func (w *worker) computeBundleGas(bundle types.Transactions, parent *types.Block
 			return nil, 0, err
 		}
 		totalGasUsed += receipt.GasUsed
+		gasFees.Add(gasFees, new(big.Int).Mul(big.NewInt(int64(totalGasUsed)), tx.GasPrice()))
 	}
 	coinbaseBalanceAfter := env.state.GetBalance(w.coinbase)
-	coinbaseDiff := new(big.Int).Sub(coinbaseBalanceAfter, coinbaseBalanceBefore)
-	totalEth := new(big.Int)
-	totalEth.Add(totalEth, coinbaseDiff)
+	coinbaseDiff := new(big.Int).Sub(new(big.Int).Sub(coinbaseBalanceAfter, gasFees), coinbaseBalanceBefore)
 
-	return totalEth, totalGasUsed, nil
+	return coinbaseDiff, totalGasUsed, nil
 }
 
 // copyReceipts makes a deep copy of the given receipts.
