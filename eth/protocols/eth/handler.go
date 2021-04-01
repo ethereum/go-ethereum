8fd1046040f810099d4661e9aaee454a57644bb3
Péter Szilágyi <peterke@gmail.com>
2021-04-01 10:24:09 +0300

    eth/protocols, prp/tracker: add support for req/rep rtt tracking

--- eth/protocols/eth/handler.go
+++ eth/protocols/eth/handler.go
@@ -223,7 +223,7 @@ func handleMessage(backend Backend, peer *Peer) error {
 	if peer.Version() >= ETH66 {
 		handlers = eth66
 	}
-	// Track the emount of time it takes to serve the request and run the handler
+	// Track the amount of time it takes to serve the request and run the handler
 	if metrics.Enabled {
 		h := fmt.Sprintf("%s/%s/%d/%#02x", p2p.HandleHistName, ProtocolName, peer.Version(), msg.Code)
 		defer func(start time.Time) {
