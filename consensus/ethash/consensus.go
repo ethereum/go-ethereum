dae9d1ae5ad7dc30f5c0dcd4a157a0252ea71e23
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2021-04-15 18:23:55 +0200

    Change difficulty calculations to accept 1 for each block

--- consensus/ethash/consensus.go
+++ consensus/ethash/consensus.go
@@ -315,6 +315,8 @@ func (ethash *Ethash) CalcDifficulty(chain consensus.ChainHeaderReader, time uin
 func CalcDifficulty(config *params.ChainConfig, time uint64, parent *types.Header) *big.Int {
 	next := new(big.Int).Add(parent.Number, big1)
 	switch {
+	case config.IsCatalyst(next):
+		return big.NewInt(1)
 	case config.IsMuirGlacier(next):
 		return calcDifficultyEip2384(time, parent)
 	case config.IsConstantinople(next):
