da28171e7a7d3381b461173b2ed80ed2cbe37dbb
Felix Lange <fjl@twurst.com>
2021-04-16 14:00:46 +0200

    eth/catalyst: add copyright header

--- eth/catalyst/api.go
+++ eth/catalyst/api.go
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-// Package catalyst implements the temporary eth1/eth2 RPC integration.
 package catalyst
 
 import (
