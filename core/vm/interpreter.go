e8d9a61ac8d6d75f53a8532196c31b2b415b445c
Aaron Buchwald <aaron.buchwald56@gmail.com>
2021-05-02 17:25:01 -0400

    Fix comment

--- core/vm/interpreter.go
+++ core/vm/interpreter.go
@@ -226,7 +226,7 @@ func (in *EVMInterpreter) Run(contract *Contract, input []byte, readOnly bool) (
 		} else if sLen > operation.maxStack {
 			return nil, &ErrStackOverflow{stackLen: sLen, limit: operation.maxStack}
 		}
-		// If the operation is valid, enforce and write restrictions
+		// If the operation is valid, enforce write restrictions
 		if in.readOnly && in.evm.chainRules.IsByzantium {
 			// If the interpreter is operating in readonly mode, make sure no
 			// state-modifying operation is performed. The 3rd stack item
