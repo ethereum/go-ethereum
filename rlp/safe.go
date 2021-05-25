bd0e83941c4d7c738339a239cc534539c695bbe3
ucwong <ucwong@126.com>
2021-05-25 13:20:48 +0800

    rlp, tests : gofmt

--- rlp/safe.go
+++ rlp/safe.go
@@ -22,5 +22,5 @@ import "reflect"
 
 // byteArrayBytes returns a slice of the byte array v.
 func byteArrayBytes(v reflect.Value) []byte {
-    return v.Slice(0, v.Len()).Bytes()
+	return v.Slice(0, v.Len()).Bytes()
 }
--- tests/fuzzers/bls12381/bls12381_fuzz.go
+++ tests/fuzzers/bls12381/bls12381_fuzz.go
@@ -159,7 +159,7 @@ func FuzzCrossG1MultiExp(data []byte) int {
 		gethPoints = append(gethPoints, new(bls12381.PointG1).Set(kp1))
 		gnarkPoints = append(gnarkPoints, *cp1)
 	}
-	if len(gethScalars) == 0{
+	if len(gethScalars) == 0 {
 		return 0
 	}
 	// compute multi exponentiation
