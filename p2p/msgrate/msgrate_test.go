83fcfbe9fb5fb0d30eb02166391b17d10e82384c
Felix Lange <fjl@twurst.com>
2021-05-27 14:50:54 +0200

    p2p/msgrate: make the capacity overflow test actually overflow

--- p2p/msgrate/msgrate_test.go
+++ p2p/msgrate/msgrate_test.go
@@ -20,7 +20,7 @@ import "testing"
 
 func TestCapacityOverflow(t *testing.T) {
 	tracker := NewTracker(nil, 1)
-	tracker.Update(1, 1, 10000)
+	tracker.Update(1, 1, 100000)
 	cap := tracker.Capacity(1, 10000000)
 	if int32(cap) < 0 {
 		t.Fatalf("Negative: %v", int32(cap))
