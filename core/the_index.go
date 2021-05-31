b9b2419c065f581de78564c561da266de3701891
Tal Kol <talkol@gmail.com>
2021-05-31 01:20:39 +0100

    added paging anc compression

--- core/the_index.go
+++ core/the_index.go
@@ -6,6 +6,10 @@ package core
 import (
 	"fmt"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"strconv"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/state"
@@ -17,6 +21,14 @@ import (
 const theIndexVerbose = true
 
 const contractShards byte = 64
+const maxFileSizeMb = 1024
+const fileIndexFormat = "%05d"
+
+const blocksFilePrefix = "blocks-"
+const accountsFilePrefix = "accounts-"
+const contractsShardFilePrefix = "contracts%02d-"
+
+var maxFileIndex map[string]int = theIndex_getCurrentMaxFileIndexes()
 
 func (bc *BlockChain) TheIndex_Hook_WriteBlockHeader(block *types.Block) {
 	if _, err := os.Stat("./the-index/"); os.IsNotExist(err) {
@@ -28,7 +40,7 @@ func (bc *BlockChain) TheIndex_Hook_WriteBlockHeader(block *types.Block) {
 	}
 
 	// write block header to blob
-	file, err := os.OpenFile("./the-index/blocks.rlp", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0755)
+	file, err := theIndex_openFile(blocksFilePrefix)
 	if err != nil {
 		log.Crit("THE-INDEX", "error", err)
 	}
@@ -80,7 +92,7 @@ func (bc *BlockChain) TheIndex_Hook_WriteContractsAndAccounts(block *types.Block
 	// write all contracts to sharded blobs
 	for shard, contractsInTheShard := range contractsPerShard {
 		// write contract to blob
-		file, err := os.OpenFile("./the-index/contracts-"+fmt.Sprintf("%02x", shard)+".rlp", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0755)
+		file, err := theIndex_openFile(fmt.Sprintf(contractsShardFilePrefix, shard))
 		if err != nil {
 			log.Crit("THE-INDEX", "error", err)
 		}
@@ -102,7 +114,7 @@ func (bc *BlockChain) TheIndex_Hook_WriteContractsAndAccounts(block *types.Block
 		}
 
 		// write combined accounts (all users) to blob
-		file, err := os.OpenFile("./the-index/accounts.rlp", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0755)
+		file, err := theIndex_openFile(accountsFilePrefix)
 		if err != nil {
 			log.Crit("THE-INDEX", "error", err)
 		}
@@ -140,4 +152,50 @@ func TheIndex_addAccountsToContracts(accounts []rlp.TheIndex_rlpAccount, contrac
 			contracts[account.Address].Balance = account.Balance
 		}
 	}
+}
+
+func theIndex_getCurrentMaxFileIndexes() map[string]int {
+	res := map[string]int{}
+	res[blocksFilePrefix] = theIndex_getCurrentMaxFileIndex(blocksFilePrefix)
+	res[accountsFilePrefix] = theIndex_getCurrentMaxFileIndex(accountsFilePrefix)
+	for shard := 0; shard < int(contractShards); shard++ {
+		filePrefix := fmt.Sprintf(contractsShardFilePrefix, shard)
+		res[filePrefix] = theIndex_getCurrentMaxFileIndex(filePrefix)
+	}
+	return res
+}
+
+func theIndex_getCurrentMaxFileIndex(prefix string) int {
+	var res = 1
+	re := regexp.MustCompile(`-(\d+)\.`)
+	matches, err := filepath.Glob("./the-index/" + prefix + "*.rlp")
+	if err != nil {
+		fmt.Println("THE-INDEX", "error", err)
+	}
+	for _, filePath := range matches {
+		indexStr := re.FindStringSubmatch(filePath)
+		var index = 0
+		if len(indexStr) >= 1 {
+			index, err = strconv.Atoi(indexStr[1])
+			if err != nil {
+				fmt.Println("THE-INDEX", "error", err)
+			}
+		}
+		if index > res {
+			res = index
+		}
+	}
+	return res
+}
+
+func theIndex_openFile(prefix string) (*os.File, error) {
+	filePath := fmt.Sprintf("./the-index/"+prefix+fileIndexFormat+".rlp", maxFileIndex[prefix])
+	stat, err := os.Stat(filePath)
+	if err == nil && stat.Size() >= maxFileSizeMb*1024*1024 {
+		// compress the file and move to the next one
+		exec.Command("gzip", filePath).Run()
+		maxFileIndex[prefix]++
+		filePath = fmt.Sprintf("./the-index/"+prefix+fileIndexFormat+".rlp", maxFileIndex[prefix])
+	}
+	return os.OpenFile(filePath, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0755)
 }
