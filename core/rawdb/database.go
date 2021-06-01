f194630fe7cc333181de567ffc050ee0e49adb10
Evolution <615598813@qq.com>
2021-06-01 22:18:07 +0800

    gofmt

--- core/rawdb/database.go
+++ core/rawdb/database.go
@@ -316,8 +316,8 @@ func InspectDatabase(db ethdb.Database, keyPrefix, keyStart []byte) error {
 		bloomTrieNodes stat
 
 		// Meta- and unaccounted data
-		metadata     stat
-		unaccounted  stat
+		metadata    stat
+		unaccounted stat
 
 		// Totals
 		total common.StorageSize
