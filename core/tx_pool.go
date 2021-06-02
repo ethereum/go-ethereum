c0f066f010c5b78c75339a164e65f9f39585a050
zuoweiliu <zuoweiliu@126.com>
2021-06-02 11:37:10 +0800

    disables price exemptions for locally submitted transactions

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -542,8 +542,8 @@ func (pool *TxPool) validateTx(tx *types.Transaction, local bool) error {
 		return ErrInvalidSender
 	}
 	// Drop non-local transactions under our own minimal accepted gas price
-	local = local || pool.locals.contains(from) // account may be local even if the transaction arrived from the network
-	if !local && tx.GasPriceIntCmp(pool.gasPrice) < 0 {
+	//local = local || pool.locals.contains(from) // account may be local even if the transaction arrived from the network
+	if tx.GasPriceIntCmp(pool.gasPrice) < 0 {
 		return ErrUnderpriced
 	}
 	// Ensure the transaction adheres to nonce ordering
