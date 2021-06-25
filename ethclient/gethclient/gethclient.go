860204d8bfd47209364e06877498cb7df1b37251
Felix Lange <fjl@twurst.com>
2021-06-25 14:56:17 +0200

    ethclient/gethclient: update docs

--- ethclient/gethclient/gethclient.go
+++ ethclient/gethclient/gethclient.go
@@ -1,3 +1,20 @@
+// Copyright 2021 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+// Package gethclient provides an RPC client for geth-specific APIs.
 package gethclient
 
 import (
@@ -14,10 +31,9 @@ import (
 	"github.com/ethereum/go-ethereum/rpc"
 )
 
-// Client is a wrapper around the rpc.Client that
-// implements geth specific functionalities.
-// Please use ethclient.Client instead if you want to
-// access the standardized Etheeum RPC functionality.
+// Client is a wrapper around rpc.Client that implements geth-specific functionality.
+//
+// If you want to use the standardized Ethereum RPC functionality, use ethclient.Client instead.
 type Client struct {
 	c *rpc.Client
 }
