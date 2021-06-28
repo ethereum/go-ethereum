d4f030e2734671a5f4288cbdde4e75820f885744
Austin Roberts <austin.roberts@openrelay.xyz>
2021-06-28 12:11:38 -0500

    Add tracing for live blocks, replace readme

--- eth/plugin_hooks.go
+++ eth/plugin_hooks.go
@@ -1,15 +1,19 @@
 package eth
 
 import (
+  "math/big"
   "github.com/ethereum/go-ethereum/plugins"
   "github.com/ethereum/go-ethereum/node"
   "github.com/ethereum/go-ethereum/params"
+  "github.com/ethereum/go-ethereum/common"
   "github.com/ethereum/go-ethereum/core/vm"
   "github.com/ethereum/go-ethereum/consensus"
   "github.com/ethereum/go-ethereum/consensus/ethash"
   "github.com/ethereum/go-ethereum/eth/ethconfig"
   "github.com/ethereum/go-ethereum/ethdb"
   "github.com/ethereum/go-ethereum/log"
+  "reflect"
+  "time"
 )
 
 func PluginCreateConsensusEngine(pl *plugins.PluginLoader, stack *node.Node, chainConfig *params.ChainConfig, config *ethash.Config, notify []string, noverify bool, db ethdb.Database) consensus.Engine {
@@ -33,7 +37,53 @@ func pluginCreateConsensusEngine(stack *node.Node, chainConfig *params.ChainConf
   return PluginCreateConsensusEngine(plugins.DefaultPluginLoader, stack, chainConfig, config, notify, noverify, db)
 }
 
+type metaTracer struct{
+  tracers []vm.Tracer
+}
+
+func (mt *metaTracer) CaptureStart(env *vm.EVM, from common.Address, to common.Address, create bool, input []byte, gas uint64, value *big.Int) {
+  for _, tracer := range mt.tracers {
+    tracer.CaptureStart(env, from, to, create, input, gas, value)
+  }
+}
+func (mt *metaTracer) CaptureState(env *vm.EVM, pc uint64, op vm.OpCode, gas, cost uint64, scope *vm.ScopeContext, rData []byte, depth int, err error) {
+  for _, tracer := range mt.tracers {
+    tracer.CaptureState(env, pc, op, gas, cost, scope, rData, depth, err)
+  }
+}
+func (mt *metaTracer) CaptureFault(env *vm.EVM, pc uint64, op vm.OpCode, gas, cost uint64, scope *vm.ScopeContext, depth int, err error) {
+  for _, tracer := range mt.tracers {
+    tracer.CaptureFault(env, pc, op, gas, cost, scope, depth, err)
+  }
+}
+func (mt *metaTracer) CaptureEnd(output []byte, gasUsed uint64, t time.Duration, err error) {
+  for _, tracer := range mt.tracers {
+    tracer.CaptureEnd(output, gasUsed, t, err)
+  }
+}
+
 func PluginUpdateBlockchainVMConfig(pl *plugins.PluginLoader, cfg *vm.Config) {
+  tracerList := plugins.Lookup("LiveTracer", func(item interface{}) bool {
+    _, ok := item.(*vm.Tracer)
+    log.Info("Item is LiveTracer", "ok", ok, "type", reflect.TypeOf(item))
+    return ok
+  })
+  if len(tracerList) > 0 {
+    mt := &metaTracer{tracers: []vm.Tracer{}}
+    for _, tracer := range(tracerList) {
+      if v, ok := tracer.(*vm.Tracer); ok {
+        log.Info("LiveTracer registered")
+        mt.tracers = append(mt.tracers, *v)
+      } else {
+        log.Info("Item is not tracer")
+      }
+    }
+    cfg.Debug = true
+    cfg.Tracer = mt
+  } else {
+    log.Warn("Module is not tracer")
+  }
+
   fnList := plugins.Lookup("UpdateBlockchainVMConfig", func(item interface{}) bool {
     _, ok := item.(func(*vm.Config))
     return ok
