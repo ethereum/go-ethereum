436f54dcd5f614cd64cfa8e495735716d0577a4c
Martin Holst Swende <martin@swende.se>
2021-07-03 18:42:48 +0200

    tests: disable legacy tests

--- tests/init_test.go
+++ tests/init_test.go
@@ -46,10 +46,11 @@ func TestMain(m *testing.M) {
 }
 
 var (
-	baseDir            = filepath.Join(".", "testdata")
-	blockTestDir       = filepath.Join(baseDir, "BlockchainTests")
-	stateTestDir       = filepath.Join(baseDir, "GeneralStateTests")
-	legacyStateTestDir = filepath.Join(baseDir, "LegacyTests", "Constantinople", "GeneralStateTests")
+	baseDir      = filepath.Join(".", "testdata")
+	blockTestDir = filepath.Join(baseDir, "BlockchainTests")
+	stateTestDir = filepath.Join(baseDir, "GeneralStateTests")
+	// TODO @holiman @MariusVanDerWijden re-enable this
+	//legacyStateTestDir = filepath.Join(baseDir, "LegacyTests", "Constantinople", "GeneralStateTests")
 	transactionTestDir = filepath.Join(baseDir, "TransactionTests")
 	vmTestDir          = filepath.Join(baseDir, "VMTests")
 	rlpTestDir         = filepath.Join(baseDir, "RLPTests")
