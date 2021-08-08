e6716d64e4bafa6edcc8943d2e3bfc4358999dbf
Martin Holst Swende <martin@swende.se>
2021-08-08 16:56:05 +0200

    cmd/evm: add difficulty calculation to t8n tool

--- cmd/evm/internal/t8ntool/transition.go
+++ cmd/evm/internal/t8ntool/transition.go
@@ -252,6 +252,19 @@ func Main(ctx *cli.Context) error {
 			return NewError(ErrorVMConfig, errors.New("EIP-1559 config but missing 'currentBaseFee' in env section"))
 		}
 	}
+	if env := prestate.Env; env.Difficulty == nil {
+		// If difficulty was not provided by caller, we need to calculate it.
+		switch {
+		case env.ParentDifficulty == nil:
+			return NewError(ErrorVMConfig, errors.New("currentDifficulty was not provided, and cannot be calculated due to missing parentDifficulty"))
+		case env.Number == 0:
+			return NewError(ErrorVMConfig, errors.New("currentDifficulty needs to be provided for block number 0"))
+		case env.Timestamp == 0:
+			return NewError(ErrorVMConfig, errors.New("currentDifficulty cannot be calculated if time is set to 0"))
+		}
+		prestate.Env.Difficulty = calcDifficulty(chainConfig, env.Number, env.Timestamp,
+			env.ParentTimestamp, env.ParentDifficulty, env.ParentUncleHash)
+	}
 	// Run the test and aggregate the result
 	s, result, err := prestate.Apply(vmConfig, chainConfig, txs, ctx.Int64(RewardFlag.Name), getTracer)
 	if err != nil {
