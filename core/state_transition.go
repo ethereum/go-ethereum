94f6474c9b18d4c9ca8a49f459be37257be02c61
lxex <liuxmzc1@163.com>
2021-08-09 18:31:43 +0800

    To keep the naming style consistent, use 'london' instead of 'isLondon'

--- core/state_transition.go
+++ core/state_transition.go
@@ -311,9 +311,9 @@ func (st *StateTransition) TransitionDb() (*ExecutionResult, error) {
 		st.state.SetNonce(msg.From(), st.state.GetNonce(sender.Address())+1)
 		ret, st.gas, vmerr = st.evm.Call(sender, st.to(), st.data, st.gas, st.value)
 	}
-	
-	isLondon := st.evm.ChainConfig().IsLondon(st.evm.Context.BlockNumber)
-	if !isLondon {
+
+	london := st.evm.ChainConfig().IsLondon(st.evm.Context.BlockNumber)
+	if !london {
 		// Before EIP-3529: refunds were capped to gasUsed / 2
 		st.refundGas(params.RefundQuotient)
 	} else {
@@ -321,7 +321,7 @@ func (st *StateTransition) TransitionDb() (*ExecutionResult, error) {
 		st.refundGas(params.RefundQuotientEIP3529)
 	}
 	effectiveTip := st.gasPrice
-	if isLondon {
+	if london {
 		effectiveTip = cmath.BigMin(st.gasTipCap, new(big.Int).Sub(st.gasFeeCap, st.evm.Context.BaseFee))
 	}
 	st.state.AddBalance(st.evm.Context.Coinbase, new(big.Int).Mul(new(big.Int).SetUint64(st.gasUsed()), effectiveTip))
