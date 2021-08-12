b6d13100301a3bbd90d77030a9a293c54c3a9296
Zachinquarantine <zachinquarantine@yahoo.com>
2021-08-12 09:30:09 -0400

    cmd/utils: move --nousb to legacy flags file

--- cmd/utils/flags_legacy.go
+++ cmd/utils/flags_legacy.go
@@ -41,6 +41,10 @@ var DeprecatedFlags = []cli.Flag{
 
 var (
 	// (Deprecated May 2020, shown in aliased flags section)
+	NoUSBFlag = cli.BoolFlag{
+		Name:  "nousb",
+		Usage: "Disables monitoring for and managing USB hardware wallets (deprecated)",
+	}
 	LegacyRPCEnabledFlag = cli.BoolFlag{
 		Name:  "rpc",
 		Usage: "Enable the HTTP-RPC server (deprecated and will be removed June 2021, use --http)",
