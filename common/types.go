d97d88955a9448f4cd9d7afce156a1ad91ff776c
jmlee <junmo.lee@snu.ac.kr>
2021-08-19 09:06:52 +0000

    implement object save/load function as/from a file

--- common/types.go
+++ common/types.go
@@ -28,6 +28,8 @@ import (
 	"reflect"
 	"strings"
 	"sync"
+	"io"
+	"os"
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"golang.org/x/crypto/sha3"
@@ -51,6 +53,54 @@ var (
 	NoExistKey = HexToHash("0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff") // very large key which will not be reached forever
 	ZeroAddress = HexToAddress("0x0")
 )
+
+// Marshal is a function that marshals the object into an io.Reader.
+// By default, it uses the JSON marshaller.
+var Marshal = func(v interface{}) (io.Reader, error) {
+	b, err := json.MarshalIndent(v, "", "\t")
+	if err != nil {
+		return nil, err
+	}
+	return bytes.NewReader(b), nil
+}
+
+// Unmarshal is a function that unmarshals the data from the reader into the specified value.
+// By default, it uses the JSON unmarshaller.
+var Unmarshal = func(r io.Reader, v interface{}) error {
+	return json.NewDecoder(r).Decode(v)
+}
+
+// var lock sync.Mutex
+
+// Save saves a representation of v to the file at path.
+func SaveAsFile(path string, v interface{}) error {
+	// lock.Lock()
+	// defer lock.Unlock()
+	f, err := os.Create(path)
+	if err != nil {
+	  return err
+	}
+	defer f.Close()
+	r, err := Marshal(v)
+	if err != nil {
+	  return err
+	}
+	_, err = io.Copy(f, r)
+	return err
+}
+
+// Load loads the file at path into v.
+// Use os.IsNotExist() to see if the returned error is due to the file being missing.
+func LoadFromFile(path string, v interface{}) error {
+	// lock.Lock()
+	// defer lock.Unlock()
+	f, err := os.Open(path)
+	if err != nil {
+	  return err
+	}
+	defer f.Close()
+	return Unmarshal(f, v)
+}
 
 // Hash represents the 32 byte Keccak256 hash of arbitrary data.
 type Hash [HashLength]byte
