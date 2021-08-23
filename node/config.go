fed45d784b569c7447f20942688ceedc4381f8ec
Zachinquarantine <zachinquarantine@yahoo.com>
2021-08-23 09:55:27 -0400

    adds nousb deprecation comment

--- node/config.go
+++ node/config.go
@@ -93,6 +93,7 @@ type Config struct {
 	InsecureUnlockAllowed bool `toml:",omitempty"`
 
 	// NoUSB disables hardware wallet monitoring and connectivity.
+	// DEPRECATED: USB monitoring is now disabled by default and must be enabled with the --usb flag. This command will be removed in the future!
 	NoUSB bool `toml:",omitempty"`
 
 	// USB enables hardware wallet monitoring and connectivity.
