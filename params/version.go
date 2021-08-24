339cf328bdbbd6baa2a6ee667200b9e74c48f5cd
Matthieu Vachon <matthieu.vachon@dfuse.io>
2021-08-24 12:10:41 -0400

    Fixed wrong version for Bor

--- params/version.go
+++ params/version.go
@@ -21,9 +21,9 @@ import (
 )
 
 const (
-	VersionMajor = 1        // Major version component of the current release
-	VersionMinor = 10       // Minor version component of the current release
-	VersionPatch = 8        // Patch version component of the current release
+	VersionMajor = 0        // Major version component of the current release
+	VersionMinor = 2        // Minor version component of the current release
+	VersionPatch = 7        // Patch version component of the current release
 	VersionMeta  = "stable" // Version metadata to append to the version string
 )
 
