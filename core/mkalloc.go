8a134014b4b370b4a3632e32a2fc8e84ee2b6947
Felix Lange <fjl@twurst.com>
2021-08-25 18:46:29 +0200

    all: add go:build lines (#23468)
    Generated by go1.17 fmt ./...

--- core/mkalloc.go
+++ core/mkalloc.go
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
+//go:build none
 // +build none
 
 /*
--- metrics/cputime_unix.go
+++ metrics/cputime_unix.go
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
+//go:build !windows && !js
 // +build !windows,!js
 
 package metrics
