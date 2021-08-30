59418436ca115f6e4320321ebe224346a3bbb6c5
Paweł Bylica <chfast@gmail.com>
2021-08-30 12:32:03 +0200

    core/vm: clear bitvec in jumpdest analysis benchmark

--- core/vm/analysis_test.go
+++ core/vm/analysis_test.go
@@ -89,6 +89,9 @@ func BenchmarkJumpdestOpAnalysis(bench *testing.B) {
 		bits := make(bitvec, len(code)/8+1+4)
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
+			for j := range bits {
+				bits[j] = 0
+			}
 			codeBitmapInternal(code, bits)
 		}
 	}
