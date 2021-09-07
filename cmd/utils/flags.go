9a0df80bbcfd849a9a56ecb0937bf8dbea8a8c79
Zachinquarantine <zachinquarantine@yahoo.com>
2021-09-07 04:52:58 -0400

    cmd/geth, cmd/utils: remove legacy rpc flags (#23358)
    * remove rpc flags
    
    * remove legacy rpc flags
    
    * remove legacy rpc flags
    
    * remove legacy rpc commands
    
    * (hopefully) fix most of the build errors
    
    * fix build errors
    
    https://app.travis-ci.com/github/ethereum/go-ethereum/jobs/530318686
    
    * cmd/utils: fix syntax error
    
    * empty commit to unbreak travis ci
    
    * fix syntax error
    
    * syntax fixes
    
    * syntax fixes
    
    * fix
    
    fixes "cmd/geth/usage.go:234:7: expected '(', found init (typecheck)"
    
    * fix
    
    * various fixes in usage.go
    
    * various fixes in flags.go
    
    * adds extra space
    
    reverts the spacing to how it was before I resolved the merge conflict
    
    * more fixes in usage.go
    
    * fix
    
    fix for cmd/geth/usage.go:243:17: expected operand, found ':=' (typecheck) in travis
    
    * Update cmd/utils/flags.go
    
    Co-authored-by: Martin Holst Swende <martin@swende.se>
    
    * fix error
    
     fixes these errors:
    cmd/utils/flags_legacy.go:21:2: "strings" imported but not used (typecheck)
    
    	"strings"
    
    	^
    
    cmd/utils/flags_legacy.go:24:2: "github.com/ethereum/go-ethereum/node" imported but not used (typecheck)
    
    	"github.com/ethereum/go-ethereum/node"
    
    	^
    
    * goimports
    
    Co-authored-by: Martin Holst Swende <martin@swende.se>

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -920,14 +920,6 @@ func SplitAndTrim(input string) (ret []string) {
 // setHTTP creates the HTTP RPC listener interface string from the set
 // command line flags, returning empty if the HTTP endpoint is disabled.
 func setHTTP(ctx *cli.Context, cfg *node.Config) {
-	if ctx.GlobalBool(LegacyRPCEnabledFlag.Name) && cfg.HTTPHost == "" {
-		log.Warn("The flag --rpc is deprecated and will be removed June 2021, please use --http")
-		cfg.HTTPHost = "127.0.0.1"
-		if ctx.GlobalIsSet(LegacyRPCListenAddrFlag.Name) {
-			cfg.HTTPHost = ctx.GlobalString(LegacyRPCListenAddrFlag.Name)
-			log.Warn("The flag --rpcaddr is deprecated and will be removed June 2021, please use --http.addr")
-		}
-	}
 	if ctx.GlobalBool(HTTPEnabledFlag.Name) && cfg.HTTPHost == "" {
 		cfg.HTTPHost = "127.0.0.1"
 		if ctx.GlobalIsSet(HTTPListenAddrFlag.Name) {
@@ -935,34 +927,18 @@ func setHTTP(ctx *cli.Context, cfg *node.Config) {
 		}
 	}
 
-	if ctx.GlobalIsSet(LegacyRPCPortFlag.Name) {
-		cfg.HTTPPort = ctx.GlobalInt(LegacyRPCPortFlag.Name)
-		log.Warn("The flag --rpcport is deprecated and will be removed June 2021, please use --http.port")
-	}
 	if ctx.GlobalIsSet(HTTPPortFlag.Name) {
 		cfg.HTTPPort = ctx.GlobalInt(HTTPPortFlag.Name)
 	}
 
-	if ctx.GlobalIsSet(LegacyRPCCORSDomainFlag.Name) {
-		cfg.HTTPCors = SplitAndTrim(ctx.GlobalString(LegacyRPCCORSDomainFlag.Name))
-		log.Warn("The flag --rpccorsdomain is deprecated and will be removed June 2021, please use --http.corsdomain")
-	}
 	if ctx.GlobalIsSet(HTTPCORSDomainFlag.Name) {
 		cfg.HTTPCors = SplitAndTrim(ctx.GlobalString(HTTPCORSDomainFlag.Name))
 	}
 
-	if ctx.GlobalIsSet(LegacyRPCApiFlag.Name) {
-		cfg.HTTPModules = SplitAndTrim(ctx.GlobalString(LegacyRPCApiFlag.Name))
-		log.Warn("The flag --rpcapi is deprecated and will be removed June 2021, please use --http.api")
-	}
 	if ctx.GlobalIsSet(HTTPApiFlag.Name) {
 		cfg.HTTPModules = SplitAndTrim(ctx.GlobalString(HTTPApiFlag.Name))
 	}
 
-	if ctx.GlobalIsSet(LegacyRPCVirtualHostsFlag.Name) {
-		cfg.HTTPVirtualHosts = SplitAndTrim(ctx.GlobalString(LegacyRPCVirtualHostsFlag.Name))
-		log.Warn("The flag --rpcvhosts is deprecated and will be removed June 2021, please use --http.vhosts")
-	}
 	if ctx.GlobalIsSet(HTTPVirtualHostsFlag.Name) {
 		cfg.HTTPVirtualHosts = SplitAndTrim(ctx.GlobalString(HTTPVirtualHostsFlag.Name))
 	}
