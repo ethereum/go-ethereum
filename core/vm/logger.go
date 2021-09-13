e540700483170b991954ec80be5f98068f94ddd9
Marius van der Wijden <m.vanderwijden@live.de>
2021-09-13 17:17:19 +0200

    Apply suggestions from code review
    Co-authored-by: Martin Holst Swende <martin@swende.se>

--- core/vm/logger.go
+++ core/vm/logger.go
@@ -46,10 +46,10 @@ func (s Storage) Copy() Storage {
 
 // LogConfig are the configuration options for structured logger the EVM
 type LogConfig struct {
-	EnableMemory     bool // disable memory capture
+	EnableMemory     bool // enable memory capture
 	DisableStack     bool // disable stack capture
 	DisableStorage   bool // disable storage capture
-	EnableReturnData bool // disable return data capture
+	EnableReturnData bool // enable return data capture
 	Debug            bool // print output during capture end
 	Limit            int  // maximum length of output, but zero means unlimited
 	// Chain overrides, can be used to execute a trace using future fork rules
