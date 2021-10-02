4a57388dd2dbdee5360f2e16cf4bb8b862ccf765
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2021-10-03 00:09:09 +0200

    review feedback: force a stack size of 8M on all linux distribs

--- build/ci.go
+++ build/ci.go
@@ -259,10 +259,11 @@ func buildFlags(env build.Environment) (flags []string) {
 	if runtime.GOOS == "darwin" {
 		ld = append(ld, "-s")
 	}
-	// Increase the thread stack size when running on alpine linux.
-	// TODO(gballet) figure out a way to detect we are running on alpine
-	// linux, as this line won't build on all platforms.
-	ld = append(ld, "-extldflags='-Wl,-z,-stack-size=0x200000'")
+	// Enforce the stacksize to 8M, which is the case on most platforms apart from
+	// alpine Linux.
+	if runtime.GOOS == "linux" {
+		ld = append(ld, "-extldflags='-Wl,-z,-stack-size=0x800000'")
+	}
 	if len(ld) > 0 {
 		flags = append(flags, "-ldflags", strings.Join(ld, " "))
 	}
