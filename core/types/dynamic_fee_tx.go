d264447f0e3cea173cc093ec8d828b33f0b5de05
Felix Lange <fjl@twurst.com>
2021-10-06 12:14:40 +0200

    core/types: remove unused method protected()

--- core/types/dynamic_fee_tx.go
+++ core/types/dynamic_fee_tx.go
@@ -84,7 +84,6 @@ func (tx *DynamicFeeTx) copy() TxData {
 // accessors for innerTx.
 func (tx *DynamicFeeTx) txType() byte           { return DynamicFeeTxType }
 func (tx *DynamicFeeTx) chainID() *big.Int      { return tx.ChainID }
-func (tx *DynamicFeeTx) protected() bool        { return true }
 func (tx *DynamicFeeTx) accessList() AccessList { return tx.AccessList }
 func (tx *DynamicFeeTx) data() []byte           { return tx.Data }
 func (tx *DynamicFeeTx) gas() uint64            { return tx.Gas }
