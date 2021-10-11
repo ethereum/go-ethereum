57c993b6cbe2ccc6e893cb047a26a4876e8df3fb
Sina Mahmoodi <itz.s1na@gmail.com>
2021-10-11 17:13:54 +0200

    eth/tracers: add GetResult to tracer interface

--- eth/tracers/native/native.go
+++ eth/tracers/native/native.go
@@ -1,8 +1,17 @@
 package native
 
-import "github.com/ethereum/go-ethereum/core/vm"
+import (
+	"encoding/json"
 
-type Constructor func() vm.Tracer
+	"github.com/ethereum/go-ethereum/core/vm"
+)
+
+type Tracer interface {
+	vm.Tracer
+	GetResult() (json.RawMessage, error)
+}
+
+type Constructor func() Tracer
 
 var tracers map[string]Constructor = make(map[string]Constructor)
 
@@ -10,7 +19,7 @@ func Register(name string, fn Constructor) {
 	tracers[name] = fn
 }
 
-func New(name string) (vm.Tracer, bool) {
+func New(name string) (Tracer, bool) {
 	if fn, ok := tracers[name]; ok {
 		return fn(), true
 	}
