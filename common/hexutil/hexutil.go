49dc5e85e19e477811f4e4ee0f47cd080b480858
lmittmann <lmittmann@users.noreply.github.com>
2021-10-21 11:43:23 +0200

    common/hexutil: improve performance of EncodeBig (#23780)
    - use Text instead of fmt.Sprintf
    - reduced allocs from 6 to 2
    - improved speed

--- common/hexutil/hexutil.go
+++ common/hexutil/hexutil.go
@@ -18,7 +18,7 @@
 Package hexutil implements hex encoding with 0x prefix.
 This encoding is used by the Ethereum RPC API to transport binary data in JSON payloads.
 
-Encoding Rules
+# Encoding Rules
 
 All hex data must have prefix "0x".
 
@@ -175,13 +175,14 @@ func MustDecodeBig(input string) *big.Int {
 }
 
 // EncodeBig encodes bigint as a hex string with 0x prefix.
-// The sign of the integer is ignored.
 func EncodeBig(bigint *big.Int) string {
-	nbits := bigint.BitLen()
-	if nbits == 0 {
+	if sign := bigint.Sign(); sign == 0 {
 		return "0x0"
+	} else if sign > 0 {
+		return "0x" + bigint.Text(16)
+	} else {
+		return "-0x" + bigint.Text(16)[1:]
 	}
-	return fmt.Sprintf("%#x", bigint)
 }
 
 func has0xPrefix(input string) bool {
