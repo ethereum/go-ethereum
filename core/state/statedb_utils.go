a1e6ae4152be490ffdba695a4b348286e7889cad
Gerui Wang <wgr523@gmail.com>
2021-11-03 21:05:25 +0800

    make statedb_utils.go (XDCValidator reader) correct, add statedb utils test

--- core/state/statedb_utils.go
+++ core/state/statedb_utils.go
@@ -66,18 +66,20 @@ func GetOpening(statedb *StateDB, address common.Address) [32]byte {
 	return ret
 }
 
+// The smart contract and the compiled byte code (in corresponding *.go file) is at commit "KYC Layer added." 7f856ffe672162dfa9c4006c89afb45a24fb7f9f
+// Notice that if smart contract and the compiled byte code (in corresponding *.go file) changes, below also changes
 var (
 	slotValidatorMapping = map[string]uint64{
 		"withdrawsState":         0,
 		"validatorsState":        1,
 		"voters":                 2,
-		"candidates":             3,
-		"candidateCount":         4,
-		"minCandidateCap":        5,
-		"minVoterCap":            6,
-		"maxValidatorNumber":     7,
-		"candidateWithdrawDelay": 8,
-		"voterWithdrawDelay":     9,
+		"candidates":             8,
+		"candidateCount":         9,
+		"minCandidateCap":        11,
+		"minVoterCap":            12,
+		"maxValidatorNumber":     13,
+		"candidateWithdrawDelay": 14,
+		"voterWithdrawDelay":     15,
 	}
 )
 
