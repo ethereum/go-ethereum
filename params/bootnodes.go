691ad78465a4d29a5b6920fa34bcd7861b114f22
Martin Holst Swende <martin@swende.se>
2021-11-08 10:46:38 +0100

    params: fix

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -45,9 +45,9 @@ var RopstenBootnodes = []string{
 // Sepolia test network.
 var SepoliaBootnodes = []string{
 	// geth
-	"enode://9246d00bc8fd1742e5ad2428b80fc4dc45d786283e05ef6edbd9002cbc335d40998444732fbe921cb88e1d2c73d1b1de53bae6a2237996e9bfe14f871baf7066@18.168.182.86:30303"
+	"enode://9246d00bc8fd1742e5ad2428b80fc4dc45d786283e05ef6edbd9002cbc335d40998444732fbe921cb88e1d2c73d1b1de53bae6a2237996e9bfe14f871baf7066@18.168.182.86:30303",
 	// besu
-	"enode://ec66ddcf1a974950bd4c782789a7e04f8aa7110a72569b6e65fcd51e937e74eed303b1ea734e4d19cfaec9fbff9b6ee65bf31dcb50ba79acce9dd63a6aca61c7@52.14.151.177:30303"
+	"enode://ec66ddcf1a974950bd4c782789a7e04f8aa7110a72569b6e65fcd51e937e74eed303b1ea734e4d19cfaec9fbff9b6ee65bf31dcb50ba79acce9dd63a6aca61c7@52.14.151.177:30303",
 }
 
 // RinkebyBootnodes are the enode URLs of the P2P bootstrap nodes running on the
