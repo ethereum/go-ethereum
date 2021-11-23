41c2f754cc223a1c1715cf78b230c49c860f4239
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2021-11-23 14:02:45 +0100

    remove unnecessary cancun block declaration in tests (#33)

--- consensus/misc/eip1559_test.go
+++ consensus/misc/eip1559_test.go
@@ -45,7 +45,6 @@ func copyConfig(original *params.ChainConfig) *params.ChainConfig {
 		BerlinBlock:             original.BerlinBlock,
 		LondonBlock:             original.LondonBlock,
 		TerminalTotalDifficulty: original.TerminalTotalDifficulty,
-		CancunBlock:             original.CancunBlock,
 		Ethash:                  original.Ethash,
 		Clique:                  original.Clique,
 	}
