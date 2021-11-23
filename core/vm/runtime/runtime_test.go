dfb8f322348bfc48b3fcdce95ec1b5bb0193ac69
Sina Mahmoodi <itz.s1na@gmail.com>
2021-11-23 13:20:43 +0100

    core/vm: minor

--- core/vm/runtime/runtime_test.go
+++ core/vm/runtime/runtime_test.go
@@ -34,11 +34,11 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/ethereum/go-ethereum/eth/tracers"
+	"github.com/ethereum/go-ethereum/eth/tracers/logger"
 	"github.com/ethereum/go-ethereum/params"
 
 	// force-load js tracers to trigger registration
 	_ "github.com/ethereum/go-ethereum/eth/tracers/js"
-	"github.com/ethereum/go-ethereum/eth/tracers/logger"
 )
 
 func TestDefaults(t *testing.T) {
