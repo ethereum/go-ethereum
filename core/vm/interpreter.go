3d3cc6c6d74323a6f5f7df1f9fad1d157df8932a
Paweł Bylica <chfast@gmail.com>
2021-11-24 16:02:12 +0100

    core/vm: use proper JumpTable type (#23967)

--- core/vm/interpreter.go
+++ core/vm/interpreter.go
@@ -32,7 +32,7 @@ type Config struct {
 	NoRecursion             bool   // Disables call, callcode, delegate call and create
 	EnablePreimageRecording bool   // Enables recording of SHA3/keccak preimages
 
-	JumpTable [256]*operation // EVM instruction table, automatically populated if unset
+	JumpTable JumpTable // EVM instruction table, automatically populated if unset
 
 	EWASMInterpreter string // External EWASM interpreter options
 	EVMInterpreter   string // External EVM interpreter options
