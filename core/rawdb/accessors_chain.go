db03faa10d402bcc70054aa2d3e70eecc37a57e2
Martin Holst Swende <martin@swende.se>
2021-12-07 17:50:58 +0100

    core, eth: improve delivery speed on header requests (#23105)
    This PR reduces the amount of work we do when answering header queries, e.g. when a peer
    is syncing from us.
    
    For some items, e.g block bodies, when we read the rlp-data from database, we plug it
    directly into the response package. We didn't do that for headers, but instead read
    headers-rlp, decode to types.Header, and re-encode to rlp. This PR changes that to keep it
    in RLP-form as much as possible. When a node is syncing from us, it typically requests 192
    contiguous headers. On master it has the following effect:
    
    - For headers not in ancient: 2 db lookups. One for translating hash->number (even though
      the request is by number), and another for reading by hash (this latter one is sometimes
      cached).
      
    - For headers in ancient: 1 file lookup/syscall for translating hash->number (even though
      the request is by number), and another for reading the header itself. After this, it
      also performes a hashing of the header, to ensure that the hash is what it expected. In
      this PR, I instead move the logic for "give me a sequence of blocks" into the lower
      layers, where the database can determine how and what to read from leveldb and/or
      ancients.
    
    There are basically four types of requests; three of them are improved this way. The
    fourth, by hash going backwards, is more tricky to optimize. However, since we know that
    the gap is 0, we can look up by the parentHash, and stlil shave off all the number->hash
    lookups.
    
    The gapped collection can be optimized similarly, as a follow-up, at least in three out of
    four cases.
    
    Co-authored-by: Felix Lange <fjl@twurst.com>

--- core/rawdb/accessors_chain.go
+++ core/rawdb/accessors_chain.go
@@ -279,6 +279,56 @@ func WriteFastTxLookupLimit(db ethdb.KeyValueWriter, number uint64) {
 	}
 }
 
+// ReadHeaderRange returns the rlp-encoded headers, starting at 'number', and going
+// backwards towards genesis. This method assumes that the caller already has
+// placed a cap on count, to prevent DoS issues.
+// Since this method operates in head-towards-genesis mode, it will return an empty
+// slice in case the head ('number') is missing. Hence, the caller must ensure that
+// the head ('number') argument is actually an existing header.
+//
+// N.B: Since the input is a number, as opposed to a hash, it's implicit that
+// this method only operates on canon headers.
+func ReadHeaderRange(db ethdb.Reader, number uint64, count uint64) []rlp.RawValue {
+	var rlpHeaders []rlp.RawValue
+	if count == 0 {
+		return rlpHeaders
+	}
+	i := number
+	if count-1 > number {
+		// It's ok to request block 0, 1 item
+		count = number + 1
+	}
+	limit, _ := db.Ancients()
+	// First read live blocks
+	if i >= limit {
+		// If we need to read live blocks, we need to figure out the hash first
+		hash := ReadCanonicalHash(db, number)
+		for ; i >= limit && count > 0; i-- {
+			if data, _ := db.Get(headerKey(i, hash)); len(data) > 0 {
+				rlpHeaders = append(rlpHeaders, data)
+				// Get the parent hash for next query
+				hash = types.HeaderParentHashFromRLP(data)
+			} else {
+				break // Maybe got moved to ancients
+			}
+			count--
+		}
+	}
+	if count == 0 {
+		return rlpHeaders
+	}
+	// read remaining from ancients
+	max := count * 700
+	data, err := db.AncientRange(freezerHeaderTable, i+1-count, count, max)
+	if err == nil && uint64(len(data)) == count {
+		// the data is on the order [h, h+1, .., n] -- reordering needed
+		for i := range data {
+			rlpHeaders = append(rlpHeaders, data[len(data)-1-i])
+		}
+	}
+	return rlpHeaders
+}
+
 // ReadHeaderRLP retrieves a block header in its raw RLP database encoding.
 func ReadHeaderRLP(db ethdb.Reader, hash common.Hash, number uint64) rlp.RawValue {
 	var data []byte
