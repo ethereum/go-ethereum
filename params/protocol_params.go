b3b8b268eb585dfd3c1c9e9bbebc55968f3bec4b
jovijovi <mageyul@hotmail.com>
2021-12-14 17:17:25 +0800

    consensus: define MaxGasLimit as a const in params (#24092)
    * consensus: use the maxGasLimit constant to check the header.GasLimit to avoid creating new variables repeatedly
    
    * consensus: check the header.GasLimit by the public constant MaxGasLimit
    
    * consensus: check the header.GasLimit by the constant MaxGasLimit

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -19,9 +19,10 @@ package params
 import "math/big"
 
 const (
-	GasLimitBoundDivisor uint64 = 1024    // The bound divisor of the gas limit, used in update calculations.
-	MinGasLimit          uint64 = 5000    // Minimum the gas limit may ever be.
-	GenesisGasLimit      uint64 = 4712388 // Gas limit of the Genesis block.
+	GasLimitBoundDivisor uint64 = 1024               // The bound divisor of the gas limit, used in update calculations.
+	MinGasLimit          uint64 = 5000               // Minimum the gas limit may ever be.
+	MaxGasLimit          uint64 = 0x7fffffffffffffff // Maximum the gas limit (2^63-1).
+	GenesisGasLimit      uint64 = 4712388            // Gas limit of the Genesis block.
 
 	MaximumExtraDataSize  uint64 = 32    // Maximum size extra data may be after Genesis.
 	ExpByteGas            uint64 = 10    // Times ceil(log256(exponent)) for the EXP instruction.
