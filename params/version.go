c227a072418626dd758ceabffd2ea7dadac6eecb
Sandeep Sreenath <sandeep.sreenath@gmail.com>
2021-12-15 17:55:18 +0530

    bumped version to beta2

--- params/version.go
+++ params/version.go
@@ -21,10 +21,10 @@ import (
 )
 
 const (
-	VersionMajor = 0        // Major version component of the current release
-	VersionMinor = 2        // Minor version component of the current release
-	VersionPatch = 13       // Patch version component of the current release
-	VersionMeta  = "beta"   // Version metadata to append to the version string
+	VersionMajor = 0       // Major version component of the current release
+	VersionMinor = 2       // Minor version component of the current release
+	VersionPatch = 13      // Patch version component of the current release
+	VersionMeta  = "beta2" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
