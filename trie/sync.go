98be5f9a7257a9145ccae8a7d913fb15077c6ce5
Water <44689567+codeoneline@users.noreply.github.com>
2022-01-04 23:23:52 +0800

    trie: fix spelling mistake (#24185)
    mispelled words in comments: th enext

--- trie/sync.go
+++ trie/sync.go
@@ -224,7 +224,7 @@ func (s *Sync) Missing(max int) (nodes []common.Hash, paths []SyncPath, codes []
 		codeHashes []common.Hash
 	)
 	for !s.queue.Empty() && (max == 0 || len(nodeHashes)+len(codeHashes) < max) {
-		// Retrieve th enext item in line
+		// Retrieve the next item in line
 		item, prio := s.queue.Peek()
 
 		// If we have too many already-pending tasks for this depth, throttle
