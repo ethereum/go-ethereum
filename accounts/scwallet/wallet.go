23471288c841fc4e6e2534aeea0263c1e2fc3eba
ucwong <ucwong@126.com>
2022-01-07 22:45:53 +0800

    accouts/scwallet: typo fix (#24207)

--- accounts/scwallet/wallet.go
+++ accounts/scwallet/wallet.go
@@ -638,7 +638,7 @@ func (w *Wallet) Derive(path accounts.DerivationPath, pin bool) (accounts.Accoun
 // accounts.
 //
 // Note, self derivation will increment the last component of the specified path
-// opposed to decending into a child path to allow discovering accounts starting
+// opposed to descending into a child path to allow discovering accounts starting
 // from non zero components.
 //
 // Some hardware wallets switched derivation paths through their evolution, so
