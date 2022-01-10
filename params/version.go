8bd07e48bde1b3e675d96de0750d7c14ca581c47
Sandeep Sreenath <ssandeep@users.noreply.github.com>
2022-01-11 00:23:34 +0530

    Update version.go

--- params/version.go
+++ params/version.go
@@ -24,7 +24,7 @@ const (
 	VersionMajor = 0       // Major version component of the current release
 	VersionMinor = 2       // Minor version component of the current release
 	VersionPatch = 13      // Patch version component of the current release
-	VersionMeta  = "beta2" // Version metadata to append to the version string
+	VersionMeta  = "stable" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
