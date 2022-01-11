52448e9585a9db3309a1344961eee1a11ca47c5e
Vie <yangchenzhong@gmail.com>
2022-01-12 04:34:53 +0800

    cmd/geth: update copyright year (#24224)
    * cmd/geth: update copyright year
    
    * cmd/geth: update copyright year

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -208,7 +208,7 @@ func init() {
 	// Initialize the CLI app and start Geth
 	app.Action = geth
 	app.HideVersion = true // we have a command to print the version
-	app.Copyright = "Copyright 2013-2021 The go-ethereum Authors"
+	app.Copyright = "Copyright 2013-2022 The go-ethereum Authors"
 	app.Commands = []cli.Command{
 		// See chaincmd.go:
 		initCommand,
