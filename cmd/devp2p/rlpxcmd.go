6ce4670bc03602bb6638c490f892e848647c15c0
Martin Holst Swende <martin@swende.se>
2022-02-04 15:24:32 +0100

    cmd/devp2p: implement snap protocol testing (#24276)
    This also contains some changes to the protocol handler to
    make the tests pass.

--- cmd/devp2p/rlpxcmd.go
+++ cmd/devp2p/rlpxcmd.go
@@ -36,6 +36,7 @@ var (
 		Subcommands: []cli.Command{
 			rlpxPingCommand,
 			rlpxEthTestCommand,
+			rlpxSnapTestCommand,
 		},
 	}
 	rlpxPingCommand = cli.Command{
@@ -48,6 +49,16 @@ var (
 		Usage:     "Runs tests against a node",
 		ArgsUsage: "<node> <chain.rlp> <genesis.json>",
 		Action:    rlpxEthTest,
+		Flags: []cli.Flag{
+			testPatternFlag,
+			testTAPFlag,
+		},
+	}
+	rlpxSnapTestCommand = cli.Command{
+		Name:      "snap-test",
+		Usage:     "Runs tests against a node",
+		ArgsUsage: "<node> <chain.rlp> <genesis.json>",
+		Action:    rlpxSnapTest,
 		Flags: []cli.Flag{
 			testPatternFlag,
 			testTAPFlag,
@@ -105,4 +116,16 @@ func rlpxEthTest(ctx *cli.Context) error {
 		return runTests(ctx, suite.EthTests())
 	}
 	return runTests(ctx, suite.AllEthTests())
+}
+
+// rlpxSnapTest runs the snap protocol test suite.
+func rlpxSnapTest(ctx *cli.Context) error {
+	if ctx.NArg() < 3 {
+		exit("missing path to chain.rlp as command-line argument")
+	}
+	suite, err := ethtest.NewSuite(getNodeArg(ctx), ctx.Args()[1], ctx.Args()[2])
+	if err != nil {
+		exit(err)
+	}
+	return runTests(ctx, suite.SnapTests())
 }
