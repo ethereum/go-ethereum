f83107f5d283237af0e2c01000b8fa1f1c044b76
George Kadianakis <desnacked@riseup.net>
2022-02-18 18:38:18 -0700

    Introduce unittests for KZG and the trusted setup

---
+++ tests/sharding_test.go
@@ -0,0 +1,105 @@
+package tests
+
+import (
+	"math"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/crypto/kzg"
+
+	gokzg "github.com/protolambda/go-kzg"
+	"github.com/protolambda/go-kzg/bls"
+)
+
+func TestGoKzg(t *testing.T) {
+	/// Test the go-kzg library for correctness
+	/// Do the trusted setup, generate a polynomial, commit to it, make proof, verify proof.
+
+	// Generate roots of unity
+	fs := gokzg.NewFFTSettings(uint8(math.Log2(kzg.CHUNKS_PER_BLOB)))
+
+	// Create a CRS with `n` elements for `s`
+	s := "1927409816240961209460912649124"
+	kzg_setup_g1, kzg_setup_g2 := gokzg.GenerateTestingSetup(s, kzg.CHUNKS_PER_BLOB)
+
+	// Wrap it all up in KZG settings
+	kzg_settings := gokzg.NewKZGSettings(fs, kzg_setup_g1, kzg_setup_g2)
+
+	kzg_setup_lagrange, err := fs.FFTG1(kzg_settings.SecretG1[:kzg.CHUNKS_PER_BLOB], true)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create testing polynomial (in coefficient form)
+	polynomial := make([]bls.Fr, kzg.CHUNKS_PER_BLOB, kzg.CHUNKS_PER_BLOB)
+	for i := uint64(0); i < kzg.CHUNKS_PER_BLOB; i++ {
+		bls.CopyFr(&polynomial[i], bls.RandomFr())
+	}
+
+	// Get polynomial in evaluation form
+	evalPoly, err := fs.FFT(polynomial, false)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Get commitments to polynomial
+	commitmentByCoeffs := kzg_settings.CommitToPoly(polynomial)
+	commitmentByEval := gokzg.CommitToEvalPoly(kzg_setup_lagrange, evalPoly)
+	if !bls.EqualG1(commitmentByEval, commitmentByCoeffs) {
+		t.Fatalf("expected commitments to be equal, but got:\nby eval: %s\nby coeffs: %s",
+			commitmentByEval, commitmentByCoeffs)
+	}
+
+	// Create proof for testing
+	x := uint64(17)
+	proof := kzg_settings.ComputeProofSingle(polynomial, x)
+
+	// Get actual evaluation at x
+	var x_fr bls.Fr
+	bls.AsFr(&x_fr, x)
+	var value bls.Fr
+	bls.EvalPolyAt(&value, polynomial, &x_fr)
+
+	// Check proof against evaluation
+	if !kzg_settings.CheckProofSingle(commitmentByEval, proof, &x_fr, &value) {
+		t.Fatal("could not verify proof")
+	}
+}
+
+func TestKzg(t *testing.T) {
+	/// Test the geth KZG module (use our trusted setup instead of creating a new one)
+
+	// First let's do some go-kzg preparations to be able to convert polynomial between coefficient and evaluation form
+	fs := gokzg.NewFFTSettings(uint8(math.Log2(kzg.CHUNKS_PER_BLOB)))
+
+	// Create testing polynomial (in coefficient form)
+	polynomial := make([]bls.Fr, kzg.CHUNKS_PER_BLOB, kzg.CHUNKS_PER_BLOB)
+	for i := uint64(0); i < kzg.CHUNKS_PER_BLOB; i++ {
+		bls.CopyFr(&polynomial[i], bls.RandomFr())
+	}
+
+	// Get polynomial in evaluation form
+	evalPoly, err := fs.FFT(polynomial, false)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Now let's start testing the kzg module
+	// Create a commitment
+	commitment := kzg.BlobToKzg(evalPoly)
+
+	// Create proof for testing
+	x := uint64(17)
+	proof := kzg.ComputeProof(polynomial, x)
+
+	// Get actual evaluation at x
+	var x_fr bls.Fr
+	bls.AsFr(&x_fr, x)
+	var value bls.Fr
+	bls.EvalPolyAt(&value, polynomial, &x_fr)
+	t.Log("value\n", bls.FrStr(&value))
+
+	// Verify kzg proof
+	if kzg.VerifyKzgProof(*commitment, x_fr, value, *proof) != true {
+		panic("failed proof verification")
+	}
+}
