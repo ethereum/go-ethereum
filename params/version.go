555e2381882c692f1925494717f2a8428edba67d
Arpit Temani <temaniarpit27@gmail.com>
2022-02-24 18:03:32 +0530

    increase block time

--- params/version.go
+++ params/version.go
@@ -24,7 +24,7 @@ const (
 	VersionMajor = 0        // Major version component of the current release
 	VersionMinor = 2        // Minor version component of the current release
 	VersionPatch = 15       // Patch version component of the current release
-	VersionMeta  = "beta-1" // Version metadata to append to the version string
+	VersionMeta  = "beta-2" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
