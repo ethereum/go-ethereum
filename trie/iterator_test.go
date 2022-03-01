789f91e27fe98506344d29b11742701b38b8a40d
Gary Rong <garyrong0905@gmail.com>
2022-03-01 13:33:45 +0800

    core, ethdb, tests, trie: introduce database snapshot

--- trie/iterator_test.go
+++ trie/iterator_test.go
@@ -475,9 +475,13 @@ func (l *loggingDb) NewBatchWithSize(size int) ethdb.Batch {
 }
 
 func (l *loggingDb) NewIterator(prefix []byte, start []byte) ethdb.Iterator {
-	fmt.Printf("NewIterator\n")
 	return l.backend.NewIterator(prefix, start)
 }
+
+func (l *loggingDb) NewSnapshot() (ethdb.Snapshot, error) {
+	return l.backend.NewSnapshot()
+}
+
 func (l *loggingDb) Stat(property string) (string, error) {
 	return l.backend.Stat(property)
 }
