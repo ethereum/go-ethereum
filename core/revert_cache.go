dcfb69a4d5963cc271b564fad5ff3408180e507b
Dmarz <danmarzec@icloud.com>
2022-03-07 14:29:08 -0700

    Reorg Feed : Update payload (#50)
    * [Feature] Reorg feed (#43)
    
    * Add trace results to NewFullBlocksWithPeers feed
    
    This commit runs the Go Call Tracer during the block import phase,
    making those results available to the NewFullBlocksWithPeers feed.
    
    The trace results are included with the transaction receipts. Even
    transactions which are simple ETH transfers will have a trace
    included.
    
    The Go Call Tracer had to be moved from the core/tracers/ package to
    core/tracers/custom/ to avoid import loops, but its functionality
    has not changed.
    
    * Add reorg feed
    
    This adds a reorg feed, capturing reorgs in the block processor
    and making them available to a reorgFeed subscription.
    
    The response object include:
    
    * common ancestor hash
    * common ancestor number
    * added block hashes
    * removed block hashes
    
    * Add json formatting
    
    Co-authored-by: Austin Roberts <austin.roberts@openrelay.xyz>
    
    * Revert "[Feature] Reorg feed (#43)" (#46)
    
    This reverts commit 2c990ef0b10c494df16d0ed7a0d540e8294501a9.
    
    * add list of tx hashes from block bodies of added and removed blocks (#49)
    
    Co-authored-by: Austin Roberts <austin.roberts@openrelay.xyz>

--- core/revert_cache.go
+++ core/revert_cache.go
@@ -1,63 +1,89 @@
 package core
 
 import (
-  "fmt"
-  "github.com/ethereum/go-ethereum/common"
-  "github.com/ethereum/go-ethereum/common/hexutil"
-  "github.com/ethereum/go-ethereum/core/types"
-  "github.com/ethereum/go-ethereum/event"
-  "github.com/ethereum/go-ethereum/accounts/abi"
-  lru "github.com/hashicorp/golang-lru"
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/event"
+	lru "github.com/hashicorp/golang-lru"
 )
 
 var (
-  revertCache *lru.Cache
-  reorgFeed event.Feed
+	revertCache *lru.Cache
+	reorgFeed   event.Feed
 )
 
 func CacheRevertReason(h, blockHash common.Hash, reason []byte) {
-  if revertCache == nil { revertCache, _ = lru.New(10000) }
-  if reason != nil {
-    key := [64]byte{}
-    copy(key[:32], blockHash[:])
-    copy(key[32:], h[:])
-    if reasonString, err := abi.UnpackRevert(reason); err == nil {
-      revertCache.Add(key, reasonString)
-    } else {
-      revertCache.Add(key, fmt.Sprintf("%#x", reason))
-    }
-  }
+	if revertCache == nil {
+		revertCache, _ = lru.New(10000)
+	}
+	if reason != nil {
+		key := [64]byte{}
+		copy(key[:32], blockHash[:])
+		copy(key[32:], h[:])
+		if reasonString, err := abi.UnpackRevert(reason); err == nil {
+			revertCache.Add(key, reasonString)
+		} else {
+			revertCache.Add(key, fmt.Sprintf("%#x", reason))
+		}
+	}
 }
 
 func GetRevertReason(h, blockHash common.Hash) (string, bool) {
-  if revertCache == nil { revertCache, _ = lru.New(10000) }
-  key := [64]byte{}
-  copy(key[:32], blockHash[:])
-  copy(key[32:], h[:])
-  if v, ok := revertCache.Get(key); ok {
-    return v.(string), true
-  }
-  return "", false
+	if revertCache == nil {
+		revertCache, _ = lru.New(10000)
+	}
+	key := [64]byte{}
+	copy(key[:32], blockHash[:])
+	copy(key[32:], h[:])
+	if v, ok := revertCache.Get(key); ok {
+		return v.(string), true
+	}
+	return "", false
 }
+
 type Reorg struct {
-	Common  common.Hash    `json:"common"`
-	Number  hexutil.Uint64 `json:"number"`
-	Removed []common.Hash  `json:"removed"`
-	Added   []common.Hash  `json:"added"`
+	Common        common.Hash     `json:"common"`
+	Number        hexutil.Uint64  `json:"number"`
+	Removed       []common.Hash   `json:"removed"`
+	RemovedHashes [][]common.Hash `json:"removedhashes"`
+	Added         []common.Hash   `json:"added"`
+	AddedHashes   [][]common.Hash `json:"addedhashes"`
 }
 
 func sendReorg(commonAncestor *types.Block, removed, added types.Blocks) {
 	reorg := &Reorg{
-		Common: commonAncestor.Hash(),
-		Number: hexutil.Uint64(commonAncestor.NumberU64()),
-		Removed: make([]common.Hash, len(removed)),
-		Added: make([]common.Hash, len(added)),
+		Common:        commonAncestor.Hash(),
+		Number:        hexutil.Uint64(commonAncestor.NumberU64()),
+		Removed:       make([]common.Hash, len(removed)),
+		RemovedHashes: make([][]common.Hash, len(removed)),
+		Added:         make([]common.Hash, len(added)),
+		AddedHashes:   make([][]common.Hash, len(added)),
 	}
+	// collect hashes from each removed block
 	for i, block := range removed {
 		reorg.Removed[i] = block.Hash()
+		blockBody := block.Body()
+		transcations := blockBody.Transactions
+		txnHashList := make([]common.Hash, len(transcations))
+		for i, txn := range transcations {
+			txnHashList[i] = txn.Hash()
+		}
+		reorg.RemovedHashes[i] = txnHashList
 	}
+	// collect hashes from each added block
 	for i, block := range added {
 		reorg.Added[i] = block.Hash()
+		blockBody := block.Body()
+		transcations := blockBody.Transactions
+		txnHashList := make([]common.Hash, len(transcations))
+		for i, txn := range transcations {
+			txnHashList[i] = txn.Hash()
+		}
+		reorg.AddedHashes[i] = txnHashList
 	}
 	reorgFeed.Send(reorg)
 }
