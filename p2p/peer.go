4f1c4231fdc53883198c0c1b092676894b558a16
Felix Lange <fjl@twurst.com>
2022-03-07 11:44:43 +0100

    p2p: define DiscReason as uint8
    All other implementations store disconnect reason as a single byte.

--- p2p/peer.go
+++ p2p/peer.go
@@ -332,11 +332,11 @@ func (p *Peer) handle(msg Msg) error {
 		msg.Discard()
 		go SendItems(p.rw, pongMsg)
 	case msg.Code == discMsg:
-		var reason [1]DiscReason
 		// This is the last message. We don't need to discard or
 		// check errors because, the connection will be closed after it.
-		rlp.Decode(msg.Payload, &reason)
-		return reason[0]
+		var m struct{ R DiscReason }
+		rlp.Decode(msg.Payload, &m)
+		return m.R
 	case msg.Code < baseProtocolLength:
 		// ignore other base protocol messages
 		return msg.Discard()
