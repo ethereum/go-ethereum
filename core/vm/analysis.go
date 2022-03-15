520e90f237007a875fd62f9c8984edb7102da5cb
BananaLF <864685021@qq.com>
2022-03-16 00:39:45 +0800

    Merge PR: Cache codeanalysis (#5)
    * op bitmap cache
    
    * op bitmap cache
    
    * remove useless code
    
    * add verify code

--- core/vm/analysis.go
+++ core/vm/analysis.go
@@ -16,6 +16,11 @@
 
 package vm
 
+import (
+	"github.com/ethereum/go-ethereum/common"
+	lru "github.com/hashicorp/golang-lru"
+)
+
 const (
 	set2BitsMask = uint16(0b1100_0000_0000_0000)
 	set3BitsMask = uint16(0b1110_0000_0000_0000)
@@ -24,6 +29,10 @@ const (
 	set6BitsMask = uint16(0b1111_1100_0000_0000)
 	set7BitsMask = uint16(0b1111_1110_0000_0000)
 )
+
+const codeBitmapCacheSize = 10000
+
+var codeBitmapCache, _ = lru.NewARC(codeBitmapCacheSize)
 
 // bitvec is a bit vector which maps bytes in a program.
 // An unset bit means the byte is an opcode, a set bit means
@@ -121,4 +130,18 @@ func codeBitmapInternal(code, bits bitvec) bitvec {
 		}
 	}
 	return bits
+}
+
+func codeBitmapWithCache(code []byte, codeHash common.Hash) bitvec {
+	if (codeHash == emptyCodeHash || codeHash == common.Hash{}) {
+		return nil
+	}
+
+	if val, ok := codeBitmapCache.Get(codeHash); ok {
+		return val.(bitvec)
+	} else {
+		val := codeBitmap(code)
+		codeBitmapCache.Add(codeHash, val)
+		return val
+	}
 }
