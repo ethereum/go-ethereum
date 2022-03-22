1720ba8533699d6e848b9a00fd99d30e6a1a8d5d
Dmarz <danmarzec@icloud.com>
2022-03-22 15:16:30 -0400

    try new struct logger tracert

--- core/state_processor.go
+++ core/state_processor.go
@@ -28,6 +28,7 @@ import (
 	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/eth/tracers/custom"
+	"github.com/ethereum/go-ethereum/eth/tracers/logger"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/params"
 )
@@ -245,8 +246,11 @@ func ApplyUnsignedTransactionWithResult(config *params.ChainConfig, bc ChainCont
 	var (
 		tracer vm.EVMLogger
 	)
+
 	// Add tracer native go tracer, code from eth/tracers/api.go
-	tracer = custom.NewCallTracer(statedb)
+	//tracer = custom.NewCallTracer(statedb)
+
+	tracer = logger.NewStructLogger(nil)
 
 	// Create a new context to be used in the EVM environment
 	blockContext := NewEVMBlockContext(header, bc, author)
