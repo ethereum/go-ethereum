bbac394897e4a2e934aef2a9be688217a1f789c9
Dmarz <danmarzec@icloud.com>
2022-03-28 13:52:47 -0400

    a lil more clean up

--- core/state_processor.go
+++ core/state_processor.go
@@ -184,8 +184,6 @@ func applyTransactionWithResult(msg types.Message, config *params.ChainConfig, b
 	// }
 
 	// Set the receipt logs and create the bloom filter.
-	// receipt.Logs = statedb.GetLogs(tx.Hash(), header.Hash())
-	receipt.Bloom = types.CreateBloom(types.Receipts{receipt})
 	receipt.BlockHash = header.Hash()
 	receipt.BlockNumber = header.Number
 	receipt.TransactionIndex = uint(statedb.TxIndex())
