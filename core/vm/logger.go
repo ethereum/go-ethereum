06190d0642afe076b257d67ad40b6b85e0a6e087
HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>
2022-04-02 11:25:07 +0800

    fix FormatLogs memory (#75)

--- core/vm/logger.go
+++ core/vm/logger.go
@@ -437,8 +437,7 @@ func FormatLogs(logs []StructLog) []types.StructLogRes {
 		if len(trace.Memory) != 0 {
 			memory := make([]string, 0, (len(trace.Memory)+31)/32)
 			for i := 0; i+32 <= len(trace.Memory); i += 32 {
-				bytes := new(big.Int).SetBytes(trace.Memory[i : i+32]).Bytes()
-				memory = append(memory, hexutil.Encode(bytes))
+				memory = append(memory, fmt.Sprintf("%x", trace.Memory[i:i+32]))
 			}
 			formatted[index].Memory = &memory
 		}
