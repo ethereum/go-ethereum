7d7a96530bc04c669aeadd5bed9e9d4cc6239bd7
jwasinger <j-wasinger@hotmail.com>
2022-04-24 21:16:49 -1000

    cmd/evm: ensure input length is even (#24721)
    * cmd/evm: ensure input length is even
    
    * cmd/evm: minor nit + lintfix
    
    Co-authored-by: Martin Holst Swende <martin@swende.se>

--- cmd/evm/runner.go
+++ cmd/evm/runner.go
@@ -246,7 +246,12 @@ func runCmd(ctx *cli.Context) error {
 	} else {
 		hexInput = []byte(ctx.GlobalString(InputFlag.Name))
 	}
-	input := common.FromHex(string(bytes.TrimSpace(hexInput)))
+	hexInput = bytes.TrimSpace(hexInput)
+	if len(hexInput)%2 != 0 {
+		fmt.Println("input length must be even")
+		os.Exit(1)
+	}
+	input := common.FromHex(string(hexInput))
 
 	var execFunc func() ([]byte, uint64, error)
 	if ctx.GlobalBool(CreateFlag.Name) {
