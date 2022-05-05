33d7a469f698f39ee083414538d124c6be3660ec
Ikko Ashimine <eltociear@gmail.com>
2022-05-06 03:20:11 +0900

    p2p/simulations: fix typo in network_test.go (#24824)

--- p2p/simulations/network_test.go
+++ p2p/simulations/network_test.go
@@ -448,7 +448,7 @@ func TestGetNodeIDs(t *testing.T) {
 	numNodes := 5
 	nodes, err := createTestNodes(numNodes, network)
 	if err != nil {
-		t.Fatalf("Could not creat test nodes %v", err)
+		t.Fatalf("Could not create test nodes %v", err)
 	}
 
 	gotNodeIDs := network.GetNodeIDs()
@@ -497,7 +497,7 @@ func TestGetNodes(t *testing.T) {
 	numNodes := 5
 	nodes, err := createTestNodes(numNodes, network)
 	if err != nil {
-		t.Fatalf("Could not creat test nodes %v", err)
+		t.Fatalf("Could not create test nodes %v", err)
 	}
 
 	gotNodes := network.GetNodes()
