8a008ee0e6f08eb73407158a6ce2716d59ae40fc
||= nil <103940716+nedifi@users.noreply.github.com>
2022-05-11 00:29:22 +0200

    cmd/geth: print info banner for --dev mode (#24759)
    Co-authored-by: nedifi <nedifi@users.noreply.github.com>
    Co-authored-by: Felix Lange <fjl@twurst.com>

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -286,6 +286,21 @@ func prepare(ctx *cli.Context) {
 
 	case ctx.GlobalIsSet(utils.DeveloperFlag.Name):
 		log.Info("Starting Geth in ephemeral dev mode...")
+		log.Warn(`You are running Geth in --dev mode. Please note the following:
+
+  1. This mode is only intended for fast, iterative development without assumptions on
+     security or persistence.
+  2. The database is created in memory unless specified otherwise. Therefore, shutting down
+     your computer or losing power will wipe your entire block data and chain state for
+     your dev environment.
+  3. A random, pre-allocated developer account will be available and unlocked as
+     eth.coinbase, which can be used for testing. The random dev account is temporary,
+     stored on a ramdisk, and will be lost if your machine is restarted.
+  4. Mining is enabled by default. However, the client will only seal blocks if transactions
+     are pending in the mempool. The miner's minimum accepted gas price is 1.
+  5. Networking is disabled; there is no listen-address, the maximum number of peers is set
+     to 0, and discovery is disabled.
+`)
 
 	case !ctx.GlobalIsSet(utils.NetworkIdFlag.Name):
 		log.Info("Starting Geth on Ethereum mainnet...")
