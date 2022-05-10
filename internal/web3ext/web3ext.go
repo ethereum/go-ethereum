5b46f1d1eb4dd4e1d7b8772134bfcea08aa849d6
Sina Mahmoodi <1591639+s1na@users.noreply.github.com>
2022-05-10 10:46:26 +0200

    internal: drop TestSignCliqueBlock (#24837)

--- internal/web3ext/web3ext.go
+++ internal/web3ext/web3ext.go
@@ -234,12 +234,6 @@ web3._extend({
 			params: 1
 		}),
 		new web3._extend.Method({
-			name: 'testSignCliqueBlock',
-			call: 'debug_testSignCliqueBlock',
-			params: 2,
-			inputFormatter: [web3._extend.formatters.inputAddressFormatter, null],
-		}),
-		new web3._extend.Method({
 			name: 'setHead',
 			call: 'debug_setHead',
 			params: 1
