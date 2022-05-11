032b98e942e58ce926f185ad9a7f17f59f7d74a3
Qian Bin <cola.tin.com@gmail.com>
2022-05-11 17:00:29 +0800

    core/vm: optimize Memory.Set32 (#24847)
    * core/vm: remove unnecessary memset for Memory.Set32
    
    * core/vm: optimize Memory.Set32

--- core/vm/memory.go
+++ core/vm/memory.go
@@ -53,10 +53,9 @@ func (m *Memory) Set32(offset uint64, val *uint256.Int) {
 	if offset+32 > uint64(len(m.store)) {
 		panic("invalid memory: store empty")
 	}
-	// Zero the memory area
-	copy(m.store[offset:offset+32], []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
 	// Fill in relevant bits
-	val.WriteToSlice(m.store[offset:])
+	b32 := val.Bytes32()
+	copy(m.store[offset:], b32[:])
 }
 
 // Resize resizes the memory to size
