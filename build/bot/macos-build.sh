0227a38105f081de99b0c826640cff1a63685ed7
Felix Lange <fjl@twurst.com>
2022-05-20 15:49:31 +0200

    build/bot: remove xctool invocation in macos build script

--- build/bot/macos-build.sh
+++ build/bot/macos-build.sh
@@ -2,6 +2,10 @@
 
 set -e -x
 
+# -- Check XCode version
+xcodebuild -version
+# xcrun simctl list
+
 # -- Build for macOS and upload to Azure
 go run build/ci.go install -dlgo
 go run build/ci.go archive -type tar # -signer OSX_SIGNING_KEY -upload gethstore/builds
@@ -13,10 +17,6 @@ go run build/ci.go archive -type tar # -signer OSX_SIGNING_KEY -upload gethstore
 # sed -i '.bak' 's/repo.join/!repo.join/g' $(dirname `gem which cocoapods`)/cocoapods/sources_manager.rb
 # git clone --depth=1 https://github.com/CocoaPods/Specs.git ~/.cocoapods/repos/master
 # pod setup --verbose
-
-# -- Check XCode version
-xctool -version
-xcrun simctl list
 
 # # -- Build for iOS and upload to Azure
 # go run build/ci.go xcode -signer IOS_SIGNING_KEY -upload gethstore/builds
