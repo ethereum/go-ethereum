3f5fe0e3875f7e56be709e853efdaf58855d68ab
Krishna Upadhyaya <krishnau1604@gmail.com>
2022-05-30 13:59:30 +0530

    Increased default value of rpc.txfeecap to 5

--- eth/ethconfig/config.go
+++ eth/ethconfig/config.go
@@ -94,7 +94,7 @@ var Defaults = Config{
 	RPCGasCap:     50000000,
 	RPCEVMTimeout: 5 * time.Second,
 	GPO:           FullNodeGPO,
-	RPCTxFeeCap:   1, // 1 ether
+	RPCTxFeeCap:   5, // 5 matic
 }
 
 func init() {
