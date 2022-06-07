7bcdbc0b396b532866a838bfdd5553e62519cc05
Felix Lange <fjl@twurst.com>
2022-06-07 17:07:58 +0200

    Update receipt.go

--- core/types/receipt.go
+++ core/types/receipt.go
@@ -267,8 +267,8 @@ func (r *Receipt) Size() common.StorageSize {
 	return size
 }
 
-// ReceiptForStorage is a wrapper around a Receipt with RLP serialization that omits the Bloom
-// field and deserialization that re-computes it.
+// ReceiptForStorage is a wrapper around a Receipt with RLP serialization
+// that omits the Bloom field and deserialization that re-computes it.
 type ReceiptForStorage Receipt
 
 // EncodeRLP implements rlp.Encoder, and flattens all content fields of a receipt
