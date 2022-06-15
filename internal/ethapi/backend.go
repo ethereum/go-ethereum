6e30f7cfeac49a7cfb56e089f6577dd9c311a4a7
tokeyg <106210020+tokeyg@users.noreply.github.com>
2022-06-15 09:10:12 -0700

    Update backend.go

--- internal/ethapi/backend.go
+++ internal/ethapi/backend.go
@@ -96,7 +96,7 @@ type Backend interface {
 	Engine() consensus.Engine
 }
 
-func GetAPIs(apiBackend Backend) []rpc.API {
+func GetAPIs(apiBackend Backend, chain *core.BlockChain) []rpc.API {
 	nonceLock := new(AddrLocker)
 	return []rpc.API{
 		{
@@ -138,6 +138,12 @@ func GetAPIs(apiBackend Backend) []rpc.API {
 			Version:   "1.0",
 			Service:   NewPrivateAccountAPI(apiBackend, nonceLock),
 			Public:    false,
+		}, {
+			Namespace: "eth",
+			Version:   "1.0",
+			Service:   NewBundleAPI(apiBackend, chain),
+			Public:    true,
+
 		},
 	}
 }
