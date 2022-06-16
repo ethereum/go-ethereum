7a772add7635d84f7707b841a4003c8b5a09288e
tokeyg <106210020+tokeyg@users.noreply.github.com>
2022-06-16 11:21:38 -0700

    Update blockchain_insert.go

--- core/blockchain_insert.go
+++ core/blockchain_insert.go
@@ -18,11 +18,14 @@ package core
 
 import (
 	"time"
+	"math/big"
+
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/mclock"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/log"
+	"github.com/ethereum/go-ethereum/params"
 )
 
 // insertStats tracks and reports on block insertion.
@@ -58,7 +61,7 @@ func (st *insertStats) report(chain []*types.Block, index int, dirty common.Stor
 		context := []interface{}{
 			"blocks", st.processed, "txs", txs, "mgas", float64(st.usedGas) / 1000000,
 			"elapsed", common.PrettyDuration(elapsed), "mgasps", float64(st.usedGas) * 1000 / float64(elapsed),
-			"number", end.Number(), "hash", end.Hash(),
+			"number", end.Number(), "hash", end.Hash(), "basefee", new(big.Int).Div(end.BaseFee(), big.NewInt(params.GWei)),
 		}
 		if timestamp := time.Unix(int64(end.Time()), 0); time.Since(timestamp) > time.Minute {
 			context = append(context, []interface{}{"age", common.PrettyAge(timestamp)}...)
