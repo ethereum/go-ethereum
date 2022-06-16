60fd90ac8a7bda29434a6cf3ab1e538fd05593b5
Felix Lange <fjl@twurst.com>
2022-06-16 17:07:42 +0200

    Update api.go

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1581,8 +1581,8 @@ func (s *PublicTransactionPoolAPI) GetRawTransactionByHash(ctx context.Context, 
 func (s *PublicTransactionPoolAPI) GetTransactionReceipt(ctx context.Context, hash common.Hash) (map[string]interface{}, error) {
 	tx, blockHash, blockNumber, index, err := s.b.GetTransaction(ctx, hash)
 	if err != nil {
-		// Returns nil on purpose instead of err
-		// When the transaction doesn't exist, the return value of this RPC call should be JSON null.
+		// When the transaction doesn't exist, the RPC method should return JSON null
+		// as per specification.
 		return nil, nil
 	}
 	receipts, err := s.b.GetReceipts(ctx, blockHash)
