c776029c6cb8934f4b114d71c3f10b44b09dd50f
Zachinquarantine <Zachinquarantine@protonmail.com>
2022-06-20 04:20:32 -0400

    cmd/faucet: more verbose message about private posts (#25129)
    * cmd/faucet: Add error message for private posts
    
    Fixes #22631
    
    * grammar

--- cmd/faucet/faucet.go
+++ cmd/faucet/faucet.go
@@ -860,7 +860,7 @@ func authFacebook(url string) (string, string, common.Address, error) {
 	address := common.HexToAddress(string(regexp.MustCompile("0x[0-9a-fA-F]{40}").Find(body)))
 	if address == (common.Address{}) {
 		//lint:ignore ST1005 This error is to be displayed in the browser
-		return "", "", common.Address{}, errors.New("No Ethereum address found to fund")
+		return "", "", common.Address{}, errors.New("No Ethereum address found to fund. Please check the post URL and verify that it can be viewed publicly.")
 	}
 	var avatar string
 	if parts = regexp.MustCompile(`src="([^"]+fbcdn\.net[^"]+)"`).FindStringSubmatch(string(body)); len(parts) == 2 {
