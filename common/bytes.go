88dbbf908fce39bed5dd6548b9852c5c63bdd957
kevin <kevin@tonkworks.com>
2022-06-29 14:15:29 -0400

    eth, common: export has0xPrefix

--- common/bytes.go
+++ common/bytes.go
@@ -47,8 +47,8 @@ func CopyBytes(b []byte) (copiedBytes []byte) {
 	return
 }
 
-// has0xPrefix validates str begins with '0x' or '0X'.
-func has0xPrefix(str string) bool {
+// Has0xPrefix validates str begins with '0x' or '0X'.
+func Has0xPrefix(str string) bool {
 	return len(str) >= 2 && str[0] == '0' && (str[1] == 'x' || str[1] == 'X')
 }
 
