033c9bb9046a8de6c9e12045d7db8a10040553b2
Felix Lange <fjl@twurst.com>
2022-06-29 20:40:36 +0200

    build: upgrade to golangci-lint v1.46.2

--- build/ci.go
+++ build/ci.go
@@ -336,7 +336,7 @@ func doLint(cmdline []string) {
 
 // downloadLinter downloads and unpacks golangci-lint.
 func downloadLinter(cachedir string) string {
-	const version = "1.45.2"
+	const version = "1.46.2"
 
 	csdb := build.MustLoadChecksums("build/checksums.txt")
 	arch := runtime.GOARCH
