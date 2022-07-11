e6b333e2dd77d67cf445df1093d5f31236b44caa
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2022-07-11 14:09:34 +0200

    upgrade go-verkle to master

--- consensus/ethash/consensus.go
+++ consensus/ethash/consensus.go
@@ -318,9 +318,9 @@ func (ethash *Ethash) verifyHeader(chain consensus.ChainHeaderReader, header, pa
 		}
 	}
 	// If all checks passed, validate any special fields for hard forks
-	//if err := misc.VerifyDAOHeaderExtraData(chain.Config(), header); err != nil {
-	//return err
-	//}
+	if err := misc.VerifyDAOHeaderExtraData(chain.Config(), header); err != nil {
+		return err
+	}
 	if err := misc.VerifyForkHashes(chain.Config(), header, uncle); err != nil {
 		return err
 	}
