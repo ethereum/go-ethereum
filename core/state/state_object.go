15650416a20a3225075b7b438e41d1b01bcaced5
dbadoy4874 <dbadoy4874@gmail.com>
2022-07-11 17:05:16 +0900

    core, eth: pre-allocate in Copy()

--- core/state/state_object.go
+++ core/state/state_object.go
@@ -49,7 +49,7 @@ func (s Storage) String() (str string) {
 }
 
 func (s Storage) Copy() Storage {
-	cpy := make(Storage)
+	cpy := make(Storage, len(s))
 	for key, value := range s {
 		cpy[key] = value
 	}
