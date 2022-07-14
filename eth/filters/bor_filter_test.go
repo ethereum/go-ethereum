0610c6a6927c0c1dea914034a57ca5bfc53e0797
Shivam Sharma <shivam691999@gmail.com>
2022-07-14 18:28:21 +0530

    chg : use memory db

--- eth/filters/bor_filter_test.go
+++ eth/filters/bor_filter_test.go
@@ -28,7 +28,7 @@ func TestBorFilters(t *testing.T) {
 	defer os.RemoveAll(dir)
 
 	var (
-		db, _   = rawdb.NewLevelDBDatabase(dir, 0, 0, "", false)
+		db      = rawdb.NewMemoryDatabase()
 		backend = &testBackend{db: db}
 		key1, _ = crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
 		addr    = crypto.PubkeyToAddress(key1.PublicKey)
