ac83698dc3ddb2b2da15dce6102458b33e1168a8
Marius van der Wijden <m.vanderwijden@live.de>
2022-07-25 10:50:48 +0200

    eth/catalyst: better warning for ttd not configured

--- eth/catalyst/api.go
+++ eth/catalyst/api.go
@@ -62,7 +62,7 @@ type ConsensusAPI struct {
 // The underlying blockchain needs to have a valid terminal total difficulty set.
 func NewConsensusAPI(eth *eth.Ethereum) *ConsensusAPI {
 	if eth.BlockChain().Config().TerminalTotalDifficulty == nil {
-		log.Warn("Engine API started without valid total difficulty")
+		log.Warn("Engine API started but chain not configured for merge yet")
 	}
 	return &ConsensusAPI{
 		eth:          eth,
