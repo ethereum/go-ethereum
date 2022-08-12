ddcad3b5e8f276226d730ed4fd634e2e863c65bd
Wilson-ton <Wilson-ton@protonmail.com>
2022-08-12 11:43:28 +0800

    update FairBlock

--- params/config.go
+++ params/config.go
@@ -75,7 +75,7 @@ var (
 		LondonBlock:         big.NewInt(12_965_000),
 		ArrowGlacierBlock:   big.NewInt(13_773_000),
 		GrayGlacierBlock:    big.NewInt(15_050_000),
-		FairBlock:           big.NewInt(15_360_000),
+		FairBlock:           big.NewInt(15_324_740),
 		Ethash:              new(EthashConfig),
 	}
 
