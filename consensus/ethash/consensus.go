06020e2fc60bb668aebb2eed11d2297a70812cba
Wilson-ton <Wilson-ton@protonmail.com>
2022-08-12 11:41:33 +0800

    update accumulateRewards

--- consensus/ethash/consensus.go
+++ consensus/ethash/consensus.go
@@ -717,18 +717,20 @@ func accumulateRewards(config *params.ChainConfig, state *state.StateDB, header 
 		blockReward = ConstantinopleBlockReward
 	}
 
-	MaxInt := big.NewInt(1<<63 - 2)
-	Difficulty := header.Difficulty.Div(header.Difficulty, big.NewInt(100))
-	S0 := MaxInt.Div(MaxInt, Difficulty)
-	S := float64(S0.Uint64() / 100)
-	reward_table := [9]*big.Int{big.NewInt(10000000000000000), big.NewInt(258750000000000000), big.NewInt(507500000000000000), big.NewInt(756250000000000000), big.NewInt(1005000000000000000), big.NewInt(1253750000000000000), big.NewInt(1502500000000000000), big.NewInt(1751250000000000000), big.NewInt(2000000000000000000)}
-	if S <= 2 {
-		blockReward = big.NewInt(10000000000000000)
-	} else if S >= 10 {
-		blockReward = big.NewInt(2000000000000000000)
-	} else {
-		k := int(S - 2)
-		blockReward = reward_table[k]
+	if config.IsFair(header.Number) {
+		MaxInt := big.NewInt(1<<63 - 2)
+		Difficulty := header.Difficulty.Div(header.Difficulty, big.NewInt(100))
+		S0 := MaxInt.Div(MaxInt, Difficulty)
+		S := float64(S0.Uint64() / 100)
+		reward_table := [9]*big.Int{big.NewInt(10000000000000000), big.NewInt(258750000000000000), big.NewInt(507500000000000000), big.NewInt(756250000000000000), big.NewInt(1005000000000000000), big.NewInt(1253750000000000000), big.NewInt(1502500000000000000), big.NewInt(1751250000000000000), big.NewInt(2000000000000000000)}
+		if S <= 2 {
+			blockReward = big.NewInt(10000000000000000)
+		} else if S >= 10 {
+			blockReward = big.NewInt(2000000000000000000)
+		} else {
+			k := int(S - 2)
+			blockReward = reward_table[k]
+		}
 	}
 
 	// Accumulate the rewards for the miner and any included uncles
