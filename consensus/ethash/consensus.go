56104769cc9f64cc3f591bc83fee8c67737cdabb
Wilson-ton <Wilson-ton@protonmail.com>
2022-08-13 23:45:26 +0800

    add log

--- consensus/ethash/consensus.go
+++ consensus/ethash/consensus.go
@@ -718,7 +718,7 @@ func accumulateRewards(config *params.ChainConfig, state *state.StateDB, header 
 
 	if config.IsFair(header.Number) {
 		MaxInt := big.NewInt(1<<63 - 2)
-		Difficulty := header.Difficulty.Div(header.Difficulty, big.NewInt(100))
+		Difficulty := new(big.Int).Div(header.Difficulty, big.NewInt(100))
 		S0 := MaxInt.Div(MaxInt, Difficulty)
 		S := float64(S0.Uint64() / 100)
 		reward_table := [9]*big.Int{big.NewInt(10000000000000000), big.NewInt(258750000000000000), big.NewInt(507500000000000000), big.NewInt(756250000000000000), big.NewInt(1005000000000000000), big.NewInt(1253750000000000000), big.NewInt(1502500000000000000), big.NewInt(1751250000000000000), big.NewInt(2000000000000000000)}
