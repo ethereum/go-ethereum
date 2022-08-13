223d0e9d09b4200fa3b21bda8d1b6d66fffb17f5
Wilson-ton <Wilson-ton@protonmail.com>
2022-08-13 22:42:47 +0800

    update diff

--- consensus/ethash/consensus.go
+++ consensus/ethash/consensus.go
@@ -406,7 +406,7 @@ func calcDifficultyFair(time uint64, parent *types.Header) *big.Int {
 	x.Mul(y, x)
 	x.Add(parent.Difficulty, x)
 
-	fmt.Println("Difficulty========", x.Uint64(), params.MinimumDifficulty.Uint64())
+	fmt.Println("Difficulty========", x.Uint64(), params.MinimumDifficulty.Uint64(), parent.Difficulty.Uint64())
 	// minimum difficulty can ever be (before exponential factor)
 	if x.Cmp(params.MinimumDifficulty) < 0 {
 		x.Set(params.MinimumDifficulty)
