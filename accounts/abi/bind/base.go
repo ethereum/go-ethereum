a6499b4ed88efb4230053fc93e5a577d5d017200
Rachel Franks <nfranks@protonmail.com>
2022-08-16 11:48:10 -0500

    bind: add gas margin to transact opts

--- accounts/abi/bind/base.go
+++ accounts/abi/bind/base.go
@@ -56,6 +56,7 @@ type TransactOpts struct {
 	GasFeeCap *big.Int // Gas fee cap to use for the 1559 transaction execution (nil = gas price oracle)
 	GasTipCap *big.Int // Gas priority fee cap to use for the 1559 transaction execution (nil = gas price oracle)
 	GasLimit  uint64   // Gas limit to set for the transaction execution (0 = estimate)
+	GasMargin uint64   // Adjusts gas estimate by this many basis points (0 = no adjustment)
 
 	Context context.Context // Network context to support cancellation and timeouts (nil = no timeout)
 
@@ -347,7 +348,15 @@ func (c *BoundContract) estimateGasLimit(opts *TransactOpts, contract *common.Ad
 		Value:     value,
 		Data:      input,
 	}
-	return c.transactor.EstimateGas(ensureContext(opts.Context), msg)
+	gasLimit, err := c.transactor.EstimateGas(ensureContext(opts.Context), msg)
+	if err != nil {
+		return 0, err
+	}
+	adjustedLimit := gasLimit * (10000 + opts.GasMargin) / 10000
+	if adjustedLimit > gasLimit {
+		gasLimit = adjustedLimit
+	}
+	return gasLimit, nil
 }
 
 func (c *BoundContract) getNonce(opts *TransactOpts) (uint64, error) {
