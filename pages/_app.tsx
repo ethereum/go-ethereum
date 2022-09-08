142aa33af80a7fa0880044d3f9305f73a552b976
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-09-08 16:18:30 -0300

    setup: init

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
+}
+
