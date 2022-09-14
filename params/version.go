bdd7474072745d7a98f1c50940557ba58c354754
Wilson-ton <Wilson-ton@protonmail.com>
2022-09-14 10:28:21 +0800

    update RomeBlock 0.0.3

--- params/version.go
+++ params/version.go
@@ -23,7 +23,7 @@ import (
 const (
 	VersionMajor = 0      // Major version component of the current release
 	VersionMinor = 0      // Minor version component of the current release
-	VersionPatch = 2      // Patch version component of the current release
+	VersionPatch = 3      // Patch version component of the current release
 	VersionMeta  = "test" // Version metadata to append to the version string
 )
 
