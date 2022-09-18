0e43ff1b339bf5ac34363b08aad086b69e16af00
Felix Lange <fjl@twurst.com>
2022-09-18 02:53:31 +0200

    cmd/devp2p: use reasonable kcp settings

--- cmd/devp2p/wormhole.go
+++ cmd/devp2p/wormhole.go
@@ -34,20 +34,20 @@ import (
 )
 
 const (
-	ecParityShards = 1
-	ecDataShards   = 1
+	ecParityShards = 3
+	ecDataShards   = 10
 )
 
 func setupKCP(s *kcp.UDPSession) {
 	s.SetMtu(1200)
-	s.SetStreamMode(true)
+	// s.SetStreamMode(true)
 
 	// https://github.com/skywind3000/kcp/blob/master/README.en.md#protocol-configuration
 	// Normal Mode: ikcp_nodelay(kcp, 0, 40, 0, 0);
 	// Turbo Mode: ikcp_nodelay(kcp, 1, 10, 2, 1);
 
-	s.SetNoDelay(1, 10, 2, 1)
-	// s.SetNoDelay(0, 40, 0, 0)
+	// s.SetNoDelay(1, 10, 2, 1)
+	s.SetNoDelay(0, 40, 0, 0)
 }
 
 func discv5WormholeSend(ctx *cli.Context) error {
