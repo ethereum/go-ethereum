79858c74b126c488be7a9ecd87d73ebe1344be15
Felix Lange <fjl@twurst.com>
2022-09-18 12:00:02 +0200

    cmd/devp2p: implement file transfer

--- cmd/devp2p/wormhole.go
+++ cmd/devp2p/wormhole.go
@@ -17,18 +17,18 @@
 package main
 
 import (
-	"crypto/rand"
-	"encoding/hex"
 	"fmt"
+	"io"
 	"net"
-
 	"os"
+
 	"sync"
 	"time"
 
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/p2p/discover"
 	"github.com/ethereum/go-ethereum/p2p/enode"
+	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/urfave/cli/v2"
 	"github.com/xtaci/kcp-go"
 )
@@ -53,6 +53,23 @@ func setupKCP(s *kcp.UDPSession) {
 func discv5WormholeSend(ctx *cli.Context) error {
 	log.Root().SetHandler(log.LvlFilterHandler(log.Lvl(ctx.Int(verbosityFlag.Name)), log.StreamHandler(os.Stderr, log.TerminalFormat(true))))
 
+	if ctx.Args().Len() != 2 {
+		return fmt.Errorf("send command needs destination node and file name as arguments")
+	}
+	n := getNodeArg(ctx)
+	filePath := ctx.Args().Get(1)
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return err
+	}
+
 	// Create discv5 session.
 	unhandled := make(chan discover.ReadPacket, 10)
 	disc, socket := startV5WithUnhandled(ctx, unhandled)
@@ -60,17 +77,12 @@ func discv5WormholeSend(ctx *cli.Context) error {
 	defer close(unhandled)
 
 	// Send request
-	n := getNodeArg(ctx)
-	resp, err := disc.TalkRequest(n, "wrm", []byte("rand"))
-	if err != nil {
+	xfer := &xferStart{Size: uint64(fileInfo.Size())}
+	if err := requestTransfer(disc, n, xfer); err != nil {
 		return err
-	}
-	if string(resp) != "ok" {
-		return fmt.Errorf("talk request rejected: %s", string(resp))
 	}
 
 	addr := &net.UDPAddr{IP: n.IP(), Port: n.UDP()}
-
 	conn := newUnhandledWrapper(addr, socket)
 	go enqueueUnhandledPackets(unhandled, conn)
 
@@ -84,20 +96,11 @@ func discv5WormholeSend(ctx *cli.Context) error {
 	setupKCP(sess)
 
 	log.Info("Transmitting data")
-	rndbuf := make([]byte, 1024)
-	msg := make([]byte, hex.EncodedLen(len(rndbuf)))
-	for i := 0; i < 10; i++ {
-		rand.Read(rndbuf)
-		hex.Encode(msg, rndbuf)
-		n, err := sess.Write(msg)
-		log.Info("Sent data", "n", n, "err", err)
+	if _, err := io.CopyN(sess, file, fileInfo.Size()); err != nil {
+		return fmt.Errorf("copy error: %v", err)
 	}
 
-	if _, err := sess.Write([]byte("FIN")); err != nil {
-		return fmt.Errorf("unable to close connection: %s", err)
-	}
-
-	// KCP writes are a bit 'async', so wait for the remote to ACK the FIN.
+	// KCP writes are a bit 'async', so wait for the remote send ACK in the end.
 	sess.SetReadDeadline(time.Now().Add(5 * time.Second))
 	ackbuf := make([]byte, 3)
 	_, err = sess.Read(ackbuf)
@@ -111,15 +114,46 @@ func discv5WormholeSend(ctx *cli.Context) error {
 func discv5WormholeReceive(ctx *cli.Context) error {
 	log.Root().SetHandler(log.LvlFilterHandler(log.Lvl(ctx.Int(verbosityFlag.Name)), log.StreamHandler(os.Stderr, log.TerminalFormat(true))))
 
-	startSession := make(chan *net.UDPAddr)
+	if ctx.Args().Len() < 1 {
+		return fmt.Errorf("receive command needs filename as argument %d", ctx.Args().Len())
+	}
+	filePath := ctx.Args().First()
+
+	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+
+	err = doReceive(ctx, file)
+	file.Close()
+	if err != nil {
+		os.Remove(filePath)
+	}
+	return err
+}
+
+func doReceive(ctx *cli.Context, file *os.File) error {
+	startSession := make(chan *xferStart)
 
 	// Setup discv5 protocol.
 	unhandled := make(chan discover.ReadPacket, 10)
 	disc, socket := startV5WithUnhandled(ctx, unhandled)
 	disc.RegisterTalkHandler("wrm", func(id enode.ID, addr *net.UDPAddr, data []byte) []byte {
-		startSession <- addr
-		return []byte("ok")
+		var startReq xferStart
+		err := rlp.DecodeBytes(data, &startReq)
+		if err != nil {
+			resp, _ := rlp.EncodeToBytes(&xferResponse{Accept: false})
+			return resp
+		}
+
+		startReq.fromNode = id
+		startReq.fromAddr = addr
+		startSession <- &startReq
+
+		resp, _ := rlp.EncodeToBytes(&xferResponse{Accept: true})
+		return resp
 	})
+
 	defer close(unhandled)
 	defer disc.Close()
 
@@ -127,11 +161,11 @@ func discv5WormholeReceive(ctx *cli.Context) error {
 	fmt.Println(disc.Self())
 
 	// Wait for talk request, then start the session.
-	addr := <-startSession
-	conn := newUnhandledWrapper(addr, socket)
+	xfer := <-startSession
+	conn := newUnhandledWrapper(xfer.fromAddr, socket)
 	go enqueueUnhandledPackets(unhandled, conn)
 
-	s, err := kcp.NewConn3(0, addr, nil, ecDataShards, ecParityShards, conn)
+	s, err := kcp.NewConn3(0, xfer.fromAddr, nil, ecDataShards, ecParityShards, conn)
 	if err != nil {
 		log.Error("Could not establish kcp session", "err", err)
 		return err
@@ -141,23 +175,46 @@ func discv5WormholeReceive(ctx *cli.Context) error {
 	log.Info("KCP socket accepted")
 	setupKCP(s)
 
-	for {
-		buf := make([]byte, 2048)
-		n, err := s.Read(buf)
-		if err != nil {
-			return err
-		}
-
-		if string(buf[:n]) == "FIN" {
-			log.Trace("connection finished")
-			break
-		}
-
-		log.Info("Read KCP data", "data", string(buf[:n]))
+	if _, err := io.CopyN(file, s, int64(xfer.Size)); err != nil {
+		return fmt.Errorf("copy failed: %v", err)
 	}
 
 	s.Write([]byte("ACK"))
 	kcpStatsDump(kcp.DefaultSnmp)
+	fmt.Println("transfer OK")
+	return nil
+}
+
+type xferStart struct {
+	Size uint64
+	// Key [16]byte
+
+	fromNode enode.ID
+	fromAddr *net.UDPAddr
+}
+
+type xferResponse struct {
+	Accept bool
+	Error  string
+	// Key [16]byte
+}
+
+func requestTransfer(disc *discover.UDPv5, node *enode.Node, xfer *xferStart) error {
+	req, err := rlp.EncodeToBytes(xfer)
+	if err != nil {
+		return err
+	}
+	resp, err := disc.TalkRequest(node, "wrm", req)
+	if err != nil {
+		return err
+	}
+	var xresp xferResponse
+	if err := rlp.DecodeBytes(resp, &xresp); err != nil {
+		return err
+	}
+	if !xresp.Accept {
+		return fmt.Errorf("transfer not accepted: %s", xresp.Error)
+	}
 	return nil
 }
 
