11b4bc08a4dc4c431a9d00ade3010c63d5a9522e
Martin Holst Swende <martin@swende.se>
2022-09-26 12:43:32 +0200

    trie: add utility print-method for nodeset

--- trie/nodeset.go
+++ trie/nodeset.go
@@ -150,6 +150,29 @@ func (set *NodeSet) Hashes() []common.Hash {
 	return ret
 }
 
+// Summary returns a string-representation of the NodeSet.
+func (set *NodeSet) Summary() string {
+	var out = new(strings.Builder)
+	fmt.Fprintf(out, "nodeset owner: %v\n", set.owner)
+	if set.updates != nil {
+		for _, key := range set.updates.order {
+			updated := set.updates.nodes[key]
+			if updated.prev != nil {
+				fmt.Fprintf(out, "  [*]: %x -> %v prev: %x\n", key, updated.hash, updated.prev)
+			} else {
+				fmt.Fprintf(out, "  [+]: %x -> %v\n", key, updated.hash)
+			}
+		}
+	}
+	for k, n := range set.deletes {
+		fmt.Fprintf(out, "  [-]: %x -> %x\n", k, n)
+	}
+	for _, n := range set.leaves {
+		fmt.Fprintf(out, "[leaf]: %v\n", n)
+	}
+	return out.String()
+}
+
 // MergedNodeSet represents a merged dirty node set for a group of tries.
 type MergedNodeSet struct {
 	sets map[common.Hash]*NodeSet
