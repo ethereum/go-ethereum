45f82e279375030e0f0212f446d44168b6c7e095
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-09-30 10:27:30 -0300

    chore: update constants

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+// internal pages
+export const DOWNLOADS_PAGE = '/downloads';
+export const DOCS_PAGE = '/docs';
+export const FAQ_PAGE = '/docs/faq';
+export const CONTRIBUTING_PAGE = `${DOCS_PAGE}/developers/contributing`;
+
+// external links
+export const ETHEREUM_ORG_URL = 'https://ethereum.org';
+export const ETHEREUM_ORG_RUN_A_NODE_URL = 'https://ethereum.org/en/run-a-node/';
+export const ETHEREUM_FOUNDATION_URL = 'https://ethereum.foundation';
+export const GETH_REPO_URL = 'https://github.com/ethereum/go-ethereum';
