1dd6bc65a5df34b2f1c0ba2346cda84870190313
Martin Holst Swende <martin@swende.se>
2022-11-02 20:20:17 +0100

    common/lru: add size-constrained lru cache

---
+++ common/lru/blob_lru.go
@@ -0,0 +1,69 @@
+// Copyright 2022 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+package lru
+
+import (
+	"math"
+
+	"github.com/hashicorp/golang-lru/simplelru"
+)
+
+// SizeConstrainedLRU is a wrapper around simplelru.LRU, which
+// adds a byte-size constraint.
+type SizeConstrainedLRU struct {
+	size    uint64
+	maxSize uint64
+	lru     *simplelru.LRU
+}
+
+// NewSizeConstraiedLRU creates a new SizeConstrainedLRU.
+func NewSizeConstraiedLRU(max uint64) *SizeConstrainedLRU {
+	lru, err := simplelru.NewLRU(math.MaxInt, nil)
+	if err != nil {
+		panic(err)
+	}
+	return &SizeConstrainedLRU{
+		size:    0,
+		maxSize: max,
+		lru:     lru,
+	}
+}
+
+// Add adds a value to the cache.  Returns true if an eviction occurred.
+func (c *SizeConstrainedLRU) Add(key string, value string) (evicted bool) {
+	targetSize := c.size + uint64(len(value))
+	for targetSize > c.maxSize {
+		evicted = true
+		_, v, ok := c.lru.RemoveOldest()
+		if !ok {
+			// list is now empty. Break
+			break
+		}
+		targetSize -= uint64(len(v.(string)))
+	}
+	c.size = targetSize
+	c.lru.Add(key, value)
+	return evicted
+}
+
+// Get looks up a key's value from the cache.
+func (c *SizeConstrainedLRU) Get(key string) (value string, ok bool) {
+	if v, ok := c.lru.Get(key); ok {
+		return v.(string), ok
+	}
+	return "", false
+}
---
+++ common/lru/blob_lru_test.go
@@ -0,0 +1,98 @@
+// Copyright 2022 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+package lru
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestBlobLru(t *testing.T) {
+	lru := NewSizeConstraiedLRU(100)
+	var want uint64
+	// Add 11 items of 10 byte each. First item should be swapped out
+	for i := 0; i < 11; i++ {
+		k := fmt.Sprintf("key-%d", i)
+		v := fmt.Sprintf("value-%04d", i)
+		lru.Add(k, v)
+		want += uint64(len(v))
+		if want > 100 {
+			want = 100
+		}
+		if have := lru.size; have != want {
+			t.Fatalf("size wrong, have %d want %d", have, want)
+		}
+	}
+	// Zero:th should be evicted
+	{
+		k := fmt.Sprintf("key-%d", 0)
+		if _, ok := lru.Get(k); ok {
+			t.Fatalf("should be evicted: %v", k)
+		}
+	}
+	// Elems 1-11 should be present
+	for i := 1; i < 11; i++ {
+		k := fmt.Sprintf("key-%d", i)
+		want := fmt.Sprintf("value-%04d", i)
+		have, ok := lru.Get(k)
+		if !ok {
+			t.Fatalf("missing key %v", k)
+		}
+		if have != want {
+			t.Fatalf("wrong value, have %v want %v", have, want)
+		}
+	}
+}
+
+// TestBlobLruOverflow tests what happens when inserting an element exceeding
+// the max size
+func TestBlobLruOverflow(t *testing.T) {
+	lru := NewSizeConstraiedLRU(100)
+	// Add 10 items of 10 byte each, filling the cache
+	for i := 0; i < 10; i++ {
+		k := fmt.Sprintf("key-%d", i)
+		v := fmt.Sprintf("value-%04d", i)
+		lru.Add(k, v)
+	}
+	// Add one single large elem. We expect it to swap out all entries.
+	{
+		k := fmt.Sprintf("large-%d", 0)
+		v := make([]byte, 200)
+		lru.Add(k, string(v))
+	}
+	// Elems 0-9 should be missing
+	for i := 1; i < 10; i++ {
+		k := fmt.Sprintf("key-%d", i)
+		if _, ok := lru.Get(k); ok {
+			t.Fatalf("should be evicted: %v", k)
+		}
+	}
+	// The size should be accurate
+	if have, want := lru.size, uint64(200); have != want {
+		t.Fatalf("size wrong, have %d want %d", have, want)
+	}
+	// Adding one small item should swap out the large one
+	{
+		i := 0
+		k := fmt.Sprintf("key-%d", i)
+		v := fmt.Sprintf("value-%04d", i)
+		lru.Add(k, v)
+		if have, want := lru.size, uint64(10); have != want {
+			t.Fatalf("size wrong, have %d want %d", have, want)
+		}
+	}
+}
