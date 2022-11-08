a406104b311026fccfa312e890a81af7bb130f82
Joe <jmcook1186@gmail.com>
2022-11-08 09:11:29 +0000

    make empty string the default case

--- signer/core/cliui.go
+++ signer/core/cliui.go
@@ -245,8 +245,7 @@ func (ui *CommandlineUI) OnApprovedTx(tx ethapi.SignTransactionResult) {
 
 func (ui *CommandlineUI) showAccounts() {
 	accounts, err := ui.api.ListAccounts(context.Background())
-	var limit int = 20 //max N accounts to print
-	var msg string = fmt.Sprintf("\nFirst %d accounts listed (%d more available).\n", limit, len(accounts)-limit)
+	var msg string = ""
 	var out = new(strings.Builder)
 	if err != nil {
 		log.Error("Error listing accounts", "err", err)
@@ -256,12 +255,12 @@ func (ui *CommandlineUI) showAccounts() {
 		fmt.Print("No accounts found\n")
 		return
 	}
-	if len(accounts) < limit {
-		limit = len(accounts)
-		msg = ""
+	if limit := 20; len(accounts) > limit {
+		msg = fmt.Sprintf("\nFirst %d accounts listed (%d more available).\n", limit, len(accounts)-limit)
+		accounts = accounts[:limit]
 	}
 	fmt.Fprint(out, "\n------- Available accounts -------\n")
-	for i, account := range accounts[:limit] {
+	for i, account := range accounts {
 		fmt.Fprintf(out, "%d. %s at %s\n", i, account.Address, account.URL)
 	}
 	fmt.Print(out.String(), msg)
