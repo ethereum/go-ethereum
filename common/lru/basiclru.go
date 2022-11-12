5808a0393f9c1270a384a467eb55c77a80dfd59a
Felix Lange <fjl@twurst.com>
2022-11-12 01:33:42 +0100

    common/lru: add some list optimizations
    Two optimizations here:
    
    - Code to add/remove elements is simplified because the list is
      now 'anchored' in a root element.
    
    - In case of eviction, the list element used by the evicted item
      is re-used, and no allocations are performed by Add.

--- common/lru/basiclru.go
+++ common/lru/basiclru.go
@@ -22,14 +22,14 @@ package lru
 // This type is not safe for concurrent use.
 // The zero value is not valid, instances must be created using NewCache.
 type BasicLRU[K comparable, V any] struct {
-	list  dlist[K]
+	list  *list[K]
 	items map[K]cacheItem[K, V]
 	cap   int
 }
 
 type cacheItem[K any, V any] struct {
 	value V
-	node  *dlistNode[K]
+	elem  *listElem[K]
 }
 
 // NewBasicLRU creates a new LRU cache.
@@ -37,10 +37,12 @@ func NewBasicLRU[K comparable, V any](capacity int) BasicLRU[K, V] {
 	if capacity < 0 {
 		capacity = 1
 	}
-	return BasicLRU[K, V]{
+	c := BasicLRU[K, V]{
 		items: make(map[K]cacheItem[K, V]),
+		list:  newList[K](),
 		cap:   capacity,
 	}
+	return c
 }
 
 // Add adds a value to the cache. Returns true if an item was evicted to store the new item.
@@ -50,19 +52,24 @@ func (c *BasicLRU[K, V]) Add(key K, value V) (evicted bool) {
 		// Already exists in cache.
 		item.value = value
 		c.items[key] = item
-		c.list.moveToFront(item.node)
+		c.list.moveToFront(item.elem)
 		return false
 	}
 
+	var elem *listElem[K]
 	if c.Len() >= c.cap {
-		// Evict an item.
-		node := c.list.removeLast()
-		delete(c.items, node.v)
+		elem = c.list.removeLast()
+		delete(c.items, elem.v)
 		evicted = true
+	} else {
+		elem = new(listElem[K])
 	}
 
 	// Store the new item.
-	c.items[key] = cacheItem[K, V]{value, c.list.push(key)}
+	// Note that, if another item was evicted, we re-use its list element here.
+	elem.v = key
+	c.items[key] = cacheItem[K, V]{value, elem}
+	c.list.pushElem(elem)
 	return evicted
 }
 
@@ -78,17 +85,18 @@ func (c *BasicLRU[K, V]) Get(key K) (value V, ok bool) {
 	if !ok {
 		return value, false
 	}
-	c.list.moveToFront(item.node)
+	c.list.moveToFront(item.elem)
 	return item.value, true
 }
 
 // GetOldest retrieves the least-recently-used item.
 // Note that this does not update the item's recency.
 func (c *BasicLRU[K, V]) GetOldest() (key K, value V, ok bool) {
-	if c.list.tail == nil {
+	lastElem := c.list.last()
+	if lastElem == nil {
 		return key, value, false
 	}
-	key = c.list.tail.v
+	key = lastElem.v
 	item := c.items[key]
 	return key, item.value, true
 }
@@ -101,10 +109,7 @@ func (c *BasicLRU[K, V]) Len() int {
 // Peek retrieves a value from the cache, but does not mark the key as recently used.
 func (c *BasicLRU[K, V]) Peek(key K) (value V, ok bool) {
 	item, ok := c.items[key]
-	if !ok {
-		return value, false
-	}
-	return item.value, true
+	return item.value, ok
 }
 
 // Purge empties the cache.
@@ -120,96 +125,99 @@ func (c *BasicLRU[K, V]) Remove(key K) bool {
 	item, ok := c.items[key]
 	if ok {
 		delete(c.items, key)
-		c.list.remove(item.node)
+		c.list.remove(item.elem)
 	}
 	return ok
 }
 
 // RemoveOldest drops the least recently used item.
 func (c *BasicLRU[K, V]) RemoveOldest() (key K, value V, ok bool) {
-	if c.list.tail == nil {
+	lastElem := c.list.last()
+	if lastElem == nil {
 		return key, value, false
 	}
-	key = c.list.tail.v
+
+	key = lastElem.v
 	item := c.items[key]
 	delete(c.items, key)
-	c.list.remove(c.list.tail)
+	c.list.remove(lastElem)
 	return key, item.value, true
 }
 
 // Keys returns all keys in the cache.
 func (c *BasicLRU[K, V]) Keys() []K {
 	keys := make([]K, 0, len(c.items))
-	for node := c.list.head; node != nil; node = node.next {
-		keys = append(keys, node.v)
-	}
-	return keys
+	return c.list.appendTo(keys)
 }
 
-// dlist is a doubly-linked list holding items of type T.
-type dlist[T any] struct {
-	head *dlistNode[T]
-	tail *dlistNode[T]
+// list is a doubly-linked list holding items of type he.
+// The zero value is not valid, use newList to create lists.
+type list[T any] struct {
+	root listElem[T]
 }
 
-type dlistNode[T any] struct {
+type listElem[T any] struct {
+	next *listElem[T]
+	prev *listElem[T]
 	v    T
-	next *dlistNode[T]
-	prev *dlistNode[T]
+}
+
+func newList[T any]() *list[T] {
+	l := new(list[T])
+	l.init()
+	return l
 }
 
 // init reinitializes the list, making it empty.
-func (l *dlist[T]) init() {
-	l.head, l.tail = nil, nil
+func (l *list[T]) init() {
+	l.root.next = &l.root
+	l.root.prev = &l.root
 }
 
-// push adds a new item to the front of the list and returns the
-func (l *dlist[T]) push(item T) *dlistNode[T] {
-	node := &dlistNode[T]{v: item}
-	l.pushNode(node)
-	return node
-}
-
-func (l *dlist[T]) pushNode(node *dlistNode[T]) {
-	if l.head == nil {
-		// List is empty, new node is head and tail.
-		l.head = node
-		l.tail = node
-	} else {
-		node.next = l.head
-		l.head.prev = node
-		l.head = node
-	}
+// push adds an element to the front of the list.
+func (l *list[T]) pushElem(e *listElem[T]) {
+	e.prev = &l.root
+	e.next = l.root.next
+	l.root.next = e
+	e.next.prev = e
 }
 
 // moveToFront makes 'node' the head of the list.
-func (l *dlist[T]) moveToFront(node *dlistNode[T]) {
-	l.remove(node)
-	l.pushNode(node)
+func (l *list[T]) moveToFront(e *listElem[T]) {
+	e.prev.next = e.next
+	e.next.prev = e.prev
+	l.pushElem(e)
 }
 
 // remove removes an element from the list.
-func (l *dlist[T]) remove(node *dlistNode[T]) {
-	if node.next != nil {
-		node.next.prev = node.prev
-	}
-	if node.prev != nil {
-		node.prev.next = node.next
-	}
-	if l.head == node {
-		l.head = node.next
-	}
-	if l.tail == node {
-		l.tail = node.prev
-	}
-	node.next, node.prev = nil, nil
+func (l *list[T]) remove(e *listElem[T]) {
+	e.prev.next = e.next
+	e.next.prev = e.prev
+	e.next, e.prev = nil, nil
 }
 
 // removeLast removes the last element of the list.
-func (l *dlist[T]) removeLast() *dlistNode[T] {
-	last := l.tail
+func (l *list[T]) removeLast() *listElem[T] {
+	last := l.last()
 	if last != nil {
 		l.remove(last)
 	}
 	return last
+}
+
+// last returns the last element of the list, or nil if the list is empty.
+func (l *list[T]) last() *listElem[T] {
+	e := l.root.prev
+	if e == &l.root {
+		return nil
+	}
+	return e
+}
+
+// appendTo appends all list elements to a slice.
+func (l *list[T]) appendTo(slice []T) []T {
+	for e := l.root.next; e != &l.root; e = e.next {
+		slice = append(slice, e.v)
+	}
+	return slice
 }
