f919886a39878e1ef548c8ec8003f7cbd58d4276
Arpit Temani <temaniarpit27@gmail.com>
2022-11-16 09:47:01 +0530

    Update sealer_test.go

--- consensus/ethash/sealer_test.go
+++ consensus/ethash/sealer_test.go
@@ -180,8 +180,6 @@ func TestRemoteMultiNotify(t *testing.T) {
 // Tests that pushing work packages fast to the miner doesn't cause any data race
 // issues in the notifications. Full pending block body / --miner.notify.full)
 func TestRemoteMultiNotifyFull(t *testing.T) {
-	t.Skip()
-
 	// Start a simple web server to capture notifications.
 	sink := make(chan map[string]interface{}, 64)
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
