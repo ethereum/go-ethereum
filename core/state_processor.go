e8648ec9088980c392a669b49674dcf474053539
yoshidan <naohiro.y@gmail.com>
2022-11-18 09:29:15 +0900

    core: Print vmerr to debug log when transaction execution fails

--- core/state_processor.go
+++ core/state_processor.go
@@ -27,6 +27,7 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/params"
 )
 
@@ -116,6 +117,7 @@ func applyTransaction(msg types.Message, config *params.ChainConfig, author *com
 	// by the tx.
 	receipt := &types.Receipt{Type: tx.Type(), PostState: root, CumulativeGasUsed: *usedGas}
 	if result.Failed() {
+		log.Debug("Transaction failed", "err", result.Unwrap())
 		receipt.Status = types.ReceiptStatusFailed
 	} else {
 		receipt.Status = types.ReceiptStatusSuccessful
