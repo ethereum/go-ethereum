07f21c5075dc9410ed3a754514e9fe4290a50050
maskpp <maskpp266@gmail.com>
2022-11-21 11:40:25 +0800

    Fix bug for TransactionData json unmarshal

--- core/types/l2trace_block.go
+++ core/types/l2trace_block.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"encoding/json"
 	"math/big"
 
 	"github.com/scroll-tech/go-ethereum/common"
@@ -11,6 +12,23 @@ type TransactionData struct {
 	IsCreate bool           `json:"isCreate"`
 	From     common.Address `json:"from"`
 	*Transaction
+}
+
+// TransactionDataAlias just used for UnmarshalJSON.
+type TransactionDataAlias TransactionData
+
+func (t *TransactionData) UnmarshalJSON(input []byte) error {
+	var jsonConfig struct {
+		TransactionDataAlias
+		*Transaction
+	}
+	jsonConfig.Transaction = &Transaction{}
+	if err := json.Unmarshal(input, &jsonConfig); err != nil {
+		return err
+	}
+	*t = TransactionData(jsonConfig.TransactionDataAlias)
+	t.Transaction = jsonConfig.Transaction
+	return nil
 }
 
 // NewTraceTransaction returns a transaction that will serialize to the trace
