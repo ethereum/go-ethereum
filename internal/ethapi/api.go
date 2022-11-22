c75e2d80421353f3b51a9a8d4cc0cfebb31685cc
Marius van der Wijden <m.vanderwijden@live.de>
2022-11-22 13:59:48 +0100

    internal/ethapi: add withdrawals to full block

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1247,6 +1247,8 @@ func RPCMarshalBlock(block *types.Block, inclTx bool, fullTx bool, config *param
 			}
 		}
 		fields["transactions"] = transactions
+		// inclTx also expands withdrawals
+		fields["withdrawals"] = block.Withdrawals()
 	}
 	uncles := block.Uncles()
 	uncleHashes := make([]common.Hash, len(uncles))
