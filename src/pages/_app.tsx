6cebcb3454dbcf9d40d790d05e1a27d48ee277ca
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-11-29 15:52:51 -0300

    chore: move favicon code to PageMetadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,11 +16,6 @@ export default function App({ Component, pageProps }: AppProps) {
     <ChakraProvider theme={theme}>
       <MDXProvider components={MDXComponents}>
         <Layout>
-          <Head>
-            <meta name='viewport' content='width=device-width, initial-scale=1' />
-            <link rel='icon' type='image/x-icon' href='/favicon.ico' />
-          </Head>
-
           <Component {...pageProps} />
         </Layout>
       </MDXProvider>
