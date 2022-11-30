3b0d117e89de1420033cef1d29a9cd4850a0a2a5
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2022-11-29 19:12:48 -0800

    debug: algolia demo keys

--- src/components/UI/Search.tsx
+++ src/components/UI/Search.tsx
@@ -3,14 +3,16 @@ import { DocSearch } from '@docsearch/react';
 import '@docsearch/css';
 
 export const Search: React.FC = () => {
-  // TODO: Replace Algolia test keys with '' when API key available for this repo (place in .env)
-  const appId = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID || 'R2IYF7ETH7'
-  const apiKey = process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY || '599cec31baffa4868cae4e79f180729b'
-  const indexName = process.env.NEXT_PUBLIC_ALGOLIA_BASE_SEARCH_INDEX_NAME || 'docsearch'
+  const appId = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID || ''
+  const apiKey = process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY || ''
+  const indexName = process.env.NEXT_PUBLIC_ALGOLIA_BASE_SEARCH_INDEX_NAME || ''
+
+  // TODO: Replace Algolia test keys with above env vars when ready
   return (
     <DocSearch
-      appId={appId}
-      apiKey={apiKey}
-      indexName={indexName} />
+      appId="R2IYF7ETH7"
+      apiKey="599cec31baffa4868cae4e79f180729b"
+      indexName="docsearch"
+    />
   );
 }
