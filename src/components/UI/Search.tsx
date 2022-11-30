5d9df5bfd40cb45bc3ca38aa1d328a2b0eab6893
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2022-11-29 18:20:40 -0800

    extract keys to .env

--- src/components/UI/Search.tsx
+++ src/components/UI/Search.tsx
@@ -2,10 +2,14 @@ import { DocSearch } from '@docsearch/react';
 
 import '@docsearch/css';
 
-export const Search: React.FC = () => (
-  <DocSearch
-    appId="R2IYF7ETH7"
-    apiKey="599cec31baffa4868cae4e79f180729b"
-    indexName="docsearch"
-  />
-)
+export const Search: React.FC = () => {
+  const appId = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID || ''
+  const apiKey = process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY || ''
+  const indexName = process.env.NEXT_PUBLIC_ALGOLIA_BASE_SEARCH_INDEX_NAME || ''
+  return (
+    <DocSearch
+      appId={appId}
+      apiKey={apiKey}
+      indexName={indexName} />
+  );
+}
