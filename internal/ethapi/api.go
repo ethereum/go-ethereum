b09461fb26f196a0fc1cf74c4a747444c7120d61
Marius van der Wijden <m.vanderwijden@live.de>
2022-12-07 11:36:00 +0100

    internal/ethapi: add comment

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1247,6 +1247,7 @@ func RPCMarshalBlock(block *types.Block, inclTx bool, fullTx bool, config *param
 		}
 		fields["transactions"] = transactions
 		// inclTx also expands withdrawals
+		// TODO @MariusVanDerWijden: add a second flag similar to inclTx to enable withdrawals
 		fields["withdrawals"] = block.Withdrawals()
 	}
 	uncles := block.Uncles()
