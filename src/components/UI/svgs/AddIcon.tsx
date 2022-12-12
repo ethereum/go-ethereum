7dc30590368276aa95a1465d6ec920bf298972d0
Corwin Smith <cssmittys@gmail.com>
2022-12-12 18:21:22 +0100

    [UI/UX bug] - Footer buttons are only partially clickable and [Perf] - Optimise SVGs  (#143)
    * Make whole footer button area clickable
    
    * optimize svgs

--- src/components/UI/svgs/AddIcon.tsx
+++ src/components/UI/svgs/AddIcon.tsx
@@ -7,12 +7,7 @@ const Icon = createIcon({
   displayName: 'AddIcon',
   viewBox: `0 0 ${w} ${h}`,
   path: (
-    <svg width={w} height={h} fill='none' xmlns='http://www.w3.org/2000/svg'>
-      <g fill='currentColor'>
-        <rect height='2' width='20' x='2' y='11'></rect>
-        <rect height='20' width='2' x='11' y='2'></rect>
-      </g>
-    </svg>
+    <svg width={w} height={h} fill="none" xmlns="http://www.w3.org/2000/svg"><g fill="currentColor"><path d="M2 11h20v2H2z"/><path d="M11 2h2v20h-2z"/></g></svg>
   )
 });
 
