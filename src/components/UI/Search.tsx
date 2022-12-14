f509950c722917c789645bb53926c1849a4797d0
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2022-12-14 10:47:38 -0800

    match any domain and remove
    makes all search results local to current domain

--- src/components/UI/Search.tsx
+++ src/components/UI/Search.tsx
@@ -6,8 +6,6 @@ export const Search: React.FC = () => {
   const appId = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID || '';
   const apiKey = process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY || '';
   const indexName = process.env.NEXT_PUBLIC_ALGOLIA_BASE_SEARCH_INDEX_NAME || '';
-  const indexDomain =
-    process.env.NEXT_PUBLIC_ALGOLIA_INDEX_DOMAIN || 'https://melodious-puffpuff-8e1109.netlify.app';
   return (
     <DocSearch
       appId={appId}
@@ -16,7 +14,7 @@ export const Search: React.FC = () => {
       transformItems={items =>
         items.map(item => ({
           ...item,
-          url: item.url.replace(indexDomain, '')
+          url: item.url.replace(/^https?:\/\/[^\/]+(?=\/)/, '')
         }))
       }
     />
