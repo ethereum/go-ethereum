a0f0b5fc86d7c3ddd53194d576b75886aca7792e
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-12-15 00:57:02 -0300

    chore: update spacing between sections on home and /downloads pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,10 @@ const HomePage: NextPage = ({}) => {
         <Stack spacing={4}>
           <HomeHero />
 
-          <Grid templateColumns={{ base: 'repeat(1, 1fr)', lg: 'repeat(2, 1fr)' }} gap={4}>
+          <Grid
+            templateColumns={{ base: 'repeat(1, 1fr)', lg: 'repeat(2, 1fr)' }}
+            gap={{ base: 4, lg: 8 }}
+          >
             <GridItem rowSpan={2}>
               {/* SECTION: What is Geth */}
               <HomeSection
