7c9aa53e34c7098085893a5fba4fef23bb1b60a0
Marius van der Wijden <m.vanderwijden@live.de>
2023-01-09 11:17:41 +0100

    internal/flags: modify test for windows

--- internal/flags/flags_test.go
+++ internal/flags/flags_test.go
@@ -19,21 +19,38 @@ package flags
 import (
 	"os"
 	"os/user"
+	"runtime"
 	"testing"
 )
 
 func TestPathExpansion(t *testing.T) {
 	user, _ := user.Current()
-	tests := map[string]string{
-		"/home/someuser/tmp":              "/home/someuser/tmp",
-		"~/tmp":                           user.HomeDir + "/tmp",
-		"~thisOtherUser/b/":               "~thisOtherUser/b",
-		"$DDDXXX/a/b":                     "/tmp/a/b",
-		"/a/b/":                           "/a/b",
-		"C:\\Documents\\Newsletters\\":    "C:\\Documents\\Newsletters\\",
-		"C:\\":                            "C:\\",
-		"\\\\.\\pipe\\\\pipe\\geth621383": "\\\\.\\pipe\\\\pipe\\geth621383",
+	var tests map[string]string
+
+	if runtime.GOOS == "windows" {
+		tests = map[string]string{
+			"/home/someuser/tmp":              "\\home\\someuser\\tmp",
+			"~/tmp":                           user.HomeDir + "\\tmp",
+			"~thisOtherUser/b/":               "~thisOtherUser\\b",
+			"$DDDXXX/a/b":                     "\\tmp\\a\\b",
+			"/a/b/":                           "\\a\\b",
+			"C:\\Documents\\Newsletters\\":    "C:\\Documents\\Newsletters\\",
+			"C:\\":                            "C:\\",
+			"\\\\.\\pipe\\\\pipe\\geth621383": "\\\\.\\pipe\\\\pipe\\geth621383",
+		}
+	} else {
+		tests = map[string]string{
+			"/home/someuser/tmp":              "/home/someuser/tmp",
+			"~/tmp":                           user.HomeDir + "/tmp",
+			"~thisOtherUser/b/":               "~thisOtherUser/b",
+			"$DDDXXX/a/b":                     "/tmp/a/b",
+			"/a/b/":                           "/a/b",
+			"C:\\Documents\\Newsletters\\":    "C:\\Documents\\Newsletters\\",
+			"C:\\":                            "C:\\",
+			"\\\\.\\pipe\\\\pipe\\geth621383": "\\\\.\\pipe\\\\pipe\\geth621383",
+		}
 	}
+
 	os.Setenv("DDDXXX", "/tmp")
 	for test, expected := range tests {
 		got := expandPath(test)
