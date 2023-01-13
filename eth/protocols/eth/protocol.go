bac9e8961adbb8c26e65e0b116813c9bf2a46918
Marius van der Wijden <m.vanderwijden@live.de>
2023-01-13 13:36:25 +0100

    eth/protocols/eth: revert accidental modification

--- eth/protocols/eth/protocol.go
+++ eth/protocols/eth/protocol.go
@@ -41,7 +41,7 @@ const ProtocolName = "eth"
 
 // ProtocolVersions are the supported versions of the `eth` protocol (first
 // is primary).
-var ProtocolVersions = []uint{ETH67, ETH68, ETH66}
+var ProtocolVersions = []uint{ETH68, ETH67, ETH66}
 
 // protocolLengths are the number of implemented message corresponding to
 // different protocol versions.
