dd26e3c1c896cc2a20424f7248c2151dac9054bf
Marius van der Wijden <m.vanderwijden@live.de>
2023-01-31 08:59:33 +0100

    eth/catalyst: add getPayloadBodies* to capabilities

--- eth/catalyst/api.go
+++ eth/catalyst/api.go
@@ -88,6 +88,8 @@ var caps = []string{
 	"engine_getPayloadV2",
 	"engine_newPayloadV1",
 	"engine_newPayloadV2",
+	"engine_getPayloadBodiesByHashV1",
+	"engine_getPayloadBodiesByRangeV1",
 }
 
 type ConsensusAPI struct {
