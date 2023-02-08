22fa4033e8fabb51c44e8d2a8c6bb695a6e9285e
Evgeny Danilenko <6655321@bk.ru>
2023-02-09 01:41:09 +0400

    Event based pprof (#732)
    * feature
    
    * Save pprof to /tmp
    
    ---------
    
    Co-authored-by: Jerry <jerrycgh@gmail.com>

--- common/set/slice.go
+++ common/set/slice.go
@@ -9,3 +9,20 @@ func New[T comparable](slice []T) map[T]struct{} {
 
 	return m
 }
+
+func ToSlice[T comparable](m map[T]struct{}) []T {
+	slice := make([]T, len(m))
+
+	var i int
+
+	for k := range m {
+		slice[i] = k
+		i++
+	}
+
+	return slice
+}
+
+func Deduplicate[T comparable](slice []T) []T {
+	return ToSlice(New(slice))
+}
