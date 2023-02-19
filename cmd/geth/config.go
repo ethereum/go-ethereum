ethereum/go-ethereum

commit 428eabe28d1077356b16f25828d78d8693a766bb

cmd/geth: support dumpconfig optionally saving to file (#18327)
* Changed dumpConfig function to optionally save to file

* Added O_TRUNC flag to file open and cleaned up code

cmd/geth/config.go
@@ -20,7 +20,6 @@ import (
 	"bufio"
 	"errors"
 	"fmt"
-	"io"
 	"math/big"
 	"os"
 	"reflect"
@@ -198,7 +197,17 @@ func dumpConfig(ctx *cli.Context) error {
 	if err != nil {
 		return err
 	}
-	io.WriteString(os.Stdout, comment)
-	os.Stdout.Write(out)
+
+	dump := os.Stdout
+	if ctx.NArg() > 0 {
+		dump, err = os.OpenFile(ctx.Args().Get(0), os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+		if err != nil {
+			return err
+		}
+		defer dump.Close()
+	}
+	dump.WriteString(comment)
+	dump.Write(out)
+
 	return nil
 }

commit b01c6edc6bb23c4bda5ab24a4171b18190f7f25a

geth/dumpconfig: fix undocumented helptext
This patch documentizes the 'saving into a file' option.
As suggested in #16383, piping dumpconfig's stdout is imcompatible to the Windows powershell due to the UTF16LE encoding.
To fix this, #18327 (428eabe) added an option to save dumpconfig in a file.
However, users cannot be informed of this feature (saving into a file) without searching the relevant github issue or seeing source code.
Thus, this patch fixes the aforementioned inconvenience by improving the description of the dumpconfig's help text.

fixes: 428eabe

Signed-off-by: Sungwoo Kim <git@sung-woo.kim>

cmd/geth/config.go
@@ -47,10 +47,10 @@ var (
 	dumpConfigCommand = &cli.Command{
 		Action:      dumpConfig,
 		Name:        "dumpconfig",
-		Usage:       "Show configuration values",
-		ArgsUsage:   "",
+		Usage:       "Export configuration values in a TOML format",
+		ArgsUsage:   "<dumpfile (optional)>",
 		Flags:       flags.Merge(nodeFlags, rpcFlags),
-		Description: `The dumpconfig command shows configuration values.`,
+		Description: `Export configuration values in TOML format (to stdout by default).`,
 	}
 
 	configFileFlag = &cli.StringFlag{