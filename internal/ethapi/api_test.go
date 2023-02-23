8a89ac9c8e989692d2aad80b52d4c32c00d26542
Martin Holst Swende <martin@swende.se>
2023-02-23 08:56:28 +0100

    internal/ethapi: refactor json marshalling test

--- internal/ethapi/api_test.go
+++ internal/ethapi/api_test.go
@@ -1,3 +1,19 @@
+// Copyright 2023 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
 package ethapi
 
 import (
@@ -9,123 +25,135 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/params"
-	"github.com/stretchr/testify/require"
 )
 
 func TestTransaction_RoundTripRpcJSON(t *testing.T) {
-	addr := common.HexToAddress("0x1234")
-	config := params.AllEthashProtocolChanges
-	signer := types.LatestSigner(config)
-	key, _ := crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
+	var (
+		config = params.AllEthashProtocolChanges
+		signer = types.LatestSigner(config)
+		key, _ = crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
+		tests  = allTransactionTypes(common.Address{0xde, 0xad}, config)
+	)
+	t.Parallel()
+	for i, tt := range tests {
+		var tx2 types.Transaction
+		tx, err := types.SignNewTx(key, signer, tt)
+		if err != nil {
+			t.Fatalf("test %d: signing failed: %v", i, err)
+		}
+		// Regular transaction
+		if data, err := json.Marshal(tx); err != nil {
+			t.Fatalf("test %d: marshalling failed; %v", i, err)
+		} else if err = tx2.UnmarshalJSON(data); err != nil {
+			t.Fatalf("test %d: sunmarshal failed: %v", i, err)
+		} else if want, have := tx.Hash(), tx2.Hash(); want != have {
+			t.Fatalf("test %d: stx changed, want %x have %x", i, want, have)
+		}
 
-	tests := allTransactionTypes(addr, config)
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tx, err := types.SignNewTx(key, signer, tt.inner)
-			require.NoError(t, err, "signing failed: %v", err)
-			rpcTx := newRPCTransaction(tx, common.Hash{}, 1234, 9, big.NewInt(10), &params.ChainConfig{})
-			require.NoError(t, err, "newRPCTransaction failed: %v", err)
-			data, err := json.Marshal(rpcTx)
-			require.NoError(t, err, "marshal failed: %v", err)
-
-			got := &types.Transaction{}
-			err = got.UnmarshalJSON(data)
-			require.NoError(t, err, "unmarshal failed: %v", err)
-
-			require.Equal(t, rpcTx.Hash, got.Hash(), "transaction changed after round trip")
-		})
+		//  rpcTransaction
+		rpcTx := newRPCTransaction(tx, common.Hash{}, 0, 0, nil, config)
+		if data, err := json.Marshal(rpcTx); err != nil {
+			t.Fatalf("test %d: marshalling failed; %v", i, err)
+		} else if err = tx2.UnmarshalJSON(data); err != nil {
+			t.Fatalf("test %d: unmarshal failed: %v", i, err)
+		} else if want, have := tx.Hash(), tx2.Hash(); want != have {
+			t.Fatalf("test %d: tx changed, want %x have %x", i, want, have)
+		}
 	}
 }
 
-func allTransactionTypes(addr common.Address, config *params.ChainConfig) []struct {
-	name  string
-	inner types.TxData
-} {
-	return []struct {
-		name  string
-		inner types.TxData
-	}{
-		{
-			name: "LegacyTx",
-			inner: &types.LegacyTx{
-				Nonce:    5,
-				GasPrice: big.NewInt(6),
-				Gas:      7,
-				To:       &addr,
-				Value:    big.NewInt(8),
-				Data:     []byte{0, 1, 2, 3, 4},
-				V:        big.NewInt(9),
-				R:        big.NewInt(10),
-				S:        big.NewInt(11),
-			},
+func allTransactionTypes(addr common.Address, config *params.ChainConfig) []types.TxData {
+	return []types.TxData{
+		&types.LegacyTx{
+			Nonce:    5,
+			GasPrice: big.NewInt(6),
+			Gas:      7,
+			To:       &addr,
+			Value:    big.NewInt(8),
+			Data:     []byte{0, 1, 2, 3, 4},
+			V:        big.NewInt(9),
+			R:        big.NewInt(10),
+			S:        big.NewInt(11),
 		},
-		{
-			name: "LegacyTxContractCreation",
-			inner: &types.LegacyTx{
-				Nonce:    5,
-				GasPrice: big.NewInt(6),
-				Gas:      7,
-				To:       nil,
-				Value:    big.NewInt(8),
-				Data:     []byte{0, 1, 2, 3, 4},
-				V:        big.NewInt(32),
-				R:        big.NewInt(10),
-				S:        big.NewInt(11),
-			},
+		&types.LegacyTx{
+			Nonce:    5,
+			GasPrice: big.NewInt(6),
+			Gas:      7,
+			To:       nil,
+			Value:    big.NewInt(8),
+			Data:     []byte{0, 1, 2, 3, 4},
+			V:        big.NewInt(32),
+			R:        big.NewInt(10),
+			S:        big.NewInt(11),
 		},
-		{
-			name: "AccessListTx",
-			inner: &types.AccessListTx{
-				ChainID:    config.ChainID,
-				Nonce:      5,
-				GasPrice:   big.NewInt(6),
-				Gas:        7,
-				To:         &addr,
-				Value:      big.NewInt(8),
-				Data:       []byte{0, 1, 2, 3, 4},
-				AccessList: types.AccessList{},
+		&types.AccessListTx{
+			ChainID:  config.ChainID,
+			Nonce:    5,
+			GasPrice: big.NewInt(6),
+			Gas:      7,
+			To:       &addr,
+			Value:    big.NewInt(8),
+			Data:     []byte{0, 1, 2, 3, 4},
+			AccessList: types.AccessList{
+				types.AccessTuple{
+					Address:     common.Address{0x2},
+					StorageKeys: []common.Hash{types.EmptyRootHash},
+				},
 			},
+			V: big.NewInt(32),
+			R: big.NewInt(10),
+			S: big.NewInt(11),
 		},
-		{
-			name: "AccessListTxContractCreation",
-			inner: &types.AccessListTx{
-				ChainID:    config.ChainID,
-				Nonce:      5,
-				GasPrice:   big.NewInt(6),
-				Gas:        7,
-				To:         nil,
-				Value:      big.NewInt(8),
-				Data:       []byte{0, 1, 2, 3, 4},
-				AccessList: types.AccessList{},
+		&types.AccessListTx{
+			ChainID:  config.ChainID,
+			Nonce:    5,
+			GasPrice: big.NewInt(6),
+			Gas:      7,
+			To:       nil,
+			Value:    big.NewInt(8),
+			Data:     []byte{0, 1, 2, 3, 4},
+			AccessList: types.AccessList{
+				types.AccessTuple{
+					Address:     common.Address{0x2},
+					StorageKeys: []common.Hash{types.EmptyRootHash},
+				},
 			},
+			V: big.NewInt(32),
+			R: big.NewInt(10),
+			S: big.NewInt(11),
 		},
-		{
-			name: "DynamicFeeTx",
-			inner: &types.DynamicFeeTx{
-				ChainID:    config.ChainID,
-				Nonce:      5,
-				GasTipCap:  big.NewInt(6),
-				GasFeeCap:  big.NewInt(9),
-				Gas:        7,
-				To:         &addr,
-				Value:      big.NewInt(8),
-				Data:       []byte{0, 1, 2, 3, 4},
-				AccessList: types.AccessList{},
+		&types.DynamicFeeTx{
+			ChainID:   config.ChainID,
+			Nonce:     5,
+			GasTipCap: big.NewInt(6),
+			GasFeeCap: big.NewInt(9),
+			Gas:       7,
+			To:        &addr,
+			Value:     big.NewInt(8),
+			Data:      []byte{0, 1, 2, 3, 4},
+			AccessList: types.AccessList{
+				types.AccessTuple{
+					Address:     common.Address{0x2},
+					StorageKeys: []common.Hash{types.EmptyRootHash},
+				},
 			},
+			V: big.NewInt(32),
+			R: big.NewInt(10),
+			S: big.NewInt(11),
 		},
-		{
-			name: "DynamicFeeTxContractCreation",
-			inner: &types.DynamicFeeTx{
-				ChainID:    config.ChainID,
-				Nonce:      5,
-				GasTipCap:  big.NewInt(6),
-				GasFeeCap:  big.NewInt(9),
-				Gas:        7,
-				To:         nil,
-				Value:      big.NewInt(8),
-				Data:       []byte{0, 1, 2, 3, 4},
-				AccessList: types.AccessList{},
-			},
+		&types.DynamicFeeTx{
+			ChainID:    config.ChainID,
+			Nonce:      5,
+			GasTipCap:  big.NewInt(6),
+			GasFeeCap:  big.NewInt(9),
+			Gas:        7,
+			To:         nil,
+			Value:      big.NewInt(8),
+			Data:       []byte{0, 1, 2, 3, 4},
+			AccessList: types.AccessList{},
+			V:          big.NewInt(32),
+			R:          big.NewInt(10),
+			S:          big.NewInt(11),
 		},
 	}
 }
