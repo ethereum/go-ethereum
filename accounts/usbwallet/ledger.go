e61909b6213fba06b98df3afd7bacdfa3136deaa
James <james@prestwi.ch>
2023-02-28 10:08:45 -0800

    lint: run gofmt

--- accounts/usbwallet/ledger.go
+++ accounts/usbwallet/ledger.go
@@ -60,7 +60,7 @@ const (
 	ledgerP1ContTransactionData     ledgerParam1 = 0x80 // Subsequent transaction data block for signing
 	ledgerP2DiscardAddressChainCode ledgerParam2 = 0x00 // Do not return the chain code along with the address
 
-	ledgerEip155Size int = 3; // Size of the EIP-155 chain_id,r,s in unsigned transactions
+	ledgerEip155Size int = 3 // Size of the EIP-155 chain_id,r,s in unsigned transactions
 )
 
 // errLedgerReplyInvalidHeader is the error message returned by a Ledger data exchange
@@ -353,7 +353,8 @@ func (w *ledgerDriver) ledgerSign(derivationPath []uint32, tx *types.Transaction
 	// Chunk size selection to mitigate an underlying RLP deserialization issue on the ledger app.
 	// https://github.com/LedgerHQ/app-ethereum/issues/409
 	chunk := 255
-	for ; len(payload) % chunk <= ledgerEip155Size; chunk-- {}
+	for ; len(payload)%chunk <= ledgerEip155Size; chunk-- {
+	}
 
 	for len(payload) > 0 {
 		// Calculate the size of the next data chunk
