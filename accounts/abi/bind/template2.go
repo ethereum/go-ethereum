6b69f4c90cd6bc53c6e7640880af2d30ee54ecd8
Sina Mahmoodi <itz.s1na@gmail.com>
2023-03-01 13:07:00 +0330

    drop unnecessary imports

--- accounts/abi/bind/template2.go
+++ accounts/abi/bind/template2.go
@@ -11,27 +11,21 @@ package {{.Package}}
 import (
 	"fmt"
 	"math/big"
-	"strings"
 	"errors"
 
-	ethereum "github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/event"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
 var (
 	_ = errors.New
 	_ = big.NewInt
-	_ = strings.NewReader
-	_ = ethereum.NotFound
 	_ = bind.Bind
 	_ = common.Big1
 	_ = types.BloomLookup
-	_ = event.NewSubscription
 	_ = abi.ConvertType
 )
 
