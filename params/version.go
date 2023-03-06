4671d97f311ff76ddc84d74070d91b06d4588faa
Jerry <jerrycgh@gmail.com>
2023-03-06 09:14:44 -0800

    Bump version to v0.3.5

--- params/version.go
+++ params/version.go
@@ -23,7 +23,7 @@ import (
 const (
 	VersionMajor = 0        // Major version component of the current release
 	VersionMinor = 3        // Minor version component of the current release
-	VersionPatch = 4        // Patch version component of the current release
+	VersionPatch = 5        // Patch version component of the current release
 	VersionMeta  = "stable" // Version metadata to append to the version string
 )
 
