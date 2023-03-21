127af384ed0491fbe71767be17c7e72aa4761fff
HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>
2023-03-21 10:04:20 +0800

    build: update version (#246)

--- params/version.go
+++ params/version.go
@@ -24,7 +24,7 @@ import (
 const (
 	VersionMajor = 0       // Major version component of the current release
 	VersionMinor = 1       // Minor version component of the current release
-	VersionPatch = 9       // Patch version component of the current release
+	VersionPatch = 10      // Patch version component of the current release
 	VersionMeta  = "alpha" // Version metadata to append to the version string
 )
 
