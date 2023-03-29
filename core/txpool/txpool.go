0defec2b08c4e97b1e00bdd2b14ed5a00774fe2b
Martin Holst Swende <martin@swende.se>
2023-03-29 07:13:32 -0400

    Update core/txpool/txpool.go

--- core/txpool/txpool.go
+++ core/txpool/txpool.go
@@ -985,7 +985,7 @@ func (pool *TxPool) addTxs(txs []*types.Transaction, local, sync bool) []error {
 		// insufficient intrinsic gas as soon as possible and cache senders
 		// in transactions before obtaining lock
 
-		if _, err := pool.validateTxBasics(tx, local); err != nil {
+		if err := pool.validateTxBasics(tx, local); err != nil {
 			errs[i] = err
 			invalidTxMeter.Mark(1)
 			continue
