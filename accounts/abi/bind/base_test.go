3ef2f7d13b865a539381af68fa8421e88852bfe5
Sina Mahmoodi <itz.s1na@gmail.com>
2023-03-30 17:04:29 +0200

    table test

--- accounts/abi/bind/base_test.go
+++ accounts/abi/bind/base_test.go
@@ -19,6 +19,7 @@ package bind_test
 import (
 	"context"
 	"errors"
+	"fmt"
 	"math/big"
 	"reflect"
 	"strings"
@@ -183,75 +184,38 @@ func TestUnpackIndexedStringTyLogIntoMap(t *testing.T) {
 		"amount": big.NewInt(1),
 		"memo":   []byte{88},
 	}
-	unpackAndCheck(t, bc, expectedReceivedMap, mockLog)
+	unpackAndCheck(t, bc, expectedReceivedMap, mockLog, "")
 }
 
-func TestLog0AndAnonymousLogIntoMap(t *testing.T) {
+func TestAnonymousLogIntoMap(t *testing.T) {
 	for i, tc := range []struct {
 		abiString string
 		topics    []common.Hash
 		expected  map[string]interface{}
 	}{
+		// Anonymous event with no indexed parameters
 		{
-			`[{"anonymous":false,"inputs":[{"indexed":false,"name":"amount","type":"uint256"}],"name":"received","type":"event"}]`,
+			`[{"anonymous":true,"inputs":[{"indexed":false,"name":"amount","type":"uint256"}],"name":"received","type":"event"}]`,
 			nil,
 			map[string]interface{}{
 				"amount": big.NewInt(1),
+			},
+		},
+		// Anonymous event with indexed parameters
+		{
+			`[{"anonymous":true,"inputs":[{"indexed":true,"name":"content","type":"bytes"},{"indexed":false,"name":"amount","type":"uint256"}],"name":"received","type":"event"}]`,
+			[]common.Hash{crypto.Keccak256Hash([]byte{1, 2, 3, 4, 5})},
+			map[string]interface{}{
+				"content": crypto.Keccak256Hash([]byte{1, 2, 3, 4, 5}),
+				"amount":  big.NewInt(1),
 			},
 		},
 	} {
 		mockLog := newMockLog(tc.topics, common.HexToHash("0x0"), "0x0000000000000000000000000000000000000000000000000000000000000001")
 		parsedAbi, _ := abi.JSON(strings.NewReader(tc.abiString))
 		bc := bind.NewBoundContract(common.HexToAddress("0x0"), parsedAbi, nil, nil, nil)
-		unpackAndCheck(t, bc, tc.expected, mockLog)
+		unpackAndCheck(t, bc, tc.expected, mockLog, fmt.Sprintf("test case %d: ", i))
 	}
-}
-
-func TestUnpackLog0IntoMap(t *testing.T) {
-	mockLog := newMockLog(nil, common.HexToHash("0x0"), "0x0000000000000000000000000000000000000000000000000000000000000001")
-
-	abiString := `[{"anonymous":false,"inputs":[{"indexed":false,"name":"amount","type":"uint256"}],"name":"received","type":"event"}]`
-	parsedAbi, _ := abi.JSON(strings.NewReader(abiString))
-	bc := bind.NewBoundContract(common.HexToAddress("0x0"), parsedAbi, nil, nil, nil)
-
-	expectedReceivedMap := map[string]interface{}{
-		"amount": big.NewInt(1),
-	}
-
-	unpackAndCheck(t, bc, expectedReceivedMap, mockLog)
-}
-
-func TestUnpackAnonymousLogIntoMap(t *testing.T) {
-	mockLog := newMockLog(nil, common.HexToHash("0x0"), "0x0000000000000000000000000000000000000000000000000000000000000001")
-
-	abiString := `[{"anonymous":true,"inputs":[{"indexed":false,"name":"amount","type":"uint256"}],"name":"received","type":"event"}]`
-	parsedAbi, _ := abi.JSON(strings.NewReader(abiString))
-	bc := bind.NewBoundContract(common.HexToAddress("0x0"), parsedAbi, nil, nil, nil)
-
-	expectedReceivedMap := map[string]interface{}{
-		"amount": big.NewInt(1),
-	}
-
-	unpackAndCheck(t, bc, expectedReceivedMap, mockLog)
-}
-
-func TestUnpackIndexedAnonymousLogIntoMap(t *testing.T) {
-	hash := crypto.Keccak256Hash([]byte{1, 2, 3, 4, 5})
-	topics := []common.Hash{
-		hash,
-	}
-
-	mockLog := newMockLog(topics, common.HexToHash("0x0"), "0x0000000000000000000000000000000000000000000000000000000000000001")
-	abiString := `[{"anonymous":true,"inputs":[{"indexed":true,"name":"content","type":"bytes"},{"indexed":false,"name":"amount","type":"uint256"}],"name":"received","type":"event"}]`
-	parsedAbi, _ := abi.JSON(strings.NewReader(abiString))
-	bc := bind.NewBoundContract(common.HexToAddress("0x0"), parsedAbi, nil, nil, nil)
-
-	expectedReceivedMap := map[string]interface{}{
-		"content": hash,
-		"amount":  big.NewInt(1),
-	}
-
-	unpackAndCheck(t, bc, expectedReceivedMap, mockLog)
 }
 
 func TestUnpackIndexedSliceTyLogIntoMap(t *testing.T) {
@@ -276,7 +240,7 @@ func TestUnpackIndexedSliceTyLogIntoMap(t *testing.T) {
 		"amount": big.NewInt(1),
 		"memo":   []byte{88},
 	}
-	unpackAndCheck(t, bc, expectedReceivedMap, mockLog)
+	unpackAndCheck(t, bc, expectedReceivedMap, mockLog, "")
 }
 
 func TestUnpackIndexedArrayTyLogIntoMap(t *testing.T) {
@@ -301,7 +265,7 @@ func TestUnpackIndexedArrayTyLogIntoMap(t *testing.T) {
 		"amount":    big.NewInt(1),
 		"memo":      []byte{88},
 	}
-	unpackAndCheck(t, bc, expectedReceivedMap, mockLog)
+	unpackAndCheck(t, bc, expectedReceivedMap, mockLog, "")
 }
 
 func TestUnpackIndexedFuncTyLogIntoMap(t *testing.T) {
@@ -327,7 +291,7 @@ func TestUnpackIndexedFuncTyLogIntoMap(t *testing.T) {
 		"amount":   big.NewInt(1),
 		"memo":     []byte{88},
 	}
-	unpackAndCheck(t, bc, expectedReceivedMap, mockLog)
+	unpackAndCheck(t, bc, expectedReceivedMap, mockLog, "")
 }
 
 func TestUnpackIndexedBytesTyLogIntoMap(t *testing.T) {
@@ -348,7 +312,7 @@ func TestUnpackIndexedBytesTyLogIntoMap(t *testing.T) {
 		"amount":  big.NewInt(1),
 		"memo":    []byte{88},
 	}
-	unpackAndCheck(t, bc, expectedReceivedMap, mockLog)
+	unpackAndCheck(t, bc, expectedReceivedMap, mockLog, "")
 }
 
 func TestTransactGasFee(t *testing.T) {
@@ -396,18 +360,18 @@ func TestTransactGasFee(t *testing.T) {
 	assert.True(mt.suggestGasPriceCalled)
 }
 
-func unpackAndCheck(t *testing.T, bc *bind.BoundContract, expected map[string]interface{}, mockLog types.Log) {
+func unpackAndCheck(t *testing.T, bc *bind.BoundContract, expected map[string]interface{}, mockLog types.Log, errPrefix string) {
 	received := make(map[string]interface{})
 	if err := bc.UnpackLogIntoMap(received, "received", mockLog); err != nil {
 		t.Error(err)
 	}
 
 	if len(received) != len(expected) {
-		t.Fatalf("unpacked map length %v not equal expected length of %v", len(received), len(expected))
+		t.Fatalf("%sunpacked map length %v not equal expected length of %v", errPrefix, len(received), len(expected))
 	}
 	for name, elem := range expected {
 		if !reflect.DeepEqual(elem, received[name]) {
-			t.Errorf("field %v does not match expected, want %v, got %v", name, elem, received[name])
+			t.Errorf("%sfield %v does not match expected, want %v, got %v", errPrefix, name, elem, received[name])
 		}
 	}
 }
