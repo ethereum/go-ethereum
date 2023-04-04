1f715acf887be87fb5902939dbe95525ddcc4219
Martin Holst Swende <martin@swende.se>
2023-04-04 08:22:35 -0400

    Update core/vm/interpreter.go
    Co-authored-by: Sina Mahmoodi <1591639+s1na@users.noreply.github.com>

--- core/vm/interpreter.go
+++ core/vm/interpreter.go
@@ -25,7 +25,9 @@ import (
 
 // Config are the configuration options for the Interpreter
 type Config struct {
-	Debug                   bool      // Opcode logger
+        // Opcode logger
+        // Deprecated: this field is not used internally anymore and will be removed.
+	Debug                   bool
 	Tracer                  EVMLogger // Opcode logger
 	NoBaseFee               bool      // Forces the EIP-1559 baseFee to 0 (needed for 0 price calls)
 	EnablePreimageRecording bool      // Enables recording of SHA3/keccak preimages
