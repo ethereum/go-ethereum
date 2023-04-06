bee03eadeb26b09878a79eabd3ed157db8c0182d
Nicolás Quiroz <nh.quiroz@gmail.com>
2023-04-06 14:08:04 +0100

    chore: move Header inside layout to make it sticky

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -12,78 +12,79 @@ export const Header: FC = () => {
   const isDark = colorMode === 'dark';
 
   return (
-    <Flex
-      mb={{ base: 4, lg: 8 }}
-      border='2px'
-      borderColor='primary'
-      justifyContent='space-between'
-      position='relative'
-    >
+    <Box bg='bg'>
       <Flex
-        p={4}
-        justifyContent='flex-start'
-        alignItems='center'
-        borderRight='2px'
+        border='2px'
         borderColor='primary'
-        flex={1}
-        gap={6}
+        justifyContent='space-between'
+        mb={{ base: 4, lg: 8 }}
       >
-        <NextLink href={'/'} passHref legacyBehavior>
-          <Link _hover={{ textDecoration: 'none' }}>
-            <Text textStyle='header-font' whiteSpace='nowrap'>
-              go-ethereum
-            </Text>
-          </Link>
-        </NextLink>
-        <Box
-          as='a'
-          href='#main-content'
-          pointerEvents='none'
-          w='0px'
-          opacity={0}
-          transition='opacity 200ms ease-in-out'
-          _focus={{
-            opacity: 1,
-            w: 'auto',
-            transition: 'opacity 200ms ease-in-out'
-          }}
-        >
-          <Text textStyle='header-font' whiteSpace='nowrap' fontSize='xs'>
-            skip to content
-          </Text>
-        </Box>
-      </Flex>
-
-      <Flex>
-        {/* HEADER BUTTONS */}
-        <Stack display={{ base: 'none', md: 'block' }}>
-          <HeaderButtons />
-        </Stack>
-
-        {/* SEARCH */}
-        <Stack display={{ base: 'none', md: 'block' }} borderRight='2px' borderColor='primary'>
-          <Search />
-        </Stack>
-
-        {/* DARK MODE SWITCH */}
-        <Box
-          as='button'
+        <Flex
           p={4}
-          borderRight={{ base: '2px', md: 'none' }}
+          justifyContent='flex-start'
+          alignItems='center'
+          borderRight='2px'
           borderColor='primary'
-          onClick={toggleColorMode}
-          _hover={{
-            bg: 'primary',
-            svg: { color: 'bg' }
-          }}
-          aria-label={`Toggle ${isDark ? 'light' : 'dark'} mode`}
+          flex={1}
+          gap={6}
         >
-          {isDark ? <SunIcon color='primary' /> : <MoonIcon color='primary' />}
-        </Box>
-      </Flex>
+          <NextLink href={'/'} passHref legacyBehavior>
+            <Link _hover={{ textDecoration: 'none' }}>
+              <Text textStyle='header-font' whiteSpace='nowrap'>
+                go-ethereum
+              </Text>
+            </Link>
+          </NextLink>
+          <Box
+            as='a'
+            href='#main-content'
+            pointerEvents='none'
+            w='0px'
+            opacity={0}
+            transition='opacity 200ms ease-in-out'
+            _focus={{
+              opacity: 1,
+              w: 'auto',
+              transition: 'opacity 200ms ease-in-out'
+            }}
+          >
+            <Text textStyle='header-font' whiteSpace='nowrap' fontSize='xs'>
+              skip to content
+            </Text>
+          </Box>
+        </Flex>
 
-      {/* MOBILE MENU */}
-      <MobileMenu />
-    </Flex>
+        <Flex>
+          {/* HEADER BUTTONS */}
+          <Stack display={{ base: 'none', md: 'block' }}>
+            <HeaderButtons />
+          </Stack>
+
+          {/* SEARCH */}
+          <Stack display={{ base: 'none', md: 'block' }} borderRight='2px' borderColor='primary'>
+            <Search />
+          </Stack>
+
+          {/* DARK MODE SWITCH */}
+          <Box
+            as='button'
+            p={4}
+            borderRight={{ base: '2px', md: 'none' }}
+            borderColor='primary'
+            onClick={toggleColorMode}
+            _hover={{
+              bg: 'primary',
+              svg: { color: 'bg' }
+            }}
+            aria-label={`Toggle ${isDark ? 'light' : 'dark'} mode`}
+          >
+            {isDark ? <SunIcon color='primary' /> : <MoonIcon color='primary' />}
+          </Box>
+        </Flex>
+
+        {/* MOBILE MENU */}
+        <MobileMenu />
+      </Flex>
+    </Box>
   );
 };
--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,22 +1,49 @@
-// Libraries
-import { Container, Flex, Stack } from '@chakra-ui/react';
-import { FC } from 'react';
+import { Box, Container, Flex, Stack } from '@chakra-ui/react';
+import { FC, useContext } from 'react';
+import { useRouter } from 'next/router';
 
-// Components
 import { Header } from '../UI';
 import { Footer } from './Footer';
+import { MobileDocsNav } from '../UI/docs';
+
+// Context
+import { NavLinksContext } from '../../context';
 
 interface Props {
   children?: React.ReactNode;
 }
 
 export const Layout: FC<Props> = ({ children }) => {
+  const router = useRouter();
+
+  const { mobileNavLinks } = useContext(NavLinksContext);
+  // console.log({ mobileNavLinks });
+
   return (
-    <Container maxW={{ base: 'full', md: 'container.2xl' }} my={{ base: 4, md: 7 }}>
-      {/* adding min-height & top margin to keep footer at the bottom of the page */}
-      <Flex direction='column' minH='calc(100vh - 3.5rem)'>
+    <Container
+      maxW={{ base: 'full', md: 'container.2xl' }}
+      my={{ base: 4, md: 7 }}
+      overflow='visible'
+    >
+      <Box
+        position='sticky'
+        top={{ base: 3, md: 7 }}
+        backdropFilter='blur(10px)'
+        opacity={0.9}
+        zIndex={9}
+      >
         <Header />
 
+        {/* `MobileDocsNav` should be rendered under `/docs` pages only */}
+        {router.asPath.startsWith('/docs') && (
+          <Stack display={{ base: 'block', lg: 'none' }} my={6}>
+            <MobileDocsNav navLinks={mobileNavLinks} />
+          </Stack>
+        )}
+      </Box>
+
+      {/* adding min-height & top margin to keep footer at the bottom of the page */}
+      <Flex direction='column' minH='calc(100vh - 3.5rem)' height='auto' overflow='auto'>
         {children}
 
         <Stack mt='auto'>
