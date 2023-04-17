0b3971a42242f5002e9cfe3453e7fe9356e49df5
marcello33 <marcelloardizzone@hotmail.it>
2023-04-17 14:22:33 +0200

    dev: chg: version bump for v0.3.9-beta (#825)

--- params/version.go
+++ params/version.go
@@ -24,7 +24,7 @@ const (
 	GitCommit    = ""
 	VersionMajor = 0      // Major version component of the current release
 	VersionMinor = 3      // Minor version component of the current release
-	VersionPatch = 8      // Patch version component of the current release
+	VersionPatch = 9      // Patch version component of the current release
 	VersionMeta  = "beta" // Version metadata to append to the version string
 )
 
