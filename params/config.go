7894cbe20572852ce694339f77ee7c8b8276baea
Liam <liam.icheng.lai@gmail.com>
2023-04-17 12:13:32 +1000

    fix config for round (#249)

--- params/config.go
+++ params/config.go
@@ -142,17 +142,17 @@ var (
 			WaitPeriod:           2,
 			MinePeriod:           2,
 		},
-		1962900: {
-			SwitchRound:          1962900,
-			CertThreshold:        50, // attack fix by reduce this number
+		1962899: {
+			SwitchRound:          1962899, // start from next round
+			CertThreshold:        50,      // attack fix by reduce this number
 			TimeoutSyncThreshold: 5,
 			TimeoutPeriod:        25,
 			WaitPeriod:           2,
 			MinePeriod:           2,
 		},
-		1964900: {
-			SwitchRound:          1964900,
-			CertThreshold:        73, // recover back to normal number, expect all nodes is back into masternode list
+		1964899: {
+			SwitchRound:          1964899, // start from next round
+			CertThreshold:        73,      // recover back to normal number, expect all nodes is back into masternode list
 			TimeoutSyncThreshold: 5,
 			TimeoutPeriod:        25,
 			WaitPeriod:           2,
