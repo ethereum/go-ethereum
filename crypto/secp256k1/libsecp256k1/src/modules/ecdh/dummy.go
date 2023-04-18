ef95d0ce6e7c8d8de1d7835ca57b151302f04fdc
yqrashawn <namy.19@gmail.com>
2023-04-18 15:45:06 +0800

    refactor: revert secp256k1 back
    Signed-off-by: yqrashawn <namy.19@gmail.com>

---
+++ crypto/secp256k1/libsecp256k1/src/modules/ecdh/dummy.go
@@ -0,0 +1,7 @@
+// +build dummy
+
+// Package c contains only a C file.
+//
+// This Go file is part of a workaround for `go mod vendor`.
+// Please see the file crypto/secp256k1/dummy.go for more information.
+package ecdh
