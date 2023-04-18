59f6ae9a00361dc9b662683561a45fd7390c30d0
joohhnnn <68833933+joohhnnn@users.noreply.github.com>
2023-04-18 17:57:08 +0800

    p2p: access embedded fields of Server directly (#27078)

--- p2p/server.go
+++ p2p/server.go
@@ -443,7 +443,7 @@ func (srv *Server) Start() (err error) {
 		return errors.New("server already running")
 	}
 	srv.running = true
-	srv.log = srv.Config.Logger
+	srv.log = srv.Logger
 	if srv.log == nil {
 		srv.log = log.Root()
 	}
@@ -501,7 +501,7 @@ func (srv *Server) setupLocalNode() error {
 	sort.Sort(capsByNameAndVersion(srv.ourHandshake.Caps))
 
 	// Create the local node.
-	db, err := enode.OpenDB(srv.Config.NodeDatabase)
+	db, err := enode.OpenDB(srv.NodeDatabase)
 	if err != nil {
 		return err
 	}
