11fafa4b889344f2b71e422ec3ea66be59e98def
Exca-DK <85954505+Exca-DK@users.noreply.github.com>
2023-04-25 12:12:34 +0200

    p2p/discover: add traffic metrics (#27008)
    Co-authored-by: Exca-DK <dev@DESKTOP-RI45P4J.localdomain>
    Co-authored-by: rjl493456442 <garyrong0905@gmail.com>

--- p2p/discover/v5_udp.go
+++ p2p/discover/v5_udp.go
@@ -142,7 +142,7 @@ func newUDPv5(conn UDPConn, ln *enode.LocalNode, cfg Config) (*UDPv5, error) {
 	cfg = cfg.withDefaults()
 	t := &UDPv5{
 		// static fields
-		conn:         conn,
+		conn:         newMeteredConn(conn),
 		localNode:    ln,
 		db:           ln.Database(),
 		netrestrict:  cfg.NetRestrict,
