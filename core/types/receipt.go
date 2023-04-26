398ed490b3774ac021983e93644f4ef0477f0758
Roberto Bayardo <bayardo@alum.mit.edu>
2023-04-25 23:37:11 -0700

    core/types: fix discrepancy in receipt.EffectiveGasPrice json encoding tags (#27114)
    Regenerate receipt json code to remove omit empty. Previously, there was a discrepancy between the generated code and the source. 
    
    ---------
    
    Co-authored-by: lightclient@protonmail.com <lightclient@protonmail.com>
    Co-authored-by: Martin Holst Swende <martin@swende.se>

--- core/types/receipt.go
+++ core/types/receipt.go
@@ -62,7 +62,7 @@ type Receipt struct {
 	TxHash            common.Hash    `json:"transactionHash" gencodec:"required"`
 	ContractAddress   common.Address `json:"contractAddress"`
 	GasUsed           uint64         `json:"gasUsed" gencodec:"required"`
-	EffectiveGasPrice *big.Int       `json:"effectiveGasPrice"`
+	EffectiveGasPrice *big.Int       `json:"effectiveGasPrice"` // required, but tag omitted for backwards compatibility
 
 	// Inclusion information: These fields provide information about the inclusion of the
 	// transaction corresponding to this receipt.
@@ -77,6 +77,7 @@ type receiptMarshaling struct {
 	Status            hexutil.Uint64
 	CumulativeGasUsed hexutil.Uint64
 	GasUsed           hexutil.Uint64
+	EffectiveGasPrice *hexutil.Big
 	BlockNumber       *hexutil.Big
 	TransactionIndex  hexutil.Uint
 }
