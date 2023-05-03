efad257b1fe6fb0eede92585f99a8b80b248856a
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2023-05-03 15:12:00 +0200

    cmd/utils: report the correct invalid block number in block import

--- cmd/utils/cmd.go
+++ cmd/utils/cmd.go
@@ -211,8 +211,8 @@ func ImportChain(chain *core.BlockChain, fn string) error {
 			log.Info("Skipping batch as all blocks present", "batch", batch, "first", blocks[0].Hash(), "last", blocks[i-1].Hash())
 			continue
 		}
-		if _, err := chain.InsertChain(missing); err != nil {
-			return fmt.Errorf("invalid block %d: %v", n, err)
+		if failindex, err := chain.InsertChain(missing); err != nil {
+			return fmt.Errorf("invalid block %d: %v", blocks[failindex].NumberU64(), err)
 		}
 	}
 	return nil
