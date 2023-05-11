c8b0afb2c449580b45171a6343ce8cc4226587e2
Delweng <delweng@gmail.com>
2023-05-11 15:09:16 +0800

    ethclient: acquire the rpc.Client (#27246)
    Signed-off-by: jsvisa <delweng@gmail.com>

--- ethclient/ethclient.go
+++ ethclient/ethclient.go
@@ -41,6 +41,7 @@ func Dial(rawurl string) (*Client, error) {
 	return DialContext(context.Background(), rawurl)
 }
 
+// DialContext connects a client to the given URL with context.
 func DialContext(ctx context.Context, rawurl string) (*Client, error) {
 	c, err := rpc.DialContext(ctx, rawurl)
 	if err != nil {
@@ -54,8 +55,14 @@ func NewClient(c *rpc.Client) *Client {
 	return &Client{c}
 }
 
+// Close closes the underlying RPC connection.
 func (ec *Client) Close() {
 	ec.c.Close()
+}
+
+// Client gets the underlying RPC client.
+func (ec *Client) Client() *rpc.Client {
+	return ec.c
 }
 
 // Blockchain Access
