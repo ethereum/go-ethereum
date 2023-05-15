72a37e8852c414d07d291593fdaf542cdfcdf4ea
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2023-05-15 11:35:50 +0200

    read from tree in state object if the snapshot is nil (#205)

--- core/state/state_object.go
+++ core/state/state_object.go
@@ -200,8 +200,9 @@ func (s *stateObject) GetCommittedState(db Database, key common.Hash) common.Has
 	}
 	// If no live objects are available, attempt to use snapshots
 	var (
-		enc []byte
-		err error
+		enc   []byte
+		err   error
+		value common.Hash
 	)
 	if s.db.snap != nil {
 		// If the object was destructed in *this* block (and potentially resurrected),
@@ -221,11 +222,20 @@ func (s *stateObject) GetCommittedState(db Database, key common.Hash) common.Has
 	}
 	// If the snapshot is unavailable or reading from it fails, load from the database.
 	if s.db.snap == nil || err != nil {
+		var tr Trie
 		if s.db.GetTrie().IsVerkle() {
-			panic("verkle trees use the snapshot")
+			tr = s.db.GetTrie()
+		} else {
+			tr = s.getTrie(db)
 		}
 		start := time.Now()
-		enc, err = s.getTrie(db).TryGet(s.address[:], key.Bytes())
+		if s.db.GetTrie().IsVerkle() {
+			var v []byte
+			v, err = tr.TryGet(s.address[:], key.Bytes())
+			copy(value[:], v)
+		} else {
+			enc, err = s.getTrie(db).TryGet(s.address[:], key.Bytes())
+		}
 		if metrics.EnabledExpensive {
 			s.db.StorageReads += time.Since(start)
 		}
@@ -234,7 +244,6 @@ func (s *stateObject) GetCommittedState(db Database, key common.Hash) common.Has
 			return common.Hash{}
 		}
 	}
-	var value common.Hash
 	if len(enc) > 0 {
 		_, content, _, err := rlp.Split(enc)
 		if err != nil {
