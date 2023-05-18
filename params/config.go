b8d22a104d0408a96fbe2f4ad7339fe1f41868ae
Péter Garamvölgyi <th307q@gmail.com>
2023-05-18 12:38:53 +0200

    Add Archimedes hard fork block number for Scroll Alpha (#331)
    * add Archimedes hard fork block number for Scroll Alpha
    
    * bump version

--- params/config.go
+++ params/config.go
@@ -275,7 +275,7 @@ var (
 		BerlinBlock:         big.NewInt(0),
 		LondonBlock:         big.NewInt(0),
 		ArrowGlacierBlock:   nil,
-		ArchimedesBlock:     nil,
+		ArchimedesBlock:     big.NewInt(2444711),
 		ShanghaiBlock:       nil,
 		Clique: &CliqueConfig{
 			Period: 3,
--- params/version.go
+++ params/version.go
@@ -23,8 +23,8 @@ import (
 
 const (
 	VersionMajor = 3       // Major version component of the current release
-	VersionMinor = 1       // Minor version component of the current release
-	VersionPatch = 12      // Patch version component of the current release
+	VersionMinor = 2       // Minor version component of the current release
+	VersionPatch = 0       // Patch version component of the current release
 	VersionMeta  = "alpha" // Version metadata to append to the version string
 )
 
