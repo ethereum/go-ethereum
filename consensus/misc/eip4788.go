d6d0d6d24e045dbd485628cb9bbf02cdba171a7f
Marius van der Wijden <m.vanderwijden@live.de>
2023-05-19 17:03:56 +0200

    consensus/misc: header

--- consensus/misc/eip4788.go
+++ consensus/misc/eip4788.go
@@ -1,3 +1,19 @@
+// Copyright 2023 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
 package misc
 
 import (
@@ -8,6 +24,7 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 )
 
+// ApplyBeaconRoot adds the beacon root from the header to the state.
 func ApplyBeaconRoot(header *types.Header, state *state.StateDB) {
 	// If EIP-4788 is enabled, we need to store the block root
 	historicalStorageAddress := common.HexToAddress("0xfffffffffffffffffffffffffffffffffffffffd")
