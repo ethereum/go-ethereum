48f68ab1e907973b3e060ea07cede5cb82e8d271
Felix Lange <fjl@twurst.com>
2023-05-23 11:02:30 +0200

    eth/filters: fix test

--- eth/filters/api_test.go
+++ eth/filters/api_test.go
@@ -56,7 +56,8 @@ func TestUnmarshalJSONNewFilterArgs(t *testing.T) {
 
 	// from, to block number
 	var test1 FilterCriteria
-	vector := fmt.Sprintf(`{"fromBlock":"%#x","toBlock":"%#x"}`, fromBlock, toBlock)
+	vector := fmt.Sprintf(`{"fromBlock":"%v","toBlock":"%v"}`, fromBlock, toBlock)
+	t.Logf("vector: %s", vector)
 	if err := json.Unmarshal([]byte(vector), &test1); err != nil {
 		t.Fatal(err)
 	}
