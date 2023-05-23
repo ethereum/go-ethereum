9231770811cda0473a7fa4e2bccc95bf62aae634
Martin Holst Swende <martin@swende.se>
2023-05-23 07:18:38 -0400

    rpc: change BlockNumber constant values to match ethclient (#27219)
    ethclient accepts certain negative block number values as specifiers for the "pending",
    "safe" and "finalized" block. In case of "pending", the value accepted by ethclient (-1)
    did not match rpc.PendingBlockNumber (-2).
    
    This wasn't really a problem, but other values accepted by ethclient did match the
    definitions in package rpc, and it's weird to have this one special case where they don't.
    
    To fix it, we decided to change the values of the constants rather than changing ethclient.
    The constant values are not otherwise significant. This is a breaking API change, but we
    believe not a dangerous one.
    
    ---------
    
    Co-authored-by: Felix Lange <fjl@twurst.com>

--- eth/filters/filter.go
+++ eth/filters/filter.go
@@ -296,6 +296,9 @@ func (f *Filter) checkMatches(ctx context.Context, header *types.Header) ([]*typ
 // pendingLogs returns the logs matching the filter criteria within the pending block.
 func (f *Filter) pendingLogs() ([]*types.Log, error) {
 	block, receipts := f.sys.backend.PendingBlockAndReceipts()
+	if block == nil {
+		return nil, errors.New("pending state not available")
+	}
 	if bloomFilter(block.Bloom(), f.addresses, f.topics) {
 		var unfiltered []*types.Log
 		for _, r := range receipts {
