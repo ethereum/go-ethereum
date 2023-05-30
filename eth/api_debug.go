d789c68b667e13eb5cefd19d09ae84f7d016df6a
jwasinger <j-wasinger@hotmail.com>
2023-05-30 14:16:28 +0200

    eth: make debug_StorageRangeAt take a block hash or number (#27328)
    eth: make StorageRangeAt take a block hash or number
    
    Co-authored-by: Martin Holst Swende <martin@swende.se>
    Co-authored-by: Sina Mahmoodi <itz.s1na@gmail.com>

--- eth/api_debug.go
+++ eth/api_debug.go
@@ -204,11 +204,16 @@ type storageEntry struct {
 }
 
 // StorageRangeAt returns the storage at the given block height and transaction index.
-func (api *DebugAPI) StorageRangeAt(ctx context.Context, blockHash common.Hash, txIndex int, contractAddress common.Address, keyStart hexutil.Bytes, maxResult int) (StorageRangeResult, error) {
-	// Retrieve the block
-	block := api.eth.blockchain.GetBlockByHash(blockHash)
+func (api *DebugAPI) StorageRangeAt(ctx context.Context, blockNrOrHash rpc.BlockNumberOrHash, txIndex int, contractAddress common.Address, keyStart hexutil.Bytes, maxResult int) (StorageRangeResult, error) {
+	var block *types.Block
+
+	block, err := api.eth.APIBackend.BlockByNumberOrHash(ctx, blockNrOrHash)
+	if err != nil {
+		return StorageRangeResult{}, err
+	}
+
 	if block == nil {
-		return StorageRangeResult{}, fmt.Errorf("block %#x not found", blockHash)
+		return StorageRangeResult{}, fmt.Errorf("block %v not found", blockNrOrHash)
 	}
 	_, _, statedb, release, err := api.eth.stateAtTransaction(ctx, block, txIndex, 0)
 	if err != nil {
