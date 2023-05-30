c56e60b850927ce8b33e929cc344ee035e00b35d
ucwong <ucwong@126.com>
2023-05-30 13:55:03 +0100

    les, eth: fix typo in comment (#27369)

--- les/client.go
+++ les/client.go
@@ -266,12 +266,12 @@ func (s *LightEthereum) prenegQuery(n *enode.Node) int {
 
 type LightDummyAPI struct{}
 
-// Etherbase is the address that mining rewards will be send to
+// Etherbase is the address that mining rewards will be sent to
 func (s *LightDummyAPI) Etherbase() (common.Address, error) {
 	return common.Address{}, errors.New("mining is not supported in light mode")
 }
 
-// Coinbase is the address that mining rewards will be send to (alias for Etherbase)
+// Coinbase is the address that mining rewards will be sent to (alias for Etherbase)
 func (s *LightDummyAPI) Coinbase() (common.Address, error) {
 	return common.Address{}, errors.New("mining is not supported in light mode")
 }
