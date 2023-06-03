f3689a028d5c104b7a923eda2fba4faa9161f700
Christopher Harrison <chris.harrison@electroneum.com>
2023-06-03 20:25:31 +0100

    Adjust RPCTxFeeCap

--- eth/ethconfig/config.go
+++ eth/ethconfig/config.go
@@ -75,7 +75,7 @@ var Defaults = Config{
 	RPCGasCap:               50000000,
 	RPCEVMTimeout:           5 * time.Second,
 	GPO:                     FullNodeGPO,
-	RPCTxFeeCap:             1, // 1 ether
+	RPCTxFeeCap:             10, // 10 ether
 }
 
 //go:generate go run github.com/fjl/gencodec -type Config -formats toml -out gen_config.go
