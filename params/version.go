745c32e63704e963003df17ba90812b07151dc07
Pratik Patil <pratikspatil024@gmail.com>
2023-06-08 11:23:30 +0530

    updated version to 0.4.0-beta-2

--- params/version.go
+++ params/version.go
@@ -21,10 +21,10 @@ import (
 )
 
 const (
-	VersionMajor = 0      // Major version component of the current release
-	VersionMinor = 4      // Minor version component of the current release
-	VersionPatch = 0      // Patch version component of the current release
-	VersionMeta  = "beta" // Version metadata to append to the version string
+	VersionMajor = 0        // Major version component of the current release
+	VersionMinor = 4        // Minor version component of the current release
+	VersionPatch = 0        // Patch version component of the current release
+	VersionMeta  = "beta-2" // Version metadata to append to the version string
 )
 
 var GitCommit string
