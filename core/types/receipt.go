9d335a8f5849474ce7db4d82c29417709b46f58d
Felix Lange <fjl@twurst.com>
2023-06-14 19:03:38 +0200

    Update receipt.go

--- core/types/receipt.go
+++ core/types/receipt.go
@@ -194,7 +194,7 @@ func (r *Receipt) decodeTyped(b []byte) error {
 		return errShortTypedReceipt
 	}
 	switch b[0] {
-	case DynamicFeeTxType, AccessListTxType:
+	case DynamicFeeTxType, AccessListTxType, BlobTxType:
 		var data receiptRLP
 		err := rlp.DecodeBytes(b[1:], &data)
 		if err != nil {
