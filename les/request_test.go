b54c50ab159f94d38f96ae13c63b742937c59c37
Gary Rong <garyrong0905@gmail.com>
2023-06-14 10:09:40 +0800

    les: fix lint

--- les/request_test.go
+++ les/request_test.go
@@ -18,12 +18,12 @@ package les
 
 import (
 	"context"
-	"github.com/ethereum/go-ethereum/core/types"
 	"testing"
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/rawdb"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/ethereum/go-ethereum/light"
