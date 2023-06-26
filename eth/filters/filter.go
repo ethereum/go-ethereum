2fed7fbe0b9cc78b79f6adad83cffa327a50a71d
jsvisa <delweng@gmail.com>
2023-06-26 23:22:39 +0800

    eth/filters: use Sign instead of Int64 to check +-
    Signed-off-by: jsvisa <delweng@gmail.com>

--- eth/filters/filter.go
+++ eth/filters/filter.go
@@ -380,10 +380,10 @@ func filterLogs(logs []*types.Log, fromBlock, toBlock *big.Int, addresses []comm
 	topicLen := len(topics)
 
 	for _, log := range logs {
-		if fromBlock != nil && fromBlock.Int64() >= 0 && fromBlock.Uint64() > log.BlockNumber {
+		if fromBlock != nil && fromBlock.Sign() >= 0 && fromBlock.Uint64() > log.BlockNumber {
 			continue
 		}
-		if toBlock != nil && toBlock.Int64() >= 0 && toBlock.Uint64() < log.BlockNumber {
+		if toBlock != nil && toBlock.Sign() >= 0 && toBlock.Uint64() < log.BlockNumber {
 			continue
 		}
 
