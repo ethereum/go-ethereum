f5d3d486e459dce29130576ae88f2324ad586b50
Marius van der Wijden <m.vanderwijden@live.de>
2023-06-28 11:06:20 +0200

    eth: send big transactions by announce/retrieve only (#27618)
    * eth: send big transactions by announce/retrieve only
    
    * Update eth/handler.go
    
    Co-authored-by: Martin Holst Swende <martin@swende.se>
    
    * eth: remove superfluous bracket
    
    * eth: add whitespace
    
    ---------
    
    Co-authored-by: Martin Holst Swende <martin@swende.se>

--- eth/handler.go
+++ eth/handler.go
@@ -45,6 +45,11 @@ const (
 	// txChanSize is the size of channel listening to NewTxsEvent.
 	// The number is referenced from the size of tx pool.
 	txChanSize = 4096
+
+	// txMaxBroadcastSize is the max size of a transaction that will be broadcasted.
+	// All transactions with a higher size will be announced and need to be fetched
+	// by the peer.
+	txMaxBroadcastSize = 4096
 )
 
 var (
@@ -564,8 +569,12 @@ func (h *handler) BroadcastTransactions(txs types.Transactions) {
 	// Broadcast transactions to a batch of peers not knowing about it
 	for _, tx := range txs {
 		peers := h.peers.peersWithoutTransaction(tx.Hash())
+
+		var numDirect int
+		if tx.Size() <= txMaxBroadcastSize {
+			numDirect = int(math.Sqrt(float64(len(peers))))
+		}
 		// Send the tx unconditionally to a subset of our peers
-		numDirect := int(math.Sqrt(float64(len(peers))))
 		for _, peer := range peers[:numDirect] {
 			txset[peer] = append(txset[peer], tx.Hash())
 		}
