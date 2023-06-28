cde2aacd1467eb213a7c066539ba4c8ef75ffe0a
allen <lvshuaino@gmail.com>
2023-06-28 15:11:45 -0400

    accounts/abi: return rawdata when revert is custom type

--- accounts/abi/abi.go
+++ accounts/abi/abi.go
@@ -21,6 +21,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/status-im/keycard-go/hexutils"
 	"io"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -255,7 +256,7 @@ func UnpackRevert(data []byte) (string, error) {
 		return "", errors.New("invalid data for unpacking")
 	}
 	if !bytes.Equal(data[:4], revertSelector) {
-		return "", errors.New("invalid data for unpacking")
+		return hexutils.BytesToHex(data), nil
 	}
 	typ, err := NewType("string", "", nil)
 	if err != nil {
