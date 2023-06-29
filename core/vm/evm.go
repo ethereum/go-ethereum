07c0e7b0b33b9055ae70f8f183a5a1e6670a27cd
Jan Christoph Bernack <jc.bernack@gmail.com>
2023-06-29 16:51:31 +0200

    skip value transfer when calling external contract

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -210,7 +210,9 @@ func (evm *EVM) Call(caller ContractRef, addr common.Address, input []byte, gas 
 		}
 		evm.StateDB.CreateAccount(addr)
 	}
-	evm.Context.Transfer(evm.StateDB, caller.Address(), addr, value)
+	if !isExternal {
+		evm.Context.Transfer(evm.StateDB, caller.Address(), addr, value)
+	}
 
 	// Capture the tracer start/end events in debug mode
 	if evm.Config.Debug {
