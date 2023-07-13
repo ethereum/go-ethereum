14e13386eec860a708b41d30dc91609e3123f99b
elizabeth <elizabethjbinks@gmail.com>
2023-07-13 14:42:37 -0400

    remove metro

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1699,17 +1699,6 @@ func SubmitTransaction(ctx context.Context, b Backend, tx *types.Transaction) (c
 		return common.Hash{}, err
 	}
 
-	// send to metro instead of eth mempool
-	txBytes, err := tx.MarshalBinary()
-	if err != nil {
-		return common.Hash{}, err
-	}
-
-	metroAPI := NewMetroAPI(b.MetroGRPCEndpoint())
-	if err := metroAPI.SubmitTransaction(txBytes); err != nil {
-		return common.Hash{}, err
-	}
-
 	// Print a log with full tx details for manual investigations and interventions
 	signer := types.MakeSigner(b.ChainConfig(), b.CurrentBlock().Number)
 	from, err := types.Sender(signer, tx)
