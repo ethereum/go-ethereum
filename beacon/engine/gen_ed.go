e86ad526404681f52f67753c843d7a65274721b6
Felix Lange <fjl@twurst.com>
2023-07-18 09:44:16 +0200

    beacon/engine, eth/catalyst: EIP-4844 updates for the engine API (#27736)
    This is a spin-out from the EIP-4844 devnet branch, containing just the Engine API modifications
    and nothing else. The newPayloadV3 endpoint won't really work in this version, but we need the
    data structures for testing so I'd like to get this in early.
    
    Co-authored-by: Marius van der Wijden <m.vanderwijden@live.de>

--- beacon/engine/gen_ed.go
+++ beacon/engine/gen_ed.go
@@ -32,6 +32,8 @@ func (e ExecutableData) MarshalJSON() ([]byte, error) {
 		BlockHash     common.Hash         `json:"blockHash"     gencodec:"required"`
 		Transactions  []hexutil.Bytes     `json:"transactions"  gencodec:"required"`
 		Withdrawals   []*types.Withdrawal `json:"withdrawals"`
+		DataGasUsed   *hexutil.Uint64     `json:"dataGasUsed"`
+		ExcessDataGas *hexutil.Uint64     `json:"excessDataGas"`
 	}
 	var enc ExecutableData
 	enc.ParentHash = e.ParentHash
@@ -54,6 +56,8 @@ func (e ExecutableData) MarshalJSON() ([]byte, error) {
 		}
 	}
 	enc.Withdrawals = e.Withdrawals
+	enc.DataGasUsed = (*hexutil.Uint64)(e.DataGasUsed)
+	enc.ExcessDataGas = (*hexutil.Uint64)(e.ExcessDataGas)
 	return json.Marshal(&enc)
 }
 
@@ -75,6 +79,8 @@ func (e *ExecutableData) UnmarshalJSON(input []byte) error {
 		BlockHash     *common.Hash        `json:"blockHash"     gencodec:"required"`
 		Transactions  []hexutil.Bytes     `json:"transactions"  gencodec:"required"`
 		Withdrawals   []*types.Withdrawal `json:"withdrawals"`
+		DataGasUsed   *hexutil.Uint64     `json:"dataGasUsed"`
+		ExcessDataGas *hexutil.Uint64     `json:"excessDataGas"`
 	}
 	var dec ExecutableData
 	if err := json.Unmarshal(input, &dec); err != nil {
@@ -141,6 +147,12 @@ func (e *ExecutableData) UnmarshalJSON(input []byte) error {
 	}
 	if dec.Withdrawals != nil {
 		e.Withdrawals = dec.Withdrawals
+	}
+	if dec.DataGasUsed != nil {
+		e.DataGasUsed = (*uint64)(dec.DataGasUsed)
+	}
+	if dec.ExcessDataGas != nil {
+		e.ExcessDataGas = (*uint64)(dec.ExcessDataGas)
 	}
 	return nil
 }
