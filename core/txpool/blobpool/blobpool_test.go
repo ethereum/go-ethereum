dd006defd197a28b2abb9183a089cd55c1ae4212
Péter Szilágyi <peterke@gmail.com>
2023-07-21 12:30:20 +0300

    core/txpool/blobpool: luv u linter

--- core/txpool/blobpool/blobpool_test.go
+++ core/txpool/blobpool/blobpool_test.go
@@ -674,7 +674,7 @@ func TestOpenHeap(t *testing.T) {
 	store, _ := billy.Open(billy.Options{Path: filepath.Join(storage, pendingTransactionStore)}, newSlotter(), nil)
 
 	// Insert a few transactions from a few accounts. To remove randomness from
-	// the heap oinitialization, use a determinsitic account/tx/priority ordering.
+	// the heap initialization, use a deterministic account/tx/priority ordering.
 	var (
 		key1, _ = crypto.GenerateKey()
 		key2, _ = crypto.GenerateKey()
