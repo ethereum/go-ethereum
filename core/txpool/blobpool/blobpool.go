b9edfc6d5b2a4f489cf4afcab1844161649d16f8
Péter Szilágyi <peterke@gmail.com>
2023-07-27 14:52:00 +0300

    core/txpool/blobpool: fix a merge conflict from a package rename

--- core/txpool/blobpool/blobpool.go
+++ core/txpool/blobpool/blobpool.go
@@ -30,6 +30,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/consensus/misc"
+	"github.com/ethereum/go-ethereum/consensus/misc/eip4844"
 	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/state"
 	"github.com/ethereum/go-ethereum/core/txpool"
@@ -402,7 +403,7 @@ func (p *BlobPool) Init(gasTip *big.Int, head *types.Header, reserve txpool.Addr
 		blobfee = uint256.MustFromBig(big.NewInt(params.BlobTxMinDataGasprice))
 	)
 	if p.head.ExcessDataGas != nil {
-		blobfee = uint256.MustFromBig(misc.CalcBlobFee(*p.head.ExcessDataGas))
+		blobfee = uint256.MustFromBig(eip4844.CalcBlobFee(*p.head.ExcessDataGas))
 	}
 	p.evict = newPriceHeap(basefee, blobfee, &p.index)
 
@@ -775,7 +776,7 @@ func (p *BlobPool) Reset(oldHead, newHead *types.Header) {
 		blobfee = uint256.MustFromBig(big.NewInt(params.BlobTxMinDataGasprice))
 	)
 	if newHead.ExcessDataGas != nil {
-		blobfee = uint256.MustFromBig(misc.CalcBlobFee(*newHead.ExcessDataGas))
+		blobfee = uint256.MustFromBig(eip4844.CalcBlobFee(*newHead.ExcessDataGas))
 	}
 	p.evict.reinit(basefee, blobfee, false)
 
