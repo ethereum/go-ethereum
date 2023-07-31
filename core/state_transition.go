73f39af9968b2c45e48436f7ecbcd4426c794353
jsvisa <delweng@gmail.com>
2023-07-31 14:19:42 +0800

    core: no comment
    Signed-off-by: jsvisa <delweng@gmail.com>

--- core/state_transition.go
+++ core/state_transition.go
@@ -329,7 +329,6 @@ func (st *StateTransition) preCheck() error {
 	if st.evm.ChainConfig().IsCancun(st.evm.Context.BlockNumber, st.evm.Context.Time) {
 		if st.blobGasUsed() > 0 {
 			// Check that the user is paying at least the current blob fee
-			// ExcessBlobGas must not be nil, which is checked in eip4844.VerifyEIP4844Header
 			blobFee := eip4844.CalcBlobFee(*st.evm.Context.ExcessBlobGas)
 			if st.msg.BlobGasFeeCap.Cmp(blobFee) < 0 {
 				return fmt.Errorf("%w: address %v have %v want %v", ErrBlobFeeCapTooLow, st.msg.From.Hex(), st.msg.BlobGasFeeCap, blobFee)
