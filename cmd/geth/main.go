e307effaf30ecdf48d7bccf5eb2e60f6a13647b2
jsvisa <delweng@gmail.com>
2023-07-31 14:33:10 +0800

    cmd/geth: disable automaxprocs log
    Signed-off-by: jsvisa <delweng@gmail.com>

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -39,13 +39,11 @@ import (
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/metrics"
 	"github.com/ethereum/go-ethereum/node"
+	"go.uber.org/automaxprocs/maxprocs"
 
 	// Force-load the tracer engines to trigger registration
 	_ "github.com/ethereum/go-ethereum/eth/tracers/js"
 	_ "github.com/ethereum/go-ethereum/eth/tracers/native"
-
-	// Automatically set GOMAXPROCS to match Linux container CPU quota.
-	_ "go.uber.org/automaxprocs"
 
 	"github.com/urfave/cli/v2"
 )
@@ -325,6 +323,9 @@ func geth(ctx *cli.Context) error {
 	if args := ctx.Args().Slice(); len(args) > 0 {
 		return fmt.Errorf("invalid command: %q", args[0])
 	}
+
+	// Automatically set GOMAXPROCS to match Linux container CPU quota.
+	maxprocs.Set(maxprocs.Logger(func(string, ...interface{}) {}))
 
 	prepare(ctx)
 	stack, backend := makeFullNode(ctx)
