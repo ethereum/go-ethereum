857476753dc8a8b3f090c60c08a05b41d42aced9
ucwong <ucwong@126.com>
2023-08-07 08:11:06 +0100

    internal: use atomic type (#27858)

--- internal/cmdtest/test_cmd.go
+++ internal/cmdtest/test_cmd.go
@@ -55,13 +55,13 @@ type TestCmd struct {
 	Err error
 }
 
-var id int32
+var id atomic.Int32
 
 // Run exec's the current binary using name as argv[0] which will trigger the
 // reexec init function for that name (e.g. "geth-test" in cmd/geth/run_test.go)
 func (tt *TestCmd) Run(name string, args ...string) {
-	id := atomic.AddInt32(&id, 1)
-	tt.stderr = &testlogger{t: tt.T, name: fmt.Sprintf("%d", id)}
+	id.Add(1)
+	tt.stderr = &testlogger{t: tt.T, name: fmt.Sprintf("%d", id.Load())}
 	tt.cmd = &exec.Cmd{
 		Path:   reexec.Self(),
 		Args:   append([]string{name}, args...),
