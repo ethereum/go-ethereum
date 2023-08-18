bebd2961167e8906a0c5e28d05b4b8207f35d8a7
Manav Darji <manavdarji.india@gmail.com>
2023-08-18 14:39:05 +0530

    ethclient: use bor_getRootHash instead of eth (#967)

--- ethclient/bor_ethclient.go
+++ ethclient/bor_ethclient.go
@@ -11,7 +11,7 @@ import (
 // GetRootHash returns the merkle root of the block headers
 func (ec *Client) GetRootHash(ctx context.Context, startBlockNumber uint64, endBlockNumber uint64) (string, error) {
 	var rootHash string
-	if err := ec.c.CallContext(ctx, &rootHash, "eth_getRootHash", startBlockNumber, endBlockNumber); err != nil {
+	if err := ec.c.CallContext(ctx, &rootHash, "bor_getRootHash", startBlockNumber, endBlockNumber); err != nil {
 		return "", err
 	}
 
