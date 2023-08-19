0d3579ca24d3c676067e5fc1b96ae3f8d5278a1f
jsvisa <delweng@gmail.com>
2023-08-19 11:00:43 +0800

    dry
    Signed-off-by: jsvisa <delweng@gmail.com>

--- internal/ethapi/api_test.go
+++ internal/ethapi/api_test.go
@@ -52,14 +52,13 @@ import (
 	"golang.org/x/exp/slices"
 )
 
-func TestTransaction_RoundTripRpcJSON(t *testing.T) {
+func testTransactionMarshal(t *testing.T, tests []txData, config *params.ChainConfig) {
+	t.Parallel()
 	var (
-		config = params.AllEthashProtocolChanges
 		signer = types.LatestSigner(config)
 		key, _ = crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
-		tests  = allTransactionTypes(common.Address{0xde, 0xad}, config)
 	)
-	t.Parallel()
+
 	for i, tt := range tests {
 		var tx2 types.Transaction
 		tx, err := types.SignNewTx(key, signer, tt.Tx)
@@ -90,45 +89,22 @@ func TestTransaction_RoundTripRpcJSON(t *testing.T) {
 	}
 }
 
+func TestTransaction_RoundTripRpcJSON(t *testing.T) {
+	var (
+		config = params.AllEthashProtocolChanges
+		tests  = allTransactionTypes(common.Address{0xde, 0xad}, config)
+	)
+	testTransactionMarshal(t, tests, config)
+}
+
 func TestTransactionBlobTx(t *testing.T) {
 	newUint64 := func(val uint64) *uint64 { return &val }
 	config := params.TestChainConfig
 	config.ShanghaiTime = newUint64(0)
 	config.CancunTime = newUint64(0)
-	var (
-		signer = types.LatestSigner(config)
-		key, _ = crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
-		tests  = allBlobTxs(common.Address{0xde, 0xad}, config)
-	)
-	t.Parallel()
-	for i, tt := range tests {
-		var tx2 types.Transaction
-		tx, err := types.SignNewTx(key, signer, tt.Tx)
-		if err != nil {
-			t.Fatalf("test %d: signing failed: %v", i, err)
-		}
-		// Regular transaction
-		if data, err := json.Marshal(tx); err != nil {
-			t.Fatalf("test %d: marshalling failed; %v", i, err)
-		} else if err = tx2.UnmarshalJSON(data); err != nil {
-			t.Fatalf("test %d: sunmarshal failed: %v", i, err)
-		} else if want, have := tx.Hash(), tx2.Hash(); want != have {
-			t.Fatalf("test %d: stx changed, want %x have %x", i, want, have)
-		}
+	tests := allBlobTxs(common.Address{0xde, 0xad}, config)
 
-		// rpcTransaction
-		rpcTx := newRPCTransaction(tx, common.Hash{}, 0, 0, 0, nil, config)
-		if data, err := json.Marshal(rpcTx); err != nil {
-			t.Fatalf("test %d: marshalling failed; %v", i, err)
-		} else if err = tx2.UnmarshalJSON(data); err != nil {
-			t.Fatalf("test %d: unmarshal failed: %v", i, err)
-		} else if want, have := tx.Hash(), tx2.Hash(); want != have {
-			t.Fatalf("test %d: tx changed, want %x have %x", i, want, have)
-		} else {
-			want, have := tt.Want, string(data)
-			require.JSONEqf(t, want, have, "test %d: rpc json not match, want %s have %s", i, want, have)
-		}
-	}
+	testTransactionMarshal(t, tests, config)
 }
 
 type txData struct {
