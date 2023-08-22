ccde357a0606eb076e3e4972c52202ba72ec5e8b
Martin Holst Swende <martin@swende.se>
2023-08-22 10:00:01 +0200

    travis, build: update Go to 1.21.0 (#27958)
    build: update to go 1.21

--- build/ci.go
+++ build/ci.go
@@ -139,7 +139,7 @@ var (
 	// This is the version of Go that will be downloaded by
 	//
 	//     go run ci.go install -dlgo
-	dlgoVersion = "1.20.7"
+	dlgoVersion = "1.21.0"
 
 	// This is the version of Go that will be used to bootstrap the PPA builder.
 	//
