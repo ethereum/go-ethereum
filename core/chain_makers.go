b88138ad05626d2080cdb1d05be5aaaa8b356506
Felix Lange <fjl@twurst.com>
2023-08-22 18:29:17 +0200

    core: add BeaconRoot to header in GenerateChain (#27974)
    Fixes an error in tests of internal/ethapi.

--- core/chain_makers.go
+++ core/chain_makers.go
@@ -411,6 +411,7 @@ func makeHeader(chain consensus.ChainReader, parent *types.Block, state *state.S
 		excessBlobGas := eip4844.CalcExcessBlobGas(parentExcessBlobGas, parentBlobGasUsed)
 		header.ExcessBlobGas = &excessBlobGas
 		header.BlobGasUsed = new(uint64)
+		header.BeaconRoot = new(common.Hash)
 	}
 	return header
 }
