3abe397540978d97cd022c645801620252eb595c
Marius van der Wijden <m.vanderwijden@live.de>
2023-08-23 20:40:56 +0200

    miner: remove artifacts

--- miner/payload_building.go
+++ miner/payload_building.go
@@ -35,12 +35,11 @@ import (
 // Check engine-api specification for more details.
 // https://github.com/ethereum/execution-apis/blob/main/src/engine/specification.md#payloadattributesv1
 type BuildPayloadArgs struct {
-	Parent       common.Hash          // The parent block to build payload on top
-	Timestamp    uint64               // The provided timestamp of generated payload
-	FeeRecipient common.Address       // The provided recipient address for collecting transaction fee
-	Random       common.Hash          // The provided randomness value
-	Withdrawals  types.Withdrawals    // The provided withdrawals
-	Transactions []*types.Transaction // Mandatory transactions to be included
+	Parent       common.Hash       // The parent block to build payload on top
+	Timestamp    uint64            // The provided timestamp of generated payload
+	FeeRecipient common.Address    // The provided recipient address for collecting transaction fee
+	Random       common.Hash       // The provided randomness value
+	Withdrawals  types.Withdrawals // The provided withdrawals
 }
 
 // Id computes an 8-byte identifier by hashing the components of the payload arguments.
@@ -52,7 +51,6 @@ func (args *BuildPayloadArgs) Id() engine.PayloadID {
 	hasher.Write(args.Random[:])
 	hasher.Write(args.FeeRecipient[:])
 	rlp.Encode(hasher, args.Withdrawals)
-	rlp.Encode(hasher, args.Transactions)
 	var out engine.PayloadID
 	copy(out[:], hasher.Sum(nil)[:8])
 	return out
