d7478f726ed311c5c07b80c578a7a81c51217263
Shivam Sharma <shivam691999@gmail.com>
2023-08-29 00:01:54 +0530

    bump : version to 0.5.0-beta-6

--- params/version.go
+++ params/version.go
@@ -24,7 +24,7 @@ const (
 	VersionMajor = 0        // Major version component of the current release
 	VersionMinor = 5        // Minor version component of the current release
 	VersionPatch = 0        // Patch version component of the current release
-	VersionMeta  = "beta-5" // Version metadata to append to the version string
+	VersionMeta  = "beta-6" // Version metadata to append to the version string
 )
 
 var GitCommit string
