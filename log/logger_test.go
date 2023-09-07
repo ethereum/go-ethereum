8e1778271a717e86ad86a4a682847bef54632710
Martin Holst Swende <martin@swende.se>
2023-09-07 13:55:23 +0200

    log: fix tests

--- log/logger_test.go
+++ log/logger_test.go
@@ -5,25 +5,7 @@ import (
 	"os"
 	"strings"
 	"testing"
-	"time"
 )
-
-func BenchmarkTraceLogging(b *testing.B) {
-	Root().SetHandler(LvlFilterHandler(LvlInfo, StreamHandler(os.Stderr, TerminalFormat(true))))
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		Trace("a message", "v", i)
-	}
-}
-
-type notimeHandler struct {
-	next Handler
-}
-
-func (n notimeHandler) Log(r *Record) error {
-	r.Time = time.Unix(0, 0)
-	return n.next.Log(r)
-}
 
 // TestLoggingWithTrace checks that if BackTraceAt is set, then the
 // gloghandler is capable of spitting out a stacktrace
@@ -34,15 +16,21 @@ func TestLoggingWithTrace(t *testing.T) {
 	{
 		glog := NewGlogHandler(StreamHandler(out, TerminalFormat(false)))
 		glog.Verbosity(LvlTrace)
-		if err := glog.BacktraceAt("logger_test.go:42"); err != nil {
+		if err := glog.BacktraceAt("logger_test.go:24"); err != nil {
 			t.Fatal(err)
 		}
-		logger.SetHandler(notimeHandler{glog})
+		logger.SetHandler(glog)
 	}
 	logger.Trace("a message", "foo", "bar") // Will be bumped to INFO
 	have := out.String()
-	wantPrefix := "INFO [01-01|01:00:00.000] a message\n\ngoroutine"
-	if len(have) < len(wantPrefix) || !strings.HasPrefix(have, wantPrefix) {
+	if !strings.HasPrefix(have, "INFO") {
+		t.Fatalf("backtraceat should bump level to info: %s", have)
+	}
+	// The timestamp is locale-dependent, so we want to trim that off
+	// "INFO [01-01|00:00:00.000] a messag ..." -> "a messag..."
+	have = strings.Split(have, "]")[1]
+	wantPrefix := " a message\n\ngoroutine"
+	if !strings.HasPrefix(have, wantPrefix) {
 		t.Errorf("\nhave: %q\nwant: %q\n", have, wantPrefix)
 	}
 }
@@ -55,14 +43,25 @@ func TestLoggingWithVmodule(t *testing.T) {
 	{
 		glog := NewGlogHandler(StreamHandler(out, TerminalFormat(false)))
 		glog.Verbosity(LvlCrit)
-		logger.SetHandler(notimeHandler{glog})
+		logger.SetHandler(glog)
 		logger.Warn("This should not be seen", "ignored", "true")
 		glog.Vmodule("logger_test.go=5")
 	}
 	logger.Trace("a message", "foo", "bar")
 	have := out.String()
-	want := "TRACE[01-01|01:00:00.000] a message                                foo=bar\n"
+	// The timestamp is locale-dependent, so we want to trim that off
+	// "INFO [01-01|00:00:00.000] a messag ..." -> "a messag..."
+	have = strings.Split(have, "]")[1]
+	want := " a message                                foo=bar\n"
 	if have != want {
 		t.Errorf("\nhave: %q\nwant: %q\n", have, want)
+	}
+}
+
+func BenchmarkTraceLogging(b *testing.B) {
+	Root().SetHandler(LvlFilterHandler(LvlInfo, StreamHandler(os.Stderr, TerminalFormat(true))))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Trace("a message", "v", i)
 	}
 }
