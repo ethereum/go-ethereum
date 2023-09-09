a89be29500a75a3e8f67d202b32416cba091d988
0xmuralik <krishna192reddy@gmail.com>
2023-09-08 18:10:07 -0700

    modify err msg

--- consensus/misc/gaslimit.go
+++ consensus/misc/gaslimit.go
@@ -36,7 +36,7 @@ func VerifyGaslimit(parentGasLimit, headerGasLimit uint64) error {
 		return fmt.Errorf("invalid gas limit: have %d, want %d +-= %d", headerGasLimit, parentGasLimit, limit-1)
 	}
 	if headerGasLimit < params.MinGasLimit {
-		return errors.New("invalid gas limit below 5000")
+		return errors.New(fmt.Sprint("invalid gas limit below ", params.MinGasLimit))
 	}
 	return nil
 }
