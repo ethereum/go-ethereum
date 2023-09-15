a9c15e644192ccc66455254f3fba628aa5d93006
phenix3443 <phenix3443@gmail.com>
2023-09-16 00:25:16 +0800

    fix: typo

--- params/config.go
+++ params/config.go
@@ -358,7 +358,7 @@ func (c *CliqueConfig) String() string {
 func (c *ChainConfig) Description() string {
 	var banner string
 
-	// Create some basinc network config output
+	// Create some basic network config output
 	network := NetworkNames[c.ChainID.String()]
 	if network == "" {
 		network = "unknown"
