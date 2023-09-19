efcabdfd4515f1ab9822b6c7a207951ce91f8f22
jsvisa <delweng@gmail.com>
2023-09-19 07:45:54 +0000

    cmd/geth/verkle: go imports
    Signed-off-by: jsvisa <delweng@gmail.com>

--- cmd/geth/verkle.go
+++ cmd/geth/verkle.go
@@ -115,7 +115,7 @@ func verifyVerkle(ctx *cli.Context) error {
 	defer stack.Close()
 
 	chaindb := utils.MakeChainDatabase(ctx, stack, true)
-    defer chaindb.Close()
+	defer chaindb.Close()
 	headBlock := rawdb.ReadHeadBlock(chaindb)
 	if headBlock == nil {
 		log.Error("Failed to load head block")
@@ -164,7 +164,7 @@ func expandVerkle(ctx *cli.Context) error {
 	defer stack.Close()
 
 	chaindb := utils.MakeChainDatabase(ctx, stack, true)
-    defer chaindb.Close()
+	defer chaindb.Close()
 	var (
 		rootC   common.Hash
 		keylist [][]byte
