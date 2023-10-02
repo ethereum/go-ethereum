144c419c6434d03125de04dc4f23633dc50b81d7
lightclient <lightclient@protonmail.com>
2023-10-02 07:26:18 -0600

    cmd/evm: fix rebase issues

--- cmd/evm/internal/t8ntool/execution.go
+++ cmd/evm/internal/t8ntool/execution.go
@@ -178,9 +178,6 @@ func (pre *Prestate) Apply(vmConfig vm.Config, chainConfig *params.ChainConfig,
 			vmContext.BlobBaseFee = eip4844.CalcBlobFee(excessBlobGas)
 		}
 	}
-	if vmContext.ExcessBlobGas != nil {
-		vmContext.BlobBaseFee = eip4844.CalcBlobFee(*vmContext.ExcessBlobGas)
-	}
 	// If DAO is supported/enabled, we need to handle it here. In geth 'proper', it's
 	// done in StateProcessor.Process(block, ...), right before transactions are applied.
 	if chainConfig.DAOForkSupport &&
