cec0b058115282168c5afc5197de3f6b5479dc4a
Ulaş Erdoğan <uerdogan2001@hotmail.com>
2023-10-07 16:08:05 +0300

    crypto/secp256r1: remove malleability check due to spec

--- crypto/secp256r1/verifier.go
+++ crypto/secp256r1/verifier.go
@@ -2,13 +2,7 @@ package secp256r1
 
 import (
 	"crypto/ecdsa"
-	"crypto/elliptic"
 	"math/big"
-)
-
-var (
-	// Half of the order of the subgroup in the elliptic curve
-	secp256k1halfN = new(big.Int).Div(elliptic.P256().Params().N, big.NewInt(2))
 )
 
 // Verifies the given signature (r, s) for the given hash and public key (x, y).
@@ -21,17 +15,7 @@ func Verify(hash []byte, r, s, x, y *big.Int) bool {
 		return false
 	}
 
-	// Check the malleability issue
-	if checkMalleability(s) {
-		return false
-	}
-
 	// Verify the signature with the public key,
 	// then return true if it's valid, false otherwise
 	return ecdsa.Verify(publicKey, hash, r, s)
-}
-
-// Check the malleability issue
-func checkMalleability(s *big.Int) bool {
-	return s.Cmp(secp256k1halfN) > 0
 }
