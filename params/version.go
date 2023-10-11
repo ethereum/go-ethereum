493a0134da13346618a43a7dafdccc1146562f3b
Liam <liam.icheng.lai@gmail.com>
2023-10-12 10:50:23 +1100

    set testnet v2 block (#330)
    * set testnet v2 block
    
    * set testnet v2 block

--- params/version.go
+++ params/version.go
@@ -23,8 +23,8 @@ import (
 const (
 	VersionMajor = 1        // Major version component of the current release
 	VersionMinor = 4        // Minor version component of the current release
-	VersionPatch = 8        // Patch version component of the current release
-	VersionMeta  = "stable" // Version metadata to append to the version string
+	VersionPatch = 9        // Patch version component of the current release
+	VersionMeta  = "beta"   // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
