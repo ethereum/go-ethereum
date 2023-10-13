4d2914cf1314357d6f602d3046f8c9c4f37b850e
Péter Szilágyi <peterke@gmail.com>
2023-10-13 22:50:20 +0300

    cmd, core, ethdb: enable Pebble on 32 bits and OpenBSD too (#28335)
    * cmd, core, ethdb: enable Pebble on 32 bits and OpenBSD too
    
    * ethdb/pebble: use Pebble's internal constant calculation

--- ethdb/pebble/pebble_test.go
+++ ethdb/pebble/pebble_test.go
@@ -14,8 +14,6 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-//go:build (arm64 || amd64) && !openbsd
-
 package pebble
 
 import (
