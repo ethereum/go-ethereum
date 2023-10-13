9a0a87f4caa9917ec2fb500fef789b336cb8f89e
Steven <asongala@163.com>
2023-10-13 14:45:26 +0800

    fix(libzkp): upgrade to use prover `v0.9.7` (#535)
    * Upgrade to use prover `v0.9.7`.
    
    * Update version.

--- params/version.go
+++ params/version.go
@@ -24,7 +24,7 @@ import (
 const (
 	VersionMajor = 5         // Major version component of the current release
 	VersionMinor = 0         // Minor version component of the current release
-	VersionPatch = 0         // Patch version component of the current release
+	VersionPatch = 1         // Patch version component of the current release
 	VersionMeta  = "mainnet" // Version metadata to append to the version string
 )
 
