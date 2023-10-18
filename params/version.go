e1898d5aea7f268fd8169214948c535f8c4bbe99
Manav Darji <manavdarji.india@gmail.com>
2023-10-18 12:18:50 +0530

    bump version to v1.1.0-beta2

--- params/version.go
+++ params/version.go
@@ -21,10 +21,10 @@ import (
 )
 
 const (
-	VersionMajor = 1      // Major version component of the current release
-	VersionMinor = 1      // Minor version component of the current release
-	VersionPatch = 0      // Patch version component of the current release
-	VersionMeta  = "beta" // Version metadata to append to the version string
+	VersionMajor = 1       // Major version component of the current release
+	VersionMinor = 1       // Minor version component of the current release
+	VersionPatch = 0       // Patch version component of the current release
+	VersionMeta  = "beta2" // Version metadata to append to the version string
 )
 
 var GitCommit string
