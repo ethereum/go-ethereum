0136aa5cccefe81a5fc73e0956dd571eca712f52
Jordan Oroshiba <jordan.oroshiba@gmail.com>
2023-10-24 09:16:52 -0700

    remove unused dependency

--- grpc/execution/server.go
+++ grpc/execution/server.go
@@ -11,7 +11,6 @@ import (
 	"github.com/ethereum/go-ethereum/beacon/engine"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/core/rawdb"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/eth"
 	"github.com/ethereum/go-ethereum/eth/catalyst"
