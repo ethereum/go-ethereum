332053779daeb591556070dc2abc456e3aabc2db
Alvaro <alvaro@chainalysis.com>
2023-10-26 22:13:19 +0200

    Fix linting

--- eth/tracers/native/call.go
+++ eth/tracers/native/call.go
@@ -37,12 +37,12 @@ func init() {
 }
 
 type callLog struct {
-	Address  common.Address `json:"address"`
-	Topics   []common.Hash  `json:"topics"`
-	Data     hexutil.Bytes  `json:"data"`
+	Address common.Address `json:"address"`
+	Topics  []common.Hash  `json:"topics"`
+	Data    hexutil.Bytes  `json:"data"`
 	// Position of the log relative to subcalls within the same trace
 	// See https://github.com/ethereum/go-ethereum/pull/28389 for details
-	Position int            `json:"position"`
+	Position int `json:"position"`
 }
 
 type callFrame struct {
