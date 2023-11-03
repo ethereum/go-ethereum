84b49d591ac8772acb42d7ac9ca0897ecc49f800
Yaroslav <hpeymark@localhost.localdomain>
2023-11-03 14:35:50 +0200

    First-Upload

---
+++ hardhat/hardhat.config.js
@@ -0,0 +1,11 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    localhost: {
+      url: "http://localhost:8545",
+    },
+  }
+};
