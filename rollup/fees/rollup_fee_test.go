76756cea1cfc78908d169ab80dd6885bb128cd28
HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>
2023-11-08 16:25:02 +0800

    gofmt rollup packages (#556)

--- rollup/fees/rollup_fee_test.go
+++ rollup/fees/rollup_fee_test.go
@@ -1,20 +1,20 @@
 package fees
 
 import (
-    "math/big"
-    "testing"
+	"math/big"
+	"testing"
 
-    "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCalculateEncodedL1DataFee(t *testing.T) {
-    l1BaseFee := new(big.Int).SetUint64(15000000)
+	l1BaseFee := new(big.Int).SetUint64(15000000)
 
-    data := []byte{0, 10, 1, 0}
-    overhead := new(big.Int).SetUint64(100)
-    scalar := new(big.Int).SetUint64(10)
+	data := []byte{0, 10, 1, 0}
+	overhead := new(big.Int).SetUint64(100)
+	scalar := new(big.Int).SetUint64(10)
 
-    expected := new(big.Int).SetUint64(184) // 184.2
-    actual := calculateEncodedL1DataFee(data, overhead, l1BaseFee, scalar)
-    assert.Equal(t, expected, actual)
+	expected := new(big.Int).SetUint64(184) // 184.2
+	actual := calculateEncodedL1DataFee(data, overhead, l1BaseFee, scalar)
+	assert.Equal(t, expected, actual)
 }
--- rollup/rcfg/config.go
+++ rollup/rcfg/config.go
@@ -1,32 +1,32 @@
 package rcfg
 
 import (
-    "math/big"
+	"math/big"
 
-    "github.com/scroll-tech/go-ethereum/common"
+	"github.com/scroll-tech/go-ethereum/common"
 )
 
 // TODO:
 // verify in consensus layer when decentralizing sequencer
 
 var (
-    // L2MessageQueueAddress is the address of the L2MessageQueue
-    // predeploy
-    // see contracts/src/L2/predeploys/L2MessageQueue.sol
-    L2MessageQueueAddress = common.HexToAddress("0x5300000000000000000000000000000000000000")
-    WithdrawTrieRootSlot  = common.BigToHash(big.NewInt(0))
+	// L2MessageQueueAddress is the address of the L2MessageQueue
+	// predeploy
+	// see contracts/src/L2/predeploys/L2MessageQueue.sol
+	L2MessageQueueAddress = common.HexToAddress("0x5300000000000000000000000000000000000000")
+	WithdrawTrieRootSlot  = common.BigToHash(big.NewInt(0))
 
-    // ScrollFeeVaultAddress is the address of the L2TxFeeVault
-    // predeploy
-    // see scroll-tech/scroll/contracts/src/L2/predeploys/L2TxFeeVault.sol
-    ScrollFeeVaultAddress = common.HexToAddress("0x5300000000000000000000000000000000000005")
+	// ScrollFeeVaultAddress is the address of the L2TxFeeVault
+	// predeploy
+	// see scroll-tech/scroll/contracts/src/L2/predeploys/L2TxFeeVault.sol
+	ScrollFeeVaultAddress = common.HexToAddress("0x5300000000000000000000000000000000000005")
 
-    // L1GasPriceOracleAddress is the address of the L1GasPriceOracle
-    // predeploy
-    // see scroll-tech/scroll/contracts/src/L2/predeploys/L1GasPriceOracle.sol
-    L1GasPriceOracleAddress = common.HexToAddress("0x5300000000000000000000000000000000000002")
-    Precision               = new(big.Int).SetUint64(1e9)
-    L1BaseFeeSlot           = common.BigToHash(big.NewInt(1))
-    OverheadSlot            = common.BigToHash(big.NewInt(2))
-    ScalarSlot              = common.BigToHash(big.NewInt(3))
+	// L1GasPriceOracleAddress is the address of the L1GasPriceOracle
+	// predeploy
+	// see scroll-tech/scroll/contracts/src/L2/predeploys/L1GasPriceOracle.sol
+	L1GasPriceOracleAddress = common.HexToAddress("0x5300000000000000000000000000000000000002")
+	Precision               = new(big.Int).SetUint64(1e9)
+	L1BaseFeeSlot           = common.BigToHash(big.NewInt(1))
+	OverheadSlot            = common.BigToHash(big.NewInt(2))
+	ScalarSlot              = common.BigToHash(big.NewInt(3))
 )
