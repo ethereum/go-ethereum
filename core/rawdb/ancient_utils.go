7072af726bdd0003876b6e77ec298fbe0517872f
Gary Rong <garyrong0905@gmail.com>
2023-11-08 09:45:38 +0800

    core/rawdb: fsync the index and data file after each freezer write

--- core/rawdb/ancient_utils.go
+++ core/rawdb/ancient_utils.go
@@ -18,6 +18,7 @@ package rawdb
 
 import (
 	"fmt"
+	"path/filepath"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethdb"
@@ -126,6 +127,8 @@ func InspectFreezerTable(ancient string, freezerName string, tableName string, s
 	switch freezerName {
 	case chainFreezerName:
 		path, tables = resolveChainFreezerDir(ancient), chainFreezerNoSnappy
+	case stateFreezerName:
+		path, tables = filepath.Join(ancient, freezerName), stateFreezerNoSnappy
 	default:
 		return fmt.Errorf("unknown freezer, supported ones: %v", freezers)
 	}
