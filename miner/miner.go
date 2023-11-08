f27d744d91575bf3fc88e7d61419db4a0776633c
kaliubuntu0206 <kaliubuntu0206@proton.me>
2023-11-08 20:06:42 +0900

    miner: fixed default gasPriceOracle min price for private networks

--- miner/miner.go
+++ miner/miner.go
@@ -58,7 +58,7 @@ type Config struct {
 // DefaultConfig contains default settings for miner.
 var DefaultConfig = Config{
 	GasCeil:  30000000,
-	GasPrice: big.NewInt(params.GWei),
+	GasPrice: big.NewInt(2 * params.Wei),
 
 	// The default recommit time is chosen as two seconds since
 	// consensus-layer usually will wait a half slot of time(6s)
