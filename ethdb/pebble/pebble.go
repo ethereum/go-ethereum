f7dde2a96c6a826a7a652589516ed22bb7f1e937
Jim McDonald <Jim@mcdee.net>
2023-11-09 14:15:22 +0000

    ethdb/pebble: add `Errorf` function to panicLogger (#28491)
    cockroachdb/pebble@422dce9 added Errorf to the Logger interface, this change makes it possible to compile geth with that version of pebble by adding the corresponding method to panicLogger.

--- ethdb/pebble/pebble.go
+++ ethdb/pebble/pebble.go
@@ -127,6 +127,9 @@ type panicLogger struct{}
 func (l panicLogger) Infof(format string, args ...interface{}) {
 }
 
+func (l panicLogger) Errorf(format string, args ...interface{}) {
+}
+
 func (l panicLogger) Fatalf(format string, args ...interface{}) {
 	panic(errors.Errorf("fatal: "+format, args...))
 }
