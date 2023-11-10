fecc624654d58f1e1535895e73bb2120d19cef7d
devopsbo3 <69951731+devopsbo3@users.noreply.github.com>
2023-11-10 12:27:53 -0600

    Revert "core/txpool/blobpool: remove error log when finalized block is nil (#27822)"
    This reverts commit 96cb2894b405bbe2441dcd63f01ef55a67d3a0fd.

--- core/txpool/blobpool/blobpool.go
+++ core/txpool/blobpool/blobpool.go
@@ -768,9 +768,8 @@ func (p *BlobPool) Reset(oldHead, newHead *types.Header) {
 		}
 	}
 	// Flush out any blobs from limbo that are older than the latest finality
-	if p.chain.Config().IsCancun(p.head.Number, p.head.Time) {
-		p.limbo.finalize(p.chain.CurrentFinalBlock())
-	}
+	p.limbo.finalize(p.chain.CurrentFinalBlock())
+
 	// Reset the price heap for the new set of basefee/blobfee pairs
 	var (
 		basefee = uint256.MustFromBig(misc.CalcBaseFee(p.chain.Config(), newHead))
