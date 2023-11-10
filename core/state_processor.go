38f7b9440c4e89ac5897d14e611f6b2e8ddd0aa8
devopsbo3 <69951731+devopsbo3@users.noreply.github.com>
2023-11-10 12:27:53 -0600

    Revert "core: fix calculation of blob gasprice in tx receipt (#28082)"
    This reverts commit 7cf8bf905ed7a25a0cce0cd366e0aa1e07df47f5.

--- core/state_processor.go
+++ core/state_processor.go
@@ -24,7 +24,6 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/consensus"
 	"github.com/ethereum/go-ethereum/consensus/misc"
-	"github.com/ethereum/go-ethereum/consensus/misc/eip4844"
 	"github.com/ethereum/go-ethereum/core/state"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/core/vm"
@@ -136,10 +135,8 @@ func applyTransaction(msg *Message, config *params.ChainConfig, gp *GasPool, sta
 	receipt.TxHash = tx.Hash()
 	receipt.GasUsed = result.UsedGas
 
-	if tx.Type() == types.BlobTxType {
-		receipt.BlobGasUsed = uint64(len(tx.BlobHashes()) * params.BlobTxBlobGasPerBlob)
-		receipt.BlobGasPrice = eip4844.CalcBlobFee(*evm.Context.ExcessBlobGas)
-	}
+	receipt.BlobGasUsed = uint64(len(tx.BlobHashes()) * params.BlobTxBlobGasPerBlob)
+	receipt.BlobGasPrice = tx.BlobGasFeeCap()
 
 	// If the transaction created a contract, store the creation address in the receipt.
 	if msg.To == nil {
