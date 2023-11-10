3531f18b6ac5186e5ca16c9ea2052ebbff74d256
devopsbo3 <69951731+devopsbo3@users.noreply.github.com>
2023-11-10 12:27:53 -0600

    Revert "core/state: fix missing import (#28010)"
    This reverts commit 1e72eeaa3de4f227b3abce918afc256bb8b32c58.

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -32,7 +32,6 @@ import (
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/metrics"
 	"github.com/ethereum/go-ethereum/params"
-	"github.com/ethereum/go-ethereum/trie"
 	"github.com/ethereum/go-ethereum/trie/trienode"
 	"github.com/ethereum/go-ethereum/trie/triestate"
 )
