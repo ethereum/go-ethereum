8ee11d565781debb560540ec732a68585ce80d55
devopsbo3 <69951731+devopsbo3@users.noreply.github.com>
2023-11-10 12:27:53 -0600

    Revert "eth/catalyst: set finalized block hash properly in dev mode (#27886)"
    This reverts commit 9070fc89a90bcfe49a513fa9ffec32e79e108135.

--- eth/catalyst/simulated_beacon.go
+++ eth/catalyst/simulated_beacon.go
@@ -32,8 +32,6 @@ import (
 	"github.com/ethereum/go-ethereum/rpc"
 )
 
-const devEpochLength = 32
-
 // withdrawalQueue implements a FIFO queue which holds withdrawals that are
 // pending inclusion.
 type withdrawalQueue struct {
@@ -159,13 +157,6 @@ func (c *SimulatedBeacon) sealBlock(withdrawals []*types.Withdrawal) error {
 	}
 	payload := envelope.ExecutionPayload
 
-	var finalizedHash common.Hash
-	if payload.Number%devEpochLength == 0 {
-		finalizedHash = payload.BlockHash
-	} else {
-		finalizedHash = c.eth.BlockChain().GetBlockByNumber((payload.Number - 1) / devEpochLength * devEpochLength).Hash()
-	}
-
 	// mark the payload as canon
 	if _, err = c.engineAPI.NewPayloadV2(*payload); err != nil {
 		return fmt.Errorf("failed to mark payload as canonical: %v", err)
@@ -173,7 +164,7 @@ func (c *SimulatedBeacon) sealBlock(withdrawals []*types.Withdrawal) error {
 	c.curForkchoiceState = engine.ForkchoiceStateV1{
 		HeadBlockHash:      payload.BlockHash,
 		SafeBlockHash:      payload.BlockHash,
-		FinalizedBlockHash: finalizedHash,
+		FinalizedBlockHash: payload.BlockHash,
 	}
 	// mark the block containing the payload as canonical
 	if _, err = c.engineAPI.ForkchoiceUpdatedV2(c.curForkchoiceState, nil); err != nil {
