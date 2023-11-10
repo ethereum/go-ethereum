2493653e39ff10e71735dc8c3e3f8f725ea5fc1f
devopsbo3 <69951731+devopsbo3@users.noreply.github.com>
2023-11-10 12:27:53 -0600

    Revert "cmd/geth: atomic types used (#27756)"
    This reverts commit a5eb124660500cc41c6baa96184e4e3003c15dc2.

--- cmd/geth/attach_test.go
+++ cmd/geth/attach_test.go
@@ -61,7 +61,7 @@ func TestRemoteDbWithHeaders(t *testing.T) {
 }
 
 func testReceiveHeaders(t *testing.T, ln net.Listener, gethArgs ...string) {
-	var ok atomic.Uint32
+	var ok uint32
 	server := &http.Server{
 		Addr: "localhost:0",
 		Handler: &testHandler{func(w http.ResponseWriter, r *http.Request) {
@@ -72,12 +72,12 @@ func testReceiveHeaders(t *testing.T, ln net.Listener, gethArgs ...string) {
 			if have, want := r.Header.Get("second"), "two"; have != want {
 				t.Fatalf("missing header, have %v want %v", have, want)
 			}
-			ok.Store(1)
+			atomic.StoreUint32(&ok, 1)
 		}}}
 	go server.Serve(ln)
 	defer server.Close()
 	runGeth(t, gethArgs...).WaitExit()
-	if ok.Load() != 1 {
+	if atomic.LoadUint32(&ok) != 1 {
 		t.Fatal("Test fail, expected invocation to succeed")
 	}
 }
