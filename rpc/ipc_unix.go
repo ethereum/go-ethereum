3e40df21568aa14dedecb9c4cd442a4e2c89ac1e
devopsbo3 <69951731+devopsbo3@users.noreply.github.com>
2023-11-10 12:27:53 -0600

    Revert "rpc: avoid use of cgo by hard-coding maxPathSize (#27447)"
    This reverts commit 63eb2e901c571ef19ee3859028bf7b818cdd0268.

--- rpc/ipc_unix.go
+++ rpc/ipc_unix.go
@@ -29,17 +29,11 @@ import (
 	"github.com/ethereum/go-ethereum/log"
 )
 
-const (
-	// On Linux, sun_path is 108 bytes in size
-	// see http://man7.org/linux/man-pages/man7/unix.7.html
-	maxPathSize = int(108)
-)
-
 // ipcListen will create a Unix socket on the given endpoint.
 func ipcListen(endpoint string) (net.Listener, error) {
 	// account for null-terminator too
-	if len(endpoint)+1 > maxPathSize {
-		log.Warn(fmt.Sprintf("The ipc endpoint is longer than %d characters. ", maxPathSize-1),
+	if len(endpoint)+1 > int(max_path_size) {
+		log.Warn(fmt.Sprintf("The ipc endpoint is longer than %d characters. ", max_path_size-1),
 			"endpoint", endpoint)
 	}
 
