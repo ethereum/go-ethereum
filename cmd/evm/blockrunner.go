19a55695100728764c9ae6200c818f6eda0b5dc3
devopsbo3 <69951731+devopsbo3@users.noreply.github.com>
2023-11-10 12:27:53 -0600

    Revert "cmd/evm: add --run option to blocktest command (#28421)"
    This reverts commit 36d01e79d56cff1f0afdae5a9b5511290b15f1fc.

--- cmd/evm/blockrunner.go
+++ cmd/evm/blockrunner.go
@@ -21,7 +21,6 @@ import (
 	"errors"
 	"fmt"
 	"os"
-	"regexp"
 	"sort"
 
 	"github.com/ethereum/go-ethereum/core/rawdb"
@@ -31,18 +30,11 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-var RunFlag = &cli.StringFlag{
-	Name:  "run",
-	Value: ".*",
-	Usage: "Run only those tests matching the regular expression.",
-}
-
 var blockTestCommand = &cli.Command{
 	Action:    blockTestCmd,
 	Name:      "blocktest",
 	Usage:     "executes the given blockchain tests",
 	ArgsUsage: "<file>",
-	Flags:     []cli.Flag{RunFlag},
 }
 
 func blockTestCmd(ctx *cli.Context) error {
@@ -69,21 +61,13 @@ func blockTestCmd(ctx *cli.Context) error {
 	if err = json.Unmarshal(src, &tests); err != nil {
 		return err
 	}
-	re, err := regexp.Compile(ctx.String(RunFlag.Name))
-	if err != nil {
-		return fmt.Errorf("invalid regex -%s: %v", RunFlag.Name, err)
-	}
-
-	// Run them in order
+	// run them in order
 	var keys []string
 	for key := range tests {
 		keys = append(keys, key)
 	}
 	sort.Strings(keys)
 	for _, name := range keys {
-		if !re.MatchString(name) {
-			continue
-		}
 		test := tests[name]
 		if err := test.Run(false, rawdb.HashScheme, tracer); err != nil {
 			return fmt.Errorf("test %v: %w", name, err)
