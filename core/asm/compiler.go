3b50f2bf7d139a9505e0bbcc12192c7f21cf2863
devopsbo3 <69951731+devopsbo3@users.noreply.github.com>
2023-11-10 12:27:53 -0600

    Revert "core/asm: remove unused return value (#27272)"
    This reverts commit a0c2037a671972c65ae083e7db7b9c4968b2744e.

--- core/asm/compiler.go
+++ core/asm/compiler.go
@@ -157,12 +157,13 @@ func (c *Compiler) compileLine() error {
 }
 
 // compileNumber compiles the number to bytes
-func (c *Compiler) compileNumber(element token) {
+func (c *Compiler) compileNumber(element token) (int, error) {
 	num := math.MustParseBig256(element.text).Bytes()
 	if len(num) == 0 {
 		num = []byte{0}
 	}
 	c.pushBin(num)
+	return len(num), nil
 }
 
 // compileElement compiles the element (push & label or both)
