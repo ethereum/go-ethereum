14489ba805050690a6767e72c3c6ef7a0ac2c11f
devopsbo3 <69951731+devopsbo3@users.noreply.github.com>
2023-11-10 12:27:53 -0600

    Revert "eth, miner: rm redundant type declare (#27713)"
    This reverts commit 0c6fb9db3591681cf576bc63a1eebc7693bd8a6f.

--- miner/payload_building_test.go
+++ miner/payload_building_test.go
@@ -84,48 +84,48 @@ func TestBuildPayload(t *testing.T) {
 func TestPayloadId(t *testing.T) {
 	ids := make(map[string]int)
 	for i, tt := range []*BuildPayloadArgs{
-		{
+		&BuildPayloadArgs{
 			Parent:       common.Hash{1},
 			Timestamp:    1,
 			Random:       common.Hash{0x1},
 			FeeRecipient: common.Address{0x1},
 		},
 		// Different parent
-		{
+		&BuildPayloadArgs{
 			Parent:       common.Hash{2},
 			Timestamp:    1,
 			Random:       common.Hash{0x1},
 			FeeRecipient: common.Address{0x1},
 		},
 		// Different timestamp
-		{
+		&BuildPayloadArgs{
 			Parent:       common.Hash{2},
 			Timestamp:    2,
 			Random:       common.Hash{0x1},
 			FeeRecipient: common.Address{0x1},
 		},
 		// Different Random
-		{
+		&BuildPayloadArgs{
 			Parent:       common.Hash{2},
 			Timestamp:    2,
 			Random:       common.Hash{0x2},
 			FeeRecipient: common.Address{0x1},
 		},
 		// Different fee-recipient
-		{
+		&BuildPayloadArgs{
 			Parent:       common.Hash{2},
 			Timestamp:    2,
 			Random:       common.Hash{0x2},
 			FeeRecipient: common.Address{0x2},
 		},
 		// Different withdrawals (non-empty)
-		{
+		&BuildPayloadArgs{
 			Parent:       common.Hash{2},
 			Timestamp:    2,
 			Random:       common.Hash{0x2},
 			FeeRecipient: common.Address{0x2},
 			Withdrawals: []*types.Withdrawal{
-				{
+				&types.Withdrawal{
 					Index:     0,
 					Validator: 0,
 					Address:   common.Address{},
@@ -134,13 +134,13 @@ func TestPayloadId(t *testing.T) {
 			},
 		},
 		// Different withdrawals (non-empty)
-		{
+		&BuildPayloadArgs{
 			Parent:       common.Hash{2},
 			Timestamp:    2,
 			Random:       common.Hash{0x2},
 			FeeRecipient: common.Address{0x2},
 			Withdrawals: []*types.Withdrawal{
-				{
+				&types.Withdrawal{
 					Index:     2,
 					Validator: 0,
 					Address:   common.Address{},
