016aaa607a71ddd64bb060f5e928111d65e7dbf3
devopsbo3 <69951731+devopsbo3@users.noreply.github.com>
2023-11-10 12:27:53 -0600

    Revert "core, params, beacon/engine: implement EIP 4788 BeaconRoot  (#27849)"
    This reverts commit 2e9477f46b099eec826aface1812843803d48231.

--- core/types/gen_header_rlp.go
+++ core/types/gen_header_rlp.go
@@ -44,8 +44,7 @@ func (obj *Header) EncodeRLP(_w io.Writer) error {
 	_tmp2 := obj.WithdrawalsHash != nil
 	_tmp3 := obj.BlobGasUsed != nil
 	_tmp4 := obj.ExcessBlobGas != nil
-	_tmp5 := obj.BeaconRoot != nil
-	if _tmp1 || _tmp2 || _tmp3 || _tmp4 || _tmp5 {
+	if _tmp1 || _tmp2 || _tmp3 || _tmp4 {
 		if obj.BaseFee == nil {
 			w.Write(rlp.EmptyString)
 		} else {
@@ -55,32 +54,25 @@ func (obj *Header) EncodeRLP(_w io.Writer) error {
 			w.WriteBigInt(obj.BaseFee)
 		}
 	}
-	if _tmp2 || _tmp3 || _tmp4 || _tmp5 {
+	if _tmp2 || _tmp3 || _tmp4 {
 		if obj.WithdrawalsHash == nil {
 			w.Write([]byte{0x80})
 		} else {
 			w.WriteBytes(obj.WithdrawalsHash[:])
 		}
 	}
-	if _tmp3 || _tmp4 || _tmp5 {
+	if _tmp3 || _tmp4 {
 		if obj.BlobGasUsed == nil {
 			w.Write([]byte{0x80})
 		} else {
 			w.WriteUint64((*obj.BlobGasUsed))
 		}
 	}
-	if _tmp4 || _tmp5 {
+	if _tmp4 {
 		if obj.ExcessBlobGas == nil {
 			w.Write([]byte{0x80})
 		} else {
 			w.WriteUint64((*obj.ExcessBlobGas))
-		}
-	}
-	if _tmp5 {
-		if obj.BeaconRoot == nil {
-			w.Write([]byte{0x80})
-		} else {
-			w.WriteBytes(obj.BeaconRoot[:])
 		}
 	}
 	w.ListEnd(_tmp0)
