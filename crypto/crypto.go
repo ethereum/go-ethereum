536b93a923779e1e70770707ea95cc5568ebeed6
haotian <haotian@haotiandeMacBook-Air.local>
2023-11-15 17:23:30 +0800

    crypto: change the comment variable order

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -109,8 +109,8 @@ func CreateAddress(b common.Address, nonce uint64) common.Address {
 	return common.BytesToAddress(Keccak256(data)[12:])
 }
 
-// CreateAddress2 creates an ethereum address given the address bytes, initial
-// contract code hash and a salt.
+// CreateAddress2 creates an ethereum address given the address bytes, a salt
+// and contract code hash.
 func CreateAddress2(b common.Address, salt [32]byte, inithash []byte) common.Address {
 	return common.BytesToAddress(Keccak256([]byte{0xff}, b.Bytes(), salt[:], inithash)[12:])
 }
