cd8c4c589ff8bc8985286385175a2e84526edcd6
Sina Mahmoodi <itz.s1na@gmail.com>
2023-11-24 18:39:09 +0330

    force load live tracer set

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -43,6 +43,7 @@ import (
 
 	// Force-load the tracer engines to trigger registration
 	_ "github.com/ethereum/go-ethereum/eth/tracers/js"
+	_ "github.com/ethereum/go-ethereum/eth/tracers/live"
 	_ "github.com/ethereum/go-ethereum/eth/tracers/native"
 
 	"github.com/urfave/cli/v2"
