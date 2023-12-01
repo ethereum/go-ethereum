bc9fae23b572ec159c086a5aa1ab78c01caae941
Liam Lai <liam.lai@datameshgroup.com.au>
2023-12-01 22:20:02 +1100

    bump version to 2 since we start use consensus version 2

--- params/version.go
+++ params/version.go
@@ -21,10 +21,10 @@ import (
 )
 
 const (
-	VersionMajor = 1       // Major version component of the current release
-	VersionMinor = 4       // Minor version component of the current release
-	VersionPatch = 10      // Patch version component of the current release
-	VersionMeta  = "beta2" // Version metadata to append to the version string
+	VersionMajor = 2       // Major version component of the current release
+	VersionMinor = 0       // Minor version component of the current release
+	VersionPatch = 0       // Patch version component of the current release
+	VersionMeta  = "beta1" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
