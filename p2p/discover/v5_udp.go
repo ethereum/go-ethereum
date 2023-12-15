74862941e52294f82d5553e4dd8ee2e8a55117dc
Felix Lange <fjl@twurst.com>
2023-12-15 17:04:53 +0100

    p2p/discover: add liveness check in collectTableNodes

--- p2p/discover/v5_udp.go
+++ p2p/discover/v5_udp.go
@@ -852,6 +852,7 @@ func (t *UDPv5) handleFindnode(p *v5wire.Findnode, fromID enode.ID, fromAddr *ne
 // collectTableNodes creates a FINDNODE result set for the given distances.
 func (t *UDPv5) collectTableNodes(rip net.IP, distances []uint, limit int) []*enode.Node {
 	var nodes []*enode.Node
+	var bn []*enode.Node
 	var processed = make(map[uint]struct{})
 	for _, dist := range distances {
 		// Reject duplicate / invalid distances.
@@ -860,20 +861,10 @@ func (t *UDPv5) collectTableNodes(rip net.IP, distances []uint, limit int) []*en
 			continue
 		}
 
-		// Get the nodes.
-		var bn []*enode.Node
-		if dist == 0 {
-			bn = []*enode.Node{t.Self()}
-		} else if dist <= 256 {
-			t.tab.mutex.Lock()
-			bn = unwrapNodes(t.tab.bucketAtDistance(int(dist)).entries)
-			t.tab.mutex.Unlock()
-		}
-		processed[dist] = struct{}{}
-
-		// Apply some pre-checks to avoid sending invalid nodes.
+		bn = t.tab.appendBucketNodes(dist, limit, bn[:0])
 		for _, n := range bn {
-			// TODO livenessChecks > 1
+			// Apply some pre-checks to avoid sending invalid nodes.
+			// Note liveness is checked by appendBucketNodes.
 			if netutil.CheckRelayIP(rip, n.IP()) != nil {
 				continue
 			}
