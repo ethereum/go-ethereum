135d31202d1357f612352034e35612e266429866
Shawn <44221603+shaspitz@users.noreply.github.com>
2023-12-20 22:57:01 -0800

    hyperlane v3.5 cli works

--- geth-poa/hyperlane-deployer/entrypoint.sh
+++ geth-poa/hyperlane-deployer/entrypoint.sh
@@ -7,7 +7,7 @@ sleep 10
 if test -f /hyperlane-monorepo/artifacts/done; then
   echo "Deploy artifacts already exist. Skipping deployment."
 else
-
+  echo "Deploying core contracts"
   # Define the expect script inline
   /usr/bin/expect <<EOF
   set timeout -1
@@ -42,13 +42,24 @@ fi
 if test -f artifacts/done-warp-route; then
   echo "Warp route already deployed. Skipping."
 else
-  echo "Deploying warp route."
-  hyperlane deploy warp \
+  echo "Deploying warp route"
+  # Define the expect script inline
+  /usr/bin/expect <<EOF
+  set timeout -1
+  spawn hyperlane deploy warp \
     --yes \
     --key $CONTRACT_DEPLOYER_PRIVATE_KEY \
     --chains /chain-config.yml \
     --config /warp-tokens.yml \
     --core /hyperlane-monorepo/artifacts/core-deployment.json
+  expect {
+    "*low balance on*" { 
+      send -- "Y\r"
+      exp_continue
+    }
+    eof
+  }
+EOF
 
   # Standardize artifact names
   for file in /hyperlane-monorepo/artifacts/warp-deployment-*.json; do
@@ -57,7 +68,8 @@ else
   for file in /hyperlane-monorepo/artifacts/warp-ui-token-config-*.json; do
     mv "$file" "/hyperlane-monorepo/artifacts/warp-ui-token-config.json"
   done
-  
+
+  # Signal done 
   touch artifacts/done-warp-route
 fi
 
