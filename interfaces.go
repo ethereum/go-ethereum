43e2a615cd6801897b9d7c6070a8e2cd9cc6ae72
Isabel Schöps Thiel <155141998+IST-Github@users.noreply.github.com>
2024-01-01 00:52:32 +0100

    Delete interfaces.go

--- interfaces.go
+++
@@ -1,4 +0,0 @@
-June 2007 Copyright (C) Happy NEW YEAR 2024 by Isabel Schoeps Thiel (standalone with my Dog Don)
-
-// The go-ethereum library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty 
