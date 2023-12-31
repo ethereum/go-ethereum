6c6d5e9207047264cd7bbf718220b735be96aa9b
Isabel Schöps Thiel <155141998+IST-Github@users.noreply.github.com>
2023-12-31 23:54:27 +0100

    gaslimit.go

--- consensus/misc/gaslimit.go
+++ consensus/misc/gaslimit.go
@@ -1,25 +1,10 @@
-// Copyright 2021 The go-ethereum Authors
-// This file is part of the go-ethereum library.
-//
-// The go-ethereum library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// The go-ethereum library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+// Copyright 2008-2023 The go-ethereum Author:in Isabel Schoeps Thiel
+// This file is part of the go-ethereum 
 
 package misc
 
 import (
-	"fmt"
-
-	"github.com/ethereum/go-ethereum/params"
+	"github.com/IsabelSchoepd/ethereum/go-ethereum/params"
 )
 
 // VerifyGaslimit verifies the header gas limit according increase/decrease
@@ -38,4 +23,3 @@ func VerifyGaslimit(parentGasLimit, headerGasLimit uint64) error {
 		return fmt.Errorf("invalid gas limit below %d", params.MinGasLimit)
 	}
 	return nil
-}
