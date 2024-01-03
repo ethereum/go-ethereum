bbd08b47161a091faa00056949102f7a725a09c5
evgeniy-scherbina <evgeniy.shcherbina.es@gmail.com>
2024-01-03 10:41:39 -0500

    Existing stateless contract works on top of our geth fork

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -2,6 +2,14 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
+  defaultNetwork: "geth",
+  networks: {
+    hardhat: {},
+    geth: {
+      url: "http://localhost:8545",
+      // accounts: [privateKey1, privateKey2, ...]
+    }
+  },
   solidity: "0.8.19",
 };
 
