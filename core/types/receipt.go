14d8266fc2c2ab6369fc8fab1b513e1f55e9be84
HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>
2024-01-15 20:49:35 +0800

    fix: fix `(r *Receipt) decodeTyped` (#617)

--- core/types/receipt.go
+++ core/types/receipt.go
@@ -209,7 +209,7 @@ func (r *Receipt) decodeTyped(b []byte) error {
 		return errShortTypedReceipt
 	}
 	switch b[0] {
-	case DynamicFeeTxType, AccessListTxType, BlobTxType:
+	case DynamicFeeTxType, AccessListTxType, BlobTxType, L1MessageTxType:
 		var data receiptRLP
 		err := rlp.DecodeBytes(b[1:], &data)
 		if err != nil {
