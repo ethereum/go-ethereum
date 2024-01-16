6a2d94e21548f92552a3e75663ff0e3321f669a5
Shivam Sharma <shivam691999@gmail.com>
2024-01-16 16:40:47 +0530

    fix : integration tests

--- eth/ethconfig/config.go
+++ eth/ethconfig/config.go
@@ -67,7 +67,7 @@ var LightClientGPO = gasprice.Config{
 
 // Defaults contains default settings for use on the Ethereum main net.
 var Defaults = Config{
-	SyncMode:           downloader.SnapSync,
+	SyncMode:           downloader.FullSync,
 	NetworkId:          0, // enable auto configuration of networkID == chainID
 	TxLookupLimit:      2350000,
 	TransactionHistory: 2350000,
