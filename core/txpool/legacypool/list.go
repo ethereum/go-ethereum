84bc4d457fdedc809036e4440041205addd7da2e
Martin Holst Swende <martin@swende.se>
2024-02-15 14:09:48 +0100

    core/txpool/legacypool: goimports fix

--- core/txpool/legacypool/list.go
+++ core/txpool/legacypool/list.go
@@ -27,7 +27,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
- 	"github.com/holiman/uint256"
+	"github.com/holiman/uint256"
 	"golang.org/x/exp/slices"
 )
 
