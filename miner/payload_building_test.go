423019c5f0ca71f004f226805fab5efa398d0b42
yzb@example.cn <yzb@example.cn>
2024-03-05 19:38:37 +0800

    test:delete needless err check

--- miner/payload_building_test.go
+++ miner/payload_building_test.go
@@ -35,7 +35,7 @@ func TestBuildPayload(t *testing.T) {
 		db        = rawdb.NewMemoryDatabase()
 		recipient = common.HexToAddress("0xdeadbeef")
 	)
-	w, b := newTestWorker(t, params.TestChainConfig, ethash.NewFaker(), db)
+	w, b := newTestWorker(t, params.TestChainConfig, ethash.NewFaker(), db, 0)
 	defer w.close()
 
 	timestamp := uint64(time.Now().Unix())
