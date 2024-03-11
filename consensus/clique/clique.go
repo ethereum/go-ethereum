0d543684faa49be367bf5d506d429f5c07d17347
Kartik Chopra <k5chopra@edu.uwaterloo.ca>
2024-03-11 12:31:47 -0400

    Reduce wiggle to 100ms.

--- consensus/clique/clique.go
+++ consensus/clique/clique.go
@@ -53,7 +53,7 @@ const (
 	inmemorySnapshots  = 128  // Number of recent vote snapshots to keep in memory
 	inmemorySignatures = 4096 // Number of recent block signatures to keep in memory
 
-	wiggleTime = 500 * time.Millisecond // Random delay (per signer) to allow concurrent signers
+	wiggleTime = 100 * time.Millisecond // Random delay (per signer) to allow concurrent signers
 )
 
 // Clique proof-of-authority protocol constants.
