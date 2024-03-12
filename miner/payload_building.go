6babd0ba102140cb6f38511148f25d1bd49aac6c
rjl493456442 <garyrong0905@gmail.com>
2024-03-12 19:51:46 +0800

    Update payload_building.go

--- miner/payload_building.go
+++ miner/payload_building.go
@@ -230,7 +230,7 @@ func (miner *Miner) buildPayload(args *BuildPayloadArgs) (*Payload, error) {
 				if r.err == nil {
 					payload.update(r, time.Since(start))
 				} else {
-					log.Info("Error while generating work", "id", payload.id, "error", r.err)
+					log.Info("Error while generating work", "id", payload.id, "err", r.err)
 				}
 				timer.Reset(miner.config.Recommit)
 			case <-payload.stop:
