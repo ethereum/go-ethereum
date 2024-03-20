6f929a0762be92130588779a8535ed0e3fc58d87
zgfzgf <48779939+zgfzgf@users.noreply.github.com>
2024-03-20 15:46:50 +0800

    core/asm: minor code-clarification (#29293)

--- core/asm/asm.go
+++ core/asm/asm.go
@@ -66,7 +66,7 @@ func (it *instructionIterator) Next() bool {
 
 	it.op = vm.OpCode(it.code[it.pc])
 	if it.op.IsPush() {
-		a := uint64(it.op) - uint64(vm.PUSH1) + 1
+		a := uint64(it.op) - uint64(vm.PUSH0)
 		u := it.pc + 1 + a
 		if uint64(len(it.code)) <= it.pc || uint64(len(it.code)) < u {
 			it.error = fmt.Errorf("incomplete push instruction at %v", it.pc)
