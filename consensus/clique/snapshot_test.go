021dcb8f1ef1e489ab4bc64394f08f0c9cdfe453
undefinedor <wanghao@imwh.net>
2024-03-23 21:55:50 +0800

    fix unreachable code

--- consensus/clique/snapshot_test.go
+++ consensus/clique/snapshot_test.go
@@ -467,7 +467,6 @@ func (tt *cliqueTest) run(t *testing.T) {
 	for j := 0; j < len(batches)-1; j++ {
 		if k, err := chain.InsertChain(batches[j]); err != nil {
 			t.Fatalf("failed to import batch %d, block %d: %v", j, k, err)
-			break
 		}
 	}
 	if _, err = chain.InsertChain(batches[len(batches)-1]); err != tt.failure {
