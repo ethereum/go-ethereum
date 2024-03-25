51ec9995cc046a3b30ce31a9c48bb40bcecf692d
Matthieu Vachon <matt@streamingfast.io>
2024-03-25 19:33:59 -0400

    Removes some leftover `err` check
    Before, `ToMessage` was returning both the resulting `Message` and an error while no error is returned now.
    
    Those error checks were probably leftover from the past.

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1186,9 +1186,6 @@ func DoEstimateGas(ctx context.Context, b Backend, args TransactionArgs, blockNr
 		return 0, err
 	}
 	call := args.ToMessage(header.BaseFee)
-	if err != nil {
-		return 0, err
-	}
 	// Run the gas estimation andwrap any revertals into a custom return
 	estimate, revert, err := gasestimator.Estimate(ctx, call, opts, gasCap)
 	if err != nil {
@@ -1519,9 +1516,6 @@ func AccessList(ctx context.Context, b Backend, blockNrOrHash rpc.BlockNumberOrH
 		// Set the accesslist to the last al
 		args.AccessList = &accessList
 		msg := args.ToMessage(header.BaseFee)
-		if err != nil {
-			return nil, 0, nil, err
-		}
 
 		// Apply the transaction with the access list tracer
 		tracer := logger.NewAccessListTracer(accessList, args.from(), to, precompiles)
