77390d30ce92eefeed3780854b65b3f6a75cd9cf
Wanwiset Peerapatanapokin <wanwiset25@gmail.com>
2024-03-26 13:22:24 +0400

    consensus/clique: add some missing checks (#22836) and updated to latest go-eth code (#474)

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -25,6 +25,7 @@ var (
 const (
 	GasLimitBoundDivisor uint64 = 1024    // The bound divisor of the gas limit, used in update calculations.
 	MinGasLimit          uint64 = 5000    // Minimum the gas limit may ever be.
+	MaxGasLimit          uint64 = 0x7fffffffffffffff // Maximum the gas limit (2^63-1).
 	GenesisGasLimit      uint64 = 4712388 // Gas limit of the Genesis block.
 	XDCGenesisGasLimit   uint64 = 84000000
 
