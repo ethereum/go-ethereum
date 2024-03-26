e25eab397edabc4d3d7de987841a406fc3c1d56f
kumakichi <xyesan@gmail.com>
2024-03-26 15:07:41 +0800

    ethdb/remotedb: fix error handling in Has/HasAncient

--- ethdb/remotedb/remotedb.go
+++ ethdb/remotedb/remotedb.go
@@ -34,7 +34,7 @@ type Database struct {
 
 func (db *Database) Has(key []byte) (bool, error) {
 	if _, err := db.Get(key); err != nil {
-		return false, nil
+		return false, err
 	}
 	return true, nil
 }
@@ -50,7 +50,7 @@ func (db *Database) Get(key []byte) ([]byte, error) {
 
 func (db *Database) HasAncient(kind string, number uint64) (bool, error) {
 	if _, err := db.Ancient(kind, number); err != nil {
-		return false, nil
+		return false, err
 	}
 	return true, nil
 }
