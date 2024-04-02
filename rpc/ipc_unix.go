e63f992fed51d5a576ea2890cd7eb3000c9e6884
Miles Chen <fearlesschenc@gmail.com>
2024-04-02 17:25:19 +0800

    rpc: fix ipc max path size (#29385)

--- rpc/ipc_unix.go
+++ rpc/ipc_unix.go
@@ -25,14 +25,16 @@ import (
 	"net"
 	"os"
 	"path/filepath"
+	"syscall"
 
 	"github.com/ethereum/go-ethereum/log"
 )
 
 const (
-	// On Linux, sun_path is 108 bytes in size
-	// see http://man7.org/linux/man-pages/man7/unix.7.html
-	maxPathSize = int(108)
+	// The limit of unix domain socket path diverse between OS, on Darwin it's 104 bytes
+	// but on Linux it's 108 byte, so we should depend on syscall.RawSockaddrUnix's
+	// definition dynamically
+	maxPathSize = len(syscall.RawSockaddrUnix{}.Path)
 )
 
 // ipcListen will create a Unix socket on the given endpoint.
