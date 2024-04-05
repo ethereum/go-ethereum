668446a7919fac2e8b86c318d74bfada2b1e76eb
smin-k <a7965344@gmail.com>
2024-04-05 17:38:21 +0900

    params/protocol_params.go: eip-2 comment

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -34,6 +34,7 @@ const (
 	CallValueTransferGas  uint64 = 9000  // Paid for CALL when the value transfer is non-zero.
 	CallNewAccountGas     uint64 = 25000 // Paid for CALL when the destination address didn't exist prior.
 	TxGas                 uint64 = 21000 // Per transaction not creating a contract. NOTE: Not payable on data of calls between transactions.
+	//eip2: 추가된 가스비 코드
 	TxGasContractCreation uint64 = 53000 // Per transaction that creates a contract. NOTE: Not payable on data of calls between transactions.
 	TxDataZeroGas         uint64 = 4     // Per byte of data attached to a transaction that equals zero. NOTE: Not payable on data of calls between transactions.
 	QuadCoeffDiv          uint64 = 512   // Divisor for the quadratic particle of the memory cost equation.
