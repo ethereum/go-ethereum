85447ec9a484a5f2fdc893d2510b5937bd52d8cb
Péter Garamvölgyi <peter@scroll.io>
2024-04-08 13:59:31 +0800

    feat(sepolia): enable Bernoulli hard fork (#688)
    * feat(sepolia): enable Bernoulli hard fork
    
    * update fork block

--- params/config.go
+++ params/config.go
@@ -317,7 +317,7 @@ var (
 		ArrowGlacierBlock:   nil,
 		ArchimedesBlock:     big.NewInt(0),
 		ShanghaiBlock:       big.NewInt(0),
-		BernoulliBlock:      nil,
+		BernoulliBlock:      big.NewInt(3747132),
 		CurieBlock:          nil,
 		Clique: &CliqueConfig{
 			Period: 3,
