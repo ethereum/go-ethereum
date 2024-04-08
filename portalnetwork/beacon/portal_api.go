23a502a2c348d3b61c5d5b8c1198fcc81395f902
Chen Kai <281165273grape@gmail.com>
2024-04-08 21:51:16 +0800

    feat:add light client types
    Signed-off-by: Chen Kai <281165273grape@gmail.com>

--- portalnetwork/beacon/portal_api.go
+++ portalnetwork/beacon/portal_api.go
@@ -1,23 +1,24 @@
-package api
+package beacon
 
 import (
 	"time"
 
-	"github.com/ethereum/go-ethereum/portalnetwork/beacon"
 	"github.com/protolambda/zrnt/eth2/beacon/capella"
 	zrntcommon "github.com/protolambda/zrnt/eth2/beacon/common"
 	"github.com/protolambda/ztyp/tree"
 )
 
+var _ ConsensusAPI = &PortalLightApi{}
+
 type PortalLightApi struct {
-	bn *beacon.BeaconNetwork
+	bn *BeaconNetwork
 }
 
 func NewPortalLightApi() *PortalLightApi {
 	return &PortalLightApi{}
 }
 
-func (api *PortalLightApi) GetUpdates(firstPeriod, count uint64) (beacon.LightClientUpdateRange, error) {
+func (api *PortalLightApi) GetUpdates(firstPeriod, count uint64) (LightClientUpdateRange, error) {
 	return api.bn.GetUpdates(firstPeriod, count)
 }
 
@@ -26,14 +27,22 @@ func (api *PortalLightApi) GetCheckpointData(checkpointHash tree.Root) (*capella
 }
 
 func (api *PortalLightApi) GetFinalityData() (*capella.LightClientFinalityUpdate, error) {
-	expectedCurrentSlot := api.bn.Spec.TimeToSlot(zrntcommon.Timestamp(time.Now().Unix()), zrntcommon.Timestamp(beacon.BeaconGenesisTime))
+	expectedCurrentSlot := api.bn.Spec.TimeToSlot(zrntcommon.Timestamp(time.Now().Unix()), zrntcommon.Timestamp(BeaconGenesisTime))
 	recentEpochStart := expectedCurrentSlot - (expectedCurrentSlot % api.bn.Spec.SLOTS_PER_EPOCH) + 1
 
 	return api.bn.GetFinalityUpdate(uint64(recentEpochStart))
 }
 
 func (api *PortalLightApi) GetOptimisticData() (*capella.LightClientOptimisticUpdate, error) {
-	expectedCurrentSlot := api.bn.Spec.TimeToSlot(zrntcommon.Timestamp(time.Now().Unix()), zrntcommon.Timestamp(beacon.BeaconGenesisTime))
+	expectedCurrentSlot := api.bn.Spec.TimeToSlot(zrntcommon.Timestamp(time.Now().Unix()), zrntcommon.Timestamp(BeaconGenesisTime))
 
 	return api.bn.GetOptimisticUpdate(uint64(expectedCurrentSlot))
+}
+
+func (api *PortalLightApi) ChainID() uint64 {
+	return 1
+}
+
+func (api *PortalLightApi) Name() string {
+	return "portal"
 }
