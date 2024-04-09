34aac1d7562bf141fe6da1d4f3cdea8819e7b23b
Aaron Chen <aaronchen.lisp@gmail.com>
2024-04-09 18:14:30 +0800

    all: use big.Sign to compare with zero (#29490)

--- signer/fourbyte/validation.go
+++ signer/fourbyte/validation.go
@@ -20,7 +20,6 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
-	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/signer/core/apitypes"
@@ -57,7 +56,7 @@ func (db *Database) ValidateTransaction(selector *string, tx *apitypes.SendTxArg
 		// e.g. https://github.com/ethereum/go-ethereum/issues/16106.
 		if len(data) == 0 {
 			// Prevent sending ether into black hole (show stopper)
-			if tx.Value.ToInt().Cmp(big.NewInt(0)) > 0 {
+			if tx.Value.ToInt().Sign() > 0 {
 				return nil, errors.New("transaction will create a contract with value but empty code")
 			}
 			// No value submitted at least, critically Warn, but don't blow up
