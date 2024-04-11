94bf10bfd9a9e29e249808be3eb13ae355b49a7f
Newt <guoching130@gmail.com>
2024-04-12 01:47:50 +0800

    core: use range loop for subpool iteration

--- core/txpool/txpool.go
+++ core/txpool/txpool.go
@@ -334,8 +334,8 @@ func (p *TxPool) Add(txs []*types.Transaction, local bool, sync bool) []error {
 	// Add the transactions split apart to the individual subpools and piece
 	// back the errors into the original sort order.
 	errsets := make([][]error, len(p.subpools))
-	for i := 0; i < len(p.subpools); i++ {
-		errsets[i] = p.subpools[i].Add(txsets[i], local, sync)
+	for i, subpool := range p.subpools {
+		errsets[i] = subpool.Add(txsets[i], local, sync)
 	}
 	errs := make([]error, len(txs))
 	for i, split := range splits {
