f5f6b5cd9c8783aef878a9b4acd2f8adbec8037b
maskpp <maskpp266@gmail.com>
2024-04-23 10:17:29 +0800

    genesis head remaining number sould be zero

--- core/txindexer.go
+++ core/txindexer.go
@@ -176,6 +176,9 @@ func (indexer *txIndexer) loop(chain *BlockChain) {
 
 // report returns the tx indexing progress.
 func (indexer *txIndexer) report(head uint64, tail *uint64) TxIndexProgress {
+	if head == 0 {
+		return TxIndexProgress{}
+	}
 	total := indexer.limit
 	if indexer.limit == 0 || total > head {
 		total = head + 1 // genesis included
