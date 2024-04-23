7b0bd8bb7606f377a5ca3ba613b7f035c5dae833
Your Name <maeng779@gmail.com>
2024-04-23 13:06:20 +0900

    cmd/faucet: add worldland network flag

--- cmd/faucet/faucet.go
+++ cmd/faucet/faucet.go
@@ -87,6 +87,10 @@ var (
 	goerliFlag  = flag.Bool("goerli", false, "Initializes the faucet with Görli network config")
 	rinkebyFlag = flag.Bool("rinkeby", false, "Initializes the faucet with Rinkeby network config")
 	sepoliaFlag = flag.Bool("sepolia", false, "Initializes the faucet with Sepolia network config")
+
+	lveFlag  = flag.Bool("lve", false, "Initializes the faucet with Lve network config")
+	SeoulFlag  = flag.Bool("seoul", false, "Initializes the faucet with Seoul network config")
+	GwangjuFlag  = flag.Bool("gwangju", false, "Initializes the faucet with Gwangju network config")
 )
 
 var (
