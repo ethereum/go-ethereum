c3d41c13181f444cfc9f14baf07a3bb0e9c109e8
SanShi2023 <136459117+SanShi2023@users.noreply.github.com>
2024-04-27 16:01:27 +0800

    Update web3ext.go Broken Link

--- internal/jsre/deps/web3.js
+++ internal/jsre/deps/web3.js
@@ -5902,7 +5902,7 @@ module.exports = Shh;
  * @author Alex Beregszaszi <alex@rtfs.hu>
  * @date 2016
  *
- * Reference: https://github.com/ethereum/go-ethereum/blob/swarm/internal/web3ext/web3ext.go#L33
+ * Reference: https://github.com/ethereum/go-ethereum/blob/master/internal/web3ext/web3ext.go#L33
  */
 
 "use strict";
