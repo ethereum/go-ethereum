7c5a8086a73871b8743bccc896f6e41c546a9f53
Nathan <Nathan.l@nodereal.io>
2024-05-07 15:25:15 +0800

    eth/gasprice: add query limit for FeeHistory to defend DDOS attack (#29644)
    * eth/gasprice: add query limit for FeeHistory to defend DDOS attack
    
    * fix return values after cherry-pick
    
    ---------
    
    Co-authored-by: Eric <45141191+zlacfzy@users.noreply.github.com>

--- eth/gasprice/feehistory.go
+++ eth/gasprice/feehistory.go
@@ -42,6 +42,7 @@ const (
 	// maxBlockFetchers is the max number of goroutines to spin up to pull blocks
 	// for the fee history calculation (mostly relevant for LES).
 	maxBlockFetchers = 4
+	maxQueryLimit    = 100
 )
 
 // blockFees represents a single block for processing
@@ -218,6 +219,9 @@ func (oracle *Oracle) FeeHistory(ctx context.Context, blocks uint64, unresolvedL
 	maxFeeHistory := oracle.maxHeaderHistory
 	if len(rewardPercentiles) != 0 {
 		maxFeeHistory = oracle.maxBlockHistory
+	}
+	if len(rewardPercentiles) > maxQueryLimit {
+		return common.Big0, nil, nil, nil, fmt.Errorf("%w: over the query limit %d", errInvalidPercentile, maxQueryLimit)
 	}
 	if blocks > maxFeeHistory {
 		log.Warn("Sanitizing fee history length", "requested", blocks, "truncated", maxFeeHistory)
