1a79f8fe58f9c42db1dcfd8599f386c8158069e8
Felix Lange <fjl@twurst.com>
2024-05-08 16:27:44 +0200

    params: begin v1.14.3 release cycle

--- params/version.go
+++ params/version.go
@@ -23,8 +23,8 @@ import (
 const (
 	VersionMajor = 1          // Major version component of the current release
 	VersionMinor = 14         // Minor version component of the current release
-	VersionPatch = 2          // Patch version component of the current release
-	VersionMeta  = "stable" // Version metadata to append to the version string
+	VersionPatch = 3          // Patch version component of the current release
+	VersionMeta  = "unstable" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
