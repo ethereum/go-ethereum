35b2d07f4bf735958b41cd141a5feda5e61be73e
Felix Lange <fjl@twurst.com>
2024-05-08 16:26:01 +0200

    params: release go-ethereum v1.14.2 stable

--- params/version.go
+++ params/version.go
@@ -24,7 +24,7 @@ const (
 	VersionMajor = 1          // Major version component of the current release
 	VersionMinor = 14         // Minor version component of the current release
 	VersionPatch = 2          // Patch version component of the current release
-	VersionMeta  = "unstable" // Version metadata to append to the version string
+	VersionMeta  = "stable" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
