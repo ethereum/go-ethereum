8919c5c0fc9d1c8e528e4b05e11644b6515bf45a
0xbeny <55846654+0xbeny@users.noreply.github.com>
2024-05-14 17:04:32 +0400

    core: deploy EIP-4788 contract in dev mode genesis (#29655)
    Co-authored-by: Felix Lange <fjl@twurst.com>

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -187,6 +187,10 @@ var (
 
 	// BeaconRootsAddress is the address where historical beacon roots are stored as per EIP-4788
 	BeaconRootsAddress = common.HexToAddress("0x000F3df6D732807Ef1319fB7B8bB8522d0Beac02")
+
+	// BeaconRootsCode is the code where historical beacon roots are stored as per EIP-4788
+	BeaconRootsCode = common.FromHex("3373fffffffffffffffffffffffffffffffffffffffe14604d57602036146024575f5ffd5b5f35801560495762001fff810690815414603c575f5ffd5b62001fff01545f5260205ff35b5f5ffd5b62001fff42064281555f359062001fff015500")
+
 	// SystemAddress is where the system-transaction is sent from as per EIP-4788
 	SystemAddress = common.HexToAddress("0xfffffffffffffffffffffffffffffffffffffffe")
 )
