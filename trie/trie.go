760f234a1ab050224ba245dbac3116f6a17d02ca
HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>
2024-05-28 22:07:38 +0800

    trie: fix two issues in trie iterator (#24539) (#778)
    * trie: fix two issues in trie iterator (#24539)
    
    * trie: fix memory leak in trie iterator
    
    In the trie iterator, live nodes are tracked in a stack while iterating.
    Popped node states should be explictly set to nil in order to get
    garbage-collected.
    
    * trie: fix empty trie iterator
    
    * fix lint
    
    * chore: auto version bump [bot]
    
    ---------
    
    Co-authored-by: rjl493456442 <garyrong0905@gmail.com>
    Co-authored-by: HAOYUatHZ <HAOYUatHZ@users.noreply.github.com>

--- trie/trie.go
+++ trie/trie.go
@@ -25,7 +25,6 @@ import (
 
 	"github.com/scroll-tech/go-ethereum/common"
 	"github.com/scroll-tech/go-ethereum/core/types"
-	"github.com/scroll-tech/go-ethereum/crypto"
 	"github.com/scroll-tech/go-ethereum/log"
 	"github.com/scroll-tech/go-ethereum/rlp"
 )
@@ -33,9 +32,6 @@ import (
 var (
 	// emptyRoot is the known root hash of an empty trie.
 	emptyRoot = common.HexToHash("56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421")
-
-	// emptyState is the known hash of an empty state trie entry.
-	emptyState = crypto.Keccak256Hash(nil)
 )
 
 // LeafCallback is a callback type invoked when a trie operation reaches a leaf
