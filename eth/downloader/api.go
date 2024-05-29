2f0e63e5ac6e1c1c85b5831a19bf7bbd6a63ca1d
trillo <trillo8652@gmail.com>
2024-05-29 21:43:07 +0900

    eth/downloader, eth/tracer: fix typos in comments (#29707)

--- eth/downloader/api.go
+++ eth/downloader/api.go
@@ -129,7 +129,7 @@ func (api *DownloaderAPI) eventLoop() {
 	}
 }
 
-// Syncing provides information when this nodes starts synchronising with the Ethereum network and when it's finished.
+// Syncing provides information when this node starts synchronising with the Ethereum network and when it's finished.
 func (api *DownloaderAPI) Syncing(ctx context.Context) (*rpc.Subscription, error) {
 	notifier, supported := rpc.NotifierFromContext(ctx)
 	if !supported {
