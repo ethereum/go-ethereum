b274a273b899528ac5cef9f7e8057e899bca379d
tianyeyouyou <150894831+tianyeyouyou@users.noreply.github.com>
2024-05-31 15:25:49 +0800

    common/math: rename variable name `int` to `n` (#29890)
    * chore: rename variable name `int` to `in`
    
    * chore: rename variable name `int` to `n`

--- common/math/big_test.go
+++ common/math/big_test.go
@@ -180,9 +180,9 @@ func BenchmarkByteAtOld(b *testing.B) {
 func TestReadBits(t *testing.T) {
 	check := func(input string) {
 		want, _ := hex.DecodeString(input)
-		int, _ := new(big.Int).SetString(input, 16)
+		n, _ := new(big.Int).SetString(input, 16)
 		buf := make([]byte, len(want))
-		ReadBits(int, buf)
+		ReadBits(n, buf)
 		if !bytes.Equal(buf, want) {
 			t.Errorf("have: %x\nwant: %x", buf, want)
 		}
