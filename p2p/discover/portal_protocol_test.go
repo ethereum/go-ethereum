1ae723db4913b3bcb56676e6ed8e6c5397e5a3a0
thinkAfCod <q315xia@163.com>
2024-06-03 12:15:04 +0800

    fix: fix test case

--- p2p/discover/portal_protocol_test.go
+++ p2p/discover/portal_protocol_test.go
@@ -114,6 +114,15 @@ func TestPortalWireProtocolUdp(t *testing.T) {
 	assert.NoError(t, err)
 	time.Sleep(10 * time.Second)
 
+	node1.putCacheNodeId(node2.localNode.Node())
+	node1.putCacheNodeId(node3.localNode.Node())
+
+	node2.putCacheNodeId(node1.localNode.Node())
+	node2.putCacheNodeId(node3.localNode.Node())
+
+	node3.putCacheNodeId(node1.localNode.Node())
+	node3.putCacheNodeId(node2.localNode.Node())
+
 	udpAddrStr1 := fmt.Sprintf("%s:%d", node1.localNode.Node().IP(), node1.localNode.Node().UDP())
 	udpAddrStr2 := fmt.Sprintf("%s:%d", node2.localNode.Node().IP(), node2.localNode.Node().UDP())
 
