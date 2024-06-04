0014eb6cf47a4430c38e2319f1e82821df6a6681
rjl493456442 <garyrong0905@gmail.com>
2024-06-04 10:07:39 +0800

    Update statedb.go

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -219,7 +219,7 @@ func (s *StateDB) StartPrefetcher(namespace string) {
 		// the prefetcher is constructed. For more details, see:
 		// https://github.com/ethereum/go-ethereum/issues/29880
 		if err := s.prefetcher.prefetch(common.Hash{}, s.originalRoot, common.Address{}, nil); err != nil {
-			log.Error("Failed to prefetch account trie", "root", s.originalRoot.String(), "err", err)
+			log.Error("Failed to prefetch account trie", "root", s.originalRoot, "err", err)
 		}
 	}
 }
