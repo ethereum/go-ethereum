fc9db9f65f40d3929d5b3674c12da9b4fed4ab21
Péter Garamvölgyi <peter@scroll.io>
2024-06-13 15:21:34 +0200

    fix(txpool): compare gas tip cap instead of effective gas tip cap (#825)
    fix: compare gas tip cap instead of effective gas tip cap

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -648,8 +648,7 @@ func (pool *TxPool) validateTx(tx *types.Transaction, local bool) error {
 		return ErrInvalidSender
 	}
 	// Drop non-local transactions under our own minimal accepted gas price or tip.
-	pendingBaseFee := pool.priced.urgent.baseFee
-	if !local && tx.EffectiveGasTipIntCmp(pool.gasPrice, pendingBaseFee) < 0 {
+	if !local && tx.GasTipCapIntCmp(pool.gasPrice) < 0 {
 		return ErrUnderpriced
 	}
 	// Ensure the transaction adheres to nonce ordering
