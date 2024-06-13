84d9e40ac5bda8188378d790cbafa746b4d251a3
Sina Mahmoodi <itz.s1na@gmail.com>
2024-06-13 14:24:05 +0200

    update contract code & addr

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -196,7 +196,7 @@ var (
 	// SystemAddress is where the system-transaction is sent from as per EIP-4788
 	SystemAddress = common.HexToAddress("0xfffffffffffffffffffffffffffffffffffffffe")
 	// HistoryStorageAddress is where the historical block hashes are stored.
-	HistoryStorageAddress = common.HexToAddress("0x25a219378dad9b3503c8268c9ca836a52427a4fb")
+	HistoryStorageAddress = common.HexToAddress("0x0aae40965e6800cd9b1f4b05ff21581047e3f91e")
 	// HistoryStorageCode is the code with getters for historical block hashes.
-	HistoryStorageCode = common.FromHex("60203611603157600143035f35116029575f356120000143116029576120005f3506545f5260205ff35b5f5f5260205ff35b5f5ffd00")
+	HistoryStorageCode = common.FromHex("3373fffffffffffffffffffffffffffffffffffffffe1460575767ffffffffffffffff5f3511605357600143035f3511604b575f35612000014311604b57611fff5f3516545f5260205ff35b5f5f5260205ff35b5f5ffd5b5f35611fff60014303165500")
 )
