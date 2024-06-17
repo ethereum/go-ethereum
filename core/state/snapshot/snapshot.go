4579ef94d8f6dd4d941f54e209e37f84ab32962f
Marius van der Wijden <m.vanderwijden@live.de>
2024-06-17 15:54:27 +0200

    core/state/snapshot: only acquire read-lock when iterating

--- core/state/snapshot/snapshot.go
+++ core/state/snapshot/snapshot.go
@@ -666,8 +666,8 @@ func diffToDisk(bottom *diffLayer) *diskLayer {
 
 // Release releases resources
 func (t *Tree) Release() {
-	t.lock.Lock()
-	defer t.lock.Unlock()
+	t.lock.RLock()
+	defer t.lock.RUnlock()
 
 	if dl := t.disklayer(); dl != nil {
 		dl.Release()
@@ -851,8 +851,8 @@ func (t *Tree) diskRoot() common.Hash {
 // generating is an internal helper function which reports whether the snapshot
 // is still under the construction.
 func (t *Tree) generating() (bool, error) {
-	t.lock.Lock()
-	defer t.lock.Unlock()
+	t.lock.RLock()
+	defer t.lock.RUnlock()
 
 	layer := t.disklayer()
 	if layer == nil {
@@ -865,8 +865,8 @@ func (t *Tree) generating() (bool, error) {
 
 // DiskRoot is a external helper function to return the disk layer root.
 func (t *Tree) DiskRoot() common.Hash {
-	t.lock.Lock()
-	defer t.lock.Unlock()
+	t.lock.RLock()
+	defer t.lock.RUnlock()
 
 	return t.diskRoot()
 }
