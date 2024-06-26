6c97819d94262eb2a581be84592555536b455602
Marius van der Wijden <m.vanderwijden@live.de>
2024-06-26 11:32:57 +0200

    core/vm: fix opExtCode{Size,Hash,Copy}

--- core/vm/instructions.go
+++ core/vm/instructions.go
@@ -357,7 +357,12 @@ func opExtCodeSize(pc *uint64, interpreter *EVMInterpreter, scope *ScopeContext)
 		witness.AddCode(interpreter.evm.StateDB.GetCode(address))
 		witness.AddCode(interpreter.evm.StateDB.ResolveCode(address))
 	}
-	slot.SetUint64(uint64(len(interpreter.evm.StateDB.ResolveCode(slot.Bytes20()))))
+	code := interpreter.evm.StateDB.GetCode(slot.Bytes20())
+	if isEOFVersion1(code) {
+		slot.SetUint64(2)
+	} else {
+		slot.SetUint64(uint64(len(interpreter.evm.StateDB.ResolveCode(slot.Bytes20()))))
+	}
 	return nil, nil
 }
 
@@ -389,6 +394,7 @@ func opExtCodeCopy(pc *uint64, interpreter *EVMInterpreter, scope *ScopeContext)
 		memOffset  = stack.pop()
 		codeOffset = stack.pop()
 		length     = stack.pop()
+		lengthU64  = length.Uint64()
 	)
 	uint64CodeOffset, overflow := codeOffset.Uint64WithOverflow()
 	if overflow {
@@ -400,8 +406,11 @@ func opExtCodeCopy(pc *uint64, interpreter *EVMInterpreter, scope *ScopeContext)
 		witness.AddCode(interpreter.evm.StateDB.GetCode(addr))
 		witness.AddCode(code)
 	}
-	codeCopy := getData(code, uint64CodeOffset, length.Uint64())
-	scope.Memory.Set(memOffset.Uint64(), length.Uint64(), codeCopy)
+	if isEOFVersion1(code) {
+		lengthU64 = 2
+	}
+	codeCopy := getData(code, uint64CodeOffset, lengthU64)
+	scope.Memory.Set(memOffset.Uint64(), lengthU64, codeCopy)
 
 	return nil, nil
 }
@@ -441,7 +450,12 @@ func opExtCodeHash(pc *uint64, interpreter *EVMInterpreter, scope *ScopeContext)
 	if interpreter.evm.StateDB.Empty(address) {
 		slot.Clear()
 	} else {
-		slot.SetBytes(interpreter.evm.StateDB.ResolveCodeHash(address).Bytes())
+		code := interpreter.evm.StateDB.GetCode(address)
+		if HasEOFByte(code) {
+			slot.SetFromHex("0x9dbf3648db8210552e9c4f75c6a1c3057c0ca432043bd648be15fe7be05646f5")
+		} else {
+			slot.SetBytes(interpreter.evm.StateDB.ResolveCodeHash(address).Bytes())
+		}
 	}
 	return nil, nil
 }
--- core/vm/interface.go
+++ core/vm/interface.go
@@ -43,7 +43,6 @@ type StateDB interface {
 	GetCodeHash(common.Address) common.Hash
 	GetCode(common.Address) []byte
 	SetCode(common.Address, []byte)
-	SetCodeEOF(common.Address, []byte)
 	GetCodeSize(common.Address) int
 
 	ResolveCodeHash(common.Address) common.Hash
