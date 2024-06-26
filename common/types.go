881ff7f7696fc22b7cc1b69048855f82fbb65d5a
lightclient <lightclient@protonmail.com>
2024-06-26 17:12:17 -0600

    all: impl eip-7702
    Co-authored-by: lightclient <lightclient@protonmail.com>
    Co-authored-by: Mario Vega <marioevz@gmail.com>

--- common/types.go
+++ common/types.go
@@ -45,6 +45,9 @@ var (
 	hashT    = reflect.TypeOf(Hash{})
 	addressT = reflect.TypeOf(Address{})
 
+	// ZeroAddress represents the zero address value.
+	ZeroAddress = Address{}
+
 	// MaxAddress represents the maximum possible address value.
 	MaxAddress = HexToAddress("0xffffffffffffffffffffffffffffffffffffffff")
 
