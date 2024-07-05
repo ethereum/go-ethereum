dcd000437657694316b40beef87a504c5d037c9e
HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>
2024-07-05 16:59:44 +0800

    fix(p2p): re-enable ENRUpdater (#869)
    * feat: enable ENRUpdater
    
    * chore: auto version bump [bot]
    
    * chore: auto version bump [bot]
    
    * chore: auto version bump [bot]
    
    ---------
    
    Co-authored-by: HAOYUatHZ <HAOYUatHZ@users.noreply.github.com>

--- eth/backend.go
+++ eth/backend.go
@@ -558,7 +558,7 @@ func (s *Ethereum) Protocols() []p2p.Protocol {
 // Start implements node.Lifecycle, starting all internal goroutines needed by the
 // Ethereum protocol implementation.
 func (s *Ethereum) Start() error {
-	//eth.StartENRUpdater(s.blockchain, s.p2pServer.LocalNode())
+	eth.StartENRUpdater(s.blockchain, s.p2pServer.LocalNode())
 
 	// Start the bloom bits servicing goroutines
 	s.startBloomHandlers(params.BloomBitsBlocks)
