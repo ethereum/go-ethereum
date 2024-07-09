d48e27038bd837802596336c6d97d0bcd96c1466
Liam <liam.icheng.lai@gmail.com>
2024-07-09 21:54:44 +1000

    xdpos 2.0 release for mainnet (#574)
    * xdpos 2.0 release for mainnet

--- params/version.go
+++ params/version.go
@@ -21,10 +21,10 @@ import (
 )
 
 const (
-	VersionMajor = 2       // Major version component of the current release
-	VersionMinor = 3       // Minor version component of the current release
-	VersionPatch = 0       // Patch version component of the current release
-	VersionMeta  = "beta1" // Version metadata to append to the version string
+	VersionMajor = 2  // Major version component of the current release
+	VersionMinor = 0  // Minor version component of the current release
+	VersionPatch = 0  // Patch version component of the current release
+	VersionMeta  = "" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
