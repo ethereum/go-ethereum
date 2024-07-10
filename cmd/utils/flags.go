cb8a9edd0309d32e50e13c50fbf2e83498594f2a
Daniel Liu <liudaniel@qq.com>
2024-07-10 09:14:46 +0800

    cmd/utils: max out the OS file allowance, don't cap to 2K (#18211)

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -818,17 +818,12 @@ func setPrefix(ctx *cli.Context, cfg *node.Config) {
 // MakeDatabaseHandles raises out the number of allowed file handles per process
 // for XDC and returns half of the allowance to assign to the database.
 func MakeDatabaseHandles() int {
-	limit, err := fdlimit.Current()
+	limit, err := fdlimit.Maximum()
 	if err != nil {
 		Fatalf("Failed to retrieve file descriptor allowance: %v", err)
 	}
-	if limit < 2048 {
-		if err := fdlimit.Raise(2048); err != nil {
-			Fatalf("Failed to raise file descriptor allowance: %v", err)
-		}
-	}
-	if limit > 2048 { // cap database file descriptors even if more is available
-		limit = 2048
+	if err := fdlimit.Raise(uint64(limit)); err != nil {
+		Fatalf("Failed to raise file descriptor allowance: %v", err)
 	}
 	return limit / 2 // Leave half for networking and other stuff
 }
