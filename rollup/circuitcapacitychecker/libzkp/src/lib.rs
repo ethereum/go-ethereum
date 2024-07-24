7b10424e16b9c1a89e69254888efdcead77cdabb
HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>
2024-07-24 22:57:15 +0800

    fix(libzkp): disable recursion_limit for rust trace deserialization (#923)
    fix(ccc): disable_recursion_limit for rust trace deserialization

--- rollup/circuitcapacitychecker/libzkp/src/lib.rs
+++ rollup/circuitcapacitychecker/libzkp/src/lib.rs
@@ -12,6 +12,8 @@ pub mod checker {
     use std::panic;
     use std::ptr::null;
     use std::ffi::CStr;
+    use serde::Deserialize as Deserializea;
+    use serde_json::Deserializer;
 
     #[derive(Debug, Clone, Deserialize, Serialize)]
     pub struct CommonResult {
@@ -48,9 +50,19 @@ pub mod checker {
     #[no_mangle]
     pub unsafe extern "C" fn parse_json_to_rust_trace(trace_json_ptr: *const c_char) -> *mut BlockTrace {
         let trace_json_cstr = unsafe { CStr::from_ptr(trace_json_ptr) };
-        let trace = serde_json::from_slice::<BlockTrace>(trace_json_cstr.to_bytes());
+        let trace_json_bytes = trace_json_cstr.to_bytes();
+        let mut deserializer = Deserializer::from_slice(trace_json_bytes);
+        deserializer.disable_recursion_limit();
+        let trace = BlockTrace::deserialize(&mut deserializer);
         match trace {
-            Err(_) => return null_mut(),
+            Err(e) => {
+                log::warn!(
+                    "failed to parse trace in parse_json_to_rust_trace, error: {:?}, trace_json_cstr: {:?}",
+                    e,
+                    trace_json_cstr,
+                );
+                return null_mut();
+            }
             Ok(t) => return Box::into_raw(Box::new(t))
         }
     }
@@ -226,10 +238,10 @@ pub mod checker {
                 ))?
                 .get_tx_num() as u64)
         })
-        .map_or_else(
-            |e| bail!("circuit capacity checker (id: {id}) error in get_tx_num: {e:?}"),
-            |result| result,
-        )
+            .map_or_else(
+                |e| bail!("circuit capacity checker (id: {id}) error in get_tx_num: {e:?}"),
+                |result| result,
+            )
     }
 
     /// # Safety
@@ -260,10 +272,10 @@ pub mod checker {
                 .set_light_mode(light_mode);
             Ok(())
         })
-        .map_or_else(
-            |e| bail!("circuit capacity checker (id: {id}) error in set_light_mode: {e:?}"),
-            |result| result,
-        )
+            .map_or_else(
+                |e| bail!("circuit capacity checker (id: {id}) error in set_light_mode: {e:?}"),
+                |result| result,
+            )
     }
 }
 
