014eb1c2ed9bc4767a68ac5f8a4ede1e4614489f
Liam <liam.icheng.lai@gmail.com>
2024-07-29 07:34:28 -0700

    lower the devnet cert to recover devnet (#592)
    * lower the devnet cert to recover devnet
    
    * lower the devnet cert to recover devnet

--- params/config.go
+++ params/config.go
@@ -94,6 +94,14 @@ var (
 			TimeoutPeriod:        30,
 			MinePeriod:           2,
 		},
+		13625858: { // 2024.07.29 RPC call and reorg sync issue
+			MaxMasternodes:       108,
+			SwitchRound:          13625858,
+			CertThreshold:        0.4,
+			TimeoutSyncThreshold: 3,
+			TimeoutPeriod:        30,
+			MinePeriod:           2,
+		},
 	}
 
 	UnitTestV2Configs = map[uint64]*V2Config{
