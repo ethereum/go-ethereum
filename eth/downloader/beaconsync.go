ethereum/go-ethereum

commit dad8f237ffa5da2c2471f2d9f32d2bc5b580f667

eth/downloader: correct sync mode logging to show old mode (#30219)
This PR fixes an issue in the setMode method of beaconBackfiller where the
log message was not displaying the previous mode correctly. The log message
now shows both the old and new sync modes.

eth/downloader/beaconsync.go
@@ -123,7 +123,8 @@ func (b *beaconBackfiller) resume() {
 func (b *beaconBackfiller) setMode(mode SyncMode) {
 	// Update the old sync mode and track if it was changed
 	b.lock.Lock()
-	updated := b.syncMode != mode
+	oldMode := b.syncMode
+	updated := oldMode != mode
 	filling := b.filling
 	b.syncMode = mode
 	b.lock.Unlock()
@@ -133,8 +134,8 @@ func (b *beaconBackfiller) setMode(mode SyncMode) {
 	if !updated || !filling {
 		return
 	}
-	log.Error("Downloader sync mode changed mid-run", "old", mode.String(), "new", mode.String())
-	b.suspend()
+	log.Error("Downloader sync mode changed mid-run", "old", oldMode.String(), "new", mode.String())
+	b.suspend()	
 	b.resume()
 }
 

commit 16cf5c5fed9f8f22ef31fcdb0057aa4ad4ea3a12

eth/downloader: gofmt (#30261)
Fixes a regression introduced in
https://github.com/ethereum/go-ethereum/pull/30219

eth/downloader/beaconsync.go
@@ -135,7 +135,7 @@ func (b *beaconBackfiller) setMode(mode SyncMode) {
 		return
 	}
 	log.Error("Downloader sync mode changed mid-run", "old", oldMode.String(), "new", mode.String())
-	b.suspend()	
+	b.suspend()
 	b.resume()
 }
 