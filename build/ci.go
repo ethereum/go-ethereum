b398df9e3bca44e649ab32174927c0482ff53edd
Jared Wasinger <j-wasinger@hotmail.com>
2024-08-12 06:26:06 -0700

    move build none back to previous spot

--- build/ci.go
+++ build/ci.go
@@ -1,6 +1,3 @@
-//go:build none
-// +build none
-
 // Copyright 2016 The go-ethereum Authors
 // This file is part of the go-ethereum library.
 //
@@ -16,6 +13,10 @@
 //
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+//go:build none
+// +build none
+
 /*
 The ci command is called from Continuous Integration scripts.
 
