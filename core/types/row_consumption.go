2f828ecdb38f4d9d7cafc0007961b6d5c9f47a8c
HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>
2024-08-13 10:01:23 +0800

    feat: allow calculating diffs between two row consumptions (#981)
    * feat: allow calculating diffs between two row consumptions (#980)
    
    * bump go version in github CI
    
    ---------
    
    Co-authored-by: Ömer Faruk Irmak <omerfirmak@gmail.com>

--- core/types/row_consumption.go
+++ core/types/row_consumption.go
@@ -1,5 +1,7 @@
 package types
 
+import "slices"
+
 type RowUsage struct {
 	IsOk            bool                 `json:"is_ok"`
 	RowNumber       uint64               `json:"row_number"`
@@ -12,4 +14,38 @@ type SubCircuitRowUsage struct {
 	RowNumber uint64 `json:"row_number" gencodec:"required"`
 }
 
+// RowConsumptionLimit is the max number of row we support per subcircuit
+const RowConsumptionLimit = 1_000_000
+
 type RowConsumption []SubCircuitRowUsage
+
+// IsOverflown returns if any subcircuits are overflown
+func (rc RowConsumption) IsOverflown() bool {
+	return slices.ContainsFunc(rc, func(scru SubCircuitRowUsage) bool {
+		return scru.RowNumber > RowConsumptionLimit
+	})
+}
+
+// Difference returns rc - other
+// Assumes that rc > other for all subcircuits
+func (rc RowConsumption) Difference(other RowConsumption) RowConsumption {
+	subCircuitMap := make(map[string]uint64, len(rc))
+	for _, detail := range rc {
+		subCircuitMap[detail.Name] = detail.RowNumber
+	}
+
+	for _, detail := range other {
+		subCircuitMap[detail.Name] -= detail.RowNumber
+	}
+
+	diff := make([]SubCircuitRowUsage, 0, len(subCircuitMap))
+	for name, rowNumDiff := range subCircuitMap {
+		if rowNumDiff > 0 {
+			diff = append(diff, SubCircuitRowUsage{
+				Name:      name,
+				RowNumber: rowNumDiff,
+			})
+		}
+	}
+	return diff
+}
