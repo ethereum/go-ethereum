a6c34acc99610457ad171f2ba7310a16a9f89faa
Felix Lange <fjl@twurst.com>
2024-08-14 19:49:00 +0200

    eth/ethconfig: improve formatting

--- eth/ethconfig/config.go
+++ eth/ethconfig/config.go
@@ -86,8 +86,9 @@ type Config struct {
 	EthDiscoveryURLs  []string
 	SnapDiscoveryURLs []string
 
-	NoPruning         bool // Whether to disable pruning and flush everything to disk
-	NoPrefetch        bool // Whether to disable prefetching and only load state on demand
+	// State options.
+	NoPruning  bool // Whether to disable pruning and flush everything to disk
+	NoPrefetch bool // Whether to disable prefetching and only load state on demand
 
 	// Deprecated: use 'TransactionHistory' instead.
 	TxLookupLimit uint64 `toml:",omitempty"` // The maximum number of blocks from head whose tx indices are reserved.
