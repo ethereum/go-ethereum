3ad818e6e0fb7c7bdf35080df8565322da11f783
lightclient <lightclient@protonmail.com>
2024-08-19 12:25:17 -0600

    eth/catalyst: typo

--- eth/catalyst/simulated_beacon_api.go
+++ eth/catalyst/simulated_beacon_api.go
@@ -65,7 +65,7 @@ func (a *simulatedBeaconAPI) loop() {
 			// It's worth noting that in case a tx ends up in the pool listed as
 			// "executable", but for whatever reason the miner does not include it in
 			// a block -- maybe the miner is enforcing a higher tip than the pool --
-			// this code will spinloopk.
+			// this code will spinloop.
 			for {
 				if executable, _ := a.sim.eth.TxPool().Stats(); executable == 0 {
 					break
