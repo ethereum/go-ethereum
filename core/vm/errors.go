1f45af089a9f2ee9cf633c74e7f9e5fdf0f8f046
Daniel Liu <liudaniel@qq.com>
2024-08-21 14:15:40 +0800

    core/vm: implement EIP 3541 (#22809)

--- core/vm/errors.go
+++ core/vm/errors.go
@@ -35,10 +35,11 @@ var (
 	ErrReturnDataOutOfBounds    = errors.New("return data out of bounds")
 	ErrGasUintOverflow          = errors.New("gas uint64 overflow")
 	ErrNonceUintOverflow        = errors.New("nonce uint64 overflow")
+	ErrInvalidCode              = errors.New("invalid code: must not begin with 0xef")
 
 	// errStopToken is an internal token indicating interpreter loop termination,
 	// never returned to outside callers.
-	errStopToken = errors.New("stop token")	
+	errStopToken = errors.New("stop token")
 )
 
 // ErrStackUnderflow wraps an evm error when the items on the stack less
