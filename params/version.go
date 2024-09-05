2415826a8a8d9ddf129c1b325e185e330a028d79
Pratik Patil <pratikspatil024@gmail.com>
2024-09-05 21:12:27 +0530

    v1.4.0-beta version bump

--- params/version.go
+++ params/version.go
@@ -21,10 +21,10 @@ import (
 )
 
 const (
-	VersionMajor = 1  // Major version component of the current release
-	VersionMinor = 3  // Minor version component of the current release
-	VersionPatch = 7  // Patch version component of the current release
-	VersionMeta  = "" // Version metadata to append to the version string
+	VersionMajor = 1      // Major version component of the current release
+	VersionMinor = 4      // Minor version component of the current release
+	VersionPatch = 0      // Patch version component of the current release
+	VersionMeta  = "beta" // Version metadata to append to the version string
 )
 
 var GitCommit string
