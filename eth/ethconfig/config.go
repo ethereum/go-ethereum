090474a6c34c18ca9d59b895e628b78b4671a3e3
Juan Palacios <juan.palacios@immutable.com>
2024-09-05 12:45:53 +1000

    Added a custom field to the ethconfig.Config to test deserialisation

--- eth/ethconfig/config.go
+++ eth/ethconfig/config.go
@@ -159,6 +159,8 @@ type Config struct {
 
 	// OverrideVerkle (TODO: remove after the fork)
 	OverrideVerkle *uint64 `toml:",omitempty"`
+
+        CustomField string     `toml:",omitempty"`
 }
 
 // CreateConsensusEngine creates a consensus engine for the given chain config.
