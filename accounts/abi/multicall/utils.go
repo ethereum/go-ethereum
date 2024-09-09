c9072e55c498635b85f9e3f4920b278acf816734
Justin Brower <jbrower95@gmail.com>
2024-09-09 11:19:41 -0400

    rm unused helpers

--- accounts/abi/multicall/utils.go
+++ accounts/abi/multicall/utils.go
@@ -7,29 +7,3 @@ func mapArray[A any, B any](coll []A, mapper func(i A, index uint64) B) []B {
 	}
 	return out
 }
-
-func filterArray[A any](coll []A, criteria func(i A) bool) []A {
-	out := []A{}
-	for _, item := range coll {
-		if criteria(item) {
-			out = append(out, item)
-		}
-	}
-	return out
-}
-
-func reduceArray[A any, B any](coll []A, processor func(accum B, next A) B, initialState B) B {
-	val := initialState
-	for _, item := range coll {
-		val = processor(val, item)
-	}
-	return val
-}
-
-func flattenArrays[A any](coll [][]A) []A {
-	out := []A{}
-	for _, arr := range coll {
-		out = append(out, arr...)
-	}
-	return out
-}
