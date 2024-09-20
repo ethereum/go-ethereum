6baf9fdf0b743bd1b24962d482d5407877c9e359
Ömer Faruk Irmak <omerfirmak@gmail.com>
2024-09-20 15:46:09 +0300

    feat(txpool): only try demoting txns from accounts that were active
    txpool demotes pending txns only if their nonce is now lower than
    the nonce in the latest state or the account no longer has enough
    funds to cover the costs. Unless the account in question was active
    since the last txpool reorg, there is no way that it's nonce changed
    or balance decreased.

--- core/txpool/legacypool/legacypool_test.go
+++ core/txpool/legacypool/legacypool_test.go
@@ -2551,7 +2551,7 @@ func benchmarkPendingDemotion(b *testing.B, size int) {
 	// Benchmark the speed of pool validation
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		pool.demoteUnexecutables()
+		pool.demoteUnexecutables(nil)
 	}
 }
 
