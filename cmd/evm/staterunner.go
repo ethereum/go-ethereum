64cbd00e86345600e6249e6e7642ff4c87c72044
Jared Wasinger <j-wasinger@hotmail.com>
2024-09-24 19:00:45 +0700

    fix goimport

--- cmd/evm/staterunner.go
+++ cmd/evm/staterunner.go
@@ -24,13 +24,12 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ethereum/go-ethereum/internal/flags"
-
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/rawdb"
 	"github.com/ethereum/go-ethereum/core/state"
 	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/ethereum/go-ethereum/eth/tracers/logger"
+	"github.com/ethereum/go-ethereum/internal/flags"
 	"github.com/ethereum/go-ethereum/tests"
 	"github.com/urfave/cli/v2"
 )
