ff4efb927a279663caee05604149fdadcd2e549e
Martin Holst Swende <martin@swende.se>
2024-10-01 14:39:30 +0200

    core/vm: license header fixes

--- core/vm/eof_validation.go
+++ core/vm/eof_validation.go
@@ -1,3 +1,5 @@
+// Copyright 2024 The go-ethereum Authors
+// This file is part of the go-ethereum library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
