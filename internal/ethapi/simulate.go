40fd887df6812033f96941181549693d2cd5444d
Sina M <1591639+s1na@users.noreply.github.com>
2024-10-01 11:36:56 +0200

    internal/ethapi: remove td field from block (#30386)
    implement https://github.com/ethereum/execution-apis/pull/570

--- internal/ethapi/simulate.go
+++ internal/ethapi/simulate.go
@@ -125,8 +125,6 @@ func (sim *simulator) execute(ctx context.Context, blocks []simBlock) ([]map[str
 	var (
 		results = make([]map[string]interface{}, len(blocks))
 		parent  = sim.base
-		// Assume same total difficulty for all simulated blocks.
-		td = sim.b.GetTd(ctx, sim.base.Hash())
 	)
 	for bi, block := range blocks {
 		result, callResults, err := sim.processBlock(ctx, &block, headers[bi], parent, headers[:bi], timeout)
@@ -134,7 +132,6 @@ func (sim *simulator) execute(ctx context.Context, blocks []simBlock) ([]map[str
 			return nil, err
 		}
 		enc := RPCMarshalBlock(result, true, sim.fullTx, sim.chainConfig)
-		enc["totalDifficulty"] = (*hexutil.Big)(td)
 		enc["calls"] = callResults
 		results[bi] = enc
 
