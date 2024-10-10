ec92add42869dad0ca70ec4014b09cb7f196de41
Liam <liam.icheng.lai@gmail.com>
2024-10-10 00:02:04 -0700

    Update to version 2.2.5 (#670)

--- params/version.go
+++ params/version.go
@@ -23,7 +23,7 @@ import (
 const (
 	VersionMajor = 2        // Major version component of the current release
 	VersionMinor = 2        // Minor version component of the current release
-	VersionPatch = 4        // Patch version component of the current release
+	VersionPatch = 5        // Patch version component of the current release
 	VersionMeta  = "stable" // Version metadata to append to the version string
 )
 
