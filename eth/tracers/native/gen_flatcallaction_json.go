19816f964ef8bc971b1c9c1527ffabce2b45ce44
jsvisa <delweng@gmail.com>
2024-10-14 16:30:51 +0800

    Revert "eth/tracers: rm the flatCall's notused CreationMethod"
    This reverts commit 2792a54adeb02032f7eace0032755c9225f75fa5.
    
    Signed-off-by: jsvisa <delweng@gmail.com>

--- eth/tracers/native/gen_flatcallaction_json.go
+++ eth/tracers/native/gen_flatcallaction_json.go
@@ -20,6 +20,7 @@ func (f flatCallAction) MarshalJSON() ([]byte, error) {
 		SelfDestructed *common.Address `json:"address,omitempty"`
 		Balance        *hexutil.Big    `json:"balance,omitempty"`
 		CallType       string          `json:"callType,omitempty"`
+		CreationMethod string          `json:"creationMethod,omitempty"`
 		From           *common.Address `json:"from,omitempty"`
 		Gas            *hexutil.Uint64 `json:"gas,omitempty"`
 		Init           *hexutil.Bytes  `json:"init,omitempty"`
@@ -34,6 +35,7 @@ func (f flatCallAction) MarshalJSON() ([]byte, error) {
 	enc.SelfDestructed = f.SelfDestructed
 	enc.Balance = (*hexutil.Big)(f.Balance)
 	enc.CallType = f.CallType
+	enc.CreationMethod = f.CreationMethod
 	enc.From = f.From
 	enc.Gas = (*hexutil.Uint64)(f.Gas)
 	enc.Init = (*hexutil.Bytes)(f.Init)
@@ -52,6 +54,7 @@ func (f *flatCallAction) UnmarshalJSON(input []byte) error {
 		SelfDestructed *common.Address `json:"address,omitempty"`
 		Balance        *hexutil.Big    `json:"balance,omitempty"`
 		CallType       *string         `json:"callType,omitempty"`
+		CreationMethod *string         `json:"creationMethod,omitempty"`
 		From           *common.Address `json:"from,omitempty"`
 		Gas            *hexutil.Uint64 `json:"gas,omitempty"`
 		Init           *hexutil.Bytes  `json:"init,omitempty"`
@@ -78,6 +81,9 @@ func (f *flatCallAction) UnmarshalJSON(input []byte) error {
 	}
 	if dec.CallType != nil {
 		f.CallType = *dec.CallType
+	}
+	if dec.CreationMethod != nil {
+		f.CreationMethod = *dec.CreationMethod
 	}
 	if dec.From != nil {
 		f.From = dec.From
