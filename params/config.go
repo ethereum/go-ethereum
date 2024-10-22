90906c8dad75e8ce65fac12302e36749f6d28f22
Jared Wasinger <j-wasinger@hotmail.com>
2024-10-22 17:17:15 +0700

    fix imports

--- params/config.go
+++ params/config.go
@@ -20,9 +20,8 @@ import (
 	"fmt"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common/math"
-
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/math"
 	"github.com/ethereum/go-ethereum/params/forks"
 )
 
