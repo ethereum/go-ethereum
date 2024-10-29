edce9ccb27a82996ab4c9a3eb6d0bd8e57b403ad
Daniel Liu <liudaniel@qq.com>
2024-10-29 14:07:10 +0800

    all: fix staticcheck warning S1024: not use x.Sub(time.Now())

--- eth/fetcher/fetcher.go
+++ eth/fetcher/fetcher.go
@@ -679,7 +679,7 @@ func (f *Fetcher) insert(peer string, block *types.Block) {
 				go f.broadcastBlock(block, true)
 			}
 		case consensus.ErrFutureBlock:
-			delay := time.Unix(block.Time().Int64(), 0).Sub(time.Now()) // nolint: gosimple
+			delay := time.Until(time.Unix(block.Time().Int64(), 0))
 			log.Info("Receive future block", "number", block.NumberU64(), "hash", block.Hash().Hex(), "delay", delay)
 			time.Sleep(delay)
 			goto again
