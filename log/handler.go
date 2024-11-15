0902dcf37c48e19100aade8bfb3c5aafa8a43438
Daniel Liu <liudaniel@qq.com>
2024-11-15 10:02:41 +0800

    log: modify lock defer unlock order in sync handler (#24667)
    This modifies the order of Lock() defer Unlock() to follow the more
    typically used pattern.

--- log/handler.go
+++ log/handler.go
@@ -52,8 +52,9 @@ func StreamHandler(wr io.Writer, fmtr Format) Handler {
 func SyncHandler(h Handler) Handler {
 	var mu sync.Mutex
 	return FuncHandler(func(r *Record) error {
-		defer mu.Unlock()
 		mu.Lock()
+		defer mu.Unlock()
+
 		return h.Log(r)
 	})
 }
