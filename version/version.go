aa636921291744af082cefbe4dd811fb58fbde63
Martin Holst Swende <martin@swende.se>
2024-11-19 16:58:52 +0100

    version: fix typo in v1.14.13 release cycle name

--- version/version.go
+++ version/version.go
@@ -17,8 +17,8 @@
 package version
 
 const (
-	Major = 1         // Major version component of the current release
-	Minor = 14        // Minor version component of the current release
-	Patch = 13        // Patch version component of the current release
-	Meta  = "ustable" // Version metadata to append to the version string
+	Major = 1          // Major version component of the current release
+	Minor = 14         // Minor version component of the current release
+	Patch = 13         // Patch version component of the current release
+	Meta  = "unstable" // Version metadata to append to the version string
 )
