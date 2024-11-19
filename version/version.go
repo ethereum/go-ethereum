293a300d64be3d9a1c2cc92c26fcff4089deadcd
Martin Holst Swende <martin@swende.se>
2024-11-19 14:46:38 +0100

    version: go-ethereum v1.14.12 stable

--- version/version.go
+++ version/version.go
@@ -17,8 +17,8 @@
 package version
 
 const (
-	Major = 1          // Major version component of the current release
-	Minor = 14         // Minor version component of the current release
-	Patch = 12         // Patch version component of the current release
-	Meta  = "unstable" // Version metadata to append to the version string
+	Major = 1        // Major version component of the current release
+	Minor = 14       // Minor version component of the current release
+	Patch = 12       // Patch version component of the current release
+	Meta  = "stable" // Version metadata to append to the version string
 )
