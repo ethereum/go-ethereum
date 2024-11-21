0d0ea46bfac137e7007bc43a015207a78bac29db
Daniel Liu <liudaniel@qq.com>
2024-11-21 18:34:32 +0800

    core: implement EIP-3651, warm coinbase (#25819)

--- core/vm/interface.go
+++ core/vm/interface.go
@@ -69,7 +69,7 @@ type StateDB interface {
 	// AddSlotToAccessList adds the given (address,slot) to the access list. This operation is safe to perform
 	// even if the feature/fork is not active yet
 	AddSlotToAccessList(addr common.Address, slot common.Hash)
-	Prepare(rules params.Rules, sender common.Address, dest *common.Address, precompiles []common.Address, txAccesses types.AccessList)
+	Prepare(rules params.Rules, sender, coinbase common.Address, dest *common.Address, precompiles []common.Address, txAccesses types.AccessList)
 
 	RevertToSnapshot(int)
 	Snapshot() int
