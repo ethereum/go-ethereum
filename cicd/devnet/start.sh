e257b521cea4118b927481cda775f9ae4025cdb5
wanwiset25 <wanwiset25@gmail.com>
2024-11-25 22:17:04 +0400

    change devnetstats url

--- cicd/devnet/start.sh
+++ cicd/devnet/start.sh
@@ -62,7 +62,7 @@ else
 fi
 
 INSTANCE_IP=$(curl https://checkip.amazonaws.com)
-netstats="${NODE_NAME}-${wallet}-${INSTANCE_IP}:xinfin_xdpos_hybrid_network_stats@devnetstats.apothem.network:2000"
+netstats="${NODE_NAME}-${wallet}-${INSTANCE_IP}:xinfin_xdpos_hybrid_network_stats@devnetstats.hashlab.apothem.network:1999"
 
 
 echo "Running a node with wallet: ${wallet} at IP: ${INSTANCE_IP}"
