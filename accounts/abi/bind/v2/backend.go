d0a8ead758a0fe7fdf605efcd16299e9342d563e
Jared Wasinger <j-wasinger@hotmail.com>
2024-11-26 19:14:19 +0700

    add copyright notices.  move v2 template to its own file.  Rename V2Backend->BackendV2

--- accounts/abi/bind/v2/backend.go
+++ accounts/abi/bind/v2/backend.go
@@ -1,3 +1,19 @@
+// Copyright 2024 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
 package v2
 
 import (
@@ -8,7 +24,7 @@ import (
 	"math/big"
 )
 
-type V2Backend interface {
+type BackendV2 interface {
 	SuggestGasPrice(ctx context.Context) (*big.Int, error)
 	PendingCodeAt(ctx context.Context, account common.Address) ([]byte, error)
 	PendingNonceAt(ctx context.Context, account common.Address) (uint64, error)
