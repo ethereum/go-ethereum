535cc44a68bb6c7f7a294c70b2fd47a14fc87b97
lightclient <lightclient@protonmail.com>
2024-11-26 17:18:02 -0700

    core: remove resolve code mechanism from statedb, put in vm

--- core/vm/interface.go
+++ core/vm/interface.go
@@ -45,9 +45,6 @@ type StateDB interface {
 	SetCode(common.Address, []byte)
 	GetCodeSize(common.Address) int
 
-	ResolveCodeHash(common.Address) common.Hash
-	ResolveCode(common.Address) []byte
-
 	AddRefund(uint64)
 	SubRefund(uint64)
 	GetRefund() uint64
