b2be754647da6a318a16dd07c4d178a45536061f
Daniel Liu <liudaniel@qq.com>
2024-11-29 16:08:31 +0800

    crypto/bn256: fix import line (#20723)

--- crypto/bn256/bn256_slow.go
+++ crypto/bn256/bn256_slow.go
@@ -14,12 +14,13 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
+//go:build !amd64 && !arm64
 // +build !amd64,!arm64
 
 // Package bn256 implements the Optimal Ate pairing over a 256-bit Barreto-Naehrig curve.
 package bn256
 
-import "github.com/XinFinOrg/XDPoSChain/crypto/bn256/google"
+import bn256 "github.com/XinFinOrg/XDPoSChain/crypto/bn256/google"
 
 // G1 is an abstract cyclic group. The zero value is suitable for use as the
 // output of an operation, but cannot be used as an input.
