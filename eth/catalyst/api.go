67b6bdc80886c7a5977bd59bbc77a10406948ddc
Jihoon Song <jihoonsong@users.noreply.github.com>
2024-11-30 17:58:06 +0900

    eth/catalyst: add `engine_updatePayloadWithInclusionListV1`

--- eth/catalyst/api.go
+++ eth/catalyst/api.go
@@ -100,6 +100,7 @@ var caps = []string{
 	"engine_getBlobsV1",
 	"engine_getBlobsV2",
 	"engine_getInclusionListV1",
+	"engine_updatePayloadWithInclusionListV1",
 	"engine_newPayloadV1",
 	"engine_newPayloadV2",
 	"engine_newPayloadV3",
@@ -597,6 +598,22 @@ func (api *ConsensusAPI) GetInclusionListV1(parentHash common.Hash) (engine.Incl
 	api.localInclusionLists.put(parentHash, inclusionList)
 
 	return inclusionList, nil
+}
+
+func (api *ConsensusAPI) UpdatePayloadWithInclusionListV1(payloadID engine.PayloadID, inclusionList engine.InclusionList) (*engine.PayloadID, error) {
+	payload := api.localBlocks.peek(payloadID)
+	if payload == nil {
+		return nil, engine.UnknownPayload
+	}
+
+	inclusionListTxs, err := engine.InclusionListToTransactions(inclusionList)
+	if err != nil {
+		return nil, err
+	}
+
+	payload.UpdateWithInclusionList(inclusionListTxs)
+
+	return &payloadID, nil
 }
 
 // Helper for NewPayload* methods.
