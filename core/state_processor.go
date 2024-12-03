3c1ae4d4b3681d923e1d97186c432c065597c27d
Sina Mahmoodi <itz.s1na@gmail.com>
2024-12-03 14:53:42 +0100

    minor refactor

--- core/state_processor.go
+++ core/state_processor.go
@@ -23,6 +23,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/consensus/misc"
 	"github.com/ethereum/go-ethereum/core/state"
+	"github.com/ethereum/go-ethereum/core/tracing"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/ethereum/go-ethereum/crypto"
@@ -221,11 +222,7 @@ func ApplyTransaction(evm *vm.EVM, gp *GasPool, statedb *state.StateDB, header *
 // contract. This method is exported to be used in tests.
 func ProcessBeaconBlockRoot(beaconRoot common.Hash, evm *vm.EVM) {
 	if tracer := evm.Config.Tracer; tracer != nil {
-		if tracer.OnSystemCallStartV2 != nil {
-			tracer.OnSystemCallStartV2(evm.GetVMContext())
-		} else if tracer.OnSystemCallStart != nil {
-			tracer.OnSystemCallStart()
-		}
+		onSystemCallStart(tracer, evm.GetVMContext())
 		if tracer.OnSystemCallEnd != nil {
 			defer tracer.OnSystemCallEnd()
 		}
@@ -249,11 +246,7 @@ func ProcessBeaconBlockRoot(beaconRoot common.Hash, evm *vm.EVM) {
 // as per EIP-2935.
 func ProcessParentBlockHash(prevHash common.Hash, evm *vm.EVM) {
 	if tracer := evm.Config.Tracer; tracer != nil {
-		if tracer.OnSystemCallStartV2 != nil {
-			tracer.OnSystemCallStartV2(evm.GetVMContext())
-		} else if tracer.OnSystemCallStart != nil {
-			tracer.OnSystemCallStart()
-		}
+		onSystemCallStart(tracer, evm.GetVMContext())
 		if tracer.OnSystemCallEnd != nil {
 			defer tracer.OnSystemCallEnd()
 		}
@@ -287,11 +280,7 @@ func ProcessConsolidationQueue(evm *vm.EVM) []byte {
 
 func processRequestsSystemCall(evm *vm.EVM, requestType byte, addr common.Address) []byte {
 	if tracer := evm.Config.Tracer; tracer != nil {
-		if tracer.OnSystemCallStartV2 != nil {
-			tracer.OnSystemCallStartV2(evm.GetVMContext())
-		} else if tracer.OnSystemCallStart != nil {
-			tracer.OnSystemCallStart()
-		}
+		onSystemCallStart(tracer, evm.GetVMContext())
 		if tracer.OnSystemCallEnd != nil {
 			defer tracer.OnSystemCallEnd()
 		}
@@ -330,4 +319,12 @@ func ParseDepositLogs(logs []*types.Log, config *params.ChainConfig) ([]byte, er
 		}
 	}
 	return deposits, nil
+}
+
+func onSystemCallStart(tracer *tracing.Hooks, ctx *tracing.VMContext) {
+	if tracer.OnSystemCallStartV2 != nil {
+		tracer.OnSystemCallStartV2(ctx)
+	} else if tracer.OnSystemCallStart != nil {
+		tracer.OnSystemCallStart()
+	}
 }
