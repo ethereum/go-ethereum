e303ae974b11f9d4a9601458b50f12c6be7657ae
Martin Holst Swende <martin@swende.se>
2024-12-03 09:04:56 +0100

    cmd/evm: post-rebase fixup

--- cmd/evm/runner.go
+++ cmd/evm/runner.go
@@ -245,8 +245,6 @@ func runCmd(ctx *cli.Context) error {
 	sdb := state.NewDatabase(triedb, nil)
 	prestate, _ = state.New(genesis.Root(), sdb)
 	chainConfig = genesisConfig.Config
-	statedb.Snapshot()
-	defer statedb.DiscardSnapshot()
 	if ctx.String(SenderFlag.Name) != "" {
 		sender = common.HexToAddress(ctx.String(SenderFlag.Name))
 	}
