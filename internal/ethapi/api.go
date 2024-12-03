d2fe42ba8f4ed4c374d4e0c3b8b696760c601ed7
colin <102356659+colinlyguo@users.noreply.github.com>
2024-12-03 13:32:37 +0700

    internal/ethapi: eth_call block parameter is optional (#28165) (#1095)
    * internal/ethapi: eth_call block parameter is optional (#28165)
    
    So apparently in the spec the base block parameter of eth_call is optional.
    I agree that "latest" is a sane default for this that most people would use.
    
    * chore: auto version bump [bot]
    
    ---------
    
    Co-authored-by: Sina Mahmoodi <1591639+s1na@users.noreply.github.com>
    Co-authored-by: colinlyguo <colinlyguo@users.noreply.github.com>

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1087,8 +1087,12 @@ func (e *revertError) ErrorData() interface{} {
 //
 // Note, this function doesn't make and changes in the state/blockchain and is
 // useful to execute and retrieve values.
-func (s *PublicBlockChainAPI) Call(ctx context.Context, args TransactionArgs, blockNrOrHash rpc.BlockNumberOrHash, overrides *StateOverride) (hexutil.Bytes, error) {
-	result, err := DoCall(ctx, s.b, args, blockNrOrHash, overrides, s.b.RPCEVMTimeout(), s.b.RPCGasCap())
+func (s *PublicBlockChainAPI) Call(ctx context.Context, args TransactionArgs, blockNrOrHash *rpc.BlockNumberOrHash, overrides *StateOverride) (hexutil.Bytes, error) {
+	if blockNrOrHash == nil {
+		latest := rpc.BlockNumberOrHashWithNumber(rpc.LatestBlockNumber)
+		blockNrOrHash = &latest
+	}
+	result, err := DoCall(ctx, s.b, args, *blockNrOrHash, overrides, s.b.RPCEVMTimeout(), s.b.RPCGasCap())
 	if err != nil {
 		return nil, err
 	}
