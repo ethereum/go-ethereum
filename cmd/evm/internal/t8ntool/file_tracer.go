4f8c66c50a7c352bc3819cbea2db0c2305809b75
Martin Holst Swende <martin@swende.se>
2024-12-06 10:56:20 +0100

    cmd/evm: better design on outputs

--- cmd/evm/internal/t8ntool/file_tracer.go
+++ cmd/evm/internal/t8ntool/file_tracer.go
@@ -17,73 +17,107 @@
 package t8ntool
 
 import (
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/tracing"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/eth/tracers"
-	"github.com/ethereum/go-ethereum/eth/tracers/logger"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/params"
-
 	"encoding/json"
 	"fmt"
 	"io"
 	"math/big"
 	"os"
 	"path/filepath"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/tracing"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/eth/tracers"
+	"github.com/ethereum/go-ethereum/log"
 )
 
 // fileWritingTracer wraps either a tracer or a logger. On tx start,
 // it instantiates a tracer/logger, creates a new file to direct output to,
 // and on tx end it closes the file.
 type fileWritingTracer struct {
-	txIndex     int
-	inner       *tracing.Hooks
-	destination io.WriteCloser
-	baseDir     string
-
-	// for json-tracing
-	logConfig *logger.Config
+	txIndex     int            // transaction counter
+	inner       *tracing.Hooks // inner hooks
+	destination io.WriteCloser // the currently open file (if any)
+	baseDir     string         // baseDir to write output-files to
+	suffix      string         // suffix is the suffix to use when creating files
 
 	// for custom tracing
-	tracerName  string
-	tracerConf  json.RawMessage
-	chainConfig *params.ChainConfig
-	getResult   func() (json.RawMessage, error)
+	getResult func() (json.RawMessage, error)
 }
 
-// jsonToFile creates hooks which uses an underlying jsonlogger, and writes the
-// jsonl-delimited output to a file, one per tx.
-func jsonToFile(baseDir string, logConfig *logger.Config, callFrames bool) *tracing.Hooks {
+func (l *fileWritingTracer) Write(p []byte) (n int, err error) {
+	if l.destination != nil {
+		return l.destination.Write(p)
+	}
+	log.Warn("Tracer wrote to non-existing output")
+	// It is tempting to return an error here, however, the json encoder
+	// will no retry writing to an io.Writer once it has returned an error once.
+	// Therefore, we must squash the error.
+	return n, nil
+}
+
+// newFileWriter creates a set of hooks which wraps inner hooks (typically a logger),
+// and writes the output to a file, one file per transaction.
+func newFileWriter(baseDir string, innerFn func(out io.Writer) *tracing.Hooks) *tracing.Hooks {
+	t := &fileWritingTracer{
+		baseDir: baseDir,
+		suffix:  "jsonl",
+	}
+	t.inner = innerFn(t) // instantiate the inner tracer
+	return t.hooks()
+}
+
+// newResultWriter creates a set of hooks wraps and invokes an underlying tracer,
+// and writes the result (getResult-output) to file, one per transaction.
+func newResultWriter(baseDir string, tracer *tracers.Tracer) *tracing.Hooks {
 	t := &fileWritingTracer{
 		baseDir:   baseDir,
-		logConfig: logConfig,
-	}
-	hooks := t.hooks()
-	if !callFrames {
-		hooks.OnEnter = nil
-	}
-	return hooks
-}
-
-// tracerToFile creates hooks which uses an underlying tracer, and writes the
-// json-result to file, one per tx.
-func tracerToFile(baseDir, tracerName string, traceConfig json.RawMessage, chainConfig *params.ChainConfig) *tracing.Hooks {
-	t := &fileWritingTracer{
-		baseDir:     baseDir,
-		tracerName:  tracerName,
-		chainConfig: chainConfig,
+		getResult: tracer.GetResult,
+		inner:     tracer.Hooks,
+		suffix:    "json",
 	}
 	return t.hooks()
 }
 
+// OnTxStart creates a new output-file specific for this transaction, and invokes
+// the inner OnTxStart handler.
+func (l *fileWritingTracer) OnTxStart(env *tracing.VMContext, tx *types.Transaction, from common.Address) {
+	// Open a new file,
+	fname := filepath.Join(l.baseDir, fmt.Sprintf("trace-%d-%v.%v", l.txIndex, tx.Hash().String(), l.suffix))
+	traceFile, err := os.Create(fname)
+	if err != nil {
+		log.Warn("Failed creating trace-file", "err", err)
+	}
+	log.Info("Created tracing-file", "path", fname)
+	l.destination = traceFile
+
+	if l.inner.OnTxStart != nil {
+		l.inner.OnTxStart(env, tx, from)
+	}
+}
+
+// OnTxEnd writes result (if getResult exist), closes any currently open output-file,
+// and invokes the inner OnTxEnd handler.
+func (l *fileWritingTracer) OnTxEnd(receipt *types.Receipt, err error) {
+	if l.inner.OnTxEnd != nil {
+		l.inner.OnTxEnd(receipt, err)
+	}
+	if l.getResult != nil && l.destination != nil {
+		if result, err := l.getResult(); result != nil {
+			json.NewEncoder(l.destination).Encode(result)
+		} else {
+			log.Warn("Error obtaining tracer result", "err", err)
+		}
+		l.destination.Close()
+		l.destination = nil
+	}
+	l.txIndex++
+}
+
 func (l *fileWritingTracer) hooks() *tracing.Hooks {
-	hooks := &tracing.Hooks{
-		OnTxStart: l.OnTxStartJSONL,
+	return &tracing.Hooks{
+		OnTxStart: l.OnTxStart,
 		OnTxEnd:   l.OnTxEnd,
-		// intentional no-op: we instantiate the l.inner on tx start, which has
-		// not yet happened at this point
-		//OnSystemCallStart: func() {},
 		OnEnter: func(depth int, typ byte, from common.Address, to common.Address, input []byte, gas uint64, value *big.Int) {
 			if l.inner != nil && l.inner.OnEnter != nil {
 				l.inner.OnEnter(depth, typ, from, to, input, gas, value)
@@ -104,66 +138,15 @@ func (l *fileWritingTracer) hooks() *tracing.Hooks {
 				l.inner.OnFault(pc, op, gas, cost, scope, depth, err)
 			}
 		},
+		OnSystemCallStart: func() {
+			if l.inner != nil && l.inner.OnSystemCallStart != nil {
+				l.inner.OnSystemCallStart()
+			}
+		},
+		OnSystemCallEnd: func() {
+			if l.inner != nil && l.inner.OnSystemCallEnd != nil {
+				l.inner.OnSystemCallEnd()
+			}
+		},
 	}
-	if len(l.tracerName) > 0 { // a custom tracer
-		hooks.OnTxStart = l.OnTxStartJSON
-	}
-	return hooks
-}
-
-// OnTxStartJSONL is the OnTxStart-handler for jsonl logger.
-func (l *fileWritingTracer) OnTxStartJSONL(env *tracing.VMContext, tx *types.Transaction, from common.Address) {
-	// Open a new file,
-	fname := filepath.Join(l.baseDir, fmt.Sprintf("trace-%d-%v.jsonl", l.txIndex, tx.Hash().String()))
-	traceFile, err := os.Create(fname)
-	if err != nil {
-		log.Warn("Failed creating trace-file", "err", err)
-	}
-	log.Debug("Created tracing-file", "path", fname)
-	l.destination = traceFile
-	l.inner = logger.NewJSONLoggerWithCallFrames(l.logConfig, traceFile)
-	if l.inner.OnTxStart != nil {
-		l.inner.OnTxStart(env, tx, from)
-	}
-}
-
-// OnTxStartJSONL is the OnTxStart-handler for custom tracer.
-func (l *fileWritingTracer) OnTxStartJSON(env *tracing.VMContext, tx *types.Transaction, from common.Address) {
-	// Open a new file,
-	fname := filepath.Join(l.baseDir, fmt.Sprintf("trace-%d-%v.json", l.txIndex, tx.Hash().String()))
-	traceFile, err := os.Create(fname)
-	if err != nil {
-		log.Warn("Failed creating trace-file", "err", err)
-	}
-	fmt.Printf("Created tracing-file %v\n", fname)
-	log.Info("Created tracing-file", "path", fname)
-	l.destination = traceFile
-	inner, err := tracers.DefaultDirectory.New(l.tracerName, nil, l.tracerConf, l.chainConfig)
-	if err != nil {
-		log.Warn("Failed instantiating tracer", "err", err)
-		return
-	}
-	l.getResult = inner.GetResult
-	l.inner = inner.Hooks
-	if l.inner.OnTxStart != nil {
-		l.inner.OnTxStart(env, tx, from)
-	}
-}
-
-func (l *fileWritingTracer) OnTxEnd(receipt *types.Receipt, err error) {
-	if l.inner.OnTxEnd != nil {
-		l.inner.OnTxEnd(receipt, err)
-	}
-	if l.getResult != nil {
-		if result, err := l.getResult(); result != nil {
-			json.NewEncoder(l.destination).Encode(result)
-		} else {
-			log.Warn("Error obtaining tracer result", "err", err)
-		}
-	}
-	if l.destination != nil { // Close old file
-		l.destination.Close()
-		l.destination = nil
-	}
-	l.txIndex++
 }
