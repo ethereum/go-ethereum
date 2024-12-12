3a983d3b3a569f5429e6d9fd57e409f5b262f551
Felix Lange <fjl@twurst.com>
2024-12-13 00:14:16 +0100

    p2p: add test for dialing with DNS hostname

--- p2p/dial.go
+++ p2p/dial.go
@@ -490,6 +490,7 @@ func (d *dialScheduler) dnsResolveHostname(n *enode.Node) (*enode.Node, error) {
 	if foundIP6.IsValid() {
 		rec.Set(enr.IPv6Addr(foundIP6))
 	}
+	rec.SetSeq(n.Seq()) // ensure seq not bumped by update
 	newNode := enode.SignNull(rec, n.ID()).WithHostname(n.Hostname())
 	d.log.Trace("Node updated from DNS lookup", "id", n.ID(), "name", n.Hostname(), "ip", newNode.IP())
 	return newNode, nil
