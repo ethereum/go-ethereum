86f89b31bd105b10ddc3246b1ed2171651579c26
Felix Lange <fjl@twurst.com>
2024-12-17 19:47:10 +0100

    core/types, internal/ethapi: fixes for prague RPC encoding (#30926)
    Fixing some issues I found while regenerating RPC tests for Prague:
    
    - Authorization signature values were not encoded as hex
    - `requestsRoot` in block should be `requestsHash`
    - `authorizationList` should work for `eth_call`

--- core/types/tx_setcode.go
+++ core/types/tx_setcode.go
@@ -83,6 +83,8 @@ type authorizationMarshaling struct {
 	ChainID hexutil.Uint64
 	Nonce   hexutil.Uint64
 	V       hexutil.Uint64
+	R       hexutil.U256
+	S       hexutil.U256
 }
 
 // SignAuth signs the provided authorization.
