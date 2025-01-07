8433ccd2afc8cae5e3c8e52ba3658b10a84efd00
nikolaivanovj <110764167+nikolaivanovj@users.noreply.github.com>
2025-01-07 18:23:13 +0200

    Update deploy.js

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -5,7 +5,15 @@ async function main() {
   const Token = await ethers.getContractFactory("Token");
   const token = await Token.deploy();
 
-  console.log("Deploy transaction hash:", token.deployTransaction.hash);
+  // Log the token object to inspect its properties
+  console.log("Token object:", token);
+
+  if (token.deployTransaction) {
+    console.log("Deploy transaction hash:", token.deployTransaction.hash);
+  } else {
+    console.error("Deploy transaction is undefined");
+  }
+
   await token.deployed();
 
   console.log("Token deployed to:", token.address);
