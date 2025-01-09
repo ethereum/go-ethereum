8570d18f105443b7df1da69edace1fc9da39feac
Gary Rong <garyrong0905@gmail.com>
2025-01-09 13:25:41 +0800

    triedb/pathdb: configure different node hasher in pathdb
    As the node hash scheme in verkle and merkle are totally different,
    the original default node hasher in pathdb is no longer suitable.
    Therefore, this pull request configures different node hasher
    respectively.

--- triedb/pathdb/database_test.go
+++ triedb/pathdb/database_test.go
@@ -458,8 +458,8 @@ func TestDatabaseRecoverable(t *testing.T) {
 		// Initial state should be recoverable
 		{types.EmptyRootHash, true},
 
-		// Initial state should be recoverable
-		{common.Hash{}, true},
+		// common.Hash{} is not a valid state root for revert
+		{common.Hash{}, false},
 
 		// Layers below current disk layer are recoverable
 		{tester.roots[index-1], true},
