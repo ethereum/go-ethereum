a0a1322d844a0c8a8a2b80fe75c611a5bb71ef72
Daniel Liu <liudaniel@qq.com>
2025-01-14 10:56:17 +0800

    accounts/usbwallet/trezor: upgrade to generate with protoc 27.1 (#30058)

--- accounts/usbwallet/trezor/trezor.go
+++ accounts/usbwallet/trezor/trezor.go
@@ -42,7 +42,7 @@
 //   - Grab the latest Go plugin `go get -u github.com/golang/protobuf/protoc-gen-go`
 //   - Vendor in the latest Go plugin `govendor fetch github.com/golang/protobuf/...`
 
-//go:generate protoc -I/usr/local/include:. --go_out=import_path=trezor:. messages.proto messages-common.proto messages-management.proto messages-ethereum.proto
+//go:generate protoc -I/usr/local/include:. --go_out=paths=source_relative:. messages.proto messages-common.proto messages-management.proto messages-ethereum.proto
 
 // Package trezor contains the wire protocol.
 package trezor
