9dda5b9819f401fed3aa24831088d5191651c987
Daniel Liu <liudaniel@qq.com>
2025-01-14 10:56:14 +0800

    accounts: more linters (#24783)

--- accounts/keystore/passphrase_test.go
+++ accounts/keystore/passphrase_test.go
@@ -52,7 +52,7 @@ func TestKeyEncryptDecrypt(t *testing.T) {
 			t.Errorf("test %d: key address mismatch: have %x, want %x", i, key.Address, address)
 		}
 		// Recrypt with a new password and start over
-		password += "new data appended"
+		password += "new data appended" // nolint: gosec
 		if keyjson, err = EncryptKey(key, password, veryLightScryptN, veryLightScryptP); err != nil {
 			t.Errorf("test %d: failed to recrypt key %v", i, err)
 		}
