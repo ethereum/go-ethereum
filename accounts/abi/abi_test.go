4c7507af85107aa035ef503bd6a58912644f6665
Daniel Liu <liudaniel@qq.com>
2025-01-14 10:56:15 +0800

    account/abi: handle solidity panic revert (#27868)
    See  https://docs.soliditylang.org/en/v0.8.21/control-structures.html#panic-via-assert-and-error-via-require

--- accounts/abi/abi_test.go
+++ accounts/abi/abi_test.go
@@ -992,37 +992,6 @@ func TestABI_MethodById(t *testing.T) {
 	}
 }
 
-func TestUnpackRevert(t *testing.T) {
-	t.Parallel()
-
-	var cases = []struct {
-		input     string
-		expect    string
-		expectErr error
-	}{
-		{"", "", errors.New("invalid data for unpacking")},
-		{"08c379a1", "", errors.New("invalid data for unpacking")},
-		{"08c379a00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000d72657665727420726561736f6e00000000000000000000000000000000000000", "revert reason", nil},
-	}
-	for index, c := range cases {
-		t.Run(fmt.Sprintf("case %d", index), func(t *testing.T) {
-			got, err := UnpackRevert(common.Hex2Bytes(c.input))
-			if c.expectErr != nil {
-				if err == nil {
-					t.Fatalf("Expected non-nil error")
-				}
-				if err.Error() != c.expectErr.Error() {
-					t.Fatalf("Expected error mismatch, want %v, got %v", c.expectErr, err)
-				}
-				return
-			}
-			if c.expect != got {
-				t.Fatalf("Output mismatch, want %v, got %v", c.expect, got)
-			}
-		})
-	}
-}
-
 func TestABI_EventById(t *testing.T) {
 	tests := []struct {
 		name  string
@@ -1191,5 +1160,38 @@ func TestUnnamedEventParam(t *testing.T) {
 	}
 	if event.Inputs[1].Name != "arg1" {
 		t.Fatalf("Could not find input")
+	}
+}
+
+func TestUnpackRevert(t *testing.T) {
+	t.Parallel()
+
+	var cases = []struct {
+		input     string
+		expect    string
+		expectErr error
+	}{
+		{"", "", errors.New("invalid data for unpacking")},
+		{"08c379a1", "", errors.New("invalid data for unpacking")},
+		{"08c379a00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000d72657665727420726561736f6e00000000000000000000000000000000000000", "revert reason", nil},
+		{"4e487b710000000000000000000000000000000000000000000000000000000000000000", "generic panic", nil},
+		{"4e487b7100000000000000000000000000000000000000000000000000000000000000ff", "unknown panic code: 0xff", nil},
+	}
+	for index, c := range cases {
+		t.Run(fmt.Sprintf("case %d", index), func(t *testing.T) {
+			got, err := UnpackRevert(common.Hex2Bytes(c.input))
+			if c.expectErr != nil {
+				if err == nil {
+					t.Fatalf("Expected non-nil error")
+				}
+				if err.Error() != c.expectErr.Error() {
+					t.Fatalf("Expected error mismatch, want %v, got %v", c.expectErr, err)
+				}
+				return
+			}
+			if c.expect != got {
+				t.Fatalf("Output mismatch, want %v, got %v", c.expect, got)
+			}
+		})
 	}
 }
