c032eed8f80d4186252e47cd3f9447411f7851be
Daniel Liu <liudaniel@qq.com>
2025-01-14 10:56:16 +0800

    accounts/abi/bind: fixed typos (#28634)
    * Update auth.go
    
    * Update backend.go
    
    * Update bind.go
    
    * Update bind_test.go

--- accounts/abi/bind/backend.go
+++ accounts/abi/bind/backend.go
@@ -75,7 +75,7 @@ type BlockHashContractCaller interface {
 	// CodeAtHash returns the code of the given account in the state at the specified block hash.
 	CodeAtHash(ctx context.Context, contract common.Address, blockHash common.Hash) ([]byte, error)
 
-	// CallContractAtHash executes an Ethereum contract all against the state at the specified block hash.
+	// CallContractAtHash executes an Ethereum contract call against the state at the specified block hash.
 	CallContractAtHash(ctx context.Context, call ethereum.CallMsg, blockHash common.Hash) ([]byte, error)
 }
 
