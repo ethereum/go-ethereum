57598e18259fe2f2fcaff6950319c270c58cb213
Daniel Liu <liudaniel@qq.com>
2025-01-14 10:56:15 +0800

    accounts/abi/bind/backend: use requested header for gas prices and gas limits (#28280)

--- accounts/abi/bind/backends/simulated.go
+++ accounts/abi/bind/backends/simulated.go
@@ -674,7 +674,7 @@ func (b *SimulatedBackend) callContract(ctx context.Context, call ethereum.CallM
 	}
 	// Ensure message is initialized properly.
 	if call.Gas == 0 {
-		call.Gas = 50000000
+		call.Gas = 10 * head.GasLimit
 	}
 	if call.Value == nil {
 		call.Value = new(big.Int)
