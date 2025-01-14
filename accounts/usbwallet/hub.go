4955476db45862abf984aaddb4166351891ffdf9
Daniel Liu <liudaniel@qq.com>
2025-01-14 10:56:08 +0800

    accounts: prefer nil slices over zero-length slices (#19079)

--- accounts/usbwallet/hub.go
+++ accounts/usbwallet/hub.go
@@ -150,8 +150,10 @@ func (hub *Hub) refreshWallets() {
 	// Transform the current list of wallets into the new one
 	hub.stateLock.Lock()
 
-	wallets := make([]accounts.Wallet, 0, len(devices))
-	events := []accounts.WalletEvent{}
+	var (
+		wallets = make([]accounts.Wallet, 0, len(devices))
+		events  []accounts.WalletEvent
+	)
 
 	for _, device := range devices {
 		url := accounts.URL{Scheme: hub.scheme, Path: device.Path}
