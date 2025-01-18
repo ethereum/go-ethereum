5b57857ccd9d31131d223b31c969e6c03ad64df3
Gary Rong <garyrong0905@gmail.com>
2025-01-18 21:01:01 +0800

    core, eth, triedb: expose historic state to RPC

--- core/blockchain_reader.go
+++ core/blockchain_reader.go
@@ -370,6 +370,13 @@ func (bc *BlockChain) StateAt(root common.Hash) (*state.StateDB, error) {
 	return state.New(root, bc.statedb)
 }
 
+// HistoricState returns a historic state specified by the given root.
+// Live states are not available and won't be served, please use `State`
+// or `StateAt` instead.
+func (bc *BlockChain) HistoricState(root common.Hash) (*state.StateDB, error) {
+	return state.New(root, state.NewHistoricDatabase(bc.db, bc.triedb))
+}
+
 // Config retrieves the chain's fork configuration.
 func (bc *BlockChain) Config() *params.ChainConfig { return bc.chainConfig }
 
