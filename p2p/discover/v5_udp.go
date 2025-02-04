b1e8a7cac0b7876c132ad0912f0c64a20e820fd2
Chen Kai <281165273grape@gmail.com>
2025-02-04 11:33:29 +0800

    rename v5respTimeout->respTimeout
    Signed-off-by: Chen Kai <281165273grape@gmail.com>

--- p2p/discover/v5_udp.go
+++ p2p/discover/v5_udp.go
@@ -60,16 +60,16 @@ type codecV5 interface {
 // UDPv5 is the implementation of protocol version 5.
 type UDPv5 struct {
 	// static fields
-	conn          UDPConn
-	tab           *Table
-	netrestrict   *netutil.Netlist
-	priv          *ecdsa.PrivateKey
-	localNode     *enode.LocalNode
-	db            *enode.DB
-	log           log.Logger
-	clock         mclock.Clock
-	validSchemes  enr.IdentityScheme
-	v5respTimeout time.Duration
+	conn         UDPConn
+	tab          *Table
+	netrestrict  *netutil.Netlist
+	priv         *ecdsa.PrivateKey
+	localNode    *enode.LocalNode
+	db           *enode.DB
+	log          log.Logger
+	clock        mclock.Clock
+	validSchemes enr.IdentityScheme
+	respTimeout  time.Duration
 
 	// misc buffers used during message handling
 	logcontext []interface{}
@@ -149,15 +149,15 @@ func newUDPv5(conn UDPConn, ln *enode.LocalNode, cfg Config) (*UDPv5, error) {
 	cfg = cfg.withDefaults()
 	t := &UDPv5{
 		// static fields
-		conn:          newMeteredConn(conn),
-		localNode:     ln,
-		db:            ln.Database(),
-		netrestrict:   cfg.NetRestrict,
-		priv:          cfg.PrivateKey,
-		log:           cfg.Log,
-		validSchemes:  cfg.ValidSchemes,
-		clock:         cfg.Clock,
-		v5respTimeout: cfg.V5RespTimeout,
+		conn:         newMeteredConn(conn),
+		localNode:    ln,
+		db:           ln.Database(),
+		netrestrict:  cfg.NetRestrict,
+		priv:         cfg.PrivateKey,
+		log:          cfg.Log,
+		validSchemes: cfg.ValidSchemes,
+		clock:        cfg.Clock,
+		respTimeout:  cfg.V5RespTimeout,
 		// channels into dispatch
 		packetInCh:    make(chan ReadPacket, 1),
 		readNextCh:    make(chan struct{}, 1),
@@ -576,7 +576,7 @@ func (t *UDPv5) startResponseTimeout(c *callV5) {
 		timer mclock.Timer
 		done  = make(chan struct{})
 	)
-	timer = t.clock.AfterFunc(t.v5respTimeout, func() {
+	timer = t.clock.AfterFunc(t.respTimeout, func() {
 		<-done
 		select {
 		case t.respTimeoutCh <- &callTimeout{c, timer}:
