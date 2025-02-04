d875add9c722fa40ed5cd0e476b9d922164a6db8
georgehao <haohongfan@gmail.com>
2025-02-04 17:45:42 +0800

    format

--- internal/syncx/mutex.go
+++ internal/syncx/mutex.go
@@ -15,6 +15,7 @@
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
 // Package syncx contains exotic synchronization primitives.
+
 package syncx
 
 // ClosableMutex is a mutex that can also be closed.
