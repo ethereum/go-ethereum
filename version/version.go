756cca7c6f1324a5acf6d39d94033a6a5c7ce4c4
Felix Lange <fjl@twurst.com>
2025-02-06 15:05:41 +0100

    version: release go-ethereum v1.15.0

--- version/version.go
+++ version/version.go
@@ -17,8 +17,8 @@
 package version
 
 const (
-	Major = 1          // Major version component of the current release
-	Minor = 15         // Minor version component of the current release
-	Patch = 0          // Patch version component of the current release
-	Meta  = "unstable" // Version metadata to append to the version string
+	Major = 1        // Major version component of the current release
+	Minor = 15       // Minor version component of the current release
+	Patch = 0        // Patch version component of the current release
+	Meta  = "stable" // Version metadata to append to the version string
 )
