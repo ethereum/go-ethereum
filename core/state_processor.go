fcdefd9e51a7479a255ee81261039de3d93a03af
Lucca Martins <lucca_martins30@yahoo.com.br>
2025-02-16 16:49:40 -0300

    including interrupt context back

--- core/state_processor.go
+++ core/state_processor.go
@@ -88,6 +88,13 @@ func (p *StateProcessor) Process(block *types.Block, statedb *state.StateDB, cfg
 	}
 	// Iterate over and process the individual transactions
 	for i, tx := range block.Transactions() {
+		if interruptCtx != nil {
+			select {
+			case <-interruptCtx.Done():
+				return nil, interruptCtx.Err()
+			default:
+			}
+		}
 		msg, err := TransactionToMessage(tx, signer, header.BaseFee)
 		if err != nil {
 			return nil, fmt.Errorf("could not apply tx %d [%v]: %w", i, tx.Hash().Hex(), err)
