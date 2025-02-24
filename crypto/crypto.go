31cc9b0d5f5f336ea1084ee5bf4d831bd7a25ca7
Marius van der Wijden <m.vanderwijden@live.de>
2025-02-24 12:23:25 +0100

    crypto: add comment to FromECDSAPub (#31241)
    closes https://github.com/ethereum/go-ethereum/issues/26240

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -184,6 +184,9 @@ func UnmarshalPubkey(pub []byte) (*ecdsa.PublicKey, error) {
 	return &ecdsa.PublicKey{Curve: S256(), X: x, Y: y}, nil
 }
 
+// FromECDSAPub converts a secp256k1 public key to bytes.
+// Note: it does not use the curve from pub, instead it always
+// encodes using secp256k1.
 func FromECDSAPub(pub *ecdsa.PublicKey) []byte {
 	if pub == nil || pub.X == nil || pub.Y == nil {
 		return nil
