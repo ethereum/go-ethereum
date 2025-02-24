dd6689a6c72bbdee0d18946b78450d30707bc77f
Lucca Martins <lucca_martins30@yahoo.com.br>
2025-02-24 18:00:16 -0300

    download dependencies to run script

--- integration-tests/rpc_test.sh
+++ integration-tests/rpc_test.sh
@@ -5,5 +5,9 @@ signersDump=$(jq . $signersFile)
 privKey=$(echo "$signersDump" | jq -r ".[0].priv_key")
 rpc_url="http://localhost:8545"
 
+cd matic-cli/tests/rpc-tests
 
-go run matic-cli/tests/rpc-tests/main.go --priv-key $privKey --rpc-url $rpc_url
+go mod tidy
+go main.go --priv-key $privKey --rpc-url $rpc_url
+
+cd -
