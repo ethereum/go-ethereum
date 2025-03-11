1911a9f23c1ecd015dc9f47a4edd14f3b52e160f
Marius van der Wijden <m.vanderwijden@live.de>
2025-03-11 12:54:40 +0100

    crypto/kzg4844: add ComputeCells to compute cell proofs

--- crypto/kzg4844/kzg4844.go
+++ crypto/kzg4844/kzg4844.go
@@ -149,6 +149,17 @@ func VerifyBlobProof(blob *Blob, commitment Commitment, proof Proof) error {
 	return gokzgVerifyBlobProof(blob, commitment, proof)
 }
 
+// ComputeCells returns the KZG cell proofs that are used to verify the blob against
+// the commitment.
+//
+// This method does not verify that the commitment is correct with respect to blob.
+func ComputeCells(blob *Blob) ([]Proof, error) {
+	if useCKZG.Load() {
+		return ckzgComputeCells(blob)
+	}
+	return gokzgComputeCells(blob)
+}
+
 // CalcBlobHashV1 calculates the 'versioned blob hash' of a commitment.
 // The given hasher must be a sha256 hash instance, otherwise the result will be invalid!
 func CalcBlobHashV1(hasher hash.Hash, commit *Commitment) (vh [32]byte) {
