5abf406387c7b16954779cf8dd4ba87bc278eb5e
islishude <islishude@gmail.com>
2025-03-12 18:58:11 +0800

    cmd/utils: streamline developer account setup and unlocking logic

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -1726,25 +1726,28 @@ func SetEthConfig(ctx *cli.Context, stack *node.Node, cfg *ethconfig.Config) {
 			Fatalf("Keystore is not available")
 		}
 
+		isKeystoreNonEmpty := len(ks.Accounts()) > 0
 		// Figure out the dev account address.
 		// setEtherbase has been called above, configuring the miner address from command line flags.
 		if cfg.Miner.PendingFeeRecipient != (common.Address{}) {
 			developer = accounts.Account{Address: cfg.Miner.PendingFeeRecipient}
+		} else if isKeystoreNonEmpty {
+			developer = ks.Accounts()[0]
 		} else {
-			if accs := ks.Accounts(); len(accs) > 0 {
-				developer = ks.Accounts()[0]
-			} else {
-				developer, err = ks.NewAccount(passphrase)
-				if err != nil {
-					Fatalf("Failed to create developer account: %v", err)
-				}
+			developer, err = ks.NewAccount(passphrase)
+			if err != nil {
+				Fatalf("Failed to create developer account: %v", err)
 			}
+		}
+		// Make sure the address is configured as fee recipient, otherwise
+		// the miner will fail to start.
+		cfg.Miner.PendingFeeRecipient = developer.Address
+
+		// try to unlock the first keystore account
+		if isKeystoreNonEmpty {
 			if err := ks.Unlock(developer, passphrase); err != nil {
 				Fatalf("Failed to unlock developer account: %v", err)
 			}
-			// Make sure the address is configured as fee recipient, otherwise
-			// the miner will fail to start.
-			cfg.Miner.PendingFeeRecipient = developer.Address
 		}
 		log.Info("Using developer account", "address", developer.Address)
 
