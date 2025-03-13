c135c408b8919f3453ae9924519b3d94dbf765d1
Felix Lange <fjl@twurst.com>
2025-03-13 09:40:00 +0100

    internal/ethapi: fix test (for now)

--- internal/ethapi/api_test.go
+++ internal/ethapi/api_test.go
@@ -520,8 +520,12 @@ func (b testBackend) BlockByNumber(ctx context.Context, number rpc.BlockNumber) 
 	if number == rpc.PendingBlockNumber {
 		return b.pending, nil
 	}
+	if number == rpc.EarliestBlockNumber {
+		number = 0
+	}
 	return b.chain.GetBlockByNumber(uint64(number)), nil
 }
+
 func (b testBackend) BlockByHash(ctx context.Context, hash common.Hash) (*types.Block, error) {
 	return b.chain.GetBlockByHash(hash), nil
 }
