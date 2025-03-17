788b01ac7571df8a6b80027223d3136199da5b49
jwasinger <j-wasinger@hotmail.com>
2025-03-17 14:38:57 +0100

    Update eth/catalyst/simulated_beacon.go
    Co-authored-by: Marius van der Wijden <m.vanderwijden@live.de>

--- eth/catalyst/simulated_beacon.go
+++ eth/catalyst/simulated_beacon.go
@@ -126,7 +126,7 @@ func NewSimulatedBeacon(period uint64, feeRecipient common.Address, eth *eth.Eth
 	}
 
 	// cap the dev mode period to a reasonable maximum value to avoid overflowing the time.Duration (int64) that it will occupy
-	var maxPeriod uint64 = 9223372036 // math.Floor(math.MaxInt64 / 1000000000)
+	var maxPeriod uint64 = math.Floor(math.MaxInt64 / 1_000_000_000)
 	if period > maxPeriod {
 		log.Warn(fmt.Sprintf("period exceeds maximum possible value (have: %d, max: %d).  Sanitizing period to maximum possible value.", period, maxPeriod))
 		period = maxPeriod
