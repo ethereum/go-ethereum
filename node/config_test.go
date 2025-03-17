4f8dd4eb7e12d1c8a1a5f16c858d696c0767325b
Daniel Liu <liudaniel@qq.com>
2025-03-17 11:29:20 +0800

    core/txpool, crypto, node: fix os.CreateTemp in test (#31172)

--- node/config_test.go
+++ node/config_test.go
@@ -53,7 +53,7 @@ func TestDatadirCreation(t *testing.T) {
 		t.Fatalf("freshly created datadir not accessible: %v", err)
 	}
 	// Verify that an impossible datadir fails creation
-	file, err := os.CreateTemp("", "")
+	file, err := os.CreateTemp(t.TempDir(), "")
 	if err != nil {
 		t.Fatalf("failed to create temporary file: %v", err)
 	}
