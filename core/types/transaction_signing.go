c3f5dde93840956cbd1d33fc74debefb0d0bbe05
Felix Lange <fjl@twurst.com>
2025-03-19 11:26:44 +0100

    core/types: add comment

--- core/types/transaction_signing.go
+++ core/types/transaction_signing.go
@@ -181,6 +181,7 @@ type Signer interface {
 }
 
 // modernSigner is the signer implementation that handles non-legacy transaction types.
+// For legacy transactions, it defers to one of the legacy signers (frontier, homestead, eip155).
 type modernSigner struct {
 	txtypes map[byte]struct{}
 	chainID *big.Int
