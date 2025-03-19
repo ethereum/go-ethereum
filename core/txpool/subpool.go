6255bba524949abc66559208cc6a196056e2b2bb
Bui Quang Minh <minh.bui@skymavis.com>
2025-03-19 15:25:26 +0700

    core/txpool: add GetMetadata to transaction pool
    With eth/68, transaction announcement must have transaction type and size. So
    in announceTransactions, we need to query the transaction from transaction pool
    with its hash. This creates overhead in case of blob transaction which needs to
    load data from billy and RLP decode. This commit creates a lightweight lookup
    from transaction hash to transaction size and a function GetMetadata to query
    transaction type and transaction size given the transaction hash.

--- core/txpool/subpool.go
+++ core/txpool/subpool.go
@@ -86,6 +86,11 @@ type PendingFilter struct {
 	OnlyBlobTxs  bool // Return only blob transactions (block blob-space filling)
 }
 
+type TxMetadata struct {
+	Type uint8
+	Size uint64
+}
+
 // SubPool represents a specialized transaction pool that lives on its own (e.g.
 // blob pool). Since independent of how many specialized pools we have, they do
 // need to be updated in lockstep and assemble into one coherent view for block
@@ -126,6 +131,10 @@ type SubPool interface {
 
 	// GetRLP returns a RLP-encoded transaction if it is contained in the pool.
 	GetRLP(hash common.Hash) []byte
+
+	// GetMetadata returns the transaction type and transaction size with the given
+	// hash.
+	GetMetadata(hash common.Hash) *TxMetadata
 
 	// GetBlobs returns a number of blobs are proofs for the given versioned hashes.
 	// This is a utility method for the engine API, enabling consensus clients to
