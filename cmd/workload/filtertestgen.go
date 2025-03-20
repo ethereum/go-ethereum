03cc2942c2c25208dac1b7c8fc1bc19f3d72fd97
Felföldi Zsolt <zsfelfoldi@gmail.com>
2025-03-20 09:23:10 +0100

    cmd/workload: fixed filter test request error handling (#31424)
    This PR fixes the broken request error handling of the workload filter
    tests. Until now `validateHistoryPruneErr` was invoked with `fq.Err` as
    an input which was always nil and a timeout or http error was reported
    as a result content mismatch.
    Also, in case of `errPrunedHistory` it is wrong to return here without
    setting an error because then it will look like a valid empty result and
    the check will later fail. So instead `errPrunedHistory` is always
    returned now (without printing an error message) and the callers of
    `run` should handle this special case (typically ignore silently).

--- cmd/workload/filtertestgen.go
+++ cmd/workload/filtertestgen.go
@@ -40,7 +40,6 @@ var (
 		Action:    filterGenCmd,
 		Flags: []cli.Flag{
 			filterQueryFileFlag,
-			filterErrorFileFlag,
 		},
 	}
 	filterQueryFileFlag = &cli.StringFlag{
@@ -72,8 +71,8 @@ func filterGenCmd(ctx *cli.Context) error {
 		query := f.newQuery()
 		query.run(f.client, nil)
 		if query.Err != nil {
-			f.errors = append(f.errors, query)
-			continue
+			query.printError()
+			exit("filter query failed")
 		}
 		if len(query.results) > 0 && len(query.results) <= maxFilterResultSize {
 			for {
@@ -90,8 +89,8 @@ func filterGenCmd(ctx *cli.Context) error {
 					)
 				}
 				if extQuery.Err != nil {
-					f.errors = append(f.errors, extQuery)
-					break
+					extQuery.printError()
+					exit("filter query failed")
 				}
 				if len(extQuery.results) > maxFilterResultSize {
 					break
@@ -101,7 +100,6 @@ func filterGenCmd(ctx *cli.Context) error {
 			f.storeQuery(query)
 			if time.Since(lastWrite) > time.Second*10 {
 				f.writeQueries()
-				f.writeErrors()
 				lastWrite = time.Now()
 			}
 		}
@@ -112,18 +110,15 @@ func filterGenCmd(ctx *cli.Context) error {
 type filterTestGen struct {
 	client    *client
 	queryFile string
-	errorFile string
 
 	finalizedBlock int64
 	queries        [filterBuckets][]*filterQuery
-	errors         []*filterQuery
 }
 
 func newFilterTestGen(ctx *cli.Context) *filterTestGen {
 	return &filterTestGen{
 		client:    makeClient(ctx),
 		queryFile: ctx.String(filterQueryFileFlag.Name),
-		errorFile: ctx.String(filterErrorFileFlag.Name),
 	}
 }
 
@@ -358,17 +353,6 @@ func (s *filterTestGen) writeQueries() {
 	}
 	json.NewEncoder(file).Encode(&s.queries)
 	file.Close()
-}
-
-// writeQueries serializes the generated errors to the error file.
-func (s *filterTestGen) writeErrors() {
-	file, err := os.Create(s.errorFile)
-	if err != nil {
-		exit(fmt.Errorf("Error creating filter error file %s: %v", s.errorFile, err))
-		return
-	}
-	defer file.Close()
-	json.NewEncoder(file).Encode(s.errors)
 }
 
 func mustGetFinalizedBlock(client *client) int64 {
