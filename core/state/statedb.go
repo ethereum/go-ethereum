2b1486186f08eb3bbb74b8eb968f182881041861
Delweng <delweng@gmail.com>
2025-03-26 12:59:40 +0800

    core/state: fix double-increment of accountLoaded counter (#31493)

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -600,7 +600,6 @@ func (s *StateDB) getStateObject(addr common.Address) *stateObject {
 	// Insert into the live set
 	obj := newObject(s, addr, acct)
 	s.setStateObject(obj)
-	s.AccountLoaded++
 	return obj
 }
 
