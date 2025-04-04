337633cbd6768bbf0b0272f701f0a596e43afb26
Sina M <1591639+s1na@users.noreply.github.com>
2025-04-04 14:07:46 +0200

    internal/era: random access to header and receipts (#31544)
    Co-authored-by: lightclient <lightclient@protonmail.com>
    
    Add GetHeaderByNumber and GetReceiptsByNumber to allow more efficient API request filling from Era files.

--- internal/era/era_test.go
+++ internal/era/era_test.go
@@ -18,12 +18,15 @@ package era
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"math/big"
 	"os"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/rlp"
 )
 
 type testchain struct {
@@ -48,9 +51,9 @@ func TestEra1Builder(t *testing.T) {
 		chain   = testchain{}
 	)
 	for i := 0; i < 128; i++ {
-		chain.headers = append(chain.headers, []byte{byte('h'), byte(i)})
-		chain.bodies = append(chain.bodies, []byte{byte('b'), byte(i)})
-		chain.receipts = append(chain.receipts, []byte{byte('r'), byte(i)})
+		chain.headers = append(chain.headers, mustEncode(&types.Header{Number: big.NewInt(int64(i))}))
+		chain.bodies = append(chain.bodies, mustEncode(&types.Body{Transactions: []*types.Transaction{types.NewTransaction(0, common.Address{byte(i)}, nil, 0, nil, nil)}}))
+		chain.receipts = append(chain.receipts, mustEncode(&types.Receipts{{CumulativeGasUsed: uint64(i)}}))
 		chain.tds = append(chain.tds, big.NewInt(int64(i)))
 	}
 
@@ -91,13 +94,25 @@ func TestEra1Builder(t *testing.T) {
 			t.Fatalf("unexpected error %v", it.Error())
 		}
 		// Check headers.
-		header, err := io.ReadAll(it.Header)
+		rawHeader, err := io.ReadAll(it.Header)
+		if err != nil {
+			t.Fatalf("error reading header from iterator: %v", err)
+		}
+		if !bytes.Equal(rawHeader, chain.headers[i]) {
+			t.Fatalf("mismatched header: want %s, got %s", chain.headers[i], rawHeader)
+		}
+		header, err := e.GetHeaderByNumber(i)
 		if err != nil {
 			t.Fatalf("error reading header: %v", err)
 		}
-		if !bytes.Equal(header, chain.headers[i]) {
-			t.Fatalf("mismatched header: want %s, got %s", chain.headers[i], header)
+		encHeader, err := rlp.EncodeToBytes(header)
+		if err != nil {
+			t.Fatalf("error encoding header: %v", err)
 		}
+		if !bytes.Equal(encHeader, chain.headers[i]) {
+			t.Fatalf("mismatched header: want %s, got %s", chain.headers[i], encHeader)
+		}
+
 		// Check bodies.
 		body, err := io.ReadAll(it.Body)
 		if err != nil {
@@ -106,13 +121,25 @@ func TestEra1Builder(t *testing.T) {
 		if !bytes.Equal(body, chain.bodies[i]) {
 			t.Fatalf("mismatched body: want %s, got %s", chain.bodies[i], body)
 		}
+
 		// Check receipts.
-		receipts, err := io.ReadAll(it.Receipts)
+		rawReceipts, err := io.ReadAll(it.Receipts)
+		if err != nil {
+			t.Fatalf("error reading receipts from iterator: %v", err)
+		}
+		if !bytes.Equal(rawReceipts, chain.receipts[i]) {
+			t.Fatalf("mismatched receipts: want %s, got %s", chain.receipts[i], rawReceipts)
+		}
+		receipts, err := e.GetReceiptsByNumber(i)
 		if err != nil {
 			t.Fatalf("error reading receipts: %v", err)
 		}
-		if !bytes.Equal(receipts, chain.receipts[i]) {
-			t.Fatalf("mismatched receipts: want %s, got %s", chain.receipts[i], receipts)
+		encReceipts, err := rlp.EncodeToBytes(receipts)
+		if err != nil {
+			t.Fatalf("error encoding receipts: %v", err)
+		}
+		if !bytes.Equal(encReceipts, chain.receipts[i]) {
+			t.Fatalf("mismatched receipts: want %s, got %s", chain.receipts[i], encReceipts)
 		}
 
 		// Check total difficulty.
@@ -143,4 +170,12 @@ func TestEraFilename(t *testing.T) {
 			t.Errorf("test %d: invalid filename: want %s, got %s", i, tt.expected, got)
 		}
 	}
+}
+
+func mustEncode(obj any) []byte {
+	b, err := rlp.EncodeToBytes(obj)
+	if err != nil {
+		panic(fmt.Sprintf("failed in encode obj: %v", err))
+	}
+	return b
 }
