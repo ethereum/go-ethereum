c91a67c4c086d7a725c2c5888ef8f400b98f2f97
Daniel Liu <139250065@qq.com>
2025-04-16 17:07:41 +0800

    common: enable cancun after 1800 blocks in eip1559 (#942)

--- common/constants.testnet.go
+++ common/constants.testnet.go
@@ -9,27 +9,27 @@ var TestnetConstant = constant{
 	blackListHFNumber: 23779191,
 	maxMasternodesV2:  15,
 
-	tip2019Block:                  big.NewInt(1),
-	tipSigning:                    big.NewInt(3000000),
-	tipRandomize:                  big.NewInt(3464000),
-	tipNoHalvingMNReward:          big.NewInt(23779191), // hardfork no halving masternodes reward
-	tipXDCX:                       big.NewInt(23779191),
-	tipXDCXLending:                big.NewInt(23779191),
-	tipXDCXCancellationFee:        big.NewInt(23779191),
-	tipTRC21Fee:                   big.NewInt(23779191),
-	tipIncreaseMasternodes:        big.NewInt(5000000),
-	blockNumberGas50x:             big.NewInt(56828700), // Target 13rd Nov 2023
-	TIPV2SwitchBlock:              big.NewInt(56828700), // Target 13rd Nov 2023
-	berlinBlock:                   big.NewInt(61290000), // Target 31st March 2024
-	londonBlock:                   big.NewInt(61290000), // Target 31st March 2024
-	mergeBlock:                    big.NewInt(61290000), // Target 31st March 2024
-	shanghaiBlock:                 big.NewInt(61290000), // Target 31st March 2024
-	tipXDCXMinerDisable:           big.NewInt(61290000), // Target 31st March 2024
-	tipXDCXReceiverDisable:        big.NewInt(66825000), // Target 26 Aug 2024
-	eip1559Block:                  big.NewInt(71550000), // Target 14th Feb 2025
-	cancunBlock:                   big.NewInt(71550000),
-	tipUpgradeReward:              big.NewInt(9999999999),
-	tipEpochHalving:               big.NewInt(9999999999),
+	tip2019Block:           big.NewInt(1),
+	tipSigning:             big.NewInt(3000000),
+	tipRandomize:           big.NewInt(3464000),
+	tipNoHalvingMNReward:   big.NewInt(23779191), // hardfork no halving masternodes reward
+	tipXDCX:                big.NewInt(23779191),
+	tipXDCXLending:         big.NewInt(23779191),
+	tipXDCXCancellationFee: big.NewInt(23779191),
+	tipTRC21Fee:            big.NewInt(23779191),
+	tipIncreaseMasternodes: big.NewInt(5000000),
+	blockNumberGas50x:      big.NewInt(56828700), // Target 13rd Nov 2023
+	TIPV2SwitchBlock:       big.NewInt(56828700), // Target 13rd Nov 2023
+	berlinBlock:            big.NewInt(61290000), // Target 31st March 2024
+	londonBlock:            big.NewInt(61290000), // Target 31st March 2024
+	mergeBlock:             big.NewInt(61290000), // Target 31st March 2024
+	shanghaiBlock:          big.NewInt(61290000), // Target 31st March 2024
+	tipXDCXMinerDisable:    big.NewInt(61290000), // Target 31st March 2024
+	tipXDCXReceiverDisable: big.NewInt(66825000), // Target 26 Aug 2024
+	eip1559Block:           big.NewInt(71550000), // Target 14th Feb 2025
+	cancunBlock:            big.NewInt(71551800),
+	tipUpgradeReward:       big.NewInt(9999999999),
+	tipEpochHalving:        big.NewInt(9999999999),
 
 	trc21IssuerSMC:         HexToAddress("0x0E2C88753131CE01c7551B726b28BFD04e44003F"),
 	xdcxListingSMC:         HexToAddress("0x14B2Bf043b9c31827A472CE4F94294fE9a6277e0"),
