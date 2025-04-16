85ce3b718239de9998bd6360ac57298a90b29fdd
Delweng <delweng@gmail.com>
2025-04-16 15:50:05 +0800

    cmd/geth: remove the unused bloomfilter.size flag (#31646)

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -100,7 +100,6 @@ var (
 		utils.LightNoSyncServeFlag, // deprecated
 		utils.EthRequiredBlocksFlag,
 		utils.LegacyWhitelistFlag, // deprecated
-		utils.BloomFilterSizeFlag,
 		utils.CacheFlag,
 		utils.CacheDatabaseFlag,
 		utils.CacheTrieFlag,
