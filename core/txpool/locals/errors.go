238e25bd88c12486d1edd8b1bf5df7d977e31656
lightclient <lightclient@protonmail.com>
2025-04-17 11:39:35 -0600

    core/txpool: move IsTemporaryReject switch from legacypool to locals

--- core/txpool/locals/errors.go
+++ core/txpool/locals/errors.go
@@ -17,6 +17,9 @@
 package locals
 
 import (
+	"errors"
+
+	"github.com/ethereum/go-ethereum/core/txpool"
 	"github.com/ethereum/go-ethereum/core/txpool/legacypool"
 )
 
@@ -24,5 +27,20 @@ import (
 // reason to reject a transaction from being included in the txpool. The result
 // may change if the txpool's state changes later.
 func IsTemporaryReject(err error) bool {
-	return legacypool.IsTemporaryReject(err)
+	switch {
+	case errors.Is(err, legacypool.ErrOutOfOrderTxFromDelegated):
+		return true
+	case errors.Is(err, txpool.ErrInflightTxLimitReached):
+		return true
+	case errors.Is(err, legacypool.ErrAuthorityReserved):
+		return true
+	case errors.Is(err, txpool.ErrUnderpriced):
+		return true
+	case errors.Is(err, legacypool.ErrTxPoolOverflow):
+		return true
+	case errors.Is(err, legacypool.ErrFutureReplacePending):
+		return true
+	default:
+		return false
+	}
 }
