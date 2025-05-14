7e0d8a02c8966b9c14b2269084435e9a2d124d6d
David <david@taiko.xyz>
2025-05-14 09:07:07 +0800

    feat(core): set `MainnetPacayaBlock` (#428)
    * feat(core): set `MainnetPacayaBlock`
    
    * delete
    
    * delete
    
    * feat: update

--- core/taiko_genesis.go
+++ core/taiko_genesis.go
@@ -18,7 +18,7 @@ var (
 	InternalDevnetPacayaBlock = new(big.Int).SetUint64(10)
 	PreconfDevnetPacayaBlock  = common.Big0
 	HeklaPacayaBlock          = new(big.Int).SetUint64(1_299_888)
-	MainnetPacayaBlock        = new(big.Int).SetUint64(999_999_999_999)
+	MainnetPacayaBlock        = new(big.Int).SetUint64(1_166_000)
 )
 
 // TaikoGenesisBlock returns the Taiko network genesis block configs.
