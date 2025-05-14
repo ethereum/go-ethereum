4a4a4f671048ec69f5fc32b0858621deb0233be2
Matthieu Vachon <matt@streamingfast.io>
2025-05-14 17:01:43 -0400

    Avoid calling `OnBlockEnd` on panic
    This avoid problem where the tracer receives `OnBlockEnd(nil)` and thus cannot know that an error happen, in which case it could flush data out.
    
    Technically, shouldn't be a big problem as since there is a panic, the block will not be committed to geth storage and will be replayed correctly.
    
    The other possibility would be to still call the tracer but passing the recovered error to `OnBlockEnd`, but this would require transforming the recovered `any` type into an `error`.

--- core/blockchain.go
+++ core/blockchain.go
@@ -1952,7 +1952,11 @@ func (bc *BlockChain) processBlock(block *types.Block, statedb *state.StateDB, s
 	}
 	if bc.logger != nil && bc.logger.OnBlockEnd != nil {
 		defer func() {
-			bc.logger.OnBlockEnd(blockEndErr)
+			if recovered := recover(); recovered != nil {
+				panic(recovered)
+			} else {
+				bc.logger.OnBlockEnd(blockEndErr)
+			}
 		}()
 	}
 
