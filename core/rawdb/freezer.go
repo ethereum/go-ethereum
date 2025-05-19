cc914b49e0166f670283e0857af708ada8fae162
Felix Lange <fjl@twurst.com>
2025-05-19 16:56:14 +0200

    core/rawdb, ethdb: remove HasAncient
    We don't use it anywhere.

--- core/rawdb/freezer.go
+++ core/rawdb/freezer.go
@@ -180,15 +180,6 @@ func (f *Freezer) AncientDatadir() (string, error) {
 	return f.datadir, nil
 }
 
-// HasAncient returns an indicator whether the specified ancient data exists
-// in the freezer.
-func (f *Freezer) HasAncient(kind string, number uint64) (bool, error) {
-	if table := f.tables[kind]; table != nil {
-		return table.has(number), nil
-	}
-	return false, nil
-}
-
 // Ancient retrieves an ancient binary blob from the append-only immutable files.
 func (f *Freezer) Ancient(kind string, number uint64) ([]byte, error) {
 	if table := f.tables[kind]; table != nil {
