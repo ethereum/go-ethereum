8cd6a29c5ec710088e65099ddac0277be22d330c
Dimo Pechenikov <dimop@axilion.com>
2025-05-23 11:13:18 +0000

    adding initialized hardhat project with precompiled Lock contrack (compilation script added), and deploy script

---
+++ hardhat.tmpl/hardhat.config.js
@@ -0,0 +1,11 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.28",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    }
+  }
+};
