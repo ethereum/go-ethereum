f7ae5b65135311b680c9c7abcee47c70eec50832
Gary Rong <garyrong0905@gmail.com>
2025-05-29 12:49:22 +0800

    core/rawdb: track the metadata key

--- core/rawdb/database.go
+++ core/rawdb/database.go
@@ -599,7 +599,7 @@ var knownMetadataKeys = [][]byte{
 	snapshotGeneratorKey, snapshotRecoveryKey, txIndexTailKey, fastTxLookupLimitKey,
 	uncleanShutdownKey, badBlockKey, transitionStatusKey, skeletonSyncStatusKey,
 	persistentStateIDKey, trieJournalKey, snapshotSyncStatusKey, snapSyncStatusFlagKey,
-	filterMapsRangeKey,
+	filterMapsRangeKey, headStateHistoryIndexKey,
 }
 
 // printChainMetadata prints out chain metadata to stderr.
