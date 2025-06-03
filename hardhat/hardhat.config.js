cc3fc032581339fc906acc932ccc979749138122
Radostina Lyubomirova <radostina.lyubomirova@amusnet.com>
2025-06-03 20:32:40 +0300

    update_test

---
+++ hardhat/hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.28",
+};
+
+
+import "@nomicfoundation/hardhat-toolbox";
+
+export default {
+  solidity: "0.8.28",
+};
