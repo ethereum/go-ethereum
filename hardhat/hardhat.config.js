75d5ff6e8dc6abc842fb585629afed46d99378bf
Radostina Lyubomirova <radostina.lyubomirova@amusnet.com>
2025-06-04 16:05:59 +0300

    test

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -2,10 +2,6 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545",
-    },
-  },
+
   solidity: "0.8.28",
 };
