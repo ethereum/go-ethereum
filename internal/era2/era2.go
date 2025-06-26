04baf07e30594f99e6434c0fad21642f21442089
shantichanal <158101918+shantichanal@users.noreply.github.com>
2025-06-26 14:47:51 +0200

    Working on implementation

---
+++ internal/era2/era2.go
@@ -0,0 +1,38 @@
+package era
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+)
+
+type BlockProofHistoricalHashesAccumulator [15]common.Hash // 15 * 32 = 480 bytes
+
+// BlockProofHistoricalRoots – Altair / Bellatrix historical_roots branch.
+type BlockProofHistoricalRoots struct {
+	BeaconBlockProof    [14]common.Hash // 448
+	BeaconBlockRoot     common.Hash     // 32
+	ExecutionBlockProof [11]common.Hash // 352
+	Slot                uint64          // 8  => 840 bytes
+}
+
+// BlockProofHistoricalSummariesCapella – Capella historical_summaries branch.
+type BlockProofHistoricalSummariesCapella struct {
+	BeaconBlockProof    [13]common.Hash // 416
+	BeaconBlockRoot     common.Hash     // 32
+	ExecutionBlockProof [11]common.Hash // 352
+	Slot                uint64          // 8  => 808 bytes
+}
+
+// BlockProofHistoricalSummariesDeneb – Deneb historical_summaries branch.
+type BlockProofHistoricalSummariesDeneb struct {
+	BeaconBlockProof    [13]common.Hash // 416
+	BeaconBlockRoot     common.Hash     // 32
+	ExecutionBlockProof [12]common.Hash // 384
+	Slot                uint64          // 8  => 840 bytes
+}
+
+type Proofs struct {
+	HistoricalHashesAccumulator BlockProofHistoricalHashesAccumulator
+	HistoricalRootsProof        *BlockProofHistoricalRoots
+	SummariesProofCapella       *BlockProofHistoricalSummariesCapella
+	SummariesProofDeneb         *BlockProofHistoricalSummariesDeneb
+}
