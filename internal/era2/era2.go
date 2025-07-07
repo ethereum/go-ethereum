bc2d2d6d7dcddb35365b3da923a857952a541510
shantichanal <158101918+shantichanal@users.noreply.github.com>
2025-07-07 12:54:15 +0200

    sequential access completed (without iterator)

--- internal/era2/era2.go
+++ internal/era2/era2.go
@@ -1,4 +1,4 @@
-package era
+package era2
 
 import (
 	"encoding/binary"
@@ -64,6 +64,7 @@ type Era2 struct {
 	buf [8]byte // buffer reading entry offset
 
 	headeroff, bodyoff, receiptsoff, tdoff, proofsoff []uint64 // offsets for each entry type
+	rootheader                                        uint64   // offset of the root header in the file if present
 	indstart                                          int64
 }
 
@@ -185,6 +186,26 @@ func (e *Era2) GetRawReceiptsFrameByNumber(n uint64) ([]byte, error) {
 	return io.ReadAll(sr)
 }
 
+func (e *Era2) GetRawProofFrameByNumber(n uint64) ([]byte, error) {
+	if len(e.proofsoff) == 0 {
+		return nil, fmt.Errorf("proofs section not present")
+	}
+	if n < e.m.start || n >= e.m.start+e.m.count {
+		return nil, fmt.Errorf("block number %d out of range [%d, %d)", n, e.m.start, e.m.start+e.m.count)
+	}
+	start := e.proofsoff[n-e.m.start]
+
+	// fallback next proof frame → AccRoot header → BlockIndex
+	var nextSec []uint64
+	if e.rootheader != 0 {
+		nextSec = []uint64{e.rootheader}
+	}
+	end := e.nextBoundary(n, e.proofsoff, nextSec)
+	length := end - start
+	sr := io.NewSectionReader(e.f, int64(start), int64(length))
+	return io.ReadAll(sr)
+}
+
 func (e *Era2) rawPayload(abs uint64) ([]byte, error) {
 	sr := io.NewSectionReader(e.f, int64(abs), e.indstart-int64(abs))
 	return io.ReadAll(sr)
@@ -269,7 +290,135 @@ func (e *Era2) loadIndex() error {
 			e.proofsoff[i] = toAbs(base + 4)
 		}
 	}
+
+	var off int64 = 0 // start at byte-0 of file
+
+	for off < e.indstart { // never enter the Block-Index TLV
+		typ, length, err := e.s.ReadMetadataAt(off)
+		if err != nil {
+			return fmt.Errorf("error reading TLV at offset %d: %w", off, err)
+		}
+		if typ == TypeAccumulatorRoot {
+			e.rootheader = uint64(off) // remember absolute header offset
+			break                      // found
+		}
+		off += 8 + int64(length) // headersize plus length to jump to next TLV header
+	}
 	return nil
+
+}
+
+func (e *Era2) BatchRange(first, count uint64, wantHeaders, wantBodies, wantReceipts, wantProofs bool) (hdrs []*types.Header, bods []*types.Body, recs []types.Receipts, prfs [][]byte, err error) {
+	if count == 0 {
+		err = fmt.Errorf("count must be greater than 0")
+		return
+	}
+
+	if first < e.m.start || first+count > e.m.start+e.m.count {
+		err = fmt.Errorf("range [%d,%d) out of bounds [%d,%d)", first, first+count, e.m.start, e.m.start+e.m.count)
+		return
+	}
+
+	id0 := first - e.m.start
+	if wantHeaders {
+		hdrs = make([]*types.Header, count)
+	}
+	if wantBodies {
+		bods = make([]*types.Body, count)
+	}
+	if wantReceipts {
+		recs = make([]types.Receipts, count)
+	}
+	if wantProofs {
+		prfs = make([][]byte, count)
+	}
+
+	stream := func(startOff uint64, endOff uint64, decode func(io.Reader, uint64) error) error {
+		length := int64(endOff) - int64(startOff)
+		r := snappy.NewReader(io.NewSectionReader(e.f, int64(startOff), length))
+		for i := uint64(0); i < count; i++ {
+			if err := decode(r, i); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	if wantHeaders {
+		err = stream(e.headeroff[id0], e.bodyoff[0], func(r io.Reader, i uint64) error {
+			var hdr types.Header
+			if err := rlp.Decode(r, &hdr); err != nil {
+				return fmt.Errorf("error decoding header for block %d: %w", first+i, err)
+			}
+			hdrs[i] = &hdr
+			return nil
+		})
+		if err != nil {
+			return
+		}
+	}
+
+	if wantBodies {
+		err = stream(e.bodyoff[id0], e.receiptsoff[0], func(r io.Reader, i uint64) error {
+			var body types.Body
+			if err := rlp.Decode(r, &body); err != nil {
+				return fmt.Errorf("error decoding body for block %d: %w", first+i, err)
+			}
+			bods[i] = &body
+			return nil
+		})
+		if err != nil {
+			return
+		}
+	}
+
+	if wantReceipts {
+		var receiptsEnd uint64
+		if len(e.tdoff) > 0 {
+			receiptsEnd = e.tdoff[0]
+		} else {
+			receiptsEnd = uint64(e.indstart)
+		}
+		err = stream(e.receiptsoff[id0], receiptsEnd, func(r io.Reader, i uint64) error {
+			var rct types.Receipts
+			if err := rlp.Decode(r, &rct); err != nil {
+				return fmt.Errorf("error decoding receipts for block %d: %w", first+i, err)
+			}
+			recs[i] = rct
+			return nil
+		})
+		if err != nil {
+			return
+		}
+	}
+
+	if wantProofs {
+		if len(prfs) == 0 {
+			err = fmt.Errorf("proofs section is not present")
+			return
+		}
+
+		for i := uint64(0); i < count; i++ {
+			start := e.proofsoff[id0+i]
+			var end uint64
+			if id0+i+1 < uint64(len(e.proofsoff)) {
+				end = e.proofsoff[id0+i+1] // next proof frame
+			} else if e.rootheader != 0 {
+				end = e.rootheader // AccRoot header
+			} else {
+				end = uint64(e.indstart) // BlockIndex header
+			}
+			length := int64(end - start)
+			frame, readErr := io.ReadAll(io.NewSectionReader(e.f, int64(start), length))
+			if readErr != nil {
+				err = fmt.Errorf("proof read %d: %w", first+i, readErr)
+				return
+			}
+			prfs[i] = frame
+		}
+	}
+
+	return
 }
 
 func reverseOrder32(b []byte) []byte {
