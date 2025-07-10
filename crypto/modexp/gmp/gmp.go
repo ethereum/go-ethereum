e131482fe298895ffc0bd1d57ed00c527ecf024f
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2025-07-11 00:07:58 +0100

    Add new modexp package

---
+++ crypto/modexp/gmp/gmp.go
@@ -0,0 +1,96 @@
+// Copyright 2024 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+package gmp
+
+// #cgo LDFLAGS: -lgmp
+// #include "modexp.h"
+import "C"
+import (
+	"errors"
+	"runtime"
+	"unsafe"
+)
+
+// ModExp performs modular exponentiation using GMP
+// This is thread safe.
+func ModExp(base, exp, mod []byte) ([]byte, error) {
+	// Handle empty modulus - return empty result (EVM behavior)
+	if len(mod) == 0 {
+		return []byte{}, nil
+	}
+
+	// Special case: zero modulus
+	allZero := true
+	for _, b := range mod {
+		if b != 0 {
+			allZero = false
+			break
+		}
+	}
+	if allZero {
+		return []byte{}, nil
+	}
+
+	// Allocate result buffer (size of modulus is the max possible result)
+	result := make([]byte, len(mod))
+	resultLen := C.size_t(len(result))
+
+	// Handle empty slices - pass a dummy non-nil pointer with length 0
+	// This avoids UB when the length is zero.
+	dummy := C.uint8_t(0)
+	var basePtr, expPtr, modPtr *C.uint8_t = &dummy, &dummy, &dummy
+
+	if len(base) > 0 {
+		basePtr = (*C.uint8_t)(unsafe.Pointer(&base[0]))
+	}
+	if len(exp) > 0 {
+		expPtr = (*C.uint8_t)(unsafe.Pointer(&exp[0]))
+	}
+	if len(mod) > 0 {
+		modPtr = (*C.uint8_t)(unsafe.Pointer(&mod[0]))
+	}
+
+	// Call C function
+	ret := C.modexp_bytes(
+		basePtr, C.size_t(len(base)),
+		expPtr, C.size_t(len(exp)),
+		modPtr, C.size_t(len(mod)),
+		(*C.uint8_t)(unsafe.Pointer(&result[0])), &resultLen,
+	)
+
+	// Keep the slices alive until after the C call completes
+	runtime.KeepAlive(base)
+	runtime.KeepAlive(exp)
+	runtime.KeepAlive(mod)
+	runtime.KeepAlive(result)
+
+	// Check for errors
+	switch ret {
+	case 0:
+		// Success - trim result to actual size
+		if resultLen == 0 {
+			return []byte{}, nil
+		}
+		return result[:resultLen], nil
+	case -1:
+		return nil, errors.New("invalid parameter")
+	case -2:
+		return nil, errors.New("result buffer too small")
+	default:
+		return nil, errors.New("unknown error")
+	}
+}
---
+++ crypto/modexp/gmp/gmp_test.go
@@ -0,0 +1,135 @@
+// Copyright 2024 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+package gmp
+
+import (
+	"bytes"
+	"math/big"
+	"testing"
+)
+
+// TestWrapperVsBigInt compares the wrapper with Go's math/big
+func TestWrapperVsBigInt(t *testing.T) {
+	tests := []struct {
+		name string
+		base string
+		exp  string
+		mod  string
+	}{
+		{
+			name: "small_numbers",
+			base: "2",
+			exp:  "10",
+			mod:  "1000",
+		},
+		{
+			name: "medium_numbers",
+			base: "123456789",
+			exp:  "987654321",
+			mod:  "1000000007",
+		},
+		{
+			name: "large_numbers",
+			base: "123456789012345678901234567890",
+			exp:  "987654321098765432109876543210",
+			mod:  "111111111111111111111111111111",
+		},
+		{
+			name: "zero_exponent",
+			base: "12345",
+			exp:  "0",
+			mod:  "67890",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Parse numbers
+			baseBig := new(big.Int)
+			baseBig.SetString(tt.base, 10)
+			expBig := new(big.Int)
+			expBig.SetString(tt.exp, 10)
+			modBig := new(big.Int)
+			modBig.SetString(tt.mod, 10)
+
+			// Test with wrapper
+			baseBytes := baseBig.Bytes()
+			expBytes := expBig.Bytes()
+			modBytes := modBig.Bytes()
+
+			wrapperResult, err := ModExp(baseBytes, expBytes, modBytes)
+			if err != nil {
+				t.Fatalf("Wrapper error: %v", err)
+			}
+
+			// Test with math/big
+			bigResult := new(big.Int)
+			bigResult.Exp(baseBig, expBig, modBig)
+
+			expectedResult := bigResult.Bytes()
+
+			// Compare results
+			if !bytes.Equal(wrapperResult, expectedResult) {
+				t.Errorf("Results differ:\nWrapper:  %x\nExpected: %x",
+					wrapperResult, expectedResult)
+			}
+		})
+	}
+}
+
+// BenchmarkWrapperVsExisting compares performance
+func BenchmarkModExp(b *testing.B) {
+	base := make([]byte, 60)
+	exp := make([]byte, 60)
+	mod := make([]byte, 60)
+
+	for i := range base {
+		base[i] = byte(i * 17)
+		exp[i] = byte(i * 31)
+		mod[i] = byte(255 - i)
+	}
+	mod[59] |= 0x01
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = ModExp(base, exp, mod)
+	}
+}
+
+func BenchmarkModExpBigInt(b *testing.B) {
+	baseBytes := make([]byte, 60)
+	expBytes := make([]byte, 60)
+	modBytes := make([]byte, 60)
+
+	for i := range baseBytes {
+		baseBytes[i] = byte(i * 17)
+		expBytes[i] = byte(i * 31)
+		modBytes[i] = byte(255 - i)
+	}
+	modBytes[59] |= 0x01
+
+	base := new(big.Int).SetBytes(baseBytes)
+	exp := new(big.Int).SetBytes(expBytes)
+	mod := new(big.Int).SetBytes(modBytes)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		result := new(big.Int)
+		result.Exp(base, exp, mod)
+		_ = result.Bytes()
+	}
+}
