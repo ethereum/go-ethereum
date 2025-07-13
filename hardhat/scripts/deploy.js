9eaebea023f8c2c580c1f19f05e455d69e731b87
Yohan9206 <yohan_s@live.com>
2025-07-13 22:25:01 +0300

    fixing hardhat project

---
+++ hardhat/scripts/deploy.js
@@ -0,0 +1,19 @@
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const Lock = await ethers.getContractFactory("Lock");
+  const lock = await Lock.deploy();
+
+  await lock.deployed();
+
+  console.log("Lock deployed to:", lock.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
