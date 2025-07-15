9a67868f8bf76c48dae1c90f78120a2dad39b8db
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2025-07-15 12:12:26 +0200

    fix some encoding issues
    Signed-off-by: Guillaume Ballet <3272758+gballet@users.noreply.github.com>

--- core/stateless/encoding.go
+++ core/stateless/encoding.go
@@ -20,6 +20,7 @@ import (
 	"io"
 
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/rlp"
 )
 
@@ -28,11 +29,11 @@ func (w *Witness) ToExtWitness() *ExtWitness {
 	ext := &ExtWitness{
 		Headers: w.Headers,
 	}
-	ext.Codes = make([][]byte, 0, len(w.Codes))
+	ext.Codes = make([]hexutil.Bytes, 0, len(w.Codes))
 	for code := range w.Codes {
 		ext.Codes = append(ext.Codes, []byte(code))
 	}
-	ext.State = make([][]byte, 0, len(w.State))
+	ext.State = make([]hexutil.Bytes, 0, len(w.State))
 	for node := range w.State {
 		ext.State = append(ext.State, []byte(node))
 	}
@@ -70,8 +71,8 @@ func (w *Witness) DecodeRLP(s *rlp.Stream) error {
 
 // ExtWitness is a witness RLP encoding for transferring across clients.
 type ExtWitness struct {
-	Headers []*types.Header
-	Codes   [][]byte
-	State   [][]byte
-	Keys    [][]byte
+	Headers []*types.Header `json:"headers"`
+	Codes   []hexutil.Bytes `json:"codes"`
+	State   []hexutil.Bytes `json:"state"`
+	Keys    []hexutil.Bytes `json:"keys"`
 }
