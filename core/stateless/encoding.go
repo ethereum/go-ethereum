59b8f7889a5964de75f659040467b37d4972d7cd
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2025-07-15 14:07:06 +0200

    fix linter issue
    Signed-off-by: Guillaume Ballet <3272758+gballet@users.noreply.github.com>

--- core/stateless/encoding.go
+++ core/stateless/encoding.go
@@ -19,8 +19,8 @@ package stateless
 import (
 	"io"
 
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/rlp"
 )
 
