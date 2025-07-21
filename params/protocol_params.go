f96f82bd6bd6bf88a10c5160e97c04b3dcafcc61
Felix Lange <fjl@twurst.com>
2025-07-21 16:26:24 +0200

    core, params: add limit for max blobs in blob transaction (#32246)
    [EIP-7594](https://eips.ethereum.org/EIPS/eip-7594) defines a limit of
    max 6 blobs per transaction. We need to enforce this limit during block
    processing.
    
    > Additionally, a limit of 6 blobs per transaction is introduced.
    Clients MUST enforce this limit when validating blob transactions at
    submission time, when received from the network, and during block
    production and processing.

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -177,6 +177,7 @@ const (
 	BlobTxBlobGasPerBlob               = 1 << 17 // Gas consumption of a single data blob (== blob byte size)
 	BlobTxMinBlobGasprice              = 1       // Minimum gas price for data blobs
 	BlobTxPointEvaluationPrecompileGas = 50000   // Gas price for the point evaluation precompile.
+	BlobTxMaxBlobs                     = 6
 	BlobBaseCost                       = 1 << 13 // Base execution gas cost for a blob.
 
 	HistoryServeWindow = 8192 // Number of blocks to serve historical block hashes for, EIP-2935.
