b2a0e088087ad912b72468213a16cb887efe6124
maskpp <maskpp266@gmail.com>
2025-07-22 15:03:48 +0800

    core/types: minimize this invalid intermediate state (#32241)

--- core/types/tx_blob.go
+++ core/types/tx_blob.go
@@ -117,15 +117,16 @@ func (sc *BlobTxSidecar) ToV1() error {
 		return nil
 	}
 	if sc.Version == BlobSidecarVersion0 {
-		sc.Proofs = make([]kzg4844.Proof, 0, len(sc.Blobs)*kzg4844.CellProofsPerBlob)
+		proofs := make([]kzg4844.Proof, 0, len(sc.Blobs)*kzg4844.CellProofsPerBlob)
 		for _, blob := range sc.Blobs {
 			cellProofs, err := kzg4844.ComputeCellProofs(&blob)
 			if err != nil {
 				return err
 			}
-			sc.Proofs = append(sc.Proofs, cellProofs...)
+			proofs = append(proofs, cellProofs...)
 		}
 		sc.Version = BlobSidecarVersion1
+		sc.Proofs = proofs
 	}
 	return nil
 }
