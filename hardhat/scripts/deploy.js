aa05aefef9e146350e3d18307cd3e2ee4548d0ee
Yohan9206 <yohan_s@live.com>
2025-07-31 23:20:40 +0300

    fixing the deploy.js

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -6,9 +6,9 @@ async function main() {
   console.log("Deploying contracts with the account:", deployer.address);
 
   const unlockTime = Math.floor(Date.now() / 1000) + 3600; // 1 hour from now
-  
+
   const Lock = await ethers.getContractFactory("Lock", deployer);
-  const lock = await Lock.deploy();
+  const lock = await Lock.deploy(unlockTime);
 
   await lock.deployed();
 
