3d4244bfc1778f63b34554661499d099c14e569d
blazejkrzak <blazej@m8b.pl>
2025-08-18 13:13:43 +0200

    Revert changes done via RONG :)

--- core/txpool/blobpool/blobpool.go
+++ core/txpool/blobpool/blobpool.go
@@ -831,9 +831,8 @@ func (p *BlobPool) Reset(oldHead, newHead *types.Header) {
 		}
 	}
 	// Flush out any blobs from limbo that are older than the latest finality
-	if p.chain.Config().IsCancun(p.head.Number, p.head.Time) {
-		p.limbo.finalize(p.chain.CurrentFinalBlock())
-	}
+	p.limbo.finalize(p.chain.CurrentFinalBlock())
+
 	// Reset the price heap for the new set of basefee/blobfee pairs
 	var (
 		basefee = uint256.MustFromBig(eip1559.CalcBaseFee(p.chain.Config(), newHead))
