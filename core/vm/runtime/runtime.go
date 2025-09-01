6bcea6f32fbc38d48b0c90bff67d49e32abebbc7
Sina Mahmoodi <itz.s1na@gmail.com>
2025-09-01 15:20:47 +0200

    fix import

--- core/vm/runtime/runtime.go
+++ core/vm/runtime/runtime.go
@@ -22,6 +22,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/state"
+	"github.com/ethereum/go-ethereum/core/tracing"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/ethereum/go-ethereum/crypto"
