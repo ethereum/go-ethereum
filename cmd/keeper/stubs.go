f390d79b6b7aa3fd9b5b660fc6ab240ebc1b1da0
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2025-09-05 20:00:04 +0200

    add a stub to fix linter

---
+++ cmd/keeper/stubs.go
@@ -0,0 +1,37 @@
+// Copyright 2025 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+//go:build !example
+
+package main
+
+import "github.com/ethereum/go-ethereum/params"
+
+// getInput is a stub implementation for when no platform-specific build tags are set.
+// This allows golangci-lint to typecheck the code without errors.
+// The actual implementations are provided in platform-specific files.
+func getInput() []byte {
+	// This is a no-op stub - real implementations are in platform-specific files
+	return nil
+}
+
+// getChainConfig is a stub implementation for when no platform-specific build tags are set.
+// This allows golangci-lint to typecheck the code without errors.
+// The actual implementations are provided in platform-specific files.
+func getChainConfig() *params.ChainConfig {
+	// This is a no-op stub - real implementations are in platform-specific files
+	return nil
+}
