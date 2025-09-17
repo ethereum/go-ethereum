5f4ac6757d07d964a54eca82fd506324a662b7ae
jsvisa <delweng@gmail.com>
2025-09-18 00:06:42 +0800

    eth/downloader: gracefully exit

--- eth/downloader/api.go
+++ eth/downloader/api.go
@@ -104,6 +104,9 @@ func (api *DownloaderAPI) eventLoop() {
 			if ev.Type == SyncStarted {
 				started = true
 			}
+		case <-sub.Err():
+			// The downloader is terminated or other internal error occurs
+			return
 		case <-checkTimer.C:
 			if !started {
 				checkTimer.Reset(checkInterval)
