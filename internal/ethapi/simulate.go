ed5fc166c78801eacfc8eba12cadb4b96fac9b2a
Felix Lange <fjl@twurst.com>
2025-09-19 00:07:05 +0200

    eth/tracers, internal/ethapi: refactor ToMessage
    SkipFromEOACheck was always passed as true, so the parameter can be removed.

--- internal/ethapi/simulate.go
+++ internal/ethapi/simulate.go
@@ -287,7 +287,7 @@ func (sim *simulator) processBlock(ctx context.Context, block *simBlock, header,
 		tracer.reset(txHash, uint(i))
 		sim.state.SetTxContext(txHash, i)
 		// EoA check is always skipped, even in validation mode.
-		msg := call.ToMessage(header.BaseFee, !sim.validate, true)
+		msg := call.ToMessage(header.BaseFee, !sim.validate)
 		result, err := applyMessageWithEVM(ctx, evm, msg, timeout, sim.gp)
 		if err != nil {
 			txErr := txValidationError(err)
