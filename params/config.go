ab95477a65387d8eb52ac985d30f594da65b5cfe
Felix Lange <fjl@twurst.com>
2025-09-18 22:58:40 +0200

    build: update to execution-spec-tests v5.0.0 (#32592)
    https://github.com/ethereum/execution-spec-tests/releases/tag/v5.0.0
    
    As of this release, execution-spec-tests also contains all state tests
    that were previously in ethereum/tests. We can probably remove the tests
    submodule now. However, this would mean we are missing the pre-cancun
    tests. Still need to figure out how to resolve this.
    
    ---------
    
    Co-authored-by: MariusVanDerWijden <m.vanderwijden@live.de>

--- params/config.go
+++ params/config.go
@@ -359,6 +359,30 @@ var (
 		Max:            9,
 		UpdateFraction: 5007716,
 	}
+	// DefaultBPO1BlobConfig is the default blob configuration for the Osaka fork.
+	DefaultBPO1BlobConfig = &BlobConfig{
+		Target:         9,
+		Max:            14,
+		UpdateFraction: 8832827,
+	}
+	// DefaultBPO1BlobConfig is the default blob configuration for the Osaka fork.
+	DefaultBPO2BlobConfig = &BlobConfig{
+		Target:         14,
+		Max:            21,
+		UpdateFraction: 13739630,
+	}
+	// DefaultBPO1BlobConfig is the default blob configuration for the Osaka fork.
+	DefaultBPO3BlobConfig = &BlobConfig{
+		Target:         21,
+		Max:            32,
+		UpdateFraction: 20609697,
+	}
+	// DefaultBPO1BlobConfig is the default blob configuration for the Osaka fork.
+	DefaultBPO4BlobConfig = &BlobConfig{
+		Target:         14,
+		Max:            21,
+		UpdateFraction: 13739630,
+	}
 	// DefaultBlobSchedule is the latest configured blob schedule for Ethereum mainnet.
 	DefaultBlobSchedule = &BlobScheduleConfig{
 		Cancun: DefaultCancunBlobConfig,
