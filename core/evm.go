103861ab873930c9d23f84f6f422f7901b657928
hero5512 <lvshuaino@gmail.com>
2025-10-02 08:34:06 -0400

    core: refactor StateProcessor to accept ChainContext interface (#32739)
    This pr implements https://github.com/ethereum/go-ethereum/issues/32733
    to make StateProcessor more customisable.
    
    ## Compatibility notes
    
    This introduces a breaking change to users using geth EVM as a library.
    The `NewStateProcessor` function now takes one parameter which has the
    chainConfig embedded instead of 2 parameters.

--- core/evm.go
+++ core/evm.go
@@ -25,21 +25,16 @@ import (
 	"github.com/ethereum/go-ethereum/core/tracing"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/core/vm"
-	"github.com/ethereum/go-ethereum/params"
 	"github.com/holiman/uint256"
 )
 
 // ChainContext supports retrieving headers and consensus parameters from the
 // current blockchain to be used during transaction processing.
 type ChainContext interface {
+	consensus.ChainHeaderReader
+
 	// Engine retrieves the chain's consensus engine.
 	Engine() consensus.Engine
-
-	// GetHeader returns the header corresponding to the hash/number argument pair.
-	GetHeader(common.Hash, uint64) *types.Header
-
-	// Config returns the chain's configuration.
-	Config() *params.ChainConfig
 }
 
 // NewEVMBlockContext creates a new context for use in the EVM.
