4747565d650aa0dd4a2d3f42ae7373519e7ab315
Alvarez <140459501+prestoalvarez@users.noreply.github.com>
2025-10-07 18:08:48 +0200

    Update abifuzzer_test.go

--- accounts/abi/abifuzzer_test.go
+++ accounts/abi/abifuzzer_test.go
@@ -48,7 +48,7 @@ var (
 	vNames   = []string{"a", "b", "c", "d", "e", "f", "g"}
 	varNames = append(vNames, names...)
 	varTypes = []string{"bool", "address", "bytes", "string",
-		"uint8", "int8", "uint8", "int8", "uint16", "int16",
+		"uint8", "int8", "uint16", "int16",
 		"uint24", "int24", "uint32", "int32", "uint40", "int40", "uint48", "int48", "uint56", "int56",
 		"uint64", "int64", "uint72", "int72", "uint80", "int80", "uint88", "int88", "uint96", "int96",
 		"uint104", "int104", "uint112", "int112", "uint120", "int120", "uint128", "int128", "uint136", "int136",
