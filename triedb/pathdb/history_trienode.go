b4a280353d34be9275316f96140a6d0da2472b89
Divyansh Mishra <divyanshmishra@Divyanshs-MacBook-Air-3.local>
2025-10-12 20:19:05 +0530

    rpc: tolerate connection reset error in TestServerWebsocketReadLimit and triedb:pathdb:cross-platformbug

--- triedb/pathdb/history_trienode.go
+++ triedb/pathdb/history_trienode.go
@@ -524,7 +524,8 @@ func newSingleTrienodeHistoryReader(id uint64, reader ethdb.AncientReader, keyRa
 	}
 	keyStart := int(keyRange.start)
 	keyLimit := int(keyRange.limit)
-	if keyLimit == math.MaxUint32 {
+	const maxKeyLimit = int(^uint32(0))
+	if keyLimit == maxKeyLimit {
 		keyLimit = len(keyData)
 	}
 	if len(keyData) < keyStart || len(keyData) < keyLimit {
