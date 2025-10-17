ac23136ae7cfaf9ce871272eaaafc949e375619b
johnzhu0908 <zelian.zhu@certik.org>
2025-10-17 18:01:28 +0800

    Revert "fix: use maximum uint64 value for receipt chain insertion"
    This reverts commit 0c125a8d009443851ee620d59bf75635b1974d44.

--- cmd/utils/cmd.go
+++ cmd/utils/cmd.go
@@ -311,7 +311,7 @@ func ImportHistory(chain *core.BlockChain, dir string, network string) error {
 					return fmt.Errorf("error reading receipts %d: %w", it.Number(), err)
 				}
 				encReceipts := types.EncodeBlockReceiptLists([]types.Receipts{receipts})
-				if _, err := chain.InsertReceiptChain([]*types.Block{block}, encReceipts, ^uint64(0)); err != nil {
+				if _, err := chain.InsertReceiptChain([]*types.Block{block}, encReceipts, 2^64-1); err != nil {
 					return fmt.Errorf("error inserting body %d: %w", it.Number(), err)
 				}
 				imported += 1
