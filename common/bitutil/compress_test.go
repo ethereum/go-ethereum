bd31f767cb404b958388d7cc7db5a345a003b1a0
Coder <161350311+MamunC0der@users.noreply.github.com>
2025-10-18 13:45:46 +0200

    Update compress_test.go

--- common/bitutil/compress_test.go
+++ common/bitutil/compress_test.go
@@ -180,7 +180,7 @@ func benchmarkEncoding(b *testing.B, bytes int, fill float64) {
 	// Reset the benchmark and measure encoding/decoding
 	b.ResetTimer()
 	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
+	for range b.Loop() {
 		bitsetDecodeBytes(bitsetEncodeBytes(data), len(data))
 	}
 }
