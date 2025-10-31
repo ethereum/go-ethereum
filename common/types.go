1ed3fd314026cac620c1ee7f7392a2a4a07a7484
Alvarez <140459501+prestoalvarez@users.noreply.github.com>
2025-10-31 13:21:17 +0100

    Update types.go

--- common/types.go
+++ common/types.go
@@ -231,7 +231,7 @@ func HexToAddress(s string) Address { return BytesToAddress(FromHex(s)) }
 // IsHexAddress verifies whether a string can represent a valid hex-encoded
 // Ethereum address or not.
 func IsHexAddress(s string) bool {
-	if has0xPrefix(s) {
+	if hexutil.Has0xPrefix(s) {
 		s = s[2:]
 	}
 	return len(s) == 2*AddressLength && isHex(s)
