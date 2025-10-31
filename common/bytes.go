72f3bfd838896576a2ae7706d4cf4662c7a347f4
Alvarez <140459501+prestoalvarez@users.noreply.github.com>
2025-10-31 13:20:44 +0100

    Update bytes.go

--- common/bytes.go
+++ common/bytes.go
@@ -27,7 +27,7 @@ import (
 // FromHex returns the bytes represented by the hexadecimal string s.
 // s may be prefixed with "0x".
 func FromHex(s string) []byte {
-	if has0xPrefix(s) {
+	if hexutil.Has0xPrefix(s) {
 		s = s[2:]
 	}
 	if len(s)%2 == 1 {
@@ -45,11 +45,6 @@ func CopyBytes(b []byte) (copiedBytes []byte) {
 	copy(copiedBytes, b)
 
 	return
-}
-
-// has0xPrefix validates str begins with '0x' or '0X'.
-func has0xPrefix(str string) bool {
-	return len(str) >= 2 && str[0] == '0' && (str[1] == 'x' || str[1] == 'X')
 }
 
 // isHexCharacter returns bool of c being a valid hexadecimal.
