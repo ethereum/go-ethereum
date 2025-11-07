f70cd5b73dd82c6b6159fea2ca97849baa3329d3
Rizky Ikwan <rizzikwann@gmail.com>
2025-11-07 08:24:11 +0100

    accounts/usbwallet: fix version check in SignTypedMessage (#33113)
    The version check incorrectly used `&&` instead of `||`, causing
    versions like v1.0.x through v1.4.x to be allowed when they should be
    rejected. These versions don't support EIP-712 signing which was
    introduced in firmware v1.5.0.

--- accounts/usbwallet/ledger.go
+++ accounts/usbwallet/ledger.go
@@ -184,7 +184,7 @@ func (w *ledgerDriver) SignTypedMessage(path accounts.DerivationPath, domainHash
 		return nil, accounts.ErrWalletClosed
 	}
 	// Ensure the wallet is capable of signing the given transaction
-	if w.version[0] < 1 && w.version[1] < 5 {
+	if w.version[0] < 1 || (w.version[0] == 1 && w.version[1] < 5) {
 		//lint:ignore ST1005 brand name displayed on the console
 		return nil, fmt.Errorf("Ledger version >= 1.5.0 required for EIP-712 signing (found version v%d.%d.%d)", w.version[0], w.version[1], w.version[2])
 	}
