6c4abe90a2798e132796a788f9a66fad82040d02
Carlos Bermudez Porto <cbermudez.dev@gmail.com>
2025-11-24 15:20:27 -0300

    internal/debug: integrate grafana pyroscope profiling support
    - Added Pyroscope profiling capabilities to the debug package.
    - Introduced command-line flags for enabling profiling and specifying the Pyroscope server URL.
    - Implemented methods to start and stop the Pyroscope profiler, including a custom logger to interface with the existing logging system.
    - Updated relevant files to accommodate the new profiling features.

--- internal/debug/api.go
+++ internal/debug/api.go
@@ -23,6 +23,7 @@ package debug
 import (
 	"bytes"
 	"errors"
+	"fmt"
 	"io"
 	"os"
 	"os/user"
@@ -37,6 +38,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/log"
+	"github.com/grafana/pyroscope-go"
 	"github.com/hashicorp/go-bexpr"
 )
 
@@ -52,6 +54,7 @@ type HandlerT struct {
 	cpuFile   string
 	traceW    io.WriteCloser
 	traceFile string
+	profiler  *pyroscope.Profiler
 }
 
 // Verbosity sets the log verbosity ceiling. The verbosity of individual packages
@@ -124,6 +127,73 @@ func (h *HandlerT) StopCPUProfile() error {
 	h.cpuW.Close()
 	h.cpuW = nil
 	h.cpuFile = ""
+	return nil
+}
+
+// Small wrapper for log.Logger to satisfy pyroscope.Logger interface
+type pyroscopeLogger struct {
+	Logger log.Logger
+}
+
+func (l *pyroscopeLogger) Infof(format string, v ...interface{}) {
+	l.Logger.Info(fmt.Sprintf(format, v...))
+}
+
+func (l *pyroscopeLogger) Debugf(format string, v ...interface{}) {
+	l.Logger.Debug(fmt.Sprintf(format, v...))
+}
+
+func (l *pyroscopeLogger) Errorf(format string, v ...interface{}) {
+	l.Logger.Error(fmt.Sprintf(format, v...))
+}
+
+// StartPyroscopeProfiler starts the Pyroscope profiler for later use
+func (h *HandlerT) StartPyroscopeProfiler(server string) error {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	if h.profiler != nil {
+		log.Info("Pyroscope profiling already started")
+		return nil
+	}
+	profiler, err := pyroscope.Start(
+		pyroscope.Config{
+			ApplicationName: "geth",
+			ServerAddress:   server,
+			Logger:          &pyroscopeLogger{Logger: log.Root().With("module", "pyroscope")},
+			// Tags: nil,
+			ProfileTypes: []pyroscope.ProfileType{
+				// Enabling all profile types
+				pyroscope.ProfileCPU,
+				pyroscope.ProfileAllocObjects,
+				pyroscope.ProfileAllocSpace,
+				pyroscope.ProfileInuseObjects,
+				pyroscope.ProfileInuseSpace,
+				pyroscope.ProfileGoroutines,
+				pyroscope.ProfileMutexCount,
+				pyroscope.ProfileMutexDuration,
+				pyroscope.ProfileBlockCount,
+				pyroscope.ProfileBlockDuration,
+			},
+		},
+	)
+	if err != nil {
+		return err
+	}
+	h.profiler = profiler
+	log.Info("Pyroscope profiling started")
+	return nil
+}
+
+// StopPyroscopeProfiler stops the Pyroscope profiler and returns the error
+func (h *HandlerT) StopPyroscopeProfiler() error {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	if h.profiler != nil {
+		err := h.profiler.Stop()
+		h.profiler = nil
+		return err
+	}
+	log.Info("Pyroscope profiling stopped")
 	return nil
 }
 
