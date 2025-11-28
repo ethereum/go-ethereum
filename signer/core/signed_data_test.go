bd502d739c1f2a2fbd41275a65bc086430af5574
oxBoni <godzilabozo@gmail.com>
2025-11-28 14:08:24 +0100

    Update signed_data_test.go

--- signer/core/signed_data_test.go
+++ signer/core/signed_data_test.go
@@ -219,7 +219,7 @@ func TestSignData(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if signature == nil || len(signature) != 65 {
+	if len(signature) != 65 {
 		t.Errorf("Expected 65 byte signature (got %d bytes)", len(signature))
 	}
 	// data/typed via SignTypeData
@@ -228,7 +228,7 @@ func TestSignData(t *testing.T) {
 	var want []byte
 	if signature, err = api.SignTypedData(context.Background(), a, typedData); err != nil {
 		t.Fatal(err)
-	} else if signature == nil || len(signature) != 65 {
+	} else if len(signature) != 65 {
 		t.Errorf("Expected 65 byte signature (got %d bytes)", len(signature))
 	} else {
 		want = signature
@@ -241,7 +241,7 @@ func TestSignData(t *testing.T) {
 		t.Fatal(err)
 	} else if signature, err = api.SignData(context.Background(), apitypes.DataTyped.Mime, a, hexutil.Encode(typedDataJson)); err != nil {
 		t.Fatal(err)
-	} else if signature == nil || len(signature) != 65 {
+	} else if len(signature) != 65 {
 		t.Errorf("Expected 65 byte signature (got %d bytes)", len(signature))
 	} else if have := signature; !bytes.Equal(have, want) {
 		t.Fatalf("want %x, have %x", want, have)
