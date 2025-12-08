66134b35dfde8cc1867d5f70756d8f07332e7ed3
Francisco Giordano <fg@frang.io>
2025-12-08 13:45:40 -0300

    core/vm: fix PC increment for EIP-8024 opcodes (#33361)
    The EIP says to increment PC by 2 _instead of_ the standard increment by
    1. The opcode handlers added in #33095 result in incrementing PC by 3,
    because they ignored the increment already present in `interpreter.go`.
    
    Does this need to be better specified in the EIP? I've added a [new test
    case](https://github.com/ethereum/EIPs/pull/10859) for it anyway.
    
    Found by @0xriptide.

--- core/vm/instructions_test.go
+++ core/vm/instructions_test.go
@@ -1107,6 +1107,11 @@ func TestEIP8024_Execution(t *testing.T) {
 			codeHex: "e8", // no operand
 			wantErr: true,
 		},
+		{
+			name:     "PC_INCREMENT",
+			codeHex:  "600060006000e80115",
+			wantVals: []uint64{1, 0, 0},
+		},
 	}
 
 	for _, tc := range tests {
@@ -1123,17 +1128,15 @@ func TestEIP8024_Execution(t *testing.T) {
 					return
 				case 0x60:
 					_, err = opPush1(&pc, evm, scope)
-					pc++
 				case 0x80:
 					dup1 := makeDup(1)
 					_, err = dup1(&pc, evm, scope)
-					pc++
 				case 0x56:
 					_, err = opJump(&pc, evm, scope)
-					pc++
 				case 0x5b:
 					_, err = opJumpdest(&pc, evm, scope)
-					pc++
+				case 0x15:
+					_, err = opIszero(&pc, evm, scope)
 				case 0xe6:
 					_, err = opDupN(&pc, evm, scope)
 				case 0xe7:
@@ -1143,6 +1146,7 @@ func TestEIP8024_Execution(t *testing.T) {
 				default:
 					err = &ErrInvalidOpCode{opcode: OpCode(op)}
 				}
+				pc++
 			}
 			if tc.wantErr {
 				if err == nil {
