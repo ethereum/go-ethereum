258c573ac2a09dcd3f80da6dc86183f1206c9380
Sina M <1591639+s1na@users.noreply.github.com>
2025-12-15 22:23:58 +0100

    Website: add arm64 build to downloads page (#33418)
    Fixes #33268
    
    ---------
    
    Co-authored-by: lightclient <lightclient@protonmail.com>

--- src/utils/getSortedReleases.ts
+++ src/utils/getSortedReleases.ts
@@ -1,6 +1,6 @@
 import { ReleaseData } from './../types';
 import { compareReleasesFn } from './compareReleasesFn';
 
-export const getSortedReleases = (releases: ReleaseData[], moreReleases: ReleaseData[] = []) => {
-  return releases.concat(moreReleases).sort(compareReleasesFn);
+export const getSortedReleases = (...releases: ReleaseData[][]) => {
+  return releases.flat().sort(compareReleasesFn);
 };
