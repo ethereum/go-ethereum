70e6a957a0cc6ecbecef70ea4492b90d030e4b9b
Charlotte <69423184+tqpcharlie@users.noreply.github.com>
2025-12-27 20:09:03 -0500

    rm ios build

--- build/bot/macos-build.sh
+++ build/bot/macos-build.sh
@@ -17,6 +17,3 @@ go run build/ci.go archive -type tar # -signer OSX_SIGNING_KEY -upload gethstore
 # sed -i '.bak' 's/repo.join/!repo.join/g' $(dirname `gem which cocoapods`)/cocoapods/sources_manager.rb
 # git clone --depth=1 https://github.com/CocoaPods/Specs.git ~/.cocoapods/repos/master
 # pod setup --verbose
-
-# # -- Build for iOS and upload to Azure
-# go run build/ci.go xcode -signer IOS_SIGNING_KEY -upload gethstore/builds
