4f54cc95bc9486cad63c9fa83954f4721e4b321f
Gary Rong <garyrong0905@gmail.com>
2026-01-28 20:52:53 +0800

    ethdb/pebble: disable seek compaction for Pebble

--- ethdb/pebble/pebble.go
+++ ethdb/pebble/pebble.go
@@ -300,6 +300,10 @@ func New(file string, cache int, handles int, namespace string, readonly bool) (
 		// debt will be less than 1GB, but with more frequent compactions scheduled.
 		L0CompactionThreshold: 2,
 	}
+	// Disable seek compaction explicitly. Check https://github.com/ethereum/go-ethereum/pull/20130
+	// for more details.
+	opt.Experimental.ReadSamplingMultiplier = -1
+
 	// These two settings define the conditions under which compaction concurrency
 	// is increased. Specifically, one additional compaction job will be enabled when:
 	// - there is one more overlapping sub-level0;
