e673c7097059410d4a67e1c51097d9bd5b2d357c
Stephen Buttolph <stephen@avalabs.org>
2026-01-28 11:08:29 -0500

    feat: expose ethapi.NewRPCTransaction and ethapi.NewRPCPendingTransaction (#260)

---
+++ internal/ethapi/api.libevm.go
@@ -0,0 +1,30 @@
+// Copyright 2026 the libevm authors.
+//
+// The libevm additions to go-ethereum are free software: you can redistribute
+// them and/or modify them under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+//
+// The libevm additions are distributed in the hope that they will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+// General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see
+// <http://www.gnu.org/licenses/>.
+
+package ethapi
+
+import (
+	"math/big"
+
+	"github.com/ava-labs/libevm/common"
+	"github.com/ava-labs/libevm/core/types"
+	"github.com/ava-labs/libevm/params"
+)
+
+// NewRPCTransaction exports the [newRPCTransaction] function.
+func NewRPCTransaction(tx *types.Transaction, blockHash common.Hash, blockNumber uint64, blockTime uint64, index uint64, baseFee *big.Int, config *params.ChainConfig) *RPCTransaction {
+	return newRPCTransaction(tx, blockHash, blockNumber, blockTime, index, baseFee, config)
+}
