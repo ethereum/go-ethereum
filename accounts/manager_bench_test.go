e4c1d3796167c1c0b5514343a01936a7afcab61e
iPLAY888 <133153661+letmehateu@users.noreply.github.com>
2026-01-31 12:40:02 +0300

    lint

--- accounts/manager_bench_test.go
+++ accounts/manager_bench_test.go
@@ -15,11 +15,21 @@ type fakeWallet struct {
 	accounts []Account
 }
 
-func (w *fakeWallet) URL() URL { return URL{} }
-func (w *fakeWallet) Status() (string, error) { return "", nil }
-func (w *fakeWallet) Open(string) error { return nil }
-func (w *fakeWallet) Close() error { return nil }
-func (w *fakeWallet) Accounts() []Account { return w.accounts }
+func (w *fakeWallet) URL() URL {
+	return URL{}
+}
+func (w *fakeWallet) Status() (string, error) {
+	return "", nil
+}
+func (w *fakeWallet) Open(string) error {
+	return nil
+}
+func (w *fakeWallet) Close() error {
+	return nil
+}
+func (w *fakeWallet) Accounts() []Account {
+	return w.accounts
+}
 func (w *fakeWallet) Contains(a Account) bool {
 	for _, x := range w.accounts {
 		if x.Address == a.Address {
@@ -28,14 +38,22 @@ func (w *fakeWallet) Contains(a Account) bool {
 	}
 	return false
 }
-func (w *fakeWallet) Derive(DerivationPath, bool) (Account, error) { return Account{}, nil }
+func (w *fakeWallet) Derive(DerivationPath, bool) (Account, error) {
+	return Account{}, nil
+}
 func (w *fakeWallet) SelfDerive([]DerivationPath, ethereum.ChainStateReader) {}
-func (w *fakeWallet) SignData(Account, string, []byte) ([]byte, error) { return nil, nil }
+func (w *fakeWallet) SignData(Account, string, []byte) ([]byte, error) {
+	return nil, nil
+}
 func (w *fakeWallet) SignDataWithPassphrase(Account, string, string, []byte) ([]byte, error) {
 	return nil, nil
 }
-func (w *fakeWallet) SignText(Account, []byte) ([]byte, error) { return nil, nil }
-func (w *fakeWallet) SignTextWithPassphrase(Account, string, []byte) ([]byte, error) { return nil, nil }
+func (w *fakeWallet) SignText(Account, []byte) ([]byte, error) {
+	return nil, nil
+}
+func (w *fakeWallet) SignTextWithPassphrase(Account, string, []byte) ([]byte, error) {
+	return nil, nil
+}
 func (w *fakeWallet) SignTx(Account, *types.Transaction, *big.Int) (*types.Transaction, error) {
 	return nil, nil
 }
