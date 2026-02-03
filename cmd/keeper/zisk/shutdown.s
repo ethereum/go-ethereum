09c79f809582f7d2d55d560281b581a81c66f82e
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2026-02-03 20:43:28 +0100

    add Kev's board definitions
    Co-authored-by: kevaundray <kevtheappdev@gmail.com>

---
+++ cmd/keeper/zisk/shutdown.s
@@ -0,0 +1,9 @@
+//go:build tamago && riscv64
+
+#include "textflag.h"
+
+// Shutdown triggers ZisK exit via ecall with a7=93
+TEXT ·Shutdown(SB),NOSPLIT|NOFRAME,$0
+	MOV	$93, A7		// CAUSE_EXIT = 93
+	ECALL			// System call to exit
+	RET			// Should never reach here
