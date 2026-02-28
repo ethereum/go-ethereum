466e3684b960af0cc84eb1a3cfa5f09e9a2fc816
Daniel Liu <139250065@qq.com>
2026-02-28 20:52:55 +0800

    fix(internal): fix error code for revert in eth_simulateV1 #33007 (#2039)

--- internal/ethapi/errors.go
+++ internal/ethapi/errors.go
@@ -85,7 +85,6 @@ const (
 	errCodeClientLimitExceeded     = -38026
 	errCodeInternalError           = -32603
 	errCodeInvalidParams           = -32602
-	errCodeReverted                = -32000
 	errCodeVMError                 = -32015
 )
 
