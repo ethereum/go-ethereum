c50e7dd240365074ed8509261b8cbf58a35047e1
muzry.li <muzry.li1@ambergroup.io>
2026-03-02 18:01:42 +0800

    eth/catalyst: return -32602 for FCU V2 wrong payloadAttributes version

--- eth/catalyst/api_test.go
+++ eth/catalyst/api_test.go
@@ -1205,6 +1205,13 @@ func TestNilWithdrawals(t *testing.T) {
 			if err == nil {
 				t.Fatal("wanted error on fcuv2 with invalid withdrawals")
 			}
+			var engineErr *engine.EngineAPIError
+			if !errors.As(err, &engineErr) {
+				t.Fatalf("expected EngineAPIError, got %T", err)
+			}
+			if have, want := engineErr.ErrorCode(), -32602; have != want {
+				t.Fatalf("wrong error code, have %d want %d", have, want)
+			}
 			continue
 		}
 		if err != nil {
