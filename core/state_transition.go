5695f8a712e4153c24007511f78b706386ebbf26
Daniel Liu <139250065@qq.com>
2026-03-06 13:47:47 +0800

    perf(core): reduce alloc #33576 (#2085)

--- core/state_transition.go
+++ core/state_transition.go
@@ -158,9 +158,9 @@ func TransactionToMessage(tx *types.Transaction, s types.Signer, balanceFee, blo
 	msg := &Message{
 		Nonce:                 tx.Nonce(),
 		GasLimit:              tx.Gas(),
-		GasPrice:              new(big.Int).Set(tx.GasPrice()),
-		GasFeeCap:             new(big.Int).Set(tx.GasFeeCap()),
-		GasTipCap:             new(big.Int).Set(tx.GasTipCap()),
+		GasPrice:              tx.GasPrice(),
+		GasFeeCap:             tx.GasFeeCap(),
+		GasTipCap:             tx.GasTipCap(),
 		To:                    tx.To(),
 		Value:                 tx.Value(),
 		Data:                  tx.Data(),
