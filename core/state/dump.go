7ca8fd281c325e18946fbf895bbead692165dee4
Gary Rong <garyrong0905@gmail.com>
2026-03-11 10:55:19 +0800

    core/state: fix next json marshalling

--- core/state/dump.go
+++ core/state/dump.go
@@ -68,7 +68,7 @@ type Dump struct {
 
 	// Next can be set to represent that this dump is only partial, and Next
 	// is where an iterator should be positioned in order to continue the dump.
-	Next []byte `json:"next,omitempty"` // nil if no more accounts
+	Next hexutil.Bytes `json:"next,omitempty"` // nil if no more accounts
 }
 
 // OnRoot implements DumpCollector interface
