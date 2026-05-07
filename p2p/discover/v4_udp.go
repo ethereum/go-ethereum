59787a1d80e64de6983bbda1ab6983e40828e4b7
Felix Lange <fjl@twurst.com>
2026-05-07 12:51:04 +0200

    p2p/discover: use bytes.Clone

--- p2p/discover/v4_udp.go
+++ p2p/discover/v4_udp.go
@@ -555,10 +555,8 @@ func (t *UDPv4) readLoop(unhandled chan<- ReadPacket) {
 		if err := t.handlePacket(from, buf[:nbytes]); err != nil && unhandled == nil {
 			t.log.Debug("Bad discv4 packet", "addr", from, "err", err)
 		} else if err != nil && unhandled != nil {
-			packet := make([]byte, nbytes)
-			copy(packet, buf[:nbytes])
-			select {
-			case unhandled <- ReadPacket{packet, from}:
+			p := ReadPacket{bytes.Clone(buf[:nbytes]), from}
+			case unhandled <- p:
 			default:
 			}
 		}
