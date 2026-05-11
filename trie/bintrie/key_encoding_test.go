ac0d154870bec804ed4036052a14089ed773cd94
Weixie Cui <cuiweixie@gmail.com>
2026-05-11 20:24:07 +0800

    trie/bintrie: fix panics for small chunk numbers

---
+++ trie/bintrie/key_encoding_test.go
@@ -0,0 +1,28 @@
+// Copyright 2025 go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+package bintrie
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/holiman/uint256"
+	"testing"
+)
+
+func TestGetBinaryTreeKeyCodeChunkPanic(t *testing.T) {
+	addr := common.HexToAddress("0x1234567890123456789012345678901234567890")
+	GetBinaryTreeKeyCodeChunk(addr, uint256.NewInt(0))
+}
