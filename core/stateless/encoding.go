4daaaadfc4706b0a49d4dfde3559de7be968c28a
Ignacio Hagopian <jsign.uy@gmail.com>
2026-05-21 16:00:57 -0300

    eth/catalyst: implement engine_newPayloadWithWitnessV5 and use witness field spec ordering (#35009)
    This PR:
    - Adds `engine_newPayloadWithWitnessV5`. The codebase already supports
    the previous `VX`, so only `V5` was missing.
    - Make the consensus witness format use the field [ordering defined in
    the
    spec](https://github.com/ethereum/execution-specs/blob/8d7e68f4b7fa58107647164d1749853a6ed00eb3/src/ethereum/forks/amsterdam/stateless_host_exec_witness.py#L175-L176)
    to make it canonical.
    
    cc @gballet
    
    ---------
    
    Co-authored-by: Guillaume Ballet <3272758+gballet@users.noreply.github.com>

--- core/stateless/encoding.go
+++ core/stateless/encoding.go
@@ -17,8 +17,10 @@
 package stateless
 
 import (
+	"bytes"
 	"errors"
 	"io"
+	"slices"
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core/types"
@@ -28,16 +30,25 @@ import (
 // ToExtWitness converts our internal witness representation to the consensus one.
 func (w *Witness) ToExtWitness() *ExtWitness {
 	ext := &ExtWitness{
-		Headers: w.Headers,
+		Headers: slices.Clone(w.Headers),
 	}
+	slices.Reverse(ext.Headers)
+
 	ext.Codes = make([]hexutil.Bytes, 0, len(w.Codes))
 	for code := range w.Codes {
 		ext.Codes = append(ext.Codes, []byte(code))
 	}
+	slices.SortFunc(ext.Codes, func(a, b hexutil.Bytes) int {
+		return bytes.Compare(a, b)
+	})
+
 	ext.State = make([]hexutil.Bytes, 0, len(w.State))
 	for node := range w.State {
 		ext.State = append(ext.State, []byte(node))
 	}
+	slices.SortFunc(ext.State, func(a, b hexutil.Bytes) int {
+		return bytes.Compare(a, b)
+	})
 	return ext
 }
 
@@ -46,7 +57,12 @@ func (w *Witness) FromExtWitness(ext *ExtWitness) error {
 	if len(ext.Headers) == 0 {
 		return errors.New("witness must contain at least one header")
 	}
-	w.Headers = ext.Headers
+	w.Headers = slices.Clone(ext.Headers)
+	// don't trust the input and sort headers in reverse order
+	// this is only useful for calling `Root`
+	slices.SortFunc(w.Headers, func(a, b *types.Header) int {
+		return b.Number.Cmp(a.Number)
+	})
 
 	w.Codes = make(map[string]struct{}, len(ext.Codes))
 	for _, code := range ext.Codes {
