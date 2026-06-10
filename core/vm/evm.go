27539d83fe5ca22a3f8ab2359b0142915d48282b
MariusVanDerWijden <m.vanderwijden@live.de>
2026-06-10 12:52:55 +0200

    core/vm: align with specs

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -288,17 +288,15 @@ func (evm *EVM) Call(caller common.Address, addr common.Address, input []byte, g
 			return nil, gas, nil
 		}
 		evm.StateDB.CreateAccount(addr)
-
-		// Charge the state gas for account creation
-		if evm.chainRules.IsAmsterdam {
-			prev, ok := gas.ChargeState(params.AccountCreationSize * evm.Context.CostPerStateByte)
-			if !ok {
-				evm.StateDB.RevertToSnapshot(snapshot)
-				return nil, gas.ExitHalt(reservoir), ErrOutOfGas
-			}
-			if evm.Config.Tracer.HasGasHook() {
-				evm.Config.Tracer.EmitGasChange(prev.AsTracing(), gas.AsTracing(), tracing.GasChangeAccountCreation)
-			}
+	}
+	if evm.chainRules.IsAmsterdam && !value.IsZero() && evm.StateDB.Empty(addr) {
+		prev, ok := gas.ChargeState(params.AccountCreationSize * evm.Context.CostPerStateByte)
+		if !ok {
+			evm.StateDB.RevertToSnapshot(snapshot)
+			return nil, gas.ExitHalt(reservoir), ErrOutOfGas
+		}
+		if evm.Config.Tracer.HasGasHook() {
+			evm.Config.Tracer.EmitGasChange(prev.AsTracing(), gas.AsTracing(), tracing.GasChangeAccountCreation)
 		}
 	}
 	// Perform the value transfer only in non-syscall mode.
@@ -553,16 +551,14 @@ func (evm *EVM) create(caller common.Address, code []byte, gas GasBudget, value 
 	snapshot := evm.StateDB.Snapshot()
 	if !evm.StateDB.Exist(address) {
 		evm.StateDB.CreateAccount(address)
-
-		// Charge the state gas for account creation
-		if evm.chainRules.IsAmsterdam {
-			prev, ok := gas.ChargeState(params.AccountCreationSize * evm.Context.CostPerStateByte)
-			if !ok {
-				return nil, common.Address{}, gas.ExitHalt(reservoir), ErrOutOfGas
-			}
-			if evm.Config.Tracer.HasGasHook() {
-				evm.Config.Tracer.EmitGasChange(prev.AsTracing(), gas.AsTracing(), tracing.GasChangeAccountCreation)
-			}
+	}
+	if evm.chainRules.IsAmsterdam {
+		prev, ok := gas.ChargeState(params.AccountCreationSize * evm.Context.CostPerStateByte)
+		if !ok {
+			return nil, common.Address{}, gas.ExitHalt(reservoir), ErrOutOfGas
+		}
+		if evm.Config.Tracer.HasGasHook() {
+			evm.Config.Tracer.EmitGasChange(prev.AsTracing(), gas.AsTracing(), tracing.GasChangeAccountCreation)
 		}
 	}
 	// CreateContract means that regardless of whether the account previously existed
