d93dda49c8e5b0530fbddc16d61592ecf7e32b78
Sina M <1591639+s1na@users.noreply.github.com>
2026-06-12 13:31:15 +0200

    internal/memlimit: respect cgroup memory cap (#34947)
    Currently geth ignores the docker `--memory` directive and doesn't
    adjust its cache size downward when necessary, potentially running into
    OOM.
    
    while gopsutil has functions like `docker/CgroupMem()` they are rather
    for reading cgroup memory limit of a container from the host.

---
+++ internal/memlimit/probe_other.go
@@ -0,0 +1,25 @@
+// Copyright 2026 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+//go:build !linux
+
+package memlimit
+
+// platformLimit reports no platform-specific limit on non-Linux
+// systems; the caller falls back to total system memory.
+func platformLimit() (uint64, Source, bool) {
+	return 0, SourceUnknown, false
+}
