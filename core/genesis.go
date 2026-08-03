825a37f8cc45eb57896ce06023f9658272f5c930
Vansh Sahay <vansh.sahay1234@gmail.com>
2026-08-03 10:39:12 +0530

    core: set constants and params with genesis setup

--- core/genesis.go
+++ core/genesis.go
@@ -720,6 +720,7 @@ func DeveloperGenesisBlock(gasLimit uint64, faucet *common.Address) *Genesis {
 			common.BytesToAddress([]byte{0x1, 00}): {Balance: big.NewInt(1)}, // P256Verify
 			// Pre-deploy system contracts
 			params.BeaconRootsAddress:        {Nonce: 1, Code: params.BeaconRootsCode, Balance: common.Big0},
+			params.IndexContractAddress:      {Nonce: 1, Code: params.IndexContractCode, Balance: common.Big0},
 			params.HistoryStorageAddress:     {Nonce: 1, Code: params.HistoryStorageCode, Balance: common.Big0},
 			params.WithdrawalQueueAddress:    {Nonce: 1, Code: params.WithdrawalQueueCode, Balance: common.Big0},
 			params.ConsolidationQueueAddress: {Nonce: 1, Code: params.ConsolidationQueueCode, Balance: common.Big0},
