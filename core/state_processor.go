5131c031dd1c9f7919903a3cf41e06c2f423e8db
rjl493456442 <garyrong0905@gmail.com>
2026-08-04 13:54:29 +0800

    cmd, consensus, core, miner: update 7997 (#35458)
    https://eips.ethereum.org/EIPS/eip-7997

--- core/state_processor.go
+++ core/state_processor.go
@@ -160,12 +160,6 @@ func PreExecution(ctx context.Context, beaconRoot *common.Hash, parent *types.He
 	var blockAccessList *bal.ConstructionBlockAccessList
 	if config.IsAmsterdam(number, time) {
 		blockAccessList = bal.NewConstructionBlockAccessList()
-
-		// EIP-7997: insert the deterministic deployment factory at the Amsterdam
-		// activation block via an irregular state transition.
-		if !config.IsAmsterdam(parent.Number, parent.Time) {
-			misc.ApplyEIP7997(evm.StateDB)
-		}
 	}
 	// EIP-4788
 	if beaconRoot != nil {
