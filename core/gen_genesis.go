b6de3d55bf34fd86a8fdaab15e05da98cf78ecaf
Mario Vega <marioevz@gmail.com>
2026-08-04 20:45:50 +0000

    core/genesis: genesis slot number parsing

--- core/gen_genesis.go
+++ core/gen_genesis.go
@@ -34,7 +34,7 @@ func (g Genesis) MarshalJSON() ([]byte, error) {
 		BaseFee       *math.HexOrDecimal256                      `json:"baseFeePerGas"`
 		ExcessBlobGas *math.HexOrDecimal64                       `json:"excessBlobGas"`
 		BlobGasUsed   *math.HexOrDecimal64                       `json:"blobGasUsed"`
-		SlotNumber    *uint64                                    `json:"slotNumber"`
+		SlotNumber    *math.HexOrDecimal64                       `json:"slotNumber"`
 	}
 	var enc Genesis
 	enc.Config = g.Config
@@ -57,7 +57,7 @@ func (g Genesis) MarshalJSON() ([]byte, error) {
 	enc.BaseFee = (*math.HexOrDecimal256)(g.BaseFee)
 	enc.ExcessBlobGas = (*math.HexOrDecimal64)(g.ExcessBlobGas)
 	enc.BlobGasUsed = (*math.HexOrDecimal64)(g.BlobGasUsed)
-	enc.SlotNumber = g.SlotNumber
+	enc.SlotNumber = (*math.HexOrDecimal64)(g.SlotNumber)
 	return json.Marshal(&enc)
 }
 
@@ -79,7 +79,7 @@ func (g *Genesis) UnmarshalJSON(input []byte) error {
 		BaseFee       *math.HexOrDecimal256                      `json:"baseFeePerGas"`
 		ExcessBlobGas *math.HexOrDecimal64                       `json:"excessBlobGas"`
 		BlobGasUsed   *math.HexOrDecimal64                       `json:"blobGasUsed"`
-		SlotNumber    *uint64                                    `json:"slotNumber"`
+		SlotNumber    *math.HexOrDecimal64                       `json:"slotNumber"`
 	}
 	var dec Genesis
 	if err := json.Unmarshal(input, &dec); err != nil {
@@ -137,7 +137,7 @@ func (g *Genesis) UnmarshalJSON(input []byte) error {
 		g.BlobGasUsed = (*uint64)(dec.BlobGasUsed)
 	}
 	if dec.SlotNumber != nil {
-		g.SlotNumber = dec.SlotNumber
+		g.SlotNumber = (*uint64)(dec.SlotNumber)
 	}
 	return nil
 }
