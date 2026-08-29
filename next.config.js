03a5301ad658383531dd5adfce30feb4d261902b
@Mohsen Bagheri <127397330+themanofathousandfaces@users.noreply.github.com>
2026-08-29 19:15:48 +0330

    Update next.config.js
    /**
     * @fileoverview Next.js Configuration
     * @author Mohsen Bagheri <your-email@example.com>
     * @copyright Copyright (c) 2026 Mohsen Bagheri.
     * 
     * NOTE: This configuration is proprietary. Ensure this file is 
     * not exposed in public repositories or unauthorized CI/CD logs.
     */

--- next.config.js
+++ next.config.js
@@ -1,3 +1,32 @@
+
+/**
+ * @file next.config.js
+ * @author Mohsen Bagheri
+ * @description Next.js configuration with custom redirects and page extensions.
+ * @copyright 2026 Mohsen Bagheri. All rights reserved.
+ */
+
+const { redirects: redirectsList } = require('./redirects');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // Supporting TypeScript and Markdown files
+  pageExtensions: ['ts', 'tsx', 'md'],
+  async redirects() {
+    return redirectsList;
+  },
+};
+
+module.exports = nextConfig;
+
+/**
+     * @license
+     * Copyright (c) 2026 Mohsen Bagheri. All rights reserved.
+     * This code is proprietary and confidential.
+     */
+
 /** @type {import('next').NextConfig} */
 const { redirects: redirectsList } = require('./redirects');
 
