03768ae9d59a0896bd2cb2b2d6339478f23c0794
Jonny Rhea <5555162+jrhea@users.noreply.github.com>
2026-09-01 20:03:56 -0500

    eth/tracers: fix test flaking on 0xEF block hashes (#35629)
    TestTracingWithOverrides deploys a random block hash as contract code
    with a 1 in 256 chance of starting with 0xEF and failing bc of EIP-3541.
    
    ---------
    
    Co-authored-by: lightclient <lightclient@protonmail.com>

--- eth/tracers/api_test.go
+++ eth/tracers/api_test.go
@@ -715,6 +715,7 @@ func TestTracingWithOverrides(t *testing.T) {
 	defer backend.teardown()
 	api := NewAPI(backend)
 	randomAccounts := newAccounts(3)
+
 	type res struct {
 		Gas         int
 		Failed      bool
@@ -820,16 +821,16 @@ func TestTracingWithOverrides(t *testing.T) {
 			},
 			want: `{"gas":72666,"failed":false,"returnValue":"0x000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"}`,
 		},
-		{ // Override blocknumber with block n+1 and query a blockhash (resolves issue #32175)
+		{ // Override blocknumber with block n+1 and deploy a zero-prefixed blockhash (resolves issue #32175)
 			blockNumber: rpc.LatestBlockNumber,
 			call: ethapi.TransactionArgs{
 				From: &accounts[0].addr,
 				Input: newRPCBytes([]byte{
 					byte(vm.PUSH1), byte(genBlocks),
 					byte(vm.BLOCKHASH),
-					byte(vm.PUSH1), 0x00,
+					byte(vm.PUSH1), 0x01,
 					byte(vm.MSTORE),
-					byte(vm.PUSH1), 0x20,
+					byte(vm.PUSH1), 0x21,
 					byte(vm.PUSH1), 0x00,
 					byte(vm.RETURN),
 				}),
@@ -837,7 +838,7 @@ func TestTracingWithOverrides(t *testing.T) {
 			config: &TraceCallConfig{
 				BlockOverrides: &override.BlockOverrides{Number: (*hexutil.Big)(big.NewInt(int64(genBlocks + 1)))},
 			},
-			want: fmt.Sprintf(`{"gas":59590,"failed":false,"returnValue":"%s"}`, backend.chain.GetHeaderByNumber(uint64(genBlocks)).Hash().Hex()),
+			want: fmt.Sprintf(`{"gas":59805,"failed":false,"returnValue":"0x00%x"}`, backend.chain.GetHeaderByNumber(uint64(genBlocks)).Hash()),
 		},
 		/*
 			pragma solidity =0.8.12;
